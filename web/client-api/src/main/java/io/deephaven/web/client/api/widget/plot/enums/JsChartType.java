6c660d4f1295bdc08e4e6f8f807aa351abd87dba
Colin Alworth <colinalworth@deephaven.io>
2021-05-28 10:48:48 -0500

    update js api enums

--- web/client-api/src/main/java/io/deephaven/web/client/api/widget/plot/enums/JsChartType.java
+++ web/client-api/src/main/java/io/deephaven/web/client/api/widget/plot/enums/JsChartType.java
@@ -1,15 +1,15 @@
 package io.deephaven.web.client.api.widget.plot.enums;
 
-import io.deephaven.web.shared.data.plot.ChartDescriptor;
+import io.deephaven.javascript.proto.dhinternal.io.deephaven.proto.console_pb.figuredescriptor.ChartDescriptor;
 import jsinterop.annotations.JsType;
 
 @JsType(name = "ChartType")
 @SuppressWarnings("unusable-by-js")
 public class JsChartType {
-    public static final ChartDescriptor.ChartType XY = ChartDescriptor.ChartType.XY;
-    public static final ChartDescriptor.ChartType PIE = ChartDescriptor.ChartType.PIE;
-    public static final ChartDescriptor.ChartType OHLC = ChartDescriptor.ChartType.OHLC;
-    public static final ChartDescriptor.ChartType CATEGORY = ChartDescriptor.ChartType.CATEGORY;
-    public static final ChartDescriptor.ChartType XYZ = ChartDescriptor.ChartType.XYZ;
-    public static final ChartDescriptor.ChartType CATEGORY_3D = ChartDescriptor.ChartType.CATEGORY_3D;
+    public static final int XY = ChartDescriptor.ChartType.getXY();
+    public static final int PIE = ChartDescriptor.ChartType.getPIE();
+    public static final int OHLC = ChartDescriptor.ChartType.getOHLC();
+    public static final int CATEGORY = ChartDescriptor.ChartType.getCATEGORY();
+    public static final int XYZ = ChartDescriptor.ChartType.getXYZ();
+    public static final int CATEGORY_3D = ChartDescriptor.ChartType.getCATEGORY_3D();
 }
