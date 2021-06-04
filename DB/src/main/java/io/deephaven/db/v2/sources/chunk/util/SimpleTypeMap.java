43539c1dcf3ad575b800ab45e9779cf05fcc7100
Ryan Caudy <ryan@deephaven.io>
2021-06-04 11:19:03 -0400

    Clean up Parquet codec handling for #570 (#680)
    * Broken WIP to get the ball rolling.
    
    * Commit my notes and WIP
    
    * Maybe this is it?

--- DB/src/main/java/io/deephaven/db/v2/sources/chunk/util/SimpleTypeMap.java
+++ DB/src/main/java/io/deephaven/db/v2/sources/chunk/util/SimpleTypeMap.java
@@ -3,9 +3,10 @@ package io.deephaven.db.v2.sources.chunk.util;
 import java.util.HashMap;
 
 public final class SimpleTypeMap<V> {
+
     public static <V> SimpleTypeMap<V> create(V forBoolean, V forChar, V forByte, V forShort, V forInt, V forLong,
                                               V forFloat, V forDouble, V forObject) {
-        final HashMap<Class, V> map = new HashMap<>();
+        final HashMap<Class<?>, V> map = new HashMap<>();
         map.put(boolean.class, forBoolean);
         map.put(char.class, forChar);
         map.put(byte.class, forByte);
@@ -17,15 +18,15 @@ public final class SimpleTypeMap<V> {
         return new SimpleTypeMap<>(map, forObject);
     }
 
-    private final HashMap<Class, V> map;
+    private final HashMap<Class<?>, V> map;
     private final V defaultValue;
 
-    private SimpleTypeMap(HashMap<Class, V> map, V defaultValue) {
+    private SimpleTypeMap(HashMap<Class<?>, V> map, V defaultValue) {
         this.map = map;
         this.defaultValue = defaultValue;
     }
 
-    public final V get(Class simpleType) {
+    public final V get(Class<?> simpleType) {
         return map.getOrDefault(simpleType, defaultValue);
     }
 }
