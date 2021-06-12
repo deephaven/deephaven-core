247b685ea0d6481ab1a960f5aba996f7cfd41d07
Cristian Ferretti <37232625+jcferretti@users.noreply.github.com>
2021-06-12 10:27:50 -0400

    Add support for reading Parquet logical type timestamp with millis and micros units. #743 (#744)
    * Add support for reading Parquet logical type timestamp with millis and micros units. #743
    
    * Adding page support.
    
    * Fixed an issue with a missed definition of convertResult.
    
    * Fixed an issue with a missed definition of convertResult.
    
    * Fixed an issue with error handling and control flow in readParquetSchema.
    
    * Followup to discussion with Ryan over slack: fixed bug for handling nulls, avoid overriding convertResult for nanos.

--- DB/src/main/java/io/deephaven/db/v2/parquet/ParquetReaderUtil.java
+++ DB/src/main/java/io/deephaven/db/v2/parquet/ParquetReaderUtil.java
@@ -184,8 +184,13 @@ public class ParquetReaderUtil {
 
             @Override
             public Optional<Class<?>> visit(final LogicalTypeAnnotation.TimestampLogicalTypeAnnotation timestampLogicalType) {
-                if (timestampLogicalType.isAdjustedToUTC() && timestampLogicalType.getUnit().equals(LogicalTypeAnnotation.TimeUnit.NANOS)) {
-                    return Optional.of(io.deephaven.db.tables.utils.DBDateTime.class);
+                if (timestampLogicalType.isAdjustedToUTC()) {
+                    switch(timestampLogicalType.getUnit()) {
+                        case MILLIS:
+                        case MICROS:
+                        case NANOS:
+                            return Optional.of(io.deephaven.db.tables.utils.DBDateTime.class);
+                    }
                 }
                 errorString.setValue("TimestampLogicalType,isAdjustedToUTC=" + timestampLogicalType.isAdjustedToUTC() + ",unit=" + timestampLogicalType.getUnit());
                 return Optional.empty();
