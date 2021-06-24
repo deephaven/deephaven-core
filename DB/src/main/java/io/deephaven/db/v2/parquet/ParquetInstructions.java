dbaafeff275842faa87cc1934186576aee137a01
Cristian Ferretti <cristianferretti@deephaven.io>
2021-06-24 00:36:26 -0400

    Made ColumnInstructions a static inner class of ParquetInstructions.

--- DB/src/main/java/io/deephaven/db/v2/parquet/ParquetInstructions.java
+++ DB/src/main/java/io/deephaven/db/v2/parquet/ParquetInstructions.java
@@ -26,6 +26,45 @@ public abstract class ParquetInstructions {
         }
     };
 
+    private static class ColumnInstructions {
+        private final String columnName;
+        String parquetColumnName;
+        private String codecName;
+        private String codecArgs;
+
+        public ColumnInstructions(final String columnName) {
+            this.columnName = columnName;
+        }
+
+        public String getColumnName() {
+            return columnName;
+        }
+
+        public String getParquetColumnName() {
+            return parquetColumnName != null ? parquetColumnName : columnName;
+        }
+        public ColumnInstructions setParquetColumnName(final String parquetColumnName) {
+            this.parquetColumnName = parquetColumnName;
+            return this;
+        }
+
+        public String getCodecName() {
+            return codecName;
+        }
+        public ColumnInstructions setCodecName(final String codecName) {
+            this.codecName = codecName;
+            return this;
+        }
+
+        public String getCodecArgs() {
+            return codecArgs;
+        }
+        public ColumnInstructions setCodecArgs(final String codecArgs) {
+            this.codecArgs = codecArgs;
+            return this;
+        }
+    }
+
     private static class ReadOnly extends ParquetInstructions {
         private final KeyedObjectHashMap<String, ColumnInstructions> columnNameToInstructions;
         // Note parquetColumnNameToInstructions may be null while columnNameToInstructions is not null;
