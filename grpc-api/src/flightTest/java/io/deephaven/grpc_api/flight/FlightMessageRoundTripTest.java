af58590b0e9de20e75553817a274faf494485e96
Colin Alworth <colinalworth@deephaven.io>
2021-07-13 13:21:26 -0500

    Update test so it sorta runs

--- grpc-api/src/flightTest/java/io/deephaven/grpc_api/flight/FlightMessageRoundTripTest.java
+++ grpc-api/src/flightTest/java/io/deephaven/grpc_api/flight/FlightMessageRoundTripTest.java
@@ -2,6 +2,7 @@ package io.deephaven.grpc_api.flight;
 
 import dagger.BindsInstance;
 import dagger.Component;
+import io.deephaven.db.tables.Table;
 import io.deephaven.db.tables.utils.TableTools;
 import io.deephaven.grpc_api.arrow.FlightServiceGrpcBinding;
 import io.deephaven.grpc_api.auth.AuthContextModule;
@@ -17,12 +18,15 @@ import io.deephaven.proto.backplane.grpc.HandshakeRequest;
 import io.deephaven.proto.backplane.grpc.HandshakeResponse;
 import io.deephaven.proto.backplane.grpc.SessionServiceGrpc;
 import io.grpc.*;
+import io.grpc.CallOptions;
+import io.grpc.netty.NettyServerBuilder;
 import io.grpc.testing.GrpcServerRule;
-import org.apache.arrow.flight.FlightClient;
-import org.apache.arrow.flight.FlightGrpcUtils;
-import org.apache.arrow.flight.Ticket;
+import org.apache.arrow.flight.*;
 import org.apache.arrow.flight.impl.Flight;
 import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.VectorSchemaRoot;
+import org.apache.arrow.vector.types.pojo.ArrowType;
+import org.apache.arrow.vector.types.pojo.Field;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -30,12 +34,13 @@ import org.junit.Test;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+import java.io.IOException;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * Deliberately much lower in scope (and running time) than BarrageMessageRoundTripTest, the only purpose
@@ -43,8 +48,8 @@ import static org.junit.Assert.assertNotNull;
  */
 public class FlightMessageRoundTripTest {
 
-    @Rule
-    public GrpcServerRule grpcServerRule = new GrpcServerRule();
+//    @Rule
+//    public GrpcServerRule grpcServerRule = new GrpcServerRule();
 
     @Singleton
     @Component(modules = {
@@ -159,21 +164,38 @@ public class FlightMessageRoundTripTest {
 
 
     @Before
-    public void setup() {
+    public void setup() throws IOException {
         TestComponent component = DaggerFlightMessageRoundTripTest_TestComponent
                 .builder()
                 .withScheduler(new Scheduler.DelegatingImpl(Executors.newSingleThreadExecutor(), Executors.newScheduledThreadPool(1)))
-                .withSessionTokenExpireTmMs(60_000)
+                .withSessionTokenExpireTmMs(60_000_000)
                 .build();
 
-        ManagedChannel clientChannel = new InterceptingManagedChannel(grpcServerRule.getChannel(), new AuthInterceptor());
+        NettyServerBuilder serverBuilder = NettyServerBuilder.forPort(0);
+        component.interceptors().forEach(serverBuilder::intercept);
+        serverBuilder.addService(component.sessionGrpcService());
+        serverBuilder.addService(component.flightService());
+        Server server = serverBuilder.build().start();
+        int actualPort = server.getPort();
 
-        client = FlightGrpcUtils.createFlightClient(new RootAllocator(), clientChannel);
-        sessionServiceClient = SessionServiceGrpc.newBlockingStub(clientChannel);
-
-        Set<ServerInterceptor> interceptors = component.interceptors();
-        grpcServerRule.getServiceRegistry().addService(ServerInterceptors.intercept(component.flightService(), interceptors.toArray(new ServerInterceptor[0])));
-        grpcServerRule.getServiceRegistry().addService(component.sessionGrpcService());
+        client = FlightClient.builder().location(Location.forGrpcInsecure("localhost", actualPort)).allocator(new RootAllocator()).intercept(info -> new FlightClientMiddleware() {
+            @Override
+            public void onBeforeSendingHeaders(CallHeaders outgoingHeaders) {
+                final UUID currSession = sessionToken;
+                if (currSession != null) {
+                    outgoingHeaders.insert(SessionServiceGrpcImpl.DEEPHAVEN_SESSION_ID, currSession.toString());
+                }
+            }
+            @Override
+            public void onHeadersReceived(CallHeaders incomingHeaders) {
+            }
+            @Override
+            public void onCallCompleted(CallStatus status) {
+            }
+        }).build();
+        sessionServiceClient = SessionServiceGrpc.newBlockingStub(ManagedChannelBuilder.forTarget("localhost:" + actualPort)
+                .usePlaintext()
+                .build());
 
         HandshakeResponse response = sessionServiceClient.newSession(HandshakeRequest.newBuilder().setAuthProtocol(1).build());
         assertNotNull(response.getSessionToken());
@@ -182,11 +204,69 @@ public class FlightMessageRoundTripTest {
         currentSession = component.sessionService().getSessionForToken(sessionToken);
     }
     @Test
-    public void testNothing() {
+    public void testSimpleEmptyTableDoGet() {
         Flight.Ticket simpleTableTicket = ExportTicketHelper.exportIdToTicket(1);
         currentSession.newExport(simpleTableTicket)
                 .submit(() -> TableTools.emptyTable(10).update("I=i"));
 
-        client.getStream(new Ticket(simpleTableTicket.getTicket().toByteArray())).next();
+        FlightStream stream = client.getStream(new Ticket(simpleTableTicket.getTicket().toByteArray()));
+        assertTrue(stream.next());
+        VectorSchemaRoot root = stream.getRoot();
+        // row count should match what we expect
+        assertEquals(10, root.getRowCount());
+
+//        root.getSchema().getCustomMetadata();
+
+        // only one column was sent
+        assertEquals(1, root.getFieldVectors().size());
+        Field i = root.getSchema().findField("I");
+
+        // all DH columns are nullable, even primitives
+        assertTrue(i.getFieldType().isNullable());
+        // verify it is a java int type, which is an arrow 32bit int
+        assertEquals(ArrowType.ArrowTypeID.Int, i.getFieldType().getType().getTypeID());
+        assertEquals(32, ((ArrowType.Int) i.getFieldType().getType()).getBitWidth());
+        assertEquals("int", i.getMetadata().get("deephaven:type"));
+
+        // verify that the server didn't send more data after the first payload
+        assertFalse(stream.next());
+    }
+
+    @Test
+    public void testRoundTripData() throws InterruptedException {
+//        assertRoundTripDataEqual(TableTools.emptyTable(0));
+//        assertRoundTripDataEqual(TableTools.emptyTable(10));
+//        assertRoundTripDataEqual(TableTools.emptyTable(10).update("empty=(String)null"));
+//        assertRoundTripDataEqual(TableTools.emptyTable(10).update("empty=(int)0"));
+        assertRoundTripDataEqual(TableTools.emptyTable(10).update("empty=\"\""));
+//        assertRoundTripDataEqual(TableTools.emptyTable(10).update("empty=\"a\""));
+    }
+    private static int nextTicket = 1;
+    private void assertRoundTripDataEqual(Table deephavenTable) throws InterruptedException {
+        // bind the table in the session
+        Flight.Ticket dhTableTicket = ExportTicketHelper.exportIdToTicket(nextTicket++);
+        currentSession.newExport(dhTableTicket).submit(() -> deephavenTable);
+
+        // fetch with DoGet
+        FlightStream stream = client.getStream(new Ticket(dhTableTicket.getTicket().toByteArray()));
+        stream.next();
+        VectorSchemaRoot root = stream.getRoot();
+
+        // turn data around and send with DoPut
+        int flightDescriptorTicketValue = nextTicket++;
+        FlightDescriptor descriptor = FlightDescriptor.path("export", flightDescriptorTicketValue + "");
+        FlightClient.ClientStreamListener putStream = client.startPut(descriptor, root, new AsyncPutListener());
+        putStream.putNext();
+
+        Thread.sleep(1000);
+        putStream.completed();
+        putStream.getResult();
+
+
+        Thread.sleep(10_000);
+        Table uploadedTable = currentSession.<Table>getExport(flightDescriptorTicketValue).get();
+
+        assertEquals(deephavenTable.size(), uploadedTable.size());
+        assertEquals(deephavenTable.getDefinition(), uploadedTable.getDefinition());
     }
 }
