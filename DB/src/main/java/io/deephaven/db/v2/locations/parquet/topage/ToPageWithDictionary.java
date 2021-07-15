02eb235093158074c99b11961ecda78f7fae752d
Cristian Ferretti <cristianferretti@deephaven.io>
2021-07-15 00:54:15 -0400

    Made work for both object and string.

--- DB/src/main/java/io/deephaven/db/v2/locations/parquet/topage/ToPageWithDictionary.java
+++ DB/src/main/java/io/deephaven/db/v2/locations/parquet/topage/ToPageWithDictionary.java
@@ -1,18 +1,17 @@
 package io.deephaven.db.v2.locations.parquet.topage;
 
-import io.deephaven.UncheckedDeephavenException;
 import io.deephaven.db.v2.sources.StringSetImpl;
 import io.deephaven.db.v2.sources.chunk.Attributes;
 import io.deephaven.db.v2.sources.chunk.ChunkType;
 import io.deephaven.db.v2.sources.chunk.ObjectChunk;
 import io.deephaven.parquet.ColumnPageReader;
 import io.deephaven.parquet.DataWithOffsets;
-import org.apache.parquet.io.api.Binary;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.nio.IntBuffer;
+import java.util.function.Function;
 
 import static io.deephaven.util.QueryConstants.NULL_INT;
 
@@ -20,10 +19,15 @@ public class ToPageWithDictionary<T, ATTR extends Attributes.Any> implements ToP
 
     private final Class<T> nativeType;
     private final Dictionary<T, ATTR> dictionary;
+    private final Function<Object, T[]> convertResultFallbackFun;
 
-    ToPageWithDictionary(final Class<T> nativeType, final Dictionary<T, ATTR> dictionary) {
+    ToPageWithDictionary(
+            final Class<T> nativeType,
+            final Dictionary<T, ATTR> dictionary,
+            final Function<Object, T[]> convertResultFallbackFun) {
         this.nativeType = nativeType;
         this.dictionary = dictionary;
+        this.convertResultFallbackFun = convertResultFallbackFun;
     }
 
     @Override
@@ -42,12 +46,7 @@ public class ToPageWithDictionary<T, ATTR extends Attributes.Any> implements ToP
     @NotNull
     public final Object getResult(final ColumnPageReader columnPageReader) throws IOException {
         if (columnPageReader.getDictionary() == null) {
-            if (nativeType == String.class) {
-                //noinspection unchecked
-                return ToStringPage.INSTANCE.getResult(columnPageReader);
-            }
-            throw new UncheckedDeephavenException("Unsupported nativeType " + nativeType.getName());
-
+            return ToPage.super.getResult(columnPageReader);
         }
         int[] keys = new int [columnPageReader.numValues()];
         IntBuffer offsets = columnPageReader.readKeyValues(IntBuffer.wrap(keys), NULL_INT);
@@ -59,19 +58,7 @@ public class ToPageWithDictionary<T, ATTR extends Attributes.Any> implements ToP
     @NotNull
     public final T[] convertResult(final Object result) {
         if (!(result instanceof int[])) {
-            if (nativeType == String.class) {
-                Binary[] from = (Binary[]) result;
-                //noinspection unchecked
-                T[] to = (T[])Array.newInstance(nativeType, from.length);
-
-                for (int i = 0; i < from.length; ++i) {
-                    //noinspection unchecked
-                    to[i] = (T) from[i].toStringUsingUTF8();
-                }
-
-                return to;
-            }
-            throw new UncheckedDeephavenException("Unsupported nativeType " + nativeType.getName());
+            return convertResultFallbackFun.apply(result);
         }
         int[] from = (int []) result;
         //noinspection unchecked
--- DB/src/main/java/io/deephaven/db/v2/locations/parquet/topage/ToStringPage.java
+++ DB/src/main/java/io/deephaven/db/v2/locations/parquet/topage/ToStringPage.java
@@ -6,18 +6,26 @@ import io.deephaven.db.v2.sources.chunk.ObjectChunk;
 import org.apache.parquet.io.api.Binary;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Array;
+
 public class ToStringPage<ATTR extends Attributes.Any> implements ToPage<ATTR, String[]> {
 
     static final ToStringPage INSTANCE = new ToStringPage();
 
     public static <ATTR extends Attributes.Any>
-    ToPage<ATTR, String[]> create(Class<?> nativeType, org.apache.parquet.column.Dictionary dictionary) {
+    ToPage<ATTR, String[]> create(final Class<?> nativeType, final org.apache.parquet.column.Dictionary dictionary) {
         if (nativeType == null || String.class.equals(nativeType)) {
             //noinspection unchecked
             return dictionary == null ? INSTANCE :
-                    new ToPageWithDictionary<>(String.class, new Dictionary<>( i ->
-                            dictionary.decodeToBinary(i).toStringUsingUTF8(),
-                            dictionary.getMaxId()+1));
+                    new ToPageWithDictionary<>(
+                            String.class,
+                            new Dictionary<>( i -> dictionary.decodeToBinary(i).toStringUsingUTF8(), dictionary.getMaxId()+1),
+                            (final Object result) -> {
+                                final Binary[] from = (Binary[]) result;
+                                final String[] to = (String[]) Array.newInstance(nativeType, from.length);
+                                return convertResult(to, from);
+                            }
+                    );
         }
 
         throw new IllegalArgumentException("The native type for a String column is " + nativeType.getCanonicalName());
@@ -40,16 +48,18 @@ public class ToStringPage<ATTR extends Attributes.Any> implements ToPage<ATTR, S
 
     @Override
     @NotNull
-    public final String[] convertResult(Object result) {
-        Binary [] from = (Binary[]) result;
-        String[] to = ObjectChunk.makeArray(from.length);
+    public final String[] convertResult(final Object result) {
+        final Binary[] from = (Binary[]) result;
+        final String[] to = ObjectChunk.makeArray(from.length);
+        return convertResult(to, from);
+    }
 
+    private static String[] convertResult(final String[] to, final Binary[] from) {
         for (int i = 0; i < to.length; ++i) {
             if (from[i] != null) {
                 to[i] = from[i].toStringUsingUTF8();
             }
         }
-
         return to;
     }
 }
