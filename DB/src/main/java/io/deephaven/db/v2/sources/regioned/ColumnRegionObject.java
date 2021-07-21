3f8b5f6196046eefb95d06229f62dc970aef6409
Ryan Caudy <ryan@deephaven.io>
2021-07-21 16:04:08 -0400

    Add constant page support for byte, char, and object.

--- DB/src/main/java/io/deephaven/db/v2/sources/regioned/ColumnRegionObject.java
+++ DB/src/main/java/io/deephaven/db/v2/sources/regioned/ColumnRegionObject.java
@@ -1,12 +1,13 @@
 package io.deephaven.db.v2.sources.regioned;
 
-import io.deephaven.db.v2.sources.chunk.Attributes;
+import io.deephaven.db.v2.sources.chunk.Attributes.Any;
+import io.deephaven.db.v2.sources.chunk.WritableChunk;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Column region interface for regions that support fetching objects.
  */
-public interface ColumnRegionObject<T, ATTR extends Attributes.Any> extends ColumnRegion<ATTR> {
+public interface ColumnRegionObject<DATA_TYPE, ATTR extends Any> extends ColumnRegion<ATTR> {
 
     /**
      * Get a single object from this region.
@@ -14,7 +15,7 @@ public interface ColumnRegionObject<T, ATTR extends Attributes.Any> extends Colu
      * @param elementIndex Element (object) index in the table's address space
      * @return The object value at the specified element (object) index
      */
-    T getObject(long elementIndex);
+    DATA_TYPE getObject(long elementIndex);
 
     /**
      * Get a single object from this region.
@@ -24,30 +25,32 @@ public interface ColumnRegionObject<T, ATTR extends Attributes.Any> extends Colu
      * @param elementIndex Element (object) index in the table's address space
      * @return The object value at the specified element (object) index
      */
-    default T getObject(@NotNull FillContext context, long elementIndex) {
+    default DATA_TYPE getObject(@NotNull final FillContext context, final long elementIndex) {
         return getObject(elementIndex);
     }
 
-    default ColumnRegionObject<T, ATTR> skipCache() {
+    default ColumnRegionObject<DATA_TYPE, ATTR> skipCache() {
         return this;
     }
 
     @Override
     Class<?> getNativeType();
 
-    static <T, ATTR extends Attributes.Any> ColumnRegionObject.Null<T, ATTR> createNull() {
+    static <T, ATTR extends Any> ColumnRegionObject.Null<T, ATTR> createNull() {
         //noinspection unchecked
         return Null.INSTANCE;
     }
 
-    class Null<T, ATTR extends Attributes.Any> extends ColumnRegion.Null<ATTR> implements ColumnRegionObject<T, ATTR> {
+    class Null<DATA_TYPE, ATTR extends Any> extends ColumnRegion.Null<ATTR> implements ColumnRegionObject<DATA_TYPE, ATTR> {
+
         @SuppressWarnings("rawtypes")
         private static final ColumnRegionObject.Null INSTANCE = new ColumnRegionObject.Null();
 
-        Null() {}
+        Null() {
+        }
 
         @Override
-        public T getObject(long elementIndex) {
+        public DATA_TYPE getObject(final long elementIndex) {
             return null;
         }
 
@@ -56,8 +59,34 @@ public interface ColumnRegionObject<T, ATTR extends Attributes.Any> extends Colu
          * @implNote This is never called, so we just return null so this class can remain a singleton.
          */
         @Override
-        public Class<T> getNativeType() {
+        public Class<DATA_TYPE> getNativeType() {
             return null;
+        }
+    }
+
+    final class Constant<DATA_TYPE, ATTR extends Any> implements ColumnRegionObject<DATA_TYPE, ATTR>, WithDefaultsForRepeatingValues<ATTR> {
+
+        private final DATA_TYPE value;
+
+        public Constant(final DATA_TYPE value) {
+            this.value = value;
+        }
+
+        @Override
+        public Class<?> getNativeType() {
+            return value.getClass();
+        }
+
+        @Override
+        public DATA_TYPE getObject(final long elementIndex) {
+            return value;
+        }
+
+        @Override
+        public void fillChunkAppend(@NotNull final FillContext context, @NotNull final WritableChunk<? super ATTR> destination, final int length) {
+            final int offset = destination.size();
+            destination.asWritableObjectChunk().fillWithValue(offset, length, value);
+            destination.setSize(offset + length);
         }
     }
 }
