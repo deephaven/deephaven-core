f76ed5a205356278cf8286dae475a48f1088f31c
Devin Smith <devinsmith@deephaven.io>
2021-07-23 11:40:55 -0700

    Scope of AllowNulls

--- qst/src/main/java/io/deephaven/annotations/AllowNulls.java
+++ qst/src/main/java/io/deephaven/annotations/AllowNulls.java
@@ -5,8 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to allow nulls in {@link org.immutables.value.Value.Immutable} collections.
+ *
+ * @see <a href="https://immutables.github.io/immutable.html#nulls-in-collection>nulls-in-collection</a>
+ */
 @Retention(RetentionPolicy.CLASS)
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
+@Target({ElementType.METHOD})
 public @interface AllowNulls {
 
 }
