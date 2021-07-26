2a129d2c2e70f76d0acb4a9bdbe3adb3daaf82ce
Devin Smith <devinsmith@deephaven.io>
2021-07-26 07:22:37 -0700

    TableCreation javadoc

--- qst/src/main/java/io/deephaven/qst/TableCreation.java
+++ qst/src/main/java/io/deephaven/qst/TableCreation.java
@@ -21,11 +21,35 @@ public interface TableCreation<TABLE> {
         return TableAdapterImpl.toTable(creation, toOps, toTable, table);
     }
 
+    /**
+     * Creates a new table.
+     *
+     * @param newTable the new table specification
+     * @return the new table
+     */
     TABLE of(NewTable newTable);
 
+    /**
+     * Creates an empty table.
+     *
+     * @param emptyTable the empty table specification
+     * @return the empty table
+     */
     TABLE of(EmptyTable emptyTable);
 
+    /**
+     * Creates a time table.
+     *
+     * @param timeTable the time table specifications
+     * @return the time table
+     */
     TABLE of(TimeTable timeTable);
 
+    /**
+     * Merges the given {@code tables}.
+     *
+     * @param tables the tables
+     * @return the merged results
+     */
     TABLE merge(Collection<TABLE> tables);
 }
