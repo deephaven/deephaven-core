12c7b55e052d555e9c394298e99cf904540368fd
Mike Bender <mikebender@deephaven.io>
2021-07-26 21:06:09 -0400

    Expose IdeConnection and getConsoleTypes to the client (#863)
    Fixes #862
    
    Client can now start a session like:
    ```
    const connection = new IdeConnection( 'http://localhost:10000');
    const consoleTypes = await connection.getConsoleTypes();
    const session = await connection.startSession(consoleTypes[0]);
    ```
    
    * Clean up based on Colin's review
    
    - Fix up IdeConnection constructor - now it takes two params, one for the URL, then another for connection options
    - Change up IdeClient to use new connection options
    - Delete the unused classes ConsoleAddress and JsWorkspaceData
    - Update some of the examples with new usage. For the examples that have code snippets written in Groovy, I didn't change anything
    
    Tested with the following code snippets. Ensured the auth token was read/decoded properly when it was passed in.
    
    // Options unset
    var c = new dh.IdeConnection('http://localhost:10000')
    
    // Options object set, no auth token set
    var options = new dh.IdeConnectionOptions();
    var c = new dh.IdeConnection('http://localhost:10000', options);
    
    // Options object set, auth token set
    var options = new dh.IdeConnectionOptions();
    options.authToken = '1234';
    var c = new dh.IdeConnection('http://localhost:10000', options);
    
    // Property map passed in with auth token set
    var c = new dh.IdeConnection('http://localhost:10000', { authToken: '1234' });

---
+++ web/client-ide/src/main/java/io/deephaven/ide/client/IdeConnectionOptions.java
@@ -0,0 +1,15 @@
+package io.deephaven.ide.client;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+
+@JsType(namespace = "dh")
+public class IdeConnectionOptions {
+    public String authToken;
+    public String serviceId;
+
+    @JsConstructor
+    public IdeConnectionOptions() {
+    }
+}
