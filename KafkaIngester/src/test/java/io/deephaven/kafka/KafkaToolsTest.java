51312dcaba6dcbfaf8d08e6ab8f129d24d05e39c
Cristian Ferretti <cristianferretti@deephaven.io>
2021-08-07 09:43:27 -0400

    Added a test case for basic types.

--- KafkaIngester/src/test/java/io/deephaven/kafka/KafkaToolsTest.java
+++ KafkaIngester/src/test/java/io/deephaven/kafka/KafkaToolsTest.java
@@ -65,4 +65,42 @@ public class KafkaToolsTest {
         assertEquals("Price", colDefs[1].getName());
         assertEquals(double.class, colDefs[1].getDataType());
     }
+
+    private static final String schemaWithBasicTypes =
+                    "  { "
+                    + "    \"type\": \"record\", "
+                    + "    \"name\": \"types_schema\","
+                    + "    \"namespace\": \"io.deephaven.test\","
+                    + "    \"fields\" : ["
+                    + "          {\"name\": \"BooleanField\", \"type\": \"boolean\" },"
+                    + "          {\"name\": \"IntField\",  \"type\": \"int\" },"
+                    + "          {\"name\": \"LongField\",  \"type\": \"long\" },"
+                    + "          {\"name\": \"FloatField\",  \"type\": \"float\" },"
+                    + "          {\"name\": \"DoubleField\",  \"type\": \"double\" },"
+                    + "          {\"name\": \"StringField\",  \"type\": \"string\" }"
+                    + "      ]"
+                    + "}"
+            ;
+
+    @Test
+    public void testAvroSChemaWithBasicTypesCoverage() {
+        final Schema avroSchema = new Schema.Parser().parse(schemaWithBasicTypes);
+        final ColumnDefinition<?>[] colDefs = KafkaTools.avroSchemaToColumnDefinitions(avroSchema);
+        final int nCols = 6;
+        assertEquals(nCols, colDefs.length);
+        int c = 0;
+        assertEquals("BooleanField", colDefs[c].getName());
+        assertEquals(Boolean.class, colDefs[c++].getDataType());
+        assertEquals("IntField", colDefs[c].getName());
+        assertEquals(int.class, colDefs[c++].getDataType());
+        assertEquals("LongField", colDefs[c].getName());
+        assertEquals(long.class, colDefs[c++].getDataType());
+        assertEquals("FloatField", colDefs[c].getName());
+        assertEquals(float.class, colDefs[c++].getDataType());
+        assertEquals("DoubleField", colDefs[c].getName());
+        assertEquals(double.class, colDefs[c++].getDataType());
+        assertEquals("StringField", colDefs[c].getName());
+        assertEquals(String.class, colDefs[c++].getDataType());
+        assertEquals(nCols, c);
+    }
 }
