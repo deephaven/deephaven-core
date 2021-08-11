c2a4e1c154003c98fd7428f08beb1246080c9fd1
alexpeters1208 <80283343+alexpeters1208@users.noreply.github.com>
2021-08-11 14:01:51 -0600

    Removed dead code in Python Utilities and corrected error messages/javadocs.

--- Integrations/src/main/java/io/deephaven/integrations/python/PythonUtilities.java
+++ Integrations/src/main/java/io/deephaven/integrations/python/PythonUtilities.java
@@ -19,28 +19,6 @@ class PythonUtilities {
      * @return python function that should be called by a listener.
      * @throws IllegalArgumentException python listener object is not a valid listener.
      */
-
-    /*
-    static PyObject pyListenerFunc(final PyObject pyObject) {
-        if (pyObject.hasAttribute("onUpdate")) {
-            PyObject pyCallable = pyObject.getAttribute("onUpdate");
-
-            if (!pyCallable.hasAttribute("__call__")) {
-                throw new IllegalArgumentException("The Python object provided has an onUpdate attribute " +
-                        "which is not callable");
-            }
-
-            return pyCallable;
-
-        } else if (pyObject.hasAttribute("__call__")) {
-            return pyObject;
-        } else {
-            throw new IllegalArgumentException("The Python object specified should either be callable, or a " +
-                    "class instance with an onUpdate method");
-        }
-    }
-     */
-
     static PyObject pyListenerFunc(final PyObject pyObject) {
         return pyCallable(pyObject, "onUpdate");
     }
@@ -53,34 +31,13 @@ class PythonUtilities {
      *                   which is callable.
      * @return pyCallable that can be called directly with arguments using pyCallable.call(...).
      */
-    /*
-    static PyObject pyApplyFunc(PyObject pyObjectIn) {
-        PyObject pyCallable;
-        if(pyObjectIn.hasAttribute("apply")){
-            pyCallable = pyObjectIn.getAttribute("apply");
-
-            if (!pyCallable.hasAttribute("__call__")){
-                throw new IllegalArgumentException("The Python object provided has an apply attribute " +
-                        "which is not callable");
-            }
-
-            return pyCallable;
-
-        } else if (pyObjectIn.hasAttribute("__call__")){
-            return pyObjectIn;
-        } else {
-            throw new IllegalArgumentException("The Python object specified should either be callable, or a " +
-                    "class instance with an apply method");
-        }
-    }
-    */
-
     static PyObject pyApplyFunc(PyObject pyObject) {
         return pyCallable(pyObject, "apply");
     }
 
+
     /**
-     * Creates a callable PyObject, either using method.apply() or __call__(), if the pyObjectIn has such methods available.
+     * Creates a callable PyObject, either using method.pyAttribute() or __call__(), if the pyObjectIn has such methods available.
      *
      * @param pyObject      the python object providing the function - must either be callable or have an `apply` attribute
      *                      which is callable.
