77cb6e90e20a9b021f6a97466b0c9a8d7449d275
JJ Brosnan <84038776+jjbrosnan@users.noreply.github.com>
2021-08-11 09:32:43 -0400

    Update types to only work if JVM is running

--- Integrations/python/deephaven/types.py
+++ Integrations/python/deephaven/types.py
@@ -8,18 +8,21 @@ Functions to return types from jpy for convenience
 
 import jpy
 
-# Java primitives
+# Only create this module if the JVM is running
+if jpy.has_jvm():
 
-Bool = jpy.get_type("boolean")
-Byte = jpy.get_type("byte")
-Char = jpy.get_type("char")
-Double = jpy.get_type("double")
-Float = jpy.get_type("float")
-Int = jpy.get_type("int")
-Long = jpy.get_type("long")
-Short = jpy.get_type("short")
-String = jpy.get_type("java.lang.String")
+    # Java primitives
 
-# Deephaven internal types
+    Bool = jpy.get_type("boolean")
+    Byte = jpy.get_type("byte")
+    Char = jpy.get_type("char")
+    Double = jpy.get_type("double")
+    Float = jpy.get_type("float")
+    Int = jpy.get_type("int")
+    Long = jpy.get_type("long")
+    Short = jpy.get_type("short")
+    String = jpy.get_type("java.lang.String")
 
-DateTime = jpy.get_type("io.deephaven.db.tables.utils.DBDateTime")
+    # Deephaven internal types
+
+    DateTime = jpy.get_type("io.deephaven.db.tables.utils.DBDateTime")
