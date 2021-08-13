a263cb9eed4125ddbedc9110181f9d1cbaf09fa5
Charles P. Wright <cpwright@gmail.com>
2021-08-13 14:35:30 -0400

    Propagate errors from Kafka to tables. (#1059)
    * propagate failures
    
    * conflict
    
    * reorder
    
    * revert
    
    * Copy join attribute.
    
    * line wrap
    
    Co-authored-by: Charles P. Wright <charleswright@illumon.com>

---
+++ KafkaIngester/src/main/java/io/deephaven/kafka/ingest/KafkaStreamConsumer.java
@@ -0,0 +1,14 @@
+package io.deephaven.kafka.ingest;
+
+import io.deephaven.stream.StreamFailureConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Consumer for lists of ConsumerRecords coming from Kafka.  The StreamFailureConsumer is extended so that we can report
+ * errors emanating from our consumer thread.
+ */
+public interface KafkaStreamConsumer extends Consumer<List<? extends ConsumerRecord<?, ?>>>, StreamFailureConsumer {
+}
