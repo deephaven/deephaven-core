fcfc4d9ffcede6d983aa4dc3a7ac9220b5c174c2
alexpeters1208 <80283343+alexpeters1208@users.noreply.github.com>
2021-08-12 20:40:09 -0600

    Started Python input validation. TODO: FutureOffset / Result column conflicts, batch size defaults, pydocs.

--- Integrations/python/deephaven/learn/__init__.py
+++ Integrations/python/deephaven/learn/__init__.py
@@ -4,7 +4,7 @@
 
 """
 Deephaven's learn module enables the use of libraries like PyTorch and Tensorflow in conjunction with Deephaven tables
-for efficient real-time data work.
+for efficient real-time data manipulation, modelling, and forecasting.
 
 Classes:
 
@@ -22,17 +22,12 @@ Functions:
                            'target' and 'features' arguments in that order
         :param inputs: the list of Input objects that determine which columns get extracted from the Deephaven table
         :param outputs: the list of Output objects that determine how to store output from model_func into the Deephaven table
+        :param batch_size:
 """
 
 import jpy
 import wrapt
-from deephaven import npy
-from deephaven import listen
-import numpy as np
-import torch
-import tensorflow as tf
 
-_called_ = False
 _Input_ = None
 _Output_ = None
 _Computer_ = None
@@ -49,9 +44,8 @@ def _defineSymbols():
     if not jpy.has_jvm():
         raise SystemError("No java functionality can be used until the JVM has been initialized through the jpy module")
 
-    global _called_, _Input_, _Output_, _Computer_, _Scatterer_
-    if not _called_:
-        _called_ = True
+    global _Input_, _Output_, _Computer_, _Scatterer_
+    if _Input_ is None:
         _Input_ = jpy.get_type("io.deephaven.integrations.learn.Input")
         _Output_ = jpy.get_type("io.deephaven.integrations.learn.Output")
         _Computer_ = jpy.get_type("io.deephaven.integrations.learn.Computer")
@@ -64,13 +58,15 @@ def _passThrough(wrapped, instance, args, kwargs):
     """
     For decoration of module methods, to define necessary symbols at runtime
 
-    :param wrapped: the method to be decorated
-    :param instance: the object to which the wrapped function was bound when it was called
-    :param args: the argument list for `wrapped`
-    :param kwargs: the keyword argument dictionary for `wrapped`
-    :return: the decorated version of the method
-    """
+    Args:
+        wrapped  (type): the method to be decorated
+        instance (type): the object to which the wrapped function was bound when it was called
+        args     (type): the argument list for `wrapped`
+        kwargs   (type): the keyword argument dictionary for `wrapped`
 
+    Returns:
+        type: the decorated version of the method.
+    """
     _defineSymbols()
     return wrapped(*args, **kwargs)
 
@@ -78,12 +74,20 @@ def _passThrough(wrapped, instance, args, kwargs):
 @_passThrough
 class Input:
     """
-    Provides an interface for getting data from Deephaven tables to Python objects that popular deep learning libraries
-    know how to use. Avoids many inefficencies of Python and allows for seamless integration of Deephaven's real-time
-    capabilities with modern AI frameworks.
+    Input specifies how to gather data from a Deephaven table into a Python object.
+
+    Attributes:
+        input (io.deephaven.integrations.Input):
     """
     def __init__(self, colNames, gatherFunc):
+        """
+        Args:
+            colNames  (:obj:`list` of :obj:`str` or str): Column name or list of column names
+        """
         self.input = _Input_(colNames, gatherFunc)
+
+    def __str__(self):
+        return "Columns: " + str([self.input.getColNames()[i] for i in range(len(self.input.getColNames()))])
 
 
 @_passThrough
@@ -94,6 +98,26 @@ class Output:
     """
     def __init__(self, colName, scatterFunc, type=None):
         self.output = _Output_(colName, scatterFunc, type)
+
+    def __str__(self):
+        print("Column: " + self.output.getColName())
+        print("Type: " + self.output.getType())
+
+
+@_passThrough
+def _validate(inputs, outputs, table):
+
+    inputColumns = [input.input.getColNames()[i] for input in inputs for i in range(len(input.input.getColNames()))]
+
+    if not all(inputColumn in list(table.getMeta().getColumn("Name").getDirect()) for inputColumn in inputColumns):
+        raise ValueError("All Input columns must exist in the given table.")
+
+    columns = [*set(inputColumns), *[output.output.getColName() for output in outputs]]
+
+    if len(columns) != len(set(columns)):
+        raise ValueError("Cannot use existing column names for Output columns.")
+
+    return
 
 
 @_passThrough
@@ -109,20 +133,18 @@ def eval(table=None, model_func=None, inputs=[], outputs=[], batch_size = None):
     :param outputs: the list of Output objects that determine how to store output from model_func into the Deephaven table
     """
 
-    # reminder to do error checking on the set of inputs and the set of outputs
+    _validate(inputs, outputs, table)
 
     if batch_size == None:
         if table.isLive():
             raise ValueError("Batch size cannot be inferred on a live table. Please specify a batch size.")
         batch_size = table.size()
 
-    #computer = _Computer_(table, model_func, [input.input for input in inputs], batch_size)
     globals()["computer"] = _Computer_(table, model_func, [input.input for input in inputs], batch_size)
 
     if not outputs == None:
-        #scatterer = _Scatterer_([output.output for output in outputs])
         globals()["scatterer"] = _Scatterer_([output.output for output in outputs])
 
         return table.update("FutureOffset = computer.compute(k)", "Clean = computer.clear()").update(scatterer.generateQueryStrings("FutureOffset")).dropColumns("FutureOffset","Clean")
 
-    return table.update(["FutureOffset = computer.compute(k)", "Clean = computer.clear()", "Result = FutureOffset.getFuture().get()"]).dropColumns("FutureOffset","Clean","Result")
+    return table.update("FutureOffset = computer.compute(k)", "Clean = computer.clear()", "Result = FutureOffset.getFuture().get()").dropColumns("FutureOffset","Clean","Result")
