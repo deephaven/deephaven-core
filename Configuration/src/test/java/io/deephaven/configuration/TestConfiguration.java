3d46a74e572e32e91fed19e4ecba3e2660525733
Devin Smith <devinsmith@deephaven.io>
2021-08-20 07:11:58 -0700

    Fix tests after styling applied

--- Configuration/src/test/java/io/deephaven/configuration/TestConfiguration.java
+++ Configuration/src/test/java/io/deephaven/configuration/TestConfiguration.java
@@ -508,14 +508,14 @@ public class TestConfiguration extends TestCase {
         assertEquals("[]", history.get(0).context);
         if ("1.8".equals(System.getProperty("java.specification.version"))) {
             assertEquals(
-                "<not from configuration file>: io.deephaven.configuration.TestConfiguration.testShowHistory(TestConfiguration.java:426)\n"
+                "<not from configuration file>: io.deephaven.configuration.TestConfiguration.testShowHistory(TestConfiguration.java:502)\n"
                     +
                     "sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n" +
                     "sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\n",
                 history.get(0).fileName);
         } else {
             assertEquals(
-                "<not from configuration file>: io.deephaven.configuration.TestConfiguration.testShowHistory(TestConfiguration.java:426)\n"
+                "<not from configuration file>: io.deephaven.configuration.TestConfiguration.testShowHistory(TestConfiguration.java:502)\n"
                     +
                     "java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n"
                     +
