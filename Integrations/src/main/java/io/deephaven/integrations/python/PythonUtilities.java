8addc734b1b446b934d429fb9c53ad99f6a55cde
Colin Alworth <colin@vertispan.com>
2021-08-20 22:57:52 -0500

    Revert "First iteration of Learn library (#974)" (#1096)
    This reverts commit 216ca7dd9edce1930decb541941babfdc21bb5f9.

--- Integrations/src/main/java/io/deephaven/integrations/python/PythonUtilities.java
+++ Integrations/src/main/java/io/deephaven/integrations/python/PythonUtilities.java
@@ -20,46 +20,20 @@ class PythonUtilities {
      * @throws IllegalArgumentException python listener object is not a valid listener.
      */
     static PyObject pyListenerFunc(final PyObject pyObject) {
-        return pyCallable(pyObject, "onUpdate");
-    }
+        if (pyObject.hasAttribute("onUpdate")) {
+            PyObject pyCallable = pyObject.getAttribute("onUpdate");
 
-
-    /**
-     * Creates a callable PyObject, either using method.apply() or __call__(), if the pyObjectIn has such methods available.
-     *
-     * @param pyObject the python object providing the function - must either be callable or have an `apply` attribute
-     *                   which is callable.
-     * @return pyCallable that can be called directly with arguments using pyCallable.call(...).
-     */
-    static PyObject pyApplyFunc(PyObject pyObject) {
-        return pyCallable(pyObject, "apply");
-    }
-
-
-    /**
-     * Creates a callable PyObject, either using method.pyAttribute() or __call__(), if the pyObjectIn has such methods available.
-     *
-     * @param pyObject      the python object providing the function - must either be callable or have an `apply` attribute
-     *                      which is callable.
-     * @param pyAttribute   the python attribute that provides the callable method.
-     * @return pyCallable that can be called directly with arguments using pyCallable.call(...).
-     */
-    static PyObject pyCallable(final PyObject pyObject, final String pyAttribute) {
-
-        PyObject pyCallable;
-        if(pyObject.hasAttribute(pyAttribute)){
-            pyCallable = pyObject.getAttribute(pyAttribute);
-
-            if (!pyCallable.hasAttribute("__call__")){
-                throw new IllegalArgumentException("The Python object provided has a \"" + pyAttribute + "\" attribute which is not callable");
+            if (!pyCallable.hasAttribute("__call__")) {
+                throw new IllegalArgumentException("The Python object provided has an onUpdate attribute " +
+                        "which is not callable");
             }
 
             return pyCallable;
-        } else if (pyObject.hasAttribute("__call__")){
+        } else if (pyObject.hasAttribute("__call__")) {
             return pyObject;
         } else {
             throw new IllegalArgumentException("The Python object specified should either be callable, or a " +
-                    "class instance with a \"" + pyAttribute + "\" method");
+                    "class instance with an onUpdate method");
         }
     }
 }
