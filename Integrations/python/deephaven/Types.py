55d96f531826e9d70f092f9999425437814ff7b7
Cristian Ferretti <cristianferretti@deephaven.io>
2021-08-20 23:21:33 -0400

    Support creating tables by columns.

--- Integrations/python/deephaven/Types.py
+++ Integrations/python/deephaven/Types.py
@@ -16,7 +16,6 @@ _table_tools_ = None
 _col_def_ = None
 _python_tools_ = None
 _qst_col_header_ = None
-_qst_col_header_of_ = None
 _qst_column_ = None
 _qst_newtable_ = None
 _table_ = None
@@ -179,50 +178,26 @@ def cols(ts):
     return r
 
 
-#
-# import dhtypes as dh
-#
-# data = [['tom', 10], ['nick', 15], ['juli', 14]]
-# columns = [ ('Name' : dh.string), ('Price' : dh.double) ]
-# t = dh.table_of(data, columns)
-#
-#
 @_passThrough
-def table_of(data, columns):
-    """
-    Create a Deephaven table; the API should feel familiar to users of Panda's DataFrame.
-
-    :param data: a list of rows, where each row is in turn a list of values.
-    :param columns: a list of tuples of the form (name, type) where name is the intended column name
-                    and type is the intended Deephaven column data type.
-    :return: a table with columns and data as specified in the arguments.
-    """
-
-    if columns is None or len(columns) == 0:
-        if data is not None or len(data) > 0:
-            raise Exception("when no column definitions are provided in the 'columns' argument, " +
-                            "only an empty table can be created, and no data can be specified; instead " +
-                            "got a non-empty 'data' argument with " + str(data))
-        return _table_tools_.emptyTable()
-
-    if isinstance(columns, str) or not isinstance(columns, collections.Sequence) or len(columns) < 1:
-        raise Exception("argument 'columns' needs to be a sequence with at least one element, " +
-                        "instead got " + str(columns) + " of type " + type(columns).__name__)
+def _getQstCol(col_name, col_type, col_data=None):
+    if col_data is None or len(col_data) < 1:
+        jclass = _jclassFromType(col_type)
+        col_header = _qst_col_header_.of(col_name, jclass)
+        return _qst_column_.empty(col_header)
+    jvalues = jpy.array(col_type, col_data)
+    if _isPrimitive(col_type):
+        return _qst_column_.ofUnsafe(col_name, jvalues)
+    else:
+        return _qst_column_.of(col_name, jvalues)
     
-    if data is None or len(data) == 0:
-        col_header = None
-        for t in columns:
-            if len(t) != 2:
-                raise Exception("only two element type tuples are supported, instead got " + str(t))
-            try:
-                if col_header is None:
-                    col_header = _qst_col_header_of_(t[0], t[1])
-                else:
-                    col_header = col_header.header(t[0], t[1])
-            except Exception as e:
-                raise Exception("Could not create column definition from " + str(t)) from e
-        return _table_.of(col_header)
 
+@_passThrough
+def _getTable(qst_cols):
+    qst_newtable = _qst_newtable_.of(qst_cols)
+    return _table_.of(qst_newtable)        
+    
+@_passThrough
+def _table_by_rows(data, columns):
     nrows = len(data)
     ncols = len(columns)
 
@@ -237,12 +212,85 @@ def table_of(data, columns):
             if len(row) < c + 1:
                 raise Exception("not enough columns provided in row " + r)
             col_data.append(row[c])
-        jvalues = jpy.array(col_type, col_data)
-        if _isPrimitive(col_type):
-            qst_col = _qst_column_.ofUnsafe(col_name, jvalues)
-        else:
-            qst_col = _qst_column_.of(col_name, jvalues)
+        qst_col = _getQstCol(col_name, col_type, col_data)
         qst_cols.append(qst_col)
 
-    qst_newtable = _qst_newtable_.of(qst_cols)
-    return _table_.of(qst_newtable)
+    return _getTable(qst_cols)
+
+
+@_passThrough
+def _table_by_cols(data:dict):
+    qst_cols = []
+    for col_name, type_values in data.items():
+        if not isinstance(type_values, collections.Sequence):
+            col_type = type_values
+            col_data = None
+        else:
+            col_type = type_values[0]
+            if len(type_values) < 2:
+                col_data = None
+            else:
+                col_data = type_values[1]
+                if not isinstance(col_data, collections.Sequence):
+                    raise Exception("'data' argument is expected to contain a dict with " +
+                                    "values of sequence type, with a first element indicating a " +
+                                    "data type, and the second element is a sequence of column values, " +
+                                    "instead got " + str(col_data) + " of type " + type(col_data).__name__)
+        qst_col = _getQstCol(col_name, col_type, col_data)
+        qst_cols.append(qst_col)
+
+    return _getTable(qst_cols)
+
+
+#
+# import dhtypes as dh
+#
+# data = [['tom', 10], ['nick', 15], ['juli', 14]]
+# columns = [ ('Name' : dh.string), ('Price' : dh.double) ]
+# t = dh.table_of(data, columns)
+#
+#
+@_passThrough
+def table_of(data, columns=None):
+    """
+    Create a Deephaven table; the API should feel familiar to users of Panda's DataFrame.
+
+    :param data: Either a sequence of rows, where each row is in turn a list of values (requires passing
+                 the additional columns parameneter to specify column names and data types) or
+                 a dict whose keys are column names and the values are either a data type, or a sequence of (1) type,
+                 for the intended Deephaven column data type, and (2) values for the column
+    :param columns: a list of tuples of the form (name, type) where name is the intended column name
+                    and type is the intended Deephaven column data type.
+    :return: a table with columns and data as specified in the arguments.
+    """
+
+    if (columns is None or len(columns) == 0) and not isinstance(data, dict):
+        if data is not None or len(data) > 0:
+            raise Exception("when no column definitions are provided in the 'columns' argument, " +
+                            "only an empty table can be created, and no data can be specified; instead " +
+                            "got a non-empty 'data' argument with " + str(data))
+        return _table_tools_.emptyTable(0)
+
+    if not isinstance(data, dict) and \
+       (isinstance(columns, str) or not isinstance(columns, collections.Sequence) or len(columns) < 1):
+        raise Exception("argument 'columns' needs to be a sequence with at least one element, " +
+                        "instead got " + str(columns) + " of type " + type(columns).__name__)
+    
+    if data is None or len(data) == 0:
+        col_header = None
+        for t in columns:
+            if len(t) != 2:
+                raise Exception("only two element type tuples are supported, instead got " + str(t))
+            try:
+                if col_header is None:
+                    col_header = _qst_col_header_.of(t[0], t[1])
+                else:
+                    col_header = col_header.header(t[0], t[1])
+            except Exception as e:
+                raise Exception("Could not create column definition from " + str(t)) from e
+        return _table_.of(col_header)
+
+    if isinstance(data, collections.Sequence):
+        return _table_by_rows(data, columns)
+    elif isinstance(data, dict):
+        return _table_by_cols(data)
