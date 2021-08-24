9b851e5c571fc0808c4a436e90cfe5b3ff177796
JJ Brosnan <84038776+jjbrosnan@users.noreply.github.com>
2021-08-24 16:20:12 -0400

    Add DynamicTableWriter to deephaven module (#1117)

--- Integrations/python/deephaven/TableManipulation/__init__.py
+++ Integrations/python/deephaven/TableManipulation/__init__.py
@@ -8,13 +8,14 @@ Functionality to display and modify tables.
 
 import jpy
 
-__all__ = ['ColumnRenderersBuilder', 'DistinctFormatter', 'DownsampledWhereFilter', 'LayoutHintBuilder',
-           'SmartKey', 'SortPair', 'TotalsTableBuilder', 'WindowCheck']
+__all__ = ['ColumnRenderersBuilder', 'DistinctFormatter', 'DownsampledWhereFilter', 'DynamicTableWriter', 
+           'LayoutHintBuilder', 'SmartKey', 'SortPair', 'TotalsTableBuilder', 'WindowCheck']
 
 # None until the first successful _defineSymbols() call
 ColumnRenderersBuilder = None   #: Class to build and parse the directive for Table.COLUMN_RENDERERS_ATTRIBUTE (io.deephaven.db.v2.ColumnRenderersBuilder).
 DistinctFormatter = None        #: Class to create distinct and unique coloration for each unique input value (io.deephaven.db.util.DBColorUtil$DistinctFormatter).
 DownsampledWhereFilter = None   #: Class to downsample time series data by calculating the bin intervals for values, and then using upperBin and lastBy to select the last row for each bin (io.deephaven.db.v2.select.DownsampledWhereFilter).
+DynamicTableWriter = None       #: Class to create a TableWriter object {@link io.deephaven.db.v2.utils.DynamicTableWriter}
 LayoutHintBuilder = None        #: Builder class for use in assembling layout hints suitable for use with {@link io.deephaven.db.tables.Table#layoutHints(LayoutHintBuilder)} or {@link io.deephaven.db.tables.Table#layoutHints(String)} (io.deephaven.db.tables.utils.LayoutHintBuilder).
 SmartKey = None                 #: A datastructure key class, where more than one value can be used as the key (io.deephaven.datastructures.util.SmartKey).
 TotalsTableBuilder = None       #: Class to define the default aggregations and display for a totals table (io.deephaven.db.v2.TotalsTableBuilder).
@@ -31,13 +32,14 @@ def _defineSymbols():
     if not jpy.has_jvm():
         raise SystemError("No java functionality can be used until the JVM has been initialized through the jpy module")
 
-    global ColumnRenderersBuilder, DistinctFormatter, DownsampledWhereFilter, LayoutHintBuilder, \
-        SmartKey, TotalsTableBuilder, SortPair
+    global ColumnRenderersBuilder, DistinctFormatter, DownsampledWhereFilter, DynamicTableWriter, \
+        LayoutHintBuilder, SmartKey, TotalsTableBuilder, SortPair
 
     if ColumnRenderersBuilder is None:
         # This will raise an exception if the desired object is not the classpath
         ColumnRenderersBuilder = jpy.get_type('io.deephaven.db.v2.ColumnRenderersBuilder')
         DistinctFormatter = jpy.get_type('io.deephaven.db.util.DBColorUtil$DistinctFormatter')
+        DynamicTableWriter = jpy.get_type('io.deephaven.db.v2.utils.DynamicTableWriter')
         DownsampledWhereFilter = jpy.get_type('io.deephaven.db.v2.select.DownsampledWhereFilter')
         LayoutHintBuilder = jpy.get_type('io.deephaven.db.tables.utils.LayoutHintBuilder')
         SmartKey = jpy.get_type('io.deephaven.datastructures.util.SmartKey')
