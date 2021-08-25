1cf704dcb1892842f1b778773f7613d5fa7a9076
Devin Smith <devinsmith@deephaven.io>
2021-08-25 15:37:01 -0700

    add connection options for flight examples

--- java-client/flight-examples/src/main/java/io/deephaven/client/examples/FlightExampleBase.java
+++ java-client/flight-examples/src/main/java/io/deephaven/client/examples/FlightExampleBase.java
@@ -20,14 +20,28 @@ abstract class FlightExampleBase implements Callable<Void> {
             defaultValue = "localhost:10000")
     String target;
 
+    @Option(names = {"-p", "--plaintext"}, description = "Use plaintext.")
+    Boolean plaintext;
+
+    @Option(names = {"-u", "--user-agent"}, description = "User-agent.")
+    String userAgent;
+
     protected abstract void execute(FlightSession flight) throws Exception;
 
     @Override
     public final Void call() throws Exception {
         BufferAllocator bufferAllocator = new RootAllocator();
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(4);
-        ManagedChannel managedChannel =
-                ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+        ManagedChannelBuilder<?> channelBuilder = ManagedChannelBuilder.forTarget(target);
+        if ((plaintext != null && plaintext) || "localhost:10000".equals(target)) {
+            channelBuilder.usePlaintext();
+        } else {
+            channelBuilder.useTransportSecurity();
+        }
+        if (userAgent != null) {
+            channelBuilder.userAgent(userAgent);
+        }
+        ManagedChannel managedChannel = channelBuilder.build();
 
         Runtime.getRuntime()
                 .addShutdownHook(new Thread(() -> onShutdown(scheduler, managedChannel)));
