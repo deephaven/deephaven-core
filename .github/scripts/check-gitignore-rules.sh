62acdc8b5c085eeff7065fb5979c584dc38f6676
Devin Smith <devinsmith@deephaven.io>
2021-08-27 09:49:47 -0700

    fix command

--- .github/scripts/check-gitignore-rules.sh
+++ .github/scripts/check-gitignore-rules.sh
@@ -4,9 +4,9 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-if [[ $(git ls-files -i --exclude-standard | wc -c) -ne 0 ]]; then
+if [[ $(git ls-files --cached --ignored --exclude-standard | wc -c) -ne 0 ]]; then
   >&2 echo ".gitignore rules is inconsistent with the checked in files"
-  >&2 git ls-files -i --exclude-standard
+  >&2 git ls-files --cached --ignored --exclude-standard
   exit 1
 fi
 
