c0cf6b8a84a55122fe46b3c428d2d75fdb151b57
jianfengmao <jianfengmao@deephaven.io>
2021-08-31 16:38:34 -0600

    attach GH tickets to skipped tests, fixes 1171

--- pyclient/tests/test_console.py
+++ pyclient/tests/test_console.py
@@ -10,7 +10,7 @@ from tests.testbase import BaseTestCase
 
 class ConsoleTestCase(BaseTestCase):
 
-    @unittest.skip("GH ticket filed.")
+    @unittest.skip("GH ticket filed #1177.")
     def test_bind_table(self):
         pa_table = csv.read_csv(self.csv_file)
         user_table = self.session.import_table(pa_table)
