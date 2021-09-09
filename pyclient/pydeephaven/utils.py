736cbe1cccfa23cc3f3e8b253ec5e42f2539fbb4
jianfengmao <jianfengmao@deephaven.io>
2021-09-09 08:33:12 -0600

    correctly set sort direction enum values, fixes #1233

---
+++ pyclient/pydeephaven/utils.py
@@ -0,0 +1,16 @@
+#
+#  Copyright (c) 2016-2021 Deephaven Data Labs and Patent Pending
+#
+import pyarrow
+
+from .dherror import DHError
+from ._arrow_flight_service import _map_arrow_type
+
+
+def is_deephaven_compatible(data_type: pyarrow.DataType) -> bool:
+    """ check if the arrow data type is supported by Deephaven. """
+    try:
+        dh_type = _map_arrow_type(data_type)
+        return True
+    except DHError:
+        return False
