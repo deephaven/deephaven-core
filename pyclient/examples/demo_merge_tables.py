d0b8c36e82539e43c80bf6887bf17b6fcc943618
jianfengmao <jianfengmao@deephaven.io>
2021-09-10 10:03:06 -0600

    added examples to showcase features of pydeephaven, fixes #1146

---
+++ pyclient/examples/demo_merge_tables.py
@@ -0,0 +1,29 @@
+#
+#  Copyright (c) 2016-2021 Deephaven Data Labs and Patent Pending
+#
+""" Demo how to merge tables in Deephaven."""
+import pandas as pd
+
+from examples.demo_import import import_taxi_records
+from examples.demo_query import demo_query
+from examples.demo_table_ops import demo_chained_table_ops
+from pydeephaven import Session
+
+
+def main():
+    with Session(host="localhost", port=10000) as dh_session:
+        taxi_data_table = import_taxi_records(dh_session)
+
+        top_5_fares_table = demo_query(dh_session=dh_session, taxi_data_table=taxi_data_table)
+        bottom_5_fares_table = demo_chained_table_ops(taxi_data_table)
+
+        combined_fares_table = dh_session.merge_tables(tables=[top_5_fares_table, bottom_5_fares_table])
+        snapshot_data = combined_fares_table.snapshot()
+        df = snapshot_data.to_pandas()
+
+        pd.set_option("max_columns", 20)
+        print(df)
+
+
+if __name__ == '__main__':
+    main()
---
+++ pyclient/examples/demo_query.py
@@ -0,0 +1,35 @@
+#
+#  Copyright (c) 2016-2021 Deephaven Data Labs and Patent Pending
+#
+"""Demo how to build and run a query on a Deephaven server."""
+import pandas as pd
+
+from examples.demo_import import import_taxi_records
+from pydeephaven import Session, Table
+
+
+def demo_query(dh_session: Session, taxi_data_table: Table) -> Table:
+    # create a query and execute it on the DH server
+    query = (dh_session.query(taxi_data_table)
+             .where(filters=["VendorID > 0"])
+             .sort(column_names=["VendorID", "fare_amount"])
+             .tail_by(num_rows=5, column_names=["VendorID"]))
+    return query.exec()
+
+
+def main():
+    with Session(host="localhost", port=10000) as dh_session:
+        taxi_data_table = import_taxi_records(dh_session)
+
+        top_5_fares_table = demo_query(dh_session=dh_session, taxi_data_table=taxi_data_table)
+
+        # download the table to the client in the form of pyarrow table and convert it into a Pandas DataFrame
+        snapshot_data = top_5_fares_table.snapshot()
+        df = snapshot_data.to_pandas()
+
+        pd.set_option("max_columns", 20)
+        print(df)
+
+
+if __name__ == '__main__':
+    main()
