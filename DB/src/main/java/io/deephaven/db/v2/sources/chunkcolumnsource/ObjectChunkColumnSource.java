999d9fb8ff1a21b807efc1ec294a83ff01379d42
Ryan Caudy <ryan@deephaven.io>
2021-09-09 22:08:22 -0400

    Fix bug in ChunkColumnSources (#1243)
    * Unit test that reproduce the issue.
    
    * Fix for char source, before replication.
    
    * Replication to address the rest of the chunk column sources.
    
    * Regenerate python docs

--- DB/src/main/java/io/deephaven/db/v2/sources/chunkcolumnsource/ObjectChunkColumnSource.java
+++ DB/src/main/java/io/deephaven/db/v2/sources/chunkcolumnsource/ObjectChunkColumnSource.java
@@ -107,8 +107,11 @@ public class ObjectChunkColumnSource<T> extends AbstractColumnSource<T> implemen
                 final int currentDestinationSize = destination.size();
                 destination.copyFromChunk(ObjectChunk, offsetWithinChunk, currentDestinationSize, length);
                 destination.setSize(currentDestinationSize + length);
-                searchStartChunkIndex.setValue(chunkIndex + 1);
                 s += length;
+                if (s <= e) {
+                    // We have more of this range to gather from a subsequent chunk.
+                    searchStartChunkIndex.setValue(chunkIndex + 1);
+                }
             }
         });
     }
