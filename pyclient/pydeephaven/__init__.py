05314c55cded8521ca6c25862dcf3257e779c7ac
Devin Smith <devinsmith@deephaven.io>
2021-09-30 15:39:51 -0700

    Bump version to 0.5.0 pre-release

--- pyclient/pydeephaven/__init__.py
+++ pyclient/pydeephaven/__init__.py
@@ -30,5 +30,5 @@ from .constants import SortDirection, MatchRule
 from ._table_interface import TableInterface
 from .query import Query
 
-__version__ = "0.4.2"
+__version__ = "0.5.0"
 __all__ = ["Session", "Table", "Query", "TableInterface", "ComboAggregation", "DHError", "SortDirection", "MatchRule"]
