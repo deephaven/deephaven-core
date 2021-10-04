79641c67949ffe799f82dc022baa9a7ec880586b
Chip Kent <5250374+chipkent@users.noreply.github.com>
2021-10-04 15:15:01 -0600

    Reorder QueryConstants by type

--- Util/src/main/java/io/deephaven/util/QueryConstants.java
+++ Util/src/main/java/io/deephaven/util/QueryConstants.java
@@ -12,6 +12,7 @@ public class QueryConstants {
 
     /////////////////////////////////////////////////////////////////
 
+
     /**
      * Null boolean value.
      */
@@ -27,44 +28,19 @@ public class QueryConstants {
     public static final char NULL_CHAR = Character.MAX_VALUE - 1;
 
     /**
-     * Null byte value.
+     * Null boxed Character value.
      */
-    public static final byte NULL_BYTE = Byte.MIN_VALUE;
-
-    /**
-     * Null short value.
-     */
-    public static final short NULL_SHORT = Short.MIN_VALUE;
-    
-    /**
-     * Null int value.
-     */
-    public static final int NULL_INT = Integer.MIN_VALUE;
-
-    /**
-     * Null long value.
-     */
-    public static final long NULL_LONG = Long.MIN_VALUE;
-
-    /**
-     * Null float value.
-     */
-    public static final float NULL_FLOAT = -Float.MAX_VALUE;
-
-    /**
-     * Null double value.
-     */
-    public static final double NULL_DOUBLE = -Double.MAX_VALUE;
+    @SuppressWarnings("unused")
+    public static final Character NULL_CHAR_BOXED = NULL_CHAR;
 
 
     /////////////////////////////////////////////////////////////////
 
 
     /**
-     * Null boxed Character value.
+     * Null byte value.
      */
-    @SuppressWarnings("unused")
-    public static final Character NULL_CHAR_BOXED = NULL_CHAR;
+    public static final byte NULL_BYTE = Byte.MIN_VALUE;
 
     /**
      * Null boxed Byte value.
@@ -73,39 +49,29 @@ public class QueryConstants {
     public static final Byte NULL_BYTE_BOXED = NULL_BYTE;
 
     /**
-     * Null boxed Short value.
+     * Maximum value of type byte.
      */
-    @SuppressWarnings("unused")
-    public static final Short NULL_SHORT_BOXED = NULL_SHORT;
+    public static final byte MAX_BYTE = Byte.MAX_VALUE;
 
     /**
-     * Null boxed Integer value.
+     * Minimum value of type byte.
      */
-    public static final Integer NULL_INT_BOXED = NULL_INT;
-
-    /**
-     * Null boxed Long value.
-     */
-    public static final Long NULL_LONG_BOXED = NULL_LONG;
-
-    /**
-     * Null boxed Float value.
-     */
-    public static final Float NULL_FLOAT_BOXED = NULL_FLOAT;
-
-    /**
-     * Null boxed Double value.
-     */
-    public static final Double NULL_DOUBLE_BOXED = NULL_DOUBLE;
+    public static final byte MIN_BYTE = Byte.MIN_VALUE + 1;
 
 
     /////////////////////////////////////////////////////////////////
 
 
     /**
-     * Maximum value of type byte.
+     * Null short value.
      */
-    public static final byte MAX_BYTE = Byte.MAX_VALUE;
+    public static final short NULL_SHORT = Short.MIN_VALUE;
+    
+    /**
+     * Null boxed Short value.
+     */
+    @SuppressWarnings("unused")
+    public static final Short NULL_SHORT_BOXED = NULL_SHORT;
 
     /**
      * Maximum value of type short.
@@ -113,9 +79,47 @@ public class QueryConstants {
     public static final short MAX_SHORT = Short.MAX_VALUE;
 
     /**
+     * Minimum value of type short.
+     */
+    public static final short MIN_SHORT = Short.MIN_VALUE + 1;
+
+
+    /////////////////////////////////////////////////////////////////
+
+
+    /**
+     * Null int value.
+     */
+    public static final int NULL_INT = Integer.MIN_VALUE;
+
+    /**
+     * Null boxed Integer value.
+     */
+    public static final Integer NULL_INT_BOXED = NULL_INT;
+
+    /**
      * Maximum value of type int.
      */
     public static final int MAX_INT = Integer.MAX_VALUE;
+
+    /**
+     * Minimum value of type int.
+     */
+    public static final int MIN_INT = Integer.MIN_VALUE + 1;
+
+
+    /////////////////////////////////////////////////////////////////
+
+
+    /**
+     * Null long value.
+     */
+    public static final long NULL_LONG = Long.MIN_VALUE;
+
+    /**
+     * Null boxed Long value.
+     */
+    public static final Long NULL_LONG_BOXED = NULL_LONG;
 
     /**
      * Maximum value of type long.
@@ -123,38 +127,28 @@ public class QueryConstants {
     public static final long MAX_LONG = Long.MAX_VALUE;
 
     /**
-     * Maximum finite value of type float.
+     * Minimum value of type long.
      */
-    public static final float MAX_FLOAT = Float.MAX_VALUE;
-
-    /**
-     * Maximum finite value of type double.
-     */
-    public static final double MAX_DOUBLE = Double.MAX_VALUE;
+    public static final long MIN_LONG = Long.MIN_VALUE + 1;
 
 
     /////////////////////////////////////////////////////////////////
 
 
     /**
-     * Minimum value of type byte.
+     * Null float value.
      */
-    public static final byte MIN_BYTE = Byte.MIN_VALUE + 1;
+    public static final float NULL_FLOAT = -Float.MAX_VALUE;
 
     /**
-     * Minimum value of type short.
+     * Null boxed Float value.
      */
-    public static final short MIN_SHORT = Short.MIN_VALUE + 1;
+    public static final Float NULL_FLOAT_BOXED = NULL_FLOAT;
 
     /**
-     * Minimum value of type int.
+     * Maximum finite value of type float.
      */
-    public static final int MIN_INT = Integer.MIN_VALUE + 1;
-
-    /**
-     * Minimum value of type long.
-     */
-    public static final long MIN_LONG = Long.MIN_VALUE + 1;
+    public static final float MAX_FLOAT = Float.MAX_VALUE;
 
     /**
      * Minimum finite value of type float.
@@ -162,27 +156,9 @@ public class QueryConstants {
     public static final float MIN_FLOAT = Float.valueOf("-0x1.fffffdp127");
 
     /**
-     * Minimum finite value of type double.
-     */
-    public static final double MIN_DOUBLE = Double.valueOf("-0x1.ffffffffffffep1023");
-
-
-    /////////////////////////////////////////////////////////////////
-
-
-    /**
      * Positive infinity of type float.
      */
     public static final float POSITIVE_INFINITY_FLOAT = Float.POSITIVE_INFINITY;
-
-    /**
-     * Positive infinity of type double.
-     */
-    public static final double POSITIVE_INFINITY_DOUBLE = Double.POSITIVE_INFINITY;
-
-
-    /////////////////////////////////////////////////////////////////
-
 
     /**
      * Negative infinity of type float.
@@ -190,18 +166,42 @@ public class QueryConstants {
     public static final float NEGATIVE_INFINITY_FLOAT = Float.NEGATIVE_INFINITY;
 
     /**
-     * Negative infinity of type double.
+     * Not-a-Number (NaN) of type float.
      */
-    public static final double NEGATIVE_INFINITY_DOUBLE = Double.NEGATIVE_INFINITY;
-
+    public static final float NAN_FLOAT = Float.NaN;
 
     /////////////////////////////////////////////////////////////////
 
 
     /**
-     * Not-a-Number (NaN) of type float.
+     * Null double value.
      */
-    public static final float NAN_FLOAT = Float.NaN;
+    public static final double NULL_DOUBLE = -Double.MAX_VALUE;
+
+    /**
+     * Null boxed Double value.
+     */
+    public static final Double NULL_DOUBLE_BOXED = NULL_DOUBLE;
+
+    /**
+     * Maximum finite value of type double.
+     */
+    public static final double MAX_DOUBLE = Double.MAX_VALUE;
+
+    /**
+     * Minimum finite value of type double.
+     */
+    public static final double MIN_DOUBLE = Double.valueOf("-0x1.ffffffffffffep1023");
+
+    /**
+     * Positive infinity of type double.
+     */
+    public static final double POSITIVE_INFINITY_DOUBLE = Double.POSITIVE_INFINITY;
+
+    /**
+     * Negative infinity of type double.
+     */
+    public static final double NEGATIVE_INFINITY_DOUBLE = Double.NEGATIVE_INFINITY;
 
     /**
      * Not-a-Number (NaN) of type double.
