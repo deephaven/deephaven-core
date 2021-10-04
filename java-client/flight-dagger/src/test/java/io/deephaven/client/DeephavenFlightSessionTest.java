ffa51f81e73e6f9ff5bb37f43b050ab7bae5ee6e
Devin Smith <devinsmith@deephaven.io>
2021-10-04 13:53:04 -0700

    Add support for java client doPut against NewTable. (#1374)

--- java-client/flight-dagger/src/test/java/io/deephaven/client/DeephavenFlightSessionTest.java
+++ java-client/flight-dagger/src/test/java/io/deephaven/client/DeephavenFlightSessionTest.java
@@ -2,7 +2,10 @@ package io.deephaven.client;
 
 import io.deephaven.api.TableOperations;
 import io.deephaven.client.impl.TableHandle;
+import io.deephaven.client.impl.TableHandle.TableHandleException;
 import io.deephaven.qst.TableCreator;
+import io.deephaven.qst.column.header.ColumnHeader;
+import io.deephaven.qst.table.NewTable;
 import io.deephaven.qst.table.TableCreatorImpl;
 import io.deephaven.qst.table.TableSpec;
 import org.apache.arrow.flight.FlightStream;
@@ -12,6 +15,7 @@ import org.apache.arrow.vector.types.pojo.FieldType;
 import org.apache.arrow.vector.types.pojo.Schema;
 import org.junit.Test;
 
+import java.time.Instant;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
@@ -46,6 +50,26 @@ public class DeephavenFlightSessionTest extends DeephavenFlightSessionTestBase {
         }
     }
 
+    // TODO (deephaven-core#1373): Hook up doPut integration unit testing
+    // @Test
+    // public void doPutStream() throws Exception {
+    // try (
+    // final TableHandle ten = flightSession.session().execute(TableSpec.empty(10).view("I=i"));
+    // // DoGet
+    // final FlightStream tenStream = flightSession.stream(ten);
+    // // DoPut
+    // final TableHandle tenAgain = flightSession.put(tenStream)) {
+    // assertThat(tenAgain.response().getSchemaHeader()).isEqualTo(ten.response().getSchemaHeader());
+    // }
+    // }
+    //
+    // @Test
+    // public void doPutNewTable() throws TableHandleException, InterruptedException {
+    // try (final TableHandle newTableHandle = flightSession.put(newTable(), bufferAllocator)) {
+    // // ignore
+    // }
+    // }
+
     private static Schema metadataLess(Schema schema) {
         return new Schema(
                 schema.getFields().stream().map(DeephavenFlightSessionTest::metadataLess).collect(Collectors.toList()),
@@ -58,5 +82,26 @@ public class DeephavenFlightSessionTest extends DeephavenFlightSessionTestBase {
 
     private static FieldType metadataLess(FieldType fieldType) {
         return new FieldType(fieldType.isNullable(), fieldType.getType(), fieldType.getDictionary(), null);
+    }
+
+    private NewTable newTable() {
+        return ColumnHeader.of(
+                ColumnHeader.ofBoolean("Boolean"),
+                ColumnHeader.ofByte("Byte"),
+                ColumnHeader.ofChar("Char"),
+                ColumnHeader.ofShort("Short"),
+                ColumnHeader.ofInt("Int"),
+                ColumnHeader.ofLong("Long"),
+                ColumnHeader.ofFloat("Float"),
+                ColumnHeader.ofDouble("Double"),
+                ColumnHeader.ofString("String"),
+                ColumnHeader.ofInstant("Instant"))
+                .start(3)
+                .row(true, (byte) 42, 'a', (short) 32_000, 1234567, 1234567890123L, 3.14f, 3.14d, "Hello, World",
+                        Instant.now())
+                .row(null, null, null, null, null, null, null, null, null, (Instant) null)
+                .row(false, (byte) -42, 'b', (short) -32_000, -1234567, -1234567890123L, -3.14f, -3.14d, "Goodbye.",
+                        Instant.ofEpochMilli(0))
+                .newTable();
     }
 }
