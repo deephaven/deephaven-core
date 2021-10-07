5acc35e8b17d155ef4f472c72bafc0aa957816fc
Devin Smith <devinsmith@deephaven.io>
2021-10-07 14:45:24 -0700

    Configure app mode to /app.d directory

--- demo/src/main/resources/scripts/prepare-worker.sh
+++ demo/src/main/resources/scripts/prepare-worker.sh
@@ -16,7 +16,7 @@ services:
       - /etc/ssl/dh:/etc/ssl/dh
       - /etc/ssl/internal:/etc/ssl/internal
     environment:
-      - JAVA_TOOL_OPTIONS=-Xmx2g -Ddeephaven.console.type=${TYPE:-python}
+      - JAVA_TOOL_OPTIONS=-Xmx2g -Ddeephaven.console.type=${TYPE:-python} -Ddeephaven.application.dir=/app.d
       - DH_TLS_CHAIN=/etc/ssl/internal/tls.crt
       - DH_TLS_KEY=/etc/ssl/internal/tls.key.pk8
 
