9093a0de292a0f5bde936997c17da74c0c95fdf9
jianfengmao <jianfengmao@deephaven.io>
2021-10-07 16:31:36 -0600

    made the server and client sphinx's conf.py consistent, fixes #1359

--- pyclient/docs/source/conf.py
+++ pyclient/docs/source/conf.py
@@ -28,7 +28,8 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.todo', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.todo', 'sphinx.ext.viewcode', "sphinx_autodoc_typehints"]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
