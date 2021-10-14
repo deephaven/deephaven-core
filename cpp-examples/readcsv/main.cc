3f174f65df147622ab2058fb234ffdfb322625a7
Cristian Ferretti <cristianferretti@deephaven.io>
2021-10-13 22:55:12 -0400

    WIP cpp csv reader.

---
+++ cpp-examples/readcsv/main.cc
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2016-2021 Deephaven Data Labs and Patent Pending
+ */
+#include <cstdlib>
+#include <iostream>
+
+#include "arrow/table.h"
+#include "arrow/array.h"
+#include "arrow/io/file.h"
+#include "arrow/csv/api.h"
+#include "arrow/status.h"
+#include "arrow/pretty_print.h"
+#include "deephaven/client/highlevel/client.h"
+#include "deephaven/client/utility/utility.h"
+
+using deephaven::client::highlevel::TableHandleManager;
+using deephaven::client::highlevel::Client;
+using deephaven::client::utility::flight::statusOrDie;
+
+arrow::Status doit(const TableHandleManager &manager, const std::string csvfn) {
+  arrow::io::IOContext io_context = arrow::io::default_io_context();
+  ARROW_ASSIGN_OR_RAISE(auto input_file,
+                        arrow::io::ReadableFile::Open(csvfn));
+  ARROW_ASSIGN_OR_RAISE(
+      auto csv_reader,
+      arrow::csv::TableReader::Make(arrow::io::default_io_context(),
+                                    input_file,
+                                    arrow::csv::ReadOptions::Defaults(),
+                                    arrow::csv::ParseOptions::Defaults(),
+                                    arrow::csv::ConvertOptions::Defaults()));
+  ARROW_ASSIGN_OR_RAISE(auto arrow_table, csv_reader->Read());
+  //  ARROW_RETURN_NOT_OK(arrow::PrettyPrint(arrow_table, {}, &std::cout));
+
+  auto wrapper = manager.createFlightWrapper();
+
+  auto [table_handle, fd] = manager.newTableHandleAndFlightDescriptor();
+
+  arrow::flight::FlightCallOptions options;
+  wrapper.addAuthHeaders(&options);
+
+  std::unique_ptr<arrow::flight::FlightStreamWriter> fsw;
+  std::unique_ptr<arrow::flight::FlightMetadataReader> fmr;
+  statusOrDie(wrapper.flightClient()->DoPut(options, fd, arrow_table->schema(), &fsw, &fmr), "DoPut failed");
+
+  auto chunks = arrow_table->columns();
+  const size_t ncols = chunks.size();
+  const size_t nchunks = chunks[0]->length();
+  for (size_t i = 0; i < nchunks; ++i) {
+    std::vector<std::shared_ptr<arrow::Array>> chunk(ncols);
+    for (size_t j = 0; j < ncols; ++j) {
+      chunk[j] = chunks[i]->chunk(j);
+    }
+    auto batch = arrow::RecordBatch::Make(arrow_table->schema(), chunk[0]->length(), chunk);
+    statusOrDie(fsw->WriteRecordBatch(*batch), "WriteRecordBatch failed");
+  }
+    
+  statusOrDie(fsw->DoneWriting(), "DoneWriting failed");
+
+  std::shared_ptr<arrow::Buffer> buf;
+  statusOrDie(fmr->ReadMetadata(&buf), "ReadMetadata failed");
+  statusOrDie(fsw->Close(), "Close failed");
+
+  // 14. Use Deephaven high level operations to fetch the table and print it
+  std::cout << "table is:\n" << table_handle.stream(true) << std::endl;
+  return arrow::Status::OK();
+}
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " csv-fn" << std::endl;
+    std::exit(1);
+  }
+  
+  const char *server = "localhost:10000";
+  auto client = Client::connect(server);
+  auto manager = client.getManager();
+
+  try {
+    auto st = doit(manager, argv[1]);
+    if (!st.ok()) {
+      std::cerr << "Failed with status " << st.ToString() << std::endl;
+    }
+  } catch (const std::runtime_error &e) {
+    std::cerr << "Caught exception: " << e.what() << '\n';
+  }
+
+  return 0;
+}
