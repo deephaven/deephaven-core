2c83cfc895a17374d8b11cb68ff8fda0966be01c
Devin Smith <devinsmith@deephaven.io>
2021-10-14 09:04:14 -0700

    Add groovy inheritance to Classpaths

--- buildSrc/src/main/groovy/Classpaths.groovy
+++ buildSrc/src/main/groovy/Classpaths.groovy
@@ -50,6 +50,9 @@ class Classpaths {
     static final String SLF4J_GROUP = 'org.slf4j'
     static final String SLF4J_VERSION = '1.7.32'
 
+    static final String GROOVY_GROUP = 'org.codehaus.groovy'
+    static final String GROOVY_VERSION = '3.0.9'
+
     static boolean addDependency(Configuration conf, String group, String name, String version, Action<? super DefaultExternalModuleDependency> configure = Actions.doNothing()) {
         if (!conf.dependencies.find { it.name == name && it.group == group}) {
             DefaultExternalModuleDependency dep = dependency group, name, version
@@ -121,5 +124,10 @@ class Classpaths {
     static void inheritSlf4j(Project p, String name, String configName) {
         Configuration config = p.configurations.getByName(configName)
         addDependency(config, SLF4J_GROUP, name, SLF4J_VERSION)
+    }
+
+    static void inheritGroovy(Project p, String name, String configName) {
+        Configuration config = p.configurations.getByName(configName)
+        addDependency(config, GROOVY_GROUP, name, GROOVY_VERSION)
     }
 }
