ffd5899f9be4fb9fe40e7bb10224b8a5291f4510
Devin Smith <devinsmith@deephaven.io>
2021-10-22 06:59:54 -0700

    Fixed compile issue

--- java-client/flight-examples/src/main/java/io/deephaven/client/examples/DeephavenTargetConverter.java
+++ java-client/flight-examples/src/main/java/io/deephaven/client/examples/DeephavenTargetConverter.java
@@ -3,10 +3,12 @@ package io.deephaven.client.examples;
 import io.deephaven.uri.DeephavenTarget;
 import picocli.CommandLine.ITypeConverter;
 
+import java.net.URI;
+
 class DeephavenTargetConverter implements ITypeConverter<DeephavenTarget> {
 
     @Override
     public DeephavenTarget convert(String value) {
-        return DeephavenTarget.of(value);
+        return DeephavenTarget.of(URI.create(value));
     }
 }
