0e3a1cc3a018786da23cb06ff8b4ccf553b40c36
Colin Alworth <colinalworth@deephaven.io>
2021-10-27 10:25:18 -0500

    class utils review feedback

--- Base/src/main/java/io/deephaven/base/ClassUtil.java
+++ Base/src/main/java/io/deephaven/base/ClassUtil.java
@@ -5,40 +5,11 @@
 package io.deephaven.base;
 
 import org.apache.commons.lang3.ClassUtils;
-import org.apache.log4j.Logger;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class ClassUtil {
-    public static String getBaseName(final String s) {
-        int i = s.lastIndexOf(".");
-        return (i == -1) ? s : s.substring(i + 1);
-    }
-
-    public static void dumpFinals(final Logger log, final String prefix, final Object p) {
-        final Class c = p.getClass();
-        Field[] fields = c.getDeclaredFields();
-        final int desiredMods = (Modifier.PUBLIC | Modifier.FINAL);
-        for (Field f : fields) {
-            if ((f.getModifiers() & desiredMods) == 0)
-                continue;
-            try {
-                final String tName = f.getType().getName();
-                final String name = f.getName();
-                final Object value = f.get(p);
-                log.info(prefix
-                        + tName
-                        + " " + name
-                        + " = " + value.toString());
-            } catch (Exception ignored) {
-            }
-        }
-    }
 
     public static <T> Class<T> generify(Class c) {
         return c;
