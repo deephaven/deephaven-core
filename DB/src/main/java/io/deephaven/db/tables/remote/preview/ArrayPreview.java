73d4ab3210a7a994f6cdd81d64bc23056d2324e1
Nathaniel Bauernfeind <natebauernfeind@deephaven.io>
2021-11-02 11:15:40 -0600

    Friendly stringing of py list wrapper

--- DB/src/main/java/io/deephaven/db/tables/remote/preview/ArrayPreview.java
+++ DB/src/main/java/io/deephaven/db/tables/remote/preview/ArrayPreview.java
@@ -2,6 +2,7 @@ package io.deephaven.db.tables.remote.preview;
 
 import io.deephaven.db.tables.dbarrays.DbArrayBase;
 import io.deephaven.db.v2.sources.chunk.ChunkType;
+import org.jpy.PyListWrapper;
 
 /**
  * A Preview Type for Arrays and DbArray. Converts long arrays to a String "[1, 2, 3, 4, 5...]"
@@ -10,14 +11,14 @@ public class ArrayPreview implements PreviewType {
     private static final int ARRAY_SIZE_CUTOFF = 5;
     private final String displayString;
 
-    public static ArrayPreview fromDbArray(DbArrayBase dbArray) {
+    public static ArrayPreview fromDbArray(final DbArrayBase<?> dbArray) {
         if (dbArray == null) {
             return null;
         }
         return new ArrayPreview(dbArray.toString(ARRAY_SIZE_CUTOFF));
     }
 
-    public static ArrayPreview fromArray(Object array) {
+    public static ArrayPreview fromArray(final Object array) {
         if (array == null) {
             return null;
         }
@@ -26,6 +27,13 @@ public class ArrayPreview implements PreviewType {
         }
         return new ArrayPreview(ChunkType.fromElementType(array.getClass().getComponentType()).dbArrayWrap(array)
                 .toString(ARRAY_SIZE_CUTOFF));
+    }
+
+    public static ArrayPreview fromPyListWrapper(final PyListWrapper list) {
+        if (list == null) {
+            return null;
+        }
+        return new ArrayPreview(list.toString(ARRAY_SIZE_CUTOFF));
     }
 
     private ArrayPreview(String displayString) {
