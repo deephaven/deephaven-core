5222b247aa249ad50de7ca7442a433f0d7afcb63
JJ Brosnan <84038776+jjbrosnan@users.noreply.github.com>
2021-11-03 10:28:01 -0400

    Push of gatherer after performance and memory leak testing

---
+++ Integrations/src/main/java/io/deephaven/integrations/learn/Gatherer.java
@@ -0,0 +1,36 @@
+package io.deephaven.integrations.learn;
+
+import io.deephaven.db.v2.sources.ColumnSource;
+
+public class Gatherer {
+
+    /**
+     * Copy data from a table into a 2d tensor.
+     *
+     * @param indexSet indices of the rows of the table to put into the tensor
+     * @param columnSources columns of data to put into the tensor
+     * @return contiguous RAM allocated for the tensor.  When a numpy tensor is passed in for this argument, jpy will handle passing the memory reference as a 1d java array here.
+     */
+    public static double[] create_2d_tensor(final IndexSet indexSet, final ColumnSource<?>[] columnSources) {
+
+        final int nRows = indexSet.getSize();
+        final int nCols = columnSources.length;
+        final double[] tensor = new double[nRows * nCols];
+
+        int j = 0;
+        for( ColumnSource<?> cs : columnSources ) {
+            int i = 0;
+
+            for( long idx : indexSet ) {
+                final int ij = nCols * i + j;
+                tensor[ij] = cs.getDouble(idx);
+                i++;
+            }
+
+            j++;
+        }
+
+        return tensor;
+    }
+
+}
