2e457fcd9a25c89a8d4124ec833593b6c951cffb
Colin Alworth <colin@vertispan.com>
2021-11-23 18:24:06 -0600

    Simplify server startup and dagger usage (#1601)
    This is a first step towards making the http server for grpc
    replacable with Jetty.
    
    Partial #1270

--- grpc-api/src/main/java/io/deephaven/grpc_api/runner/DeephavenApiServer.java
+++ grpc-api/src/main/java/io/deephaven/grpc_api/runner/DeephavenApiServer.java
@@ -6,7 +6,6 @@ import io.deephaven.db.v2.utils.MemoryTableLoggers;
 import io.deephaven.db.v2.utils.ProcessMemoryTracker;
 import io.deephaven.db.v2.utils.UpdatePerformanceTracker;
 import io.deephaven.grpc_api.appmode.ApplicationInjector;
-import io.deephaven.grpc_api.appmode.ApplicationServiceGrpcImpl;
 import io.deephaven.grpc_api.console.ConsoleServiceGrpcImpl;
 import io.deephaven.grpc_api.log.LogInit;
 import io.deephaven.grpc_api.session.SessionService;
@@ -15,6 +14,7 @@ import io.deephaven.grpc_api.uri.UriResolvers;
 import io.deephaven.internal.log.LoggerFactory;
 import io.deephaven.io.logger.Logger;
 import io.deephaven.grpc_api.uri.UriResolversInstance;
+import io.deephaven.util.annotations.VisibleForTesting;
 import io.deephaven.util.process.ProcessEnvironment;
 import io.deephaven.util.process.ShutdownManager;
 import io.grpc.Server;
@@ -23,44 +23,20 @@ import javax.inject.Inject;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Entrypoint for the Deephaven gRPC server, starting the various engine and script components, running any specified
+ * application, and enabling the gRPC endpoints to be accessed by consumers.
+ */
 public class DeephavenApiServer {
-
     private static final Logger log = LoggerFactory.getLogger(DeephavenApiServer.class);
-
-    public static void start(DeephavenApiServer server, SessionService sessionService)
-            throws IOException, ClassNotFoundException, InterruptedException {
-        // Stop accepting new gRPC requests.
-        ProcessEnvironment.getGlobalShutdownManager().registerTask(ShutdownManager.OrderingCategory.FIRST,
-                server.server::shutdown);
-
-        // Close outstanding sessions to give any gRPCs closure.
-        ProcessEnvironment.getGlobalShutdownManager().registerTask(ShutdownManager.OrderingCategory.MIDDLE,
-                sessionService::onShutdown);
-
-        // Finally wait for gRPC to exit now.
-        ProcessEnvironment.getGlobalShutdownManager().registerTask(ShutdownManager.OrderingCategory.LAST, () -> {
-            try {
-                if (!server.server.awaitTermination(10, TimeUnit.SECONDS)) {
-                    log.error().append(
-                            "The gRPC server did not terminate in a reasonable amount of time. Invoking shutdownNow().")
-                            .endl();
-                    server.server.shutdownNow();
-                }
-            } catch (final InterruptedException ignored) {
-            }
-        });
-
-        server.start();
-        server.blockUntilShutdown();
-    }
 
     private final Server server;
     private final LiveTableMonitor ltm;
     private final LogInit logInit;
     private final ConsoleServiceGrpcImpl consoleService;
     private final ApplicationInjector applicationInjector;
-    private final ApplicationServiceGrpcImpl applicationService;
     private final UriResolvers uriResolvers;
+    private final SessionService sessionService;
 
     @Inject
     public DeephavenApiServer(
@@ -69,22 +45,54 @@ public class DeephavenApiServer {
             final LogInit logInit,
             final ConsoleServiceGrpcImpl consoleService,
             final ApplicationInjector applicationInjector,
-            final ApplicationServiceGrpcImpl applicationService,
-            final UriResolvers uriResolvers) {
+            final UriResolvers uriResolvers,
+            final SessionService sessionService) {
         this.server = server;
         this.ltm = ltm;
         this.logInit = logInit;
         this.consoleService = consoleService;
         this.applicationInjector = applicationInjector;
-        this.applicationService = applicationService;
         this.uriResolvers = uriResolvers;
+        this.sessionService = sessionService;
     }
 
+    @VisibleForTesting
     public Server server() {
         return server;
     }
 
-    public void start() throws IOException, ClassNotFoundException {
+    /**
+     * Starts the various server components, and blocks until the gRPC server has shut down. That shutdown is mediated
+     * by the ShutdownManager, and will call the gRPC server to shut it down when the process is itself shutting down.
+     * Only once that is complete will this method return.
+     *
+     * @throws IOException thrown in event of an error with logging, finding and running an application, and starting
+     *         the gRPC service.
+     * @throws ClassNotFoundException thrown if a class can't be found while finding and running an application.
+     * @throws InterruptedException thrown if this thread is interrupted while blocking for the server to halt.
+     */
+    public void run() throws IOException, ClassNotFoundException, InterruptedException {
+        // Stop accepting new gRPC requests.
+        ProcessEnvironment.getGlobalShutdownManager().registerTask(ShutdownManager.OrderingCategory.FIRST,
+                server::shutdown);
+
+        // Close outstanding sessions to give any gRPCs closure.
+        ProcessEnvironment.getGlobalShutdownManager().registerTask(ShutdownManager.OrderingCategory.MIDDLE,
+                sessionService::onShutdown);
+
+        // Finally wait for gRPC to exit now.
+        ProcessEnvironment.getGlobalShutdownManager().registerTask(ShutdownManager.OrderingCategory.LAST, () -> {
+            try {
+                if (!server.awaitTermination(10, TimeUnit.SECONDS)) {
+                    log.error().append(
+                            "The gRPC server did not terminate in a reasonable amount of time. Invoking shutdownNow().")
+                            .endl();
+                    server.shutdownNow();
+                }
+            } catch (final InterruptedException ignored) {
+            }
+        });
+
         log.info().append("Configuring logging...").endl();
         logInit.run();
 
@@ -103,18 +111,17 @@ public class DeephavenApiServer {
         UpdatePerformanceTracker.start();
         ProcessMemoryTracker.start();
 
+        for (UriResolver resolver : uriResolvers.resolvers()) {
+            log.debug().append("Found table resolver ").append(resolver.getClass().toString()).endl();
+        }
+        UriResolversInstance.init(uriResolvers);
+
         // inject applications before we start the gRPC server
         applicationInjector.run();
 
-        {
-            for (UriResolver resolver : uriResolvers.resolvers()) {
-                log.info().append("Found table resolver ").append(resolver.getClass().toString()).endl();
-            }
-            UriResolversInstance.init(uriResolvers);
-        }
-
         log.info().append("Starting server...").endl();
         server.start();
+        server.awaitTermination();
     }
 
     void startForUnitTests() throws IOException {
@@ -122,7 +129,4 @@ public class DeephavenApiServer {
         server.start();
     }
 
-    private void blockUntilShutdown() throws InterruptedException {
-        server.awaitTermination();
-    }
 }
