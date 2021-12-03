beefd80b9bf24ad68f65075d60bdfd14ab0420fd
Chip Kent <chipkent@deephaven.io>
2021-12-03 14:32:32 -0700

    Added the code generator

---
+++ codegen/src/main/java/io/deephaven/generator/primitivetemplate/PrimitiveType.java
@@ -0,0 +1,139 @@
+package io.deephaven.generator.primitivetemplate;
+
+import java.util.Objects;
+
+/**
+ * Data describing a primitive type.
+ */
+public class PrimitiveType {
+
+    private final String primitive;
+    private final String boxed;
+    private final String dbArray;
+    private final String dbArrayDirect;
+    private final String nullValue;
+    private final String posInf;
+    private final String negInf;
+    private final ValueType valueType;
+
+    /**
+     * Create a primitive type description.
+     *
+     * @param primitive name of the primitive
+     * @param boxed name of the boxed primitive
+     * @param dbArray name of the DbArray interface
+     * @param dbArrayDirect name of the concrete DbArray wrapper
+     * @param nullValue null value
+     * @param posInf maximum value
+     * @param negInf minimum value
+     * @param valueType type of value
+     */
+    public PrimitiveType(final String primitive, final String boxed, final String dbArray, final String dbArrayDirect, final String nullValue, final String posInf, final String negInf, final ValueType valueType) {
+        this.primitive = primitive;
+        this.boxed = boxed;
+        this.dbArray = dbArray;
+        this.dbArrayDirect = dbArrayDirect;
+        this.nullValue = nullValue;
+        this.posInf = posInf;
+        this.negInf = negInf;
+        this.valueType = valueType;
+    }
+
+    /**
+     * Gets the name of the primitive.
+     *
+     * @return name of the primitive.
+     */
+    public String getPrimitive() {
+        return primitive;
+    }
+
+    /**
+     * Gets the name of the boxed primitive.
+     *
+     * @return name of the boxed primitive.
+     */
+    public String getBoxed() {
+        return boxed;
+    }
+
+    /**
+     * Gets the DbArray interface of the primitive.
+     *
+     * @return DbArray interface of the primitive.
+     */
+    public String getDbArray() {
+        return dbArray;
+    }
+
+    /**
+     * Gets the direct DbArray wrapper of the primitive.
+     *
+     * @return direct DbArray wrapper of the primitive.
+     */
+    public String getDbArrayDirect() {
+        return dbArrayDirect;
+    }
+
+    /**
+     * Gets the null value of the primitive.
+     *
+     * @return null value of the primitive.
+     */
+    public String getNull() {
+        return nullValue;
+    }
+
+    /**
+     * Gets the maximum value of the primitive.
+     *
+     * @return maximum value of the primitive.
+     */
+    public String getPosInf() {
+        return posInf;
+    }
+
+    /**
+     * Gets the minimum value of the primitive.
+     *
+     * @return minimum value of the primitive.
+     */
+    public String getNegInf() {
+        return negInf;
+    }
+
+    /**
+     * Gets the value type of the primitive.
+     *
+     * @return value type of the primitive.
+     */
+    public ValueType getValueType() {
+        return valueType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PrimitiveType)) return false;
+        PrimitiveType that = (PrimitiveType) o;
+        return Objects.equals(primitive, that.primitive);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(primitive);
+    }
+
+    public static PrimitiveType[] primitiveTypes() {
+        return new PrimitiveType[] {
+                new PrimitiveType("Boolean", "Boolean", "DbArray<Boolean>", "DbArrayDirect<Boolean>", "NULL_BOOLEAN", null, null, ValueType.BOOLEAN),
+                new PrimitiveType("char", "Character", "DbCharArray", "DbCharArrayDirect", "NULL_CHAR", null, null, ValueType.CHARACTER),
+                new PrimitiveType("byte", "Byte", "DbByteArray", "DbByteArrayDirect","NULL_BYTE", "POS_INF_BYTE", "NEG_INF_BYTE", ValueType.INTEGER),
+                new PrimitiveType("short", "Short", "DbShortArray","DbShortArrayDirect", "NULL_SHORT", "POS_INF_SHORT", "NEG_INF_SHORT", ValueType.INTEGER),
+                new PrimitiveType("int", "Integer", "DbIntArray", "DbIntArrayDirect","NULL_INT", "POS_INF_INT", "NEG_INF_INT", ValueType.INTEGER),
+                new PrimitiveType("long", "Long", "DbLongArray", "DbLongArrayDirect","NULL_LONG", "POS_INF_LONG", "NEG_INF_LONG", ValueType.INTEGER),
+                new PrimitiveType("float", "Float", "DbFloatArray", "DbFloatArrayDirect","NULL_FLOAT", "POS_INF_FLOAT", "NEG_INF_FLOAT",ValueType.FLOATING_POINT),
+                new PrimitiveType("double", "Double", "DbDoubleArray", "DbDoubleArrayDirect","NULL_DOUBLE", "POS_INF_DOUBLE", "NEG_INF_DOUBLE", ValueType.FLOATING_POINT),
+        };
+    }
+}
