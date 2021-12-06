a51ebe31b01c0ca49f162d8bcb13489d504dbd96
Chip Kent <chipkent@deephaven.io>
2021-12-06 11:16:56 -0700

    Fixed buggy shutdown condition as well as buggy unit tests.

--- ModelFarm/src/main/java/io/deephaven/modelfarm/ModelFarmBase.java
+++ ModelFarm/src/main/java/io/deephaven/modelfarm/ModelFarmBase.java
@@ -56,7 +56,7 @@ public abstract class ModelFarmBase<DATATYPE> implements ModelFarm {
          * Performs an operation using data from a query.
          *
          * @param usePrev Whether to use the previous data at a given row key when retrieving data (i.e. if
-         *        {@code true}, use {@link ColumnSource#getPrev} instead of {@link ColumnSource#get}).
+         *                {@code true}, use {@link ColumnSource#getPrev} instead of {@link ColumnSource#get}).
          */
         void retrieveData(boolean usePrev);
 
@@ -151,7 +151,7 @@ public abstract class ModelFarmBase<DATATYPE> implements ModelFarm {
      * Create a multithreaded resource to execute data driven models.
      *
      * @param nThreads number of worker threads.
-     * @param model model to execute.
+     * @param model    model to execute.
      */
     @SuppressWarnings("WeakerAccess")
     protected ModelFarmBase(final int nThreads, final Model<DATATYPE> model) {
@@ -183,7 +183,7 @@ public abstract class ModelFarmBase<DATATYPE> implements ModelFarm {
     /**
      * Interface for getting the most recent row data for a unique identifier.
      *
-     * @param <KEYTYPE> unique ID key type
+     * @param <KEYTYPE>  unique ID key type
      * @param <DATATYPE> data type
      */
     @FunctionalInterface
@@ -193,7 +193,7 @@ public abstract class ModelFarmBase<DATATYPE> implements ModelFarm {
          *
          * @param key unique identifier
          * @return most recent row data for the unique identifier, or null, if there is no data for the unique
-         *         identifier.
+         * identifier.
          */
         DATATYPE get(final KEYTYPE key);
     }
@@ -340,8 +340,8 @@ public abstract class ModelFarmBase<DATATYPE> implements ModelFarm {
                 timeout == Long.MAX_VALUE ? Long.MAX_VALUE : System.currentTimeMillis() + unit.toMillis(timeout);
         boolean allThreadsTerminated = false;
 
-        while (!allThreadsTerminated && System.currentTimeMillis() < timeoutMillis) {
-            synchronized (ModelFarmBase.this) {
+        synchronized (ModelFarmBase.this) {
+            while (!allThreadsTerminated && System.currentTimeMillis() < timeoutMillis) {
                 if (!threads.isEmpty()) {
                     try {
                         // Wait for the state to change. (The last thread to exit will update the state to TERMINATED.)
@@ -357,6 +357,8 @@ public abstract class ModelFarmBase<DATATYPE> implements ModelFarm {
                             throw new RuntimeException("Interrupted while awaiting ModelFarm termination.", e);
                         }
                     }
+                } else {
+                    allThreadsTerminated = true;
                 }
             }
         }
