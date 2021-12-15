f13ca943cbded77c89fa49c30ce2dd6aeafaf7c9
Devin Smith <devinsmith@deephaven.io>
2021-12-15 15:07:18 -0800

    Latest from review.
    Follow up #1702
    Follow up #1703

--- docker/server/src/main/configure/properties.py
+++ docker/server/src/main/configure/properties.py
@@ -3,8 +3,13 @@
 import sys
 import jpyutil
 
-print("jpy.pythonPrefix={}".format(sys.prefix))
-print("jpy.pythonExecutable={}".format(sys.executable))
-print("jpy.pythonLib={}".format(jpyutil._find_python_dll_file(fail=True)))
-print("jpy.jpyLib={}".format(jpyutil._get_module_path('jpy', fail=True)))
-print("jpy.jdlLib={}".format(jpyutil._get_module_path('jdl', fail=True)))
+properties = {
+    'jpy.pythonPrefix': sys.prefix,
+    'jpy.programName': sys.executable,
+    'jpy.pythonLib': jpyutil._find_python_dll_file(fail=True),
+    'jpy.jpyLib': jpyutil._get_module_path('jpy', fail=True),
+    'jpy.jdlLib': jpyutil._get_module_path('jdl', fail=True)
+}
+
+for key, value in properties.items():
+    print(f'{key}={value}')
