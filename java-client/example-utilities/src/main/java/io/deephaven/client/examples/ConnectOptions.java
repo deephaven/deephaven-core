dd6cc3abe60b29b4a0d2f4486c8289ca93088891
Devin Smith <devinsmith@deephaven.io>
2021-12-29 08:19:59 -0800

    Extracted java-client/example-utilities (#1748)
    Allows session-examples to use DeephavenTarget as --target

--- java-client/example-utilities/src/main/java/io/deephaven/client/examples/ConnectOptions.java
+++ java-client/example-utilities/src/main/java/io/deephaven/client/examples/ConnectOptions.java
@@ -1,18 +1,25 @@
 package io.deephaven.client.examples;
 
-import io.deephaven.client.impl.ChannelHelper;
 import io.deephaven.uri.DeephavenTarget;
+import io.deephaven.uri.DeephavenUri;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import picocli.CommandLine.Option;
 
 public class ConnectOptions {
 
+    public static final String DEFAULT_HOST = "localhost";
+
+    public static final int DEFAULT_PORT = 10000;
+
     public static final DeephavenTarget DEFAULT_TARGET = DeephavenTarget.builder()
-            .host("localhost")
-            .port(10000)
+            .host(DEFAULT_HOST)
+            .port(DEFAULT_PORT)
             .isSecure(false)
             .build();
+
+    public static final String DEFAULT_TARGET_VALUE =
+            DeephavenUri.PLAINTEXT_SCHEME + "://" + DEFAULT_HOST + ":" + DEFAULT_PORT;
 
     public static ManagedChannel open(ConnectOptions options) {
         if (options == null) {
@@ -24,14 +31,20 @@ public class ConnectOptions {
     }
 
     @Option(names = {"-t", "--target"}, description = "The target, defaults to ${DEFAULT-VALUE}",
-            defaultValue = "dh+plain://localhost:10000", converter = DeephavenTargetConverter.class)
+            defaultValue = DEFAULT_TARGET_VALUE, converter = DeephavenTargetConverter.class)
     DeephavenTarget target;
 
     @Option(names = {"-u", "--user-agent"}, description = "The user-agent.")
     String userAgent;
 
     public ManagedChannel open() {
-        final ManagedChannelBuilder<?> builder = ChannelHelper.channelBuilder(target);
+        final ManagedChannelBuilder<?> builder =
+                ManagedChannelBuilder.forAddress(target.host(), target.port().orElse(DEFAULT_PORT));
+        if (target.isSecure()) {
+            builder.useTransportSecurity();
+        } else {
+            builder.usePlaintext();
+        }
         if (userAgent != null) {
             builder.userAgent(userAgent);
         }
