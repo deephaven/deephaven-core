45970fd4720454f890b9926926f8cf5bf55e45f7
Devin Smith <devinsmith@deephaven.io>
2022-01-11 15:49:24 -0800

    recursive fetch

--- java-client/session-examples/src/main/java/io/deephaven/client/examples/FetchObject.java
+++ java-client/session-examples/src/main/java/io/deephaven/client/examples/FetchObject.java
@@ -2,6 +2,7 @@ package io.deephaven.client.examples;
 
 import io.deephaven.client.impl.ExportId;
 import io.deephaven.client.impl.FetchedObject;
+import io.deephaven.client.impl.HasTicketId;
 import io.deephaven.client.impl.Session;
 import picocli.CommandLine;
 import picocli.CommandLine.ArgGroup;
@@ -9,9 +10,11 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 import java.io.BufferedOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutionException;
 
 @Command(name = "fetch-object", mixinStandardHelpOptions = true,
         description = "Fetch object", version = "0.1.0")
@@ -23,9 +26,20 @@ class FetchObject extends SingleSessionExampleBase {
     @Option(names = {"-f", "--file"}, description = "The output file, otherwise goes to STDOUT.")
     Path file;
 
+    @Option(names = {"-r", "--recursive"}, description = "If the program should recursively fetch.")
+    boolean recursive;
+
     @Override
     protected void execute(Session session) throws Exception {
+        show(session, ticket);
+    }
+
+    private void show(Session session, HasTicketId ticket) throws IOException, ExecutionException, InterruptedException {
         final FetchedObject customObject = session.fetchObject(ticket).get();
+        show(session, customObject);
+    }
+
+    private void show(Session session, FetchedObject customObject) throws IOException, ExecutionException, InterruptedException {
         final String type = customObject.type();
         System.err.println("type: " + type);
         System.err.println("size: " + customObject.size());
@@ -38,6 +52,16 @@ class FetchObject extends SingleSessionExampleBase {
             }
         } else {
             customObject.writeTo(System.out);
+        }
+        if (recursive) {
+            for (ExportId exportId : customObject.exportIds()) {
+                try {
+                    show(session, exportId);
+                } catch (ExecutionException e) {
+                    // ignore
+                    // TODO: better RPC return around fetchObject when exportId is not fetchable
+                }
+            }
         }
     }
 
