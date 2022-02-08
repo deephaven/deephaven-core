6e734a95b4cc20452c6093c6141c02ceae382744
Devin Smith <devinsmith@deephaven.io>
2022-02-08 08:02:57 -0800

    Setup column legalizers. Extract time zone map into static.

--- extensions/csv/src/main/java/io/deephaven/csv/CsvTools.java
+++ extensions/csv/src/main/java/io/deephaven/csv/CsvTools.java
@@ -4,7 +4,7 @@
 
 package io.deephaven.csv;
 
-import gnu.trove.map.hash.TIntObjectHashMap;
+import io.deephaven.api.util.NameValidator;
 import io.deephaven.base.Procedure;
 import io.deephaven.chunk.ByteChunk;
 import io.deephaven.chunk.CharChunk;
@@ -22,17 +22,12 @@ import io.deephaven.chunk.WritableLongChunk;
 import io.deephaven.chunk.WritableShortChunk;
 import io.deephaven.chunk.attributes.Values;
 import io.deephaven.csv.CsvSpecs.Builder;
-import io.deephaven.csv.containers.ByteSlice;
 import io.deephaven.csv.reading.CsvReader;
 import io.deephaven.csv.sinks.Sink;
 import io.deephaven.csv.sinks.SinkFactory;
 import io.deephaven.csv.sinks.Source;
-import io.deephaven.csv.tokenization.RangeTests;
-import io.deephaven.csv.tokenization.Tokenizer;
 import io.deephaven.csv.tokenization.Tokenizer.CustomTimeZoneParser;
 import io.deephaven.csv.util.CsvReaderException;
-import io.deephaven.csv.util.MutableLong;
-import io.deephaven.csv.util.MutableObject;
 import io.deephaven.datastructures.util.CollectionUtil;
 import io.deephaven.engine.rowset.RowSequence;
 import io.deephaven.engine.rowset.RowSequenceFactory;
@@ -81,12 +76,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Utilities for reading and writing CSV files to and from {@link Table}s
@@ -100,12 +96,17 @@ public class CsvTools {
     public final static boolean NULLS_AS_EMPTY_DEFAULT = true;
 
     /**
-     * Creates a {@link Builder} with {@link CsvTools}-specific values. Includes a {@link CustomTimeZoneParser}.
+     * Creates a {@link Builder} with {@link CsvTools}-specific values. Sets {@link ColumnNameLegalizer#INSTANCE} as
+     * {@link Builder#headerLegalizer(Function)} and {@link Builder#headerValidator(Predicate)}; sets a new instance of
+     * {@link DeephavenTimeZoneParser} as {@link Builder#customTimeZoneParser(CustomTimeZoneParser)}.
      *
      * @return the builder
      */
     public static Builder builder() {
-        return CsvSpecs.builder().customTimeZoneParser(new DeephavenTimeZoneParser());
+        return CsvSpecs.builder()
+                .headerLegalizer(ColumnNameLegalizer.INSTANCE)
+                .headerValidator(ColumnNameLegalizer.INSTANCE)
+                .customTimeZoneParser(new DeephavenTimeZoneParser());
     }
 
     /**
@@ -1246,96 +1247,4 @@ public class CsvTools {
                 MyDateTimeAsLongSink::new, QueryConstants.NULL_LONG);
     }
 
-    private static final class DeephavenTimeZoneParser implements Tokenizer.CustomTimeZoneParser {
-        private static final String DEEPHAVEN_TZ_PREFIX = "TZ_";
-        private static final int MAX_DEEPHAVEN_TZ_LENGTH = 3;
-
-        private final TIntObjectHashMap<ZoneId> zoneIdMap = new TIntObjectHashMap<>();
-
-        private int lastTzKey = -1;
-        private ZoneId lastZoneId = null;
-
-        public DeephavenTimeZoneParser() {
-            for (TimeZone zone : TimeZone.values()) {
-                final String zname = zone.name();
-                if (!zname.startsWith(DEEPHAVEN_TZ_PREFIX)) {
-                    throw new RuntimeException("Logic error: unexpected enum in DBTimeZone: " + zname);
-                }
-                final String zSuffix = zname.substring(DEEPHAVEN_TZ_PREFIX.length());
-                final int zlen = zSuffix.length();
-                if (zlen > MAX_DEEPHAVEN_TZ_LENGTH) {
-                    throw new RuntimeException("Logic error: unexpectedly-long enum in DBTimeZone: " + zname);
-                }
-                final byte[] data = new byte[zlen];
-                for (int ii = 0; ii < zlen; ++ii) {
-                    final char ch = zSuffix.charAt(ii);
-                    if (!RangeTests.isUpper(ch)) {
-                        throw new RuntimeException("Logic error: unexpected character in DBTimeZone name: " + zname);
-                    }
-                    data[ii] = (byte) ch;
-                }
-                final ByteSlice bs = new ByteSlice(data, 0, data.length);
-                final int tzKey = tryParseTzKey(bs);
-                if (tzKey < 0) {
-                    throw new RuntimeException("Logic error: can't parse DBTimeZone as key: " + zname);
-                }
-                final ZoneId zoneId = zone.getTimeZone().toTimeZone().toZoneId();
-                zoneIdMap.put(tzKey, zoneId);
-            }
-        }
-
-        @Override
-        public boolean tryParse(ByteSlice bs, MutableObject<ZoneId> zoneId, MutableLong offsetSeconds) {
-            if (bs.size() == 0 || bs.front() != ' ') {
-                return false;
-            }
-            final int savedBegin = bs.begin();
-            bs.setBegin(bs.begin() + 1);
-            final int tzKey = tryParseTzKey(bs);
-            if (tzKey < 0) {
-                bs.setBegin(savedBegin);
-                return false;
-            }
-            if (tzKey != lastTzKey) {
-                final ZoneId res = zoneIdMap.get(tzKey);
-                if (res == null) {
-                    bs.setBegin(savedBegin);
-                    return false;
-                }
-                lastTzKey = tzKey;
-                lastZoneId = res;
-            }
-            zoneId.setValue(lastZoneId);
-            offsetSeconds.setValue(0);
-            return true;
-        }
-
-        /**
-         * Take up to three uppercase characters from a TimeZone string and pack them into an integer.
-         *
-         * @param bs A ByteSlice holding the timezone key.
-         * @return The characters packed into an int, or -1 if there are too many or too few characters, or if the
-         *         characters are not uppercase ASCII.
-         */
-        private static int tryParseTzKey(final ByteSlice bs) {
-            int res = 0;
-            int current;
-            for (current = bs.begin(); current != bs.end(); ++current) {
-                if (current - bs.begin() > MAX_DEEPHAVEN_TZ_LENGTH) {
-                    return -1;
-                }
-                final char ch = RangeTests.toUpper((char) bs.data()[current]);
-                if (!RangeTests.isUpper(ch)) {
-                    // If it's some nonalphabetic character
-                    break;
-                }
-                res = res * 26 + (ch - 'A');
-            }
-            if (current - bs.begin() == 0) {
-                return -1;
-            }
-            bs.setBegin(current);
-            return res;
-        }
-    }
 }
