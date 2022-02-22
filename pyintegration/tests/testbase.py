3030c22e53d24fb5a642788ac0212258ef4829ac
jianfengmao <jianfengmao@deephaven.io>
2022-02-22 15:45:45 -0700

    Fix a potential locking issue in the test code

--- pyintegration/tests/testbase.py
+++ pyintegration/tests/testbase.py
@@ -27,9 +27,12 @@ class BaseTestCase(unittest.TestCase):
     def wait_ticking_table_update(self, table: Table, row_count: int, timeout: int):
         j_ugp = jpy.get_type("io.deephaven.engine.updategraph.UpdateGraphProcessor").DEFAULT
         j_exclusive_lock = j_ugp.exclusiveLock()
-        j_exclusive_lock.lock()
-        retry_count = timeout
-        while table.size < row_count and retry_count > 0:
-            table.j_table.awaitUpdate(1000)
-            retry_count -= 1
-        self.assertEqual(table.size, row_count)
+        try:
+            j_exclusive_lock.lock()
+            retry_count = timeout
+            while table.size < row_count and retry_count > 0:
+                table.j_table.awaitUpdate(1000)
+                retry_count -= 1
+            self.assertEqual(table.size, row_count)
+        finally:
+            j_exclusive_lock.unlock()
