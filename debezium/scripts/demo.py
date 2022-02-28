b2afd9dcfb984afb777b48784b63beccfdcdaab5
Cristian Ferretti <cristianferretti@deephaven.io>
2022-02-28 12:14:19 -0500

    Removed unused imports from debezium/scripts/demo.py.

--- debezium/scripts/demo.py
+++ debezium/scripts/demo.py
@@ -3,9 +3,7 @@ import deephaven.ConsumeKafka as ck
 import deephaven.ProduceKafka as pk
 import deephaven.Types as dh
 from deephaven import Aggregation as agg, as_list
-from deephaven import PythonFunction as pyfun
 import deephaven.TableManipulation.WindowCheck as wck
-import jpy
 
 server_name = 'mysql'
 db_name='shop'
