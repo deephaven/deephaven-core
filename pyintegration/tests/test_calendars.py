78d8a8e0128a07e541d8525212dd90427afb9418
jianfengmao <jianfengmao@deephaven.io>
2022-03-08 19:31:26 -0700

    Expand the wrapping and add required test cases

--- pyintegration/tests/test_calendars.py
+++ pyintegration/tests/test_calendars.py
@@ -4,38 +4,114 @@
 
 import unittest
 
-from deephaven2 import DHError
-from deephaven2.calendars import calendar_names, default_calendar_name, Calendar
+from deephaven2 import DHError, time
+from deephaven2.calendars import calendar_names, default_calendar_name, BusinessCalendar
+from deephaven2.config import get_server_timezone
 from tests.testbase import BaseTestCase
 
 
 class CalendarsTestCase(BaseTestCase):
+    def setUp(self) -> None:
+        self.test_calendar = BusinessCalendar("USNYSE")
+        self.b_day1 = "2022-01-03"
+        self.b_day = "2022-03-08"
+        self.prev_b_day = "2022-03-07"
+        self.prev_nb_day = "2022-03-06"
+        self.next_b_day = "2022-03-09"
+        self.next_nb_day = "2022-03-12"
+        self.last_b_day_month = "2022-03-31"
+        self.last_b_day_week = "2022-03-11"
+
     def test_calendars(self):
         with self.subTest(msg="calendarNames() test"):
-            names = calendar_names()
-            self.assertTrue(isinstance(names, list) and len(names) > 0)
+            cal_names = calendar_names()
+            self.assertTrue(isinstance(cal_names, list) and len(cal_names) > 0)
 
         with self.subTest(msg="getDefaultName() test"):
-            name = default_calendar_name()
-            self.assertIsNotNone(name)
+            def_cal_name = default_calendar_name()
+            self.assertIsNotNone(def_cal_name)
 
         with self.subTest(msg="calendar() construction"):
-            self.assertIsNotNone(Calendar())
+            self.assertIsNotNone(BusinessCalendar())
 
         with self.subTest(msg="calendar(invalid name) construction"):
-            self.assertRaises(DHError, Calendar, "garbage_name")
+            self.assertRaises(DHError, BusinessCalendar, "garbage_name")
 
         with self.subTest(msg="calendar(valid name) construction"):
-            self.assertIsNotNone(Calendar(name))
+            self.assertIsNotNone(BusinessCalendar(cal_names[0]))
 
-    def test_calendar(self):
-        default_calendar = Calendar()
+    def test_business_period(self):
+        b_periods = self.test_calendar.get_business_schedule(self.b_day1).business_periods
+        self.assertEqual(len(b_periods), 1)
+        p = b_periods[0]
+        s = time.format_datetime(p.start_time, time.TimeZone.NY)
+        self.assertEqual(p.start_time, time.to_datetime(s))
+        s = time.format_datetime(p.end_time, time.TimeZone.NY)
+        self.assertEqual(p.end_time, time.to_datetime(s))
+        self.assertEqual(p.length, 6.5 * 60 * 60 * 10 ** 9)
+
+    def test_business_schedule_business_day(self):
+        b_schedule = self.test_calendar.get_business_schedule(self.nb_day)
+        self.assertFalse(b_schedule.is_business_day())
+
+        b_schedule = self.test_calendar.get_business_schedule(self.b_day)
+        self.assertTrue(b_schedule.is_business_day())
+
+        b_period = b_schedule.business_periods[0]
+        self.assertEqual(b_period.start_time, b_schedule.start_of_day)
+        self.assertEqual(b_period.end_time, b_schedule.end_of_day)
+
+        self.assertTrue(b_schedule.is_business_time(b_period.start_time))
+        self.assertTrue(b_schedule.is_business_time(b_period.end_time))
+        non_b_time = time.minus_nanos(b_schedule.start_of_day, 1)
+        self.assertFalse(b_schedule.is_business_time(non_b_time))
+        non_b_time = time.plus_nanos(b_schedule.end_of_day, 1)
+        self.assertFalse(b_schedule.is_business_time(non_b_time))
+
+        b_time = time.plus_nanos(b_schedule.start_of_day, 10 * 10 ** 9)
+        self.assertEqual(10 * 10 ** 9, b_schedule.business_time_elapsed(b_time))
+        b_time = time.plus_nanos(b_schedule.end_of_day, 10 * 10 ** 9)
+        self.assertEqual(b_period.length, b_schedule.business_time_elapsed(b_time))
+
+    def test_business_calendar(self):
+        default_calendar = BusinessCalendar()
         self.assertIsNotNone(default_calendar.name)
         self.assertIsNotNone(default_calendar.current_day)
         self.assertIsNotNone(default_calendar.previous_day)
         self.assertIsNotNone(default_calendar.next_day)
         self.assertIsNotNone(default_calendar.day_of_week.name)
-        self.assertIsNotNone(default_calendar.is_business_day)
+        self.assertEqual(default_calendar.time_zone, get_server_timezone())
+        self.assertIn(default_calendar.is_business_day, {True, False})
+
+        self.assertEqual(self.test_calendar.default_business_periods, ["09:30,16:00"])
+        self.assertEqual(self.test_calendar.standard_business_day_length, 6.5 * 60 * 60 * 10 ** 9)
+        self.assertEqual(self.test_calendar.previous_business_day(self.b_day), self.prev_b_day)
+        self.assertEqual(self.test_calendar.previous_non_business_day(self.b_day), self.prev_nb_day)
+        self.assertEqual(self.test_calendar.next_business_day(self.b_day), self.next_b_day)
+        self.assertEqual(self.test_calendar.next_non_business_day(self.b_day), self.next_nb_day)
+
+        self.assertEqual(self.test_calendar.business_days_in_range(self.prev_nb_day, self.b_day),
+                         [self.prev_b_day, self.b_day])
+        self.assertEqual(self.test_calendar.non_business_days_in_range(self.prev_nb_day, self.b_day),
+                         [self.prev_nb_day])
+
+        self.assertEqual(self.test_calendar.number_of_business_days(self.prev_nb_day, self.next_b_day), 2)
+        self.assertEqual(
+            self.test_calendar.number_of_business_days(self.prev_nb_day, self.next_b_day, end_inclusive=True), 3)
+        self.assertEqual(self.test_calendar.number_of_non_business_days(self.prev_nb_day, self.next_nb_day), 1)
+        self.assertEqual(
+            self.test_calendar.number_of_non_business_days(self.prev_nb_day, self.next_nb_day, end_inclusive=True), 2)
+
+        self.assertFalse(self.test_calendar.is_last_business_day_of_month(self.b_day))
+        self.assertFalse(self.test_calendar.is_last_business_day_of_week(self.b_day))
+        self.assertTrue(self.test_calendar.is_last_business_day_of_month(self.last_b_day_month))
+        self.assertTrue(self.test_calendar.is_last_business_day_of_week(self.last_b_day_week))
+
+    def test_business_schedule_non_business_day(self):
+        default_calendar = BusinessCalendar()
+        business_schedule = default_calendar.get_business_schedule("2022-01-01")
+        self.assertEqual(business_schedule.business_periods, [])
+        self.assertFalse(business_schedule.is_business_day())
 
 
 if __name__ == '__main__':
