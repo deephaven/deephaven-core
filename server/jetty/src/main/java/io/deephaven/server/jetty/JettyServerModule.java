7c419ce628f78b09d17d4e8661acf102309555da
James X Nelson <jamesnelson@deephaven.io>
2022-03-25 02:59:01 -0600

    Add DH_TLS_CHAIN and DH_TLS_KEY env vars to enable TLS in server

--- server/jetty/src/main/java/io/deephaven/server/jetty/JettyServerModule.java
+++ server/jetty/src/main/java/io/deephaven/server/jetty/JettyServerModule.java
@@ -9,6 +9,7 @@ import io.grpc.servlet.jakarta.ServletAdapter;
 import io.grpc.servlet.jakarta.ServletServerBuilder;
 
 import javax.inject.Named;
+import java.io.File;
 import java.util.Set;
 
 @Module
@@ -25,6 +26,20 @@ public class JettyServerModule {
             final Set<BindableService> services,
             final Set<ServerInterceptor> interceptors) {
         final ServletServerBuilder serverBuilder = new ServletServerBuilder();
+
+        String chainLoc = System.getenv("DH_TLS_CHAIN");
+        if (chainLoc != null) {
+            String chainKey = System.getenv("DH_TLS_KEY");
+            final File chain = new File(chainLoc);
+            final File key = new File(chainKey);
+            if (!chain.exists()) {
+                throw new IllegalArgumentException("TLS chain " + chain + " does not exist!");
+            }
+            if (!key.exists()) {
+                throw new IllegalArgumentException("TLS key " + key + " does not exist!");
+            }
+            serverBuilder.useTransportSecurity(chain, key);
+        }
         services.forEach(serverBuilder::addService);
         interceptors.forEach(serverBuilder::intercept);
 
--- server/netty/src/main/java/io/deephaven/server/netty/NettyServerModule.java
+++ server/netty/src/main/java/io/deephaven/server/netty/NettyServerModule.java
@@ -9,6 +9,7 @@ import io.grpc.ServerBuilder;
 import io.grpc.ServerInterceptor;
 
 import javax.inject.Named;
+import java.io.File;
 import java.io.IOException;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -22,6 +23,20 @@ public class NettyServerModule {
             Set<BindableService> services,
             Set<ServerInterceptor> interceptors) {
         ServerBuilder<?> serverBuilder = ServerBuilder.forPort(port);
+
+        String chainLoc = System.getenv("DH_TLS_CHAIN");
+        if (chainLoc != null) {
+            String chainKey = System.getenv("DH_TLS_KEY");
+            final File chain = new File(chainLoc);
+            final File key = new File(chainKey);
+            if (!chain.exists()) {
+                throw new IllegalArgumentException("TLS chain " + chain + " does not exist!");
+            }
+            if (!key.exists()) {
+                throw new IllegalArgumentException("TLS key " + key + " does not exist!");
+            }
+            serverBuilder.useTransportSecurity(chain, key);
+        }
         services.forEach(serverBuilder::addService);
         interceptors.forEach(serverBuilder::intercept);
 
