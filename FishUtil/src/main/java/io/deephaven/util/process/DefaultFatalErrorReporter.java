12959122d8ce04bd58eb5c1de091ac71762dad7b
James X Nelson <jamesnelson@deephaven.io>
2022-03-25 04:07:51 -0600

    Random changes that look worth merging

--- FishUtil/src/main/java/io/deephaven/util/process/DefaultFatalErrorReporter.java
+++ FishUtil/src/main/java/io/deephaven/util/process/DefaultFatalErrorReporter.java
@@ -31,6 +31,10 @@ public class DefaultFatalErrorReporter extends FatalErrorReporterBase {
         err.println(String.format("%s: %s",
                 initiateShutdown ? "Initiating shutdown due to" : "After shutdown initiated", message));
         throwable.printStackTrace(err);
+        if (err != System.err) {
+            throwable.printStackTrace(System.err);
+        }
+        err.flush();
 
         if (initiateShutdown) {
             // We can't universally call System.exit(...) on this thread, unless we have very tight
