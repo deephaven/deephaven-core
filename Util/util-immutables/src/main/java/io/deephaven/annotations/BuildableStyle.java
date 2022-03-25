b1c019cbebff532aa1f0e1505c0e9e3014f66f1d
Ryan Caudy <ryan@deephaven.io>
2022-03-25 17:39:57 -0400

    Correct BuildableStyle JavaDoc

--- Util/util-immutables/src/main/java/io/deephaven/annotations/BuildableStyle.java
+++ Util/util-immutables/src/main/java/io/deephaven/annotations/BuildableStyle.java
@@ -9,8 +9,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * A simple style is for objects that are simple to build. Not recommended for objects with more than two fields. Not
- * applicable for objects with default fields.
+ * A style for objects that should declare a builder interface to use for construction. Recommended for objects with
+ * more than two fields, or default fields.
  */
 @Target({ElementType.TYPE, ElementType.PACKAGE})
 @Retention(RetentionPolicy.CLASS)
