1ae9c3cb1ea5321ab01cd1b90e3955cd54376a3f
Devin Smith <devinsmith@deephaven.io>
2022-03-30 14:58:01 -0700

    spotless

--- server/jetty-app/src/main/java/io/deephaven/server/jetty/JettyAppMain.java
+++ server/jetty-app/src/main/java/io/deephaven/server/jetty/JettyAppMain.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class JettyAppMain {
-    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException, TimeoutException {
+    public static void main(String[] args)
+            throws IOException, InterruptedException, ClassNotFoundException, TimeoutException {
         JettyMain.main(args);
     }
 }
