51b9da73197d893a9915c7d17e6bffe6e378626c
Amanda L Martin <hythloda@gmail.com>
2022-04-06 16:03:26 -0600

    Revert "debexium v2 changes (#2209)"
    This reverts commit 67ca4f1a785029652a363e6f7caca926b464637b.

--- debezium/scripts/demo.py
+++ debezium/scripts/demo.py
@@ -1,11 +1,9 @@
-import deephaven.stream.kafka.cdc as cc
-from deephaven import kafka_consumer as ck
-from deephaven import kafka_producer as pk
-from deephaven.stream.kafka.consumer import TableType, KeyValueSpec
-from deephaven import dtypes as dh
-from deephaven import agg as agg
-
-#import deephaven.TableManipulation.WindowCheck as wck
+import deephaven_legacy.ConsumeCdc as cc
+import deephaven_legacy.ConsumeKafka as ck
+import deephaven_legacy.ProduceKafka as pk
+import deephaven_legacy.Types as dh
+from deephaven_legacy import Aggregation as agg, as_list
+import deephaven_legacy.TableManipulation.WindowCheck as wck
 
 server_name = 'mysql'
 db_name='shop'
@@ -18,7 +16,7 @@ kafka_base_properties = {
 
 
 def make_cdc_table(table_name:str):
-    return cc.consume(
+    return cc.consumeToTable(
         kafka_base_properties,
         cc.cdc_short_spec(server_name,
                           db_name,
@@ -38,136 +36,133 @@ consume_properties = {
     }
 }
 
-pageviews = ck.consume(
+pageviews = ck.consumeToTable(
     consume_properties,
     topic = 'pageviews',
     offsets = ck.ALL_PARTITIONS_SEEK_TO_BEGINNING,
-    key_spec = KeyValueSpec.IGNORE,
-    value_spec = ck.json_spec([ ('user_id', dh.int_),
+    key = ck.IGNORE,
+    value = ck.json([ ('user_id', dh.long_),
                       ('url', dh.string),
                       ('channel', dh.string),
-                      ('received_at', dh.DateTime) ]),
-    table_type = TableType.Append
+                      ('received_at', dh.datetime) ]),
+    table_type = 'append'
 )
 
 pageviews_stg = pageviews \
-    .update_view([
+    .updateView(
         'url_path = url.split(`/`)',
         'pageview_type = url_path[1]',
         'target_id = Long.parseLong(url_path[2])'
-    ]).drop_columns('url_path')
+    ).dropColumns('url_path')
 
-purchases_by_item = purchases.agg_by(
-    [
-        agg.sum_(['revenue = purchase_price']),
-        agg.count_('orders'),
-        agg.sum_(['items_sold = quantity'])
-    ],
+purchases_by_item = purchases.aggBy(
+    as_list([
+        agg.AggSum('revenue = purchase_price'),
+        agg.AggCount('orders'),
+        agg.AggSum('items_sold = quantity')
+    ]),
     'item_id'
 )
 
 pageviews_by_item = pageviews_stg \
-    .where(['pageview_type = `products`']) \
-    .count_by('pageviews', ['item_id = target_id'])
+    .where('pageview_type = `products`') \
+    .countBy('pageviews', 'item_id = target_id')
 
 item_summary = items \
-    .view(['item_id = id', 'name', 'category']) \
-    .natural_join(purchases_by_item, on = ['item_id']) \
-    .natural_join(pageviews_by_item, on = ['item_id']) \
-    .drop_columns('item_id') \
-    .move_columns_down(['revenue', 'pageviews']) \
-    .update_view(['conversion_rate = orders / (double) pageviews'])
+    .view('item_id = id', 'name', 'category') \
+    .naturalJoin(purchases_by_item, 'item_id') \
+    .naturalJoin(pageviews_by_item, 'item_id') \
+    .dropColumns('item_id') \
+    .moveColumnsDown('revenue', 'pageviews') \
+    .updateView('conversion_rate = orders / (double) pageviews')
 
 # These two 'top_*' tables match the 'Business Intelligence: Metabase' / dashboard
 # part of the original example.
 top_viewed_items = item_summary \
-    .sort_descending('pageviews') \
+    .sortDescending('pageviews') \
     .head(20)
 
 top_converting_items = item_summary \
-    .sort_descending('conversion_rate') \
+    .sortDescending('conversion_rate') \
     .head(20)
 
 minute_in_nanos = 60 * 1000 * 1000 * 1000
 
-#profile_views_per_minute_last_10 = \
-#    wck.addTimeWindow(
-#       pageviews_stg.where('pageview_type = `profiles`'),
-#        'received_at',
-#        10*minute_in_nanos,
-#        'in_last_10min'
-#    ).where([
-#        'in_last_10min = true'
-#    ]).update_view([
-#        'received_at_minute = lower_bin(received_at, minute_in_nanos)'
-#    ]).view([
-#        'user_id = target_id',
-#        'received_at_minute'
-#    ]).count_by([
-#        'pageviews',
-#        'user_id',
-#        'received_at_minute'
-#    ]).sort([
-#        'user_id',
-#        'received_at_minute'
-#    ])
+profile_views_per_minute_last_10 = \
+    wck.addTimeWindow(
+        pageviews_stg.where('pageview_type = `profiles`'),
+        'received_at',
+        10*minute_in_nanos,
+        'in_last_10min'
+    ).where(
+        'in_last_10min = true'
+    ).updateView(
+        'received_at_minute = lowerBin(received_at, minute_in_nanos)'
+    ).view(
+        'user_id = target_id',
+        'received_at_minute'
+    ).countBy(
+        'pageviews',
+        'user_id',
+        'received_at_minute'
+    ).sort(
+        'user_id',
+        'received_at_minute'
+    )
 
 profile_views = pageviews_stg \
-    .view([
+    .view(
         'owner_id = target_id',
         'viewer_id = user_id',
         'received_at'
-    ]).sort([
+    ).sort(
         'received_at'
-    ]).tail_by(10, ['owner_id'])
+    ).tailBy(10, 'owner_id')
 
 profile_views_enriched = profile_views \
-    .natural_join(users, on = ['owner_id = id'], joins = ['owner_email = email']) \
-    .natural_join(users, on = ['viewer_id = id'], joins = ['viewer_email = email']) \
-    .move_columns_down('received_at')
+    .naturalJoin(users, 'owner_id = id', 'owner_email = email') \
+    .naturalJoin(users, 'viewer_id = id', 'viewer_email = email') \
+    .moveColumnsDown('received_at')
 
-dd_flagged_profiles = ck.consume(
+dd_flagged_profiles = ck.consumeToTable(
     consume_properties,
     topic = 'dd_flagged_profiles',
     offsets = ck.ALL_PARTITIONS_SEEK_TO_BEGINNING,
-    key_spec = KeyValueSpec.IGNORE,
-    value_spec = ck.simple_spec('user_id_str', dh.string),
-    table_type = TableType.Append
-).view(['user_id = Long.parseLong(user_id_str.substring(1, user_id_str.length() - 1))'])  # strip quotes
+    key = ck.IGNORE,
+    value = ck.simple('user_id_str', dh.string),
+    table_type = 'append'
+).view('user_id = Long.parseLong(user_id_str.substring(1, user_id_str.length() - 1))')  # strip quotes
 
 dd_flagged_profile_view = dd_flagged_profiles \
-    .
-
-
-(pageviews_stg, on = ['user_id'])
+    .join(pageviews_stg, 'user_id')
 
 high_value_users = purchases \
-    .update_view([
+    .updateView(
         'purchase_total = purchase_price * quantity'
-    ]).agg_by([
-            agg.sum_(['lifetime_value = purchase_total']),
-            agg.count_('purchases')
-        ],
+    ).aggBy(
+        as_list([
+            agg.AggSum('lifetime_value = purchase_total'),
+            agg.AggCount('purchases'),
+        ]),
         'user_id'
-    )\
-    .where(['lifetime_value > 10000']) \
-    .natural_join(users, 
-                  ['user_id = id'], ['email']) \
-    .view(['id = user_id', 'email', 'lifetime_value', 'purchases'])  # column rename and reorder
+    ) \
+    .where('lifetime_value > 10000') \
+    .naturalJoin(users, 'user_id = id', 'email') \
+    .view('id = user_id', 'email', 'lifetime_value', 'purchases')  # column rename and reorder
 
 schema_namespace = 'io.deephaven.examples'
 
-cancel_callback = pk.produce(
+cancel_callback = pk.produceFromTable(
     high_value_users,
     kafka_base_properties,
     topic = 'high_value_users_sink',
-    key_spec = pk.avro_spec(
+    key = pk.avro(
         'high_value_users_sink_key',
         publish_schema = True,
         schema_namespace = schema_namespace,
-        include_only_columns = ['user_id']
+        include_only_columns = [ 'user_id' ]
     ),
-    value_spec = pk.avro_spec(
+    value = pk.avro(
         'high_value_users_sink_value',
         publish_schema = True,
         schema_namespace = schema_namespace,
@@ -179,19 +174,18 @@ cancel_callback = pk.produce(
     last_by_key_columns = True
 )
 
-hvu_test = ck.consume(
+hvu_test = ck.consumeToTable(
     consume_properties,
     topic = 'high_value_users_sink',
     offsets = ck.ALL_PARTITIONS_SEEK_TO_BEGINNING,
-    key_spec = KeyValueSpec.IGNORE,
-    value_spec = ck.avro_spec('high_value_users_sink_value'),
-    table_type = TableType.Append
+    key = ck.IGNORE,
+    value = ck.avro('high_value_users_sink_value'),
+    table_type = 'append'
 )
 
 pageviews_summary = pageviews_stg \
-    .agg_by(
-        [
-            agg.count_('total'),
-            agg.max_(['max_received_at = received_at'])
-        ]) \
-    .update(['dt_ms = (DateTime.now() - max_received_at)/1_000_000.0'])
+    .aggBy(
+        as_list([
+            agg.AggCount('total'),
+            agg.AggMax('max_received_at = received_at')])) \
+    .update('dt_ms = (DateTime.now() - max_received_at)/1_000_000.0')
