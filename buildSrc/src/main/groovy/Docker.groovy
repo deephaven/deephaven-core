0fdadc28fc9f4be1a7c7b37fc5b3df1ba5c9281e
Colin Alworth <colinalworth@deephaven.io>
2022-04-11 13:04:52 -0500

    Continue refactoring, with new internal plugin

--- buildSrc/src/main/groovy/Docker.groovy
+++ buildSrc/src/main/groovy/Docker.groovy
@@ -530,7 +530,7 @@ class Docker {
         return registerDockerTask(project, taskName) { DockerTaskConfig config ->
             config.copyIn { Sync sync ->
                 sync.from(sourcePath) { CopySpec copySpec ->
-                    copySpec.exclude 'build'
+                    copySpec.exclude 'build', 'dist'
                     copySpec.into 'src'
                 }
             }
