cee7cb3748f0adf81b9c37bd72836245e5b55bd0
jianfengmao <jianfengmao@deephaven.io>
2022-04-14 12:50:56 -0600

    Fix a test code error

--- py/server/tests/test_experimental.py
+++ py/server/tests/test_experimental.py
@@ -5,7 +5,6 @@ import unittest
 
 from deephaven import time_table
 from deephaven.experimental import time_window
-
 from tests.testbase import BaseTestCase
 
 
@@ -14,7 +13,7 @@ class ExperimentalTestCase(BaseTestCase):
     def test_time_window(self):
         with self.ugp_lock_exclusive():
             source_table = time_table("00:00:00.0000001").update(["TS=currentTime()"])
-        t = time_window(source_table, ts_col="TS", window=10 ** 7, bool_col="InWindow")
+            t = time_window(source_table, ts_col="TS", window=10 ** 7, bool_col="InWindow")
 
         self.assertEqual("InWindow", t.columns[-1].name)
         self.wait_ticking_table_update(t, row_count=20, timeout=5)
