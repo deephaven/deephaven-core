e10740910b87241eb8c95278fc08ce1266ff9c88
Cristian Ferretti <cristianferretti@deephaven.io>
2022-04-19 13:42:37 -0400

    Fix flakiness in test_perfmon.py for test_metrics_get_counters.

--- py/server/tests/test_perfmon.py
+++ py/server/tests/test_perfmon.py
@@ -19,7 +19,7 @@ def create_some_counters():
 
 class PerfmonTestCase(BaseTestCase):
 
-    def do_not_test_metrics_get_counters(self):
+    def test_metrics_get_counters(self):
         metrics_reset_counters()
         t = create_some_counters()
         # counters should accumulate some values after performing some operations
@@ -29,11 +29,13 @@ class PerfmonTestCase(BaseTestCase):
         counters_str2 = metrics_get_counters()
         self.assertNotEqual(counters_str, counters_str2)
 
-        # after reset and performing the same operation again, the counters' values should be the same as the 1st one
+        # After reset and performing the same operation again, the counters' values should reset.
+        # We can't ensure they are exactly the same as before, because the engine state may be
+        # captured in some non-exactly-zero-counters due to other pre-existing table updates.
         metrics_reset_counters()
         t = create_some_counters()
         counters_str3 = metrics_get_counters()
-        self.assertEqual(counters_str, counters_str3)
+        self.assertNotEqual(counters_str2, counters_str3)
 
     def test_process_logs(self):
         log_table = process_info_log()
