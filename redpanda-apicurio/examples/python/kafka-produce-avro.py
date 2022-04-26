c7b260cfcccb4255d1ac25d6adeee4da7d59db51
Cristian Ferretti <37232625+jcferretti@users.noreply.github.com>
2022-04-26 12:23:11 -0400

    Updated python manual tests/examples for the TableType append change. (#2310)
    * Updated python manual tests/examples for the TableType append change.
    
    * Ported redpanda/examples/python/kafka-produce.py script to py2.

--- redpanda-apicurio/examples/python/kafka-produce-avro.py
+++ redpanda-apicurio/examples/python/kafka-produce-avro.py
@@ -40,7 +40,7 @@
 #
 # From the web IDE, run:
 #
-#    > t = kc.consume({'bootstrap.servers' : 'redpanda:29092', 'schema.registry.url' : 'http://registry:8080/api/ccompat'}, 'share_price', value_spec=kc.avro_spec('share_price_record'), table_type=TableType.Append)
+#    > t = kc.consume({'bootstrap.servers' : 'redpanda:29092', 'schema.registry.url' : 'http://registry:8080/api/ccompat'}, 'share_price', value_spec=kc.avro_spec('share_price_record'), table_type=TableType.append())
 #
 # The last command above should create a table with columns: [ KafkaPartition, KafkaOffset, KafkaTimestamp, Symbol, Price ]
 # Run this script on the host (not on a docker image) to generate one row:
