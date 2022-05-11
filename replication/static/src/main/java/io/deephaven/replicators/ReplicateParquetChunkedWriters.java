5f60d8193f0d31874da27e1bee4c743cd71e7585
Colin Alworth <colinalworth@deephaven.io>
2022-05-11 11:41:31 -0500

    Fix up replication code to expect literals, not patterns

--- replication/static/src/main/java/io/deephaven/replicators/ReplicateParquetChunkedWriters.java
+++ replication/static/src/main/java/io/deephaven/replicators/ReplicateParquetChunkedWriters.java
@@ -10,9 +10,9 @@ public class ReplicateParquetChunkedWriters {
 
         ReplicatePrimitiveCode.intToLongAndFloatingPoints(
                 "extensions/parquet/base/src/main/java/io/deephaven/parquet/base/PlainIntChunkedWriter.java",
-                "int pageSize", "IntBuffer.allocate(4)", "int originalLimit", "int writeBulk", "int valueCount",
+                "int pageSize", "IntBuffer.allocate\\(4\\)", "int originalLimit", "int writeBulk", "int valueCount",
                 "int rowCount",
-                "int nullCount", "writeInt\\(", "IntBuffer repeatCount", "length != Integer.MIN_VALUE", "int length",
+                "int nullCount", "writeInt\\(", "IntBuffer repeatCount", "length != Integer\\.MIN_VALUE", "int length",
                 "int i = 0;", "int targetCapacity", "IntBuffer nullOffsets");
     }
 
