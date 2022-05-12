6c230c2b26745d7a3bba21ac6b96b7174dd825b3
Cristian Ferretti <cristianferretti@deephaven.io>
2022-05-11 22:33:52 -0400

    First version of full test.

--- engine/rowset/src/test/java/io/deephaven/engine/rowset/impl/rsp/RspBitmapTimeDrivenTest.java
+++ engine/rowset/src/test/java/io/deephaven/engine/rowset/impl/rsp/RspBitmapTimeDrivenTest.java
@@ -8,24 +8,30 @@ import org.junit.experimental.categories.Category;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.function.BiFunction;
 
 import static io.deephaven.engine.rowset.impl.rsp.RspArray.BLOCK_LAST;
+import static io.deephaven.engine.rowset.impl.rsp.RspArray.BLOCK_SIZE;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertFalse;
 
 @Category(OutOfBandTest.class)
 public class RspBitmapTimeDrivenTest {
 
-    public static final long DEFAULT_PER_TEST_TIME_BUDGET_MILLIS = 3 * 60 * 1000;
+    public static final int RANDOM_SEED = 2;
+    public static final long DEFAULT_PER_TEST_TIME_BUDGET_MILLIS = 2 * 60 * 1000;
     public static final long LOG_PERIOD_MILLIS = 10 * 1000;
 
     // See comment about spec value meanings in caller.
     public static final int SPEC_OPTIONS = 6;
+
+    // Each position in the spec array represents one block in a series of consecutive RSP blocks
+    // in the result.  Position i in the spec array represents the block starting at i*BLOCK_SIZE
     private static OrderedLongSet fromBlockSpec(final int[] spec) {
         final OrderedLongSet.BuilderSequential b = new OrderedLongSetBuilderSequential();
         for (int i = 0; i < spec.length; ++i) {
-            final long baseKey = 65536L * i;
+            final long baseKey = BLOCK_SIZE * (long) i;
             switch (spec[i]) {
                 case 0: // Key not present.
                     break;
@@ -46,7 +52,7 @@ public class RspBitmapTimeDrivenTest {
                     b.appendRange(baseKey, baseKey + BLOCK_LAST);
                     break;
                 default:
-                    throw new IllegalStateException("Unespected spec");
+                    throw new IllegalStateException("Unexpected i=" + i + ", spec[i]=" + spec[i]);
             }
         }
         return b.getTreeIndexImpl();
@@ -55,7 +61,7 @@ public class RspBitmapTimeDrivenTest {
     private static String toStr(final int[] spec) {
         final StringBuilder sb = new StringBuilder("[");
         for (int i = 0; i < spec.length; ++i) {
-            final long baseKey = 65536L * i;
+            final long baseKey = BLOCK_SIZE * (long) i;
             sb.append(" ").append(baseKey).append(" : ");
             switch (spec[i]) {
                 case 0: // Key not present.
@@ -77,7 +83,7 @@ public class RspBitmapTimeDrivenTest {
                     sb.append("{0->}");
                     break;
                 default:
-                    throw new IllegalStateException("Unespected spec");
+                    throw new IllegalStateException("Unexpected i=" + i + ", spec[i]=" + spec[i]);
             }
             if (i < spec.length - 1) {
                 sb.append(", ");
@@ -87,6 +93,16 @@ public class RspBitmapTimeDrivenTest {
         return sb.toString();
     }
 
+    private static final Random rand = new Random(RANDOM_SEED);
+
+    private static void random(final int[] spec) {
+        for (int i = 0; i < spec.length; ++i) {
+            spec[i] = rand.nextInt(SPEC_OPTIONS);
+        }
+    }
+
+    // Do a "manual" increment of spec as if it was a number represented in base SPEC_OPTIONS
+    // with one digit per array position.
     private static boolean increment(final int[] spec) {
         for (int i = spec.length - 1; i >= 0; --i) {
             final int v = ++spec[i];
@@ -98,12 +114,13 @@ public class RspBitmapTimeDrivenTest {
         return false;
     }
 
-    private static void checkEquals(final String msg, OrderedLongSet oset0, OrderedLongSet oset1) {
+    private static void checkEquals(final String msg, final OrderedLongSet oset0, final OrderedLongSet oset1) {
         assertEquals(msg, oset0.ixCardinality(), oset1.ixCardinality());
         assertTrue(msg, oset0.ixSubsetOf(oset1));
     }
 
-    private static long pow(int a, int b) {
+    // return a^b.
+    private static long pow(final int a, int b) {
         long r = 1;
         while (b > 0) {
             r *= a;
@@ -112,33 +129,45 @@ public class RspBitmapTimeDrivenTest {
         return r;
     }
 
-    @Test
-    public void testBinaryOpsExhaustive() {
-        final Map<String, BiFunction<RspBitmap, RspBitmap, RspBitmap>> rspOps = new HashMap<>();
-        rspOps.put("andNotEquals", RspBitmap::andNotEquals);
-        rspOps.put("andEquals", RspBitmap::andEquals);
-        rspOps.put("orEquals", RspBitmap::orEquals);
-        final Map<String, BiFunction<OrderedLongSet, OrderedLongSet, OrderedLongSet>> osetOps = new HashMap<>();
-        osetOps.put("andNotEquals", (x, y) -> x.ixCowRef().ixRemove(y));
-        osetOps.put("andEquals", (x, y) -> x.ixCowRef().ixRetain(y));
-        osetOps.put("orEquals", (x, y) -> x.ixCowRef().ixInsert(y));
-        for (String opName : rspOps.keySet()) {
-            binaryOpsExhaustiveHelper(opName, rspOps.get(opName), osetOps.get(opName), 4, TestSequenceMode.EXHAUSTIVE);
-        }
+    enum Operation {
+        AND_NOT_EQUALS,
+        AND_EQUALS,
+        OR_EQUALS
+    }
+    private static final Map<Operation, BiFunction<RspBitmap, RspBitmap, RspBitmap>> rspOps = new HashMap<>();
+    static {
+        rspOps.put(Operation.AND_NOT_EQUALS, RspBitmap::andNotEquals);
+        rspOps.put(Operation.AND_EQUALS, RspBitmap::andEquals);
+        rspOps.put(Operation.OR_EQUALS, RspBitmap::orEquals);
+    }
+    private static final Map<Operation, BiFunction<OrderedLongSet, OrderedLongSet, OrderedLongSet>> osetOps =
+            new HashMap<>();
+    static {
+        osetOps.put(Operation.AND_NOT_EQUALS, (x, y) -> x.ixCowRef().ixRemove(y));
+        osetOps.put(Operation.AND_EQUALS, (x, y) -> x.ixCowRef().ixRetain(y));
+        osetOps.put(Operation.OR_EQUALS, (x, y) -> x.ixCowRef().ixInsert(y));
     }
 
     enum TestSequenceMode {
-        EXHAUSTIVE,
-        RANDOM
+        EXHAUSTIVE, RANDOM
     }
 
-    private void binaryOpsExhaustiveHelper(
-            final String opName,
+    private static String toTimeStr(final long millis) {
+        final long totalDeciSeconds = millis / 100;
+        final long totalSeconds = totalDeciSeconds / 10;
+        final long deciSeconds = totalDeciSeconds % 10;
+        final long minutes = totalSeconds / 60;
+        final long seconds = totalSeconds % 60;
+        return String.format("%dm%d.%ds", minutes, seconds, deciSeconds);
+    }
+
+    private static void binaryOpsExhaustiveHelper(
+            final Operation op,
             final BiFunction<RspBitmap, RspBitmap, RspBitmap> rspOp,
             final BiFunction<OrderedLongSet, OrderedLongSet, OrderedLongSet> osetOp,
             final int nblocks,
-            final TestSequenceMode mode
-            ) {
+            final TestSequenceMode mode,
+            final long testTimeBudgetMillis) {
         final long nspecs = pow(SPEC_OPTIONS, nblocks);
         final long totalChecks = nspecs * nspecs;
         // We will use an int value to represent a spec of whether to have for a given key:
@@ -146,50 +175,72 @@ public class RspBitmapTimeDrivenTest {
         // 1: Singleton container with element zero.
         // 2: Singleton container with element one.
         // 3: Container with elements zero and one.
-        // 4: Container with elements from two to 65535.
+        // 4: Container with elements from two to BLOCK_LAST.
         // 5: Full block span.
         final int[] spec0 = new int[nblocks];
         final int[] spec1 = new int[nblocks];
-        // avoid testing against the fully empty case.
-        increment(spec0);
-        increment(spec1);
         long check = 0;
         final long start = System.currentTimeMillis();
+        final long deadline = start + testTimeBudgetMillis;
         long lastLog = start;
         long nextLog = start + LOG_PERIOD_MILLIS;
         long lastLogChecksDone = 0;
-        final String testName = "binaryOpsExhaustive-" + opName;
-        do {
-            do {
-                final long now = System.currentTimeMillis();
-                if (now >= nextLog) {
-                    final long checksDoneSinceLastLog = check - lastLogChecksDone;
-                    final long millis = now - lastLog;
-                    System.out.printf("%s: In the last %.1f seconds ran %.1f checks per second; %d checks remaining.%n",
-                            testName,
-                            millis / 1000.0,
-                            (1000.0 * checksDoneSinceLastLog) / millis,
-                            totalChecks - check);
-                    lastLog = now;
-                    nextLog = now + LOG_PERIOD_MILLIS;
-                    lastLogChecksDone = check;
-                }
-                binaryOpHelper(opName, rspOp, osetOp, spec0, spec1);
-                ++check;
-            } while (increment(spec0));
-        } while (increment(spec1));
-        System.out.printf("%s: Done in %.1f seconds.%n",
+        final String testName = "binaryOps-" + op + "-" + mode + "-" + toTimeStr(testTimeBudgetMillis);
+        System.out.printf("%s: Starting, search space for %d blocks is %d combinations.%n", testName, nblocks, totalChecks);
+        while (true) {
+            final long now = System.currentTimeMillis();
+            if (now > deadline) {
+                break;
+            }
+            boolean finished = false;
+            switch (mode) {
+                case RANDOM:
+                    random(spec0);
+                    random(spec1);
+                case EXHAUSTIVE:
+                    // we always increment both in the first pass, to avoid
+                    // checking against the "all empty" case.
+                    final boolean more0 = increment(spec0);
+                    if (!more0 || check == 0) {
+                        final boolean more1 = increment(spec1);
+                        if (!more1) {
+                            finished = true;
+                        }
+                    }
+                    break;
+            }
+            if (finished) {
+                break;
+            }
+            if (now >= nextLog) {
+                final long checksDoneSinceLastLog = check - lastLogChecksDone;
+                final long millis = now - lastLog;
+                System.out.printf(
+                        "%s: In the last %.1f seconds ran %.1f checks per second; %.3f%% of the space covered; %s to test deadline%n",
+                        testName,
+                        millis / 1000.0,
+                        (1000.0 * checksDoneSinceLastLog) / millis,
+                        (100.0 * check) / totalChecks,
+                        toTimeStr(deadline - now));
+                lastLog = now;
+                nextLog = now + LOG_PERIOD_MILLIS;
+                lastLogChecksDone = check;
+            }
+            binaryOpHelper(op, rspOp, osetOp, spec0, spec1);
+            ++check;
+        }
+        System.out.printf("%s: Done in %s.%n",
                 testName,
-                (System.currentTimeMillis() - start) / 1000.0);
+                toTimeStr(System.currentTimeMillis() - start));
     }
 
-    private void binaryOpHelper(
-            final String opName,
+    private static void binaryOpHelper(
+            final Operation op,
             final BiFunction<RspBitmap, RspBitmap, RspBitmap> rspOp,
             final BiFunction<OrderedLongSet, OrderedLongSet, OrderedLongSet> osetOp,
             final int[] spec0,
             final int[] spec1) {
-        final String msg = opName
+        final String msg = op
                 + " && spec0==" + toStr(spec0)
                 + " && spec1==" + toStr(spec1);
         final OrderedLongSet oset0 = fromBlockSpec(spec0);
@@ -201,5 +252,34 @@ public class RspBitmapTimeDrivenTest {
         final RspBitmap rspResult = rspOp.apply(r0, r1);
         final OrderedLongSet osetResult = osetOp.apply(oset0, oset1);
         checkEquals(msg, osetResult, rspResult);
+    }
+
+    @Test
+    public void testBinaryOpsExhaustive() {
+        for (Operation op : rspOps.keySet()) {
+            binaryOpsExhaustiveHelper(op, rspOps.get(op), osetOps.get(op), 4, TestSequenceMode.EXHAUSTIVE,
+                    DEFAULT_PER_TEST_TIME_BUDGET_MILLIS);
+        }
+    }
+
+    @Test
+    public void testAndNotEqualsRandom() {
+        final Operation op = Operation.AND_NOT_EQUALS;
+        binaryOpsExhaustiveHelper(op, rspOps.get(op), osetOps.get(op), 8, TestSequenceMode.RANDOM,
+                DEFAULT_PER_TEST_TIME_BUDGET_MILLIS);
+    }
+
+    @Test
+    public void testAndEqualsRandom() {
+        final Operation op = Operation.AND_EQUALS;
+        binaryOpsExhaustiveHelper(op, rspOps.get(op), osetOps.get(op), 8, TestSequenceMode.RANDOM,
+                DEFAULT_PER_TEST_TIME_BUDGET_MILLIS);
+    }
+
+    @Test
+    public void testOrEqualsRandom() {
+        final Operation op = Operation.OR_EQUALS;
+        binaryOpsExhaustiveHelper(op, rspOps.get(op), osetOps.get(op), 8, TestSequenceMode.RANDOM,
+                DEFAULT_PER_TEST_TIME_BUDGET_MILLIS);
     }
 }
