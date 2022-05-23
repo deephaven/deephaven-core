2d85b98145d90cfa1ad9ea5be77801d556219189
Devin Smith <devinsmith@deephaven.io>
2022-05-23 14:21:37 -0700

    Add ciphers for Windows IE clients

--- ssl/config/src/main/java/io/deephaven/ssl/config/CiphersModern.java
+++ ssl/config/src/main/java/io/deephaven/ssl/config/CiphersModern.java
@@ -32,7 +32,9 @@ public abstract class CiphersModern implements Ciphers {
                 // TLS 1.2
                 "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
                 "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-                "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256");
+                "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
+                "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+                "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256");
     }
 
     @Override
