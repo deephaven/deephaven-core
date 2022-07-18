8e6dcb142702bfc2921a0d04bc8868d46f06ec6c
Jianfeng Mao <4297243+jmao-denver@users.noreply.github.com>
2022-07-18 08:33:41 -0600

    Add support for single string arg for DTW (#2645)

--- py/server/tests/test_table_factory.py
+++ py/server/tests/test_table_factory.py
@@ -169,15 +169,30 @@ class TableFactoryTestCase(BaseTestCase):
                 b_array = dtypes.array(dtypes.byte, [1, 1, 1])
                 s_array = dtypes.array(dtypes.short, [128, 228, 328])
                 i_array = dtypes.array(dtypes.int32, [32768, 42768, 52768])
-                l_array = dtypes.array(dtypes.long, [2**32, 2**33, 2**36])
+                l_array = dtypes.array(dtypes.long, [2 ** 32, 2 ** 33, 2 ** 36])
                 f_array = dtypes.array(dtypes.float32, [1.0, 1.1, 1.2])
-                d_array = dtypes.array(dtypes.double, [1.0/2**32, 1.1/2**33, 1.2/2**36])
+                d_array = dtypes.array(dtypes.double, [1.0 / 2 ** 32, 1.1 / 2 ** 33, 1.2 / 2 ** 36])
                 str_array = dtypes.array(dtypes.string, ["some", "not so random", "text"])
-                table_writer.write_row(b_array, s_array,i_array,l_array,f_array,d_array,str_array
+                table_writer.write_row(b_array, s_array, i_array, l_array, f_array, d_array, str_array
                                        )
                 t = table_writer.table
                 self.wait_ticking_table_update(t, row_count=1, timeout=5)
                 self.assertNotIn("null", t.to_string())
+
+    def test_dtw_single_string_arg(self):
+        col_defs = {"A_String": dtypes.string}
+        table_writer = DynamicTableWriter(col_defs)
+        table_writer.write_row("Hello world!")
+        t = table_writer.table
+        self.wait_ticking_table_update(t, row_count=1, timeout=5)
+        self.assertIn("Hello", t.to_string())
+
+        col_defs = {"A_Long": dtypes.long}
+        table_writer = DynamicTableWriter(col_defs)
+        table_writer.write_row(10**10)
+        t = table_writer.table
+        self.wait_ticking_table_update(t, row_count=1, timeout=5)
+        self.assertIn("10000000000", t.to_string())
 
 
 if __name__ == '__main__':
