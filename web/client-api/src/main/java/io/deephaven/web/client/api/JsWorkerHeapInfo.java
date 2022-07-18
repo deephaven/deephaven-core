8e7fef3a4dd221e657a134013cf0fdcdce9c8e75
mikebender <mikebender@deephaven.io>
2022-07-18 11:35:09 -0400

    Wire up HeapInfo to JsWorkerHeapInfo

--- web/client-api/src/main/java/io/deephaven/web/client/api/JsWorkerHeapInfo.java
+++ web/client-api/src/main/java/io/deephaven/web/client/api/JsWorkerHeapInfo.java
@@ -5,24 +5,28 @@ import io.deephaven.web.shared.data.WorkerHeapInfo;
 import jsinterop.annotations.JsProperty;
 
 public class JsWorkerHeapInfo {
-    private HeapInfo heapInfo;
+    private long maximumHeapSize;
+    private long freeMemory;
+    private long totalHeapSize;
 
     public JsWorkerHeapInfo(HeapInfo heapInfo){
-        this.heapInfo = heapInfo;
+        this.maximumHeapSize = Long.parseLong(heapInfo.getMaximumHeapSize());
+        this.freeMemory = Long.parseLong(heapInfo.getFreeMemory());
+        this.totalHeapSize = Long.parseLong(heapInfo.getTotalHeapSize());
     }
 
     @JsProperty
     public long getMaximumHeapSize() {
-        return heapInfo.getMaximumHeapSize();
+        return maximumHeapSize;
     }
 
     @JsProperty
     public long getFreeMemory() {
-        return heapInfo.getFreeMemory();
+        return freeMemory;
     }
 
     @JsProperty
     public long getTotalHeapSize() {
-        return heapInfo.getTotalHeapSize();
+        return totalHeapSize;
     }
 }
