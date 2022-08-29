f55fcb767ae53ac1cab006bb259ea2e27b189dda
Colin Alworth <colinalworth@deephaven.io>
2022-08-29 11:13:48 -0500

    Review feedback

--- py/server/deephaven_internal/stream.py
+++ py/server/deephaven_internal/stream.py
@@ -10,31 +10,28 @@ class TeeStream(io.TextIOBase):
     processing take place before writing.
     """
 
-    encoding = 'UTF-8'
-    closed = False
-
-    @staticmethod
-    def split(py_stream, java_stream):
+    @classmethod
+    def split(cls, py_stream, java_stream):
         return TeeStream(
-            py_stream,
-            True,
-            lambda t: java_stream.write(bytes(t, py_stream.encoding)),
-            lambda: java_stream.flush(),
-            lambda: java_stream.close()
+            orig_stream=py_stream,
+            should_write_to_orig_stream=True,
+            write_func=lambda t: java_stream.write(bytes(t, py_stream.encoding)),
+            flush_func=lambda: java_stream.flush(),
+            close_func=lambda: java_stream.close()
         )
 
-    @staticmethod
-    def redirect(py_stream, java_stream):
+    @classmethod
+    def redirect(cls, py_stream, java_stream):
         if hasattr(py_stream, "encoding"):
             encoding = py_stream.encoding
         else:
             encoding = 'UTF-8'
         return TeeStream(
-            py_stream,
-            True,
-            lambda t: java_stream.write(bytes(t, encoding)),
-            lambda: java_stream.flush(),
-            lambda: java_stream.close()
+            orig_stream=py_stream,
+            should_write_to_orig_stream=False,
+            write_func=lambda t: java_stream.write(bytes(t, encoding)),
+            flush_func=lambda: java_stream.flush(),
+            close_func=lambda: java_stream.close()
         )
 
     def __init__(self, orig_stream, should_write_to_orig_stream, write_func, flush_func, close_func):
@@ -57,10 +54,6 @@ class TeeStream(io.TextIOBase):
         self.flush_func = flush_func
         self.close_func = close_func
 
-        if hasattr(orig_stream, 'encoding') and orig_stream.encoding is not None:
-            # Read this in the constructor, since encoding is read as an attr, not a method
-            self.encoding = orig_stream.encoding
-
     def isatty(self):
         if not hasattr(self._stream, "isatty"):
             return False
@@ -70,6 +63,13 @@ class TeeStream(io.TextIOBase):
         if not hasattr(self._stream, "fileno"):
             return None
         return self._stream.fileno()
+
+    @property
+    def encoding(self):
+        if hasattr(self._stream, 'encoding') and self._stream.encoding is not None:
+            return self._stream.encoding
+        else:
+            return 'UTF-8'
 
     def write(self, string):
         self.write_func(string)
@@ -82,8 +82,7 @@ class TeeStream(io.TextIOBase):
             self._stream.flush()
 
     def close(self):
+        super().close()
         self.close_func()
-        self.closed = True
         if self.should_write_to_orig_stream:
             self._stream.close()
-
