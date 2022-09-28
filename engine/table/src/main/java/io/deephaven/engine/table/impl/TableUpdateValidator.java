d379f90b2ec540692a281ec2d4f7abe3eafd5c9f
Ryan Caudy <rcaudy@gmail.com>
2022-09-28 17:53:06 -0400

    QueryTable.snapshot fix for modification propagation and row set cleanup. TableUpdateValidator enhancement. Port some DHE fixes. (#2921)
    * QueryTable.snapshot: Fix modification propagation (now always uses remove + add in shift-aware style)
    * QueryTable.snapshot: Avoid RowSet resource leaks when not notifying
    * TableUpdateValidator: Verify modified RowSet does not overlap added or removed
    * Port DH-11935: ModifiedColumnSet incorrectly propagates ALL to ALL
    * Port DH-12666: Remove unnecessary selectDistinct from whereIn and whereNotIn for Static Tables

--- engine/table/src/main/java/io/deephaven/engine/table/impl/TableUpdateValidator.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/TableUpdateValidator.java
@@ -195,6 +195,14 @@ public class TableUpdateValidator implements QueryTable.Operation {
 
             // modified
             updateValues(upstream.modifiedColumnSet(), upstream.modified(), false);
+            if (upstream.added().overlaps(upstream.modified())) {
+                noteIssue(() -> "added contains rows that are modified (post-shift): "
+                        + upstream.added().intersect(upstream.modified()));
+            }
+            if (upstream.removed().overlaps(upstream.getModifiedPreShift())) {
+                noteIssue(() -> "removed contains rows that are modified (pre-shift): "
+                        + upstream.removed().intersect(upstream.getModifiedPreShift()));
+            }
 
             if (!issues.isEmpty()) {
                 StringBuilder result =
