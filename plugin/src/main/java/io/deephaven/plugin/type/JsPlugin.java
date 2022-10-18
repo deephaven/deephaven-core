355e6f3d8a56bd7a804133bf30c5fc8dcf7683c7
Devin Smith <devinsmith@deephaven.io>
2022-10-18 10:53:38 -0700

    add link to js repo

--- plugin/src/main/java/io/deephaven/plugin/type/JsPlugin.java
+++ plugin/src/main/java/io/deephaven/plugin/type/JsPlugin.java
@@ -10,6 +10,8 @@ import java.nio.file.Path;
 
 /**
  * A js plugin. Useful for adding custom JS code to the server that can be passed to the web client.
+ *
+ * @see <a href="https://github.com/deephaven/js-plugin-template">js-plugin-template</a>
  */
 public interface JsPlugin extends Plugin {
 
