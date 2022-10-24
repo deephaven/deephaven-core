f138e1240072220d66b20673ce3b76145eff68eb
Devin Smith <devinsmith@deephaven.io>
2022-10-24 07:57:16 -0700

    SystemClockMillis

---
+++ Base/src/main/java/io/deephaven/base/clock/SystemClockMillis.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2016-2022 Deephaven Data Labs and Patent Pending
+ */
+package io.deephaven.base.clock;
+
+import java.time.Instant;
+
+/**
+ * A clock based off of {@link System#currentTimeMillis()}.
+ *
+ * <p>
+ * This clock differs from {@link SystemClock} in that all timestamp methods have millisecond-level resolution.
+ */
+public enum SystemClockMillis implements Clock {
+    INSTANCE;
+
+    /**
+     * Equivalent to {@link System#currentTimeMillis()}.
+     */
+    @Override
+    public long currentTimeMillis() {
+        return System.currentTimeMillis();
+    }
+
+    /**
+     * Equivalent to {@code System.currentTimeMillis() * 1_000}.
+     */
+    @Override
+    public long currentTimeMicros() {
+        return System.currentTimeMillis() * 1_000;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * Calculates the time from {@link java.time.Clock#systemUTC()}. If you don't need nanosecond resolution, prefer
+     * {@link #currentTimeMillis()}.
+     *
+     * <p>
+     * Note: this method may allocate.
+     */
+    @Override
+    public long currentTimeNanos() {
+        return System.currentTimeMillis() * 1_000_000;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * Equivalent to {@code Instant.ofEpochMilli(System.currentTimeMillis())}.
+     */
+    @Override
+    public Instant instantNanos() {
+        return Instant.ofEpochMilli(System.currentTimeMillis());
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * Equivalent to {@code Instant.ofEpochMilli(System.currentTimeMillis())}.
+     */
+    @Override
+    public Instant instantMillis() {
+        return Instant.ofEpochMilli(System.currentTimeMillis());
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * Equivalent to {@link System#nanoTime()}.
+     */
+    @Override
+    public long nanoTime() {
+        return System.nanoTime();
+    }
+}
