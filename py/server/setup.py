82510d0361a11b33c1bfe806f6fe5abd675de0ef
Devin Smith <devinsmith@deephaven.io>
2022-11-05 14:59:04 -0700

    Bump base images to jpy 0.13.0

--- py/server/setup.py
+++ py/server/setup.py
@@ -47,12 +47,13 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     keywords='Deephaven Development',
     python_requires='>=3.7',
     install_requires=[
-        'jpy>=0.12.0',
+        'jpy>=0.13.0',
         'deephaven-plugin',
         'numpy',
         'pandas',
