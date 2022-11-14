74ee14043a8297a5db024ba881e114bf5947e4f8
rbasralian <rbasralian@deephaven.io>
2022-11-14 11:23:21 -0500

    support for ingesting from InputStreams instead of strings. comments. fix support for null/missing subtable data.

--- extensions/json/src/main/java/io/deephaven/jsoningester/StreamJsonMessage.java
+++ extensions/json/src/main/java/io/deephaven/jsoningester/StreamJsonMessage.java
@@ -18,16 +18,16 @@ public class StreamJsonMessage extends BaseMessageMetadata implements JsonMessag
     /**
      * Create a new instance of this class.
      *
-     * @param sentTime The time (if available) when this message was sent
-     * @param receiveTime The time (reported by subscriber) when this message was received.
-     * @param ingestTime The time when this message was finished processing by its ingester and was ready to be flushed.
-     * @param messageId The unique, monotonically-increasing ID for this message.
+     * @param sentTime      The time (if available) when this message was sent
+     * @param receiveTime   The time (reported by subscriber) when this message was received.
+     * @param ingestTime    The time when this message was finished processing by its ingester and was ready to be flushed.
+     * @param messageId     The unique, monotonically-increasing ID for this message.
      * @param messageNumber The sequential number indicating the sequence this message was received in by the ingester.
-     * @param inputStream The stream containing the message body.
-     * @param afterParse Runnable to run after closing the input stream (e.g. to close an http response)
+     * @param inputStream   The stream containing the message body.
+     * @param afterParse    Runnable to run after closing the input stream (e.g. to close an http response)
      */
     public StreamJsonMessage(DateTime sentTime, DateTime receiveTime, DateTime ingestTime, String messageId,
-            long messageNumber, InputStream inputStream, Runnable afterParse) {
+                             long messageNumber, InputStream inputStream, Runnable afterParse) {
         super(sentTime, receiveTime, ingestTime, messageId, messageNumber);
         this.inputStream = inputStream;
         this.afterParse = afterParse;
