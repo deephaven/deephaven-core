326bee0f43bbc03381db868bc7b2b7f05aa7b1c2
Ryan Caudy <rcaudy@gmail.com>
2022-11-21 18:24:08 -0500

    State work for snapshots.

--- engine/api/src/main/java/io/deephaven/engine/table/hierarchical/HierarchicalTable.java
+++ engine/api/src/main/java/io/deephaven/engine/table/hierarchical/HierarchicalTable.java
@@ -1,13 +1,13 @@
 package io.deephaven.engine.table.hierarchical;
 
+import io.deephaven.api.ColumnName;
 import io.deephaven.chunk.WritableChunk;
 import io.deephaven.chunk.attributes.Values;
-import io.deephaven.engine.liveness.LivenessManager;
 import io.deephaven.engine.rowset.RowSequence;
 import io.deephaven.engine.table.AttributeMap;
-import io.deephaven.engine.table.Context;
 import io.deephaven.engine.table.GridAttributes;
 import io.deephaven.engine.table.Table;
+import io.deephaven.util.SafeCloseable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -33,11 +33,22 @@ public interface HierarchicalTable<IFACE_TYPE extends HierarchicalTable<IFACE_TY
      */
     Table getRoot();
 
-    Context makeSnapshotContext(@NotNull Table keyTable, @Nullable LivenessManager manager);
+    /**
+     * Opaque interface for objects used to cache snapshot state across multiple invocations of
+     * {@link #fillSnapshotChunks(SnapshotState, Table, BitSet, RowSequence, WritableChunk[])}.
+     */
+    interface SnapshotState extends SafeCloseable {
+    }
+
+    /**
+     * Make a re-usable snapshot state.
+     */
+    SnapshotState makeSnapshotState();
 
     /**
      * Populate data chunks for a snapshot of this HierarchicalTable.
      *
+     * @param snapshotState Snapshot state object used to cache data across invocations
      * @param keyTable Type-specific "key" table specifying expanded and contracted nodes
      * @param columns Optional bit-set of columns to include, {@code null} to include all columns
      * @param rows Position-space rows to include from the expanded data specified by {@code keyTable}
@@ -45,7 +56,9 @@ public interface HierarchicalTable<IFACE_TYPE extends HierarchicalTable<IFACE_TY
      * @return The total expanded data size or an estimate thereof
      */
     long fillSnapshotChunks(
+            @NotNull SnapshotState snapshotState,
             @NotNull Table keyTable,
+            @NotNull ColumnName keyTableExpandDescendantsColumn,
             @Nullable BitSet columns,
             @NotNull RowSequence rows,
             @NotNull WritableChunk<? extends Values>[] destinations);
--- engine/table/src/main/java/io/deephaven/engine/table/impl/hierarchical/ClientView.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/hierarchical/ClientView.java
@@ -7,12 +7,16 @@ import io.deephaven.engine.liveness.LivenessArtifact;
 import io.deephaven.engine.table.Table;
 import io.deephaven.engine.table.hierarchical.HierarchicalTable;
 import io.deephaven.engine.table.impl.MemoizedOperationKey;
+import io.deephaven.engine.table.impl.SortOperation;
 import io.deephaven.engine.table.impl.select.WhereFilter;
+import io.deephaven.engine.table.impl.sources.RedirectedColumnSource;
+import io.deephaven.engine.table.impl.util.RowRedirection;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.ref.WeakReference;
 import java.util.Collection;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 /**
  * Immutable client "view" state for a {@link HierarchicalTableImpl}.
@@ -22,8 +26,8 @@ public final class ClientView<IFACE_TYPE extends HierarchicalTable<IFACE_TYPE>, 
         implements MemoizedOperationKey.Provider {
 
     /**
-     * The "base" HierarchicalTableImpl for this instance, which may be {@code HierarchicalTableImpl.this}.
-     * Client operations should be applied to the base in order to create new results.
+     * The "base" HierarchicalTableImpl for this instance, which may be {@code HierarchicalTableImpl.this}. Client
+     * operations should be applied to the base in order to create new results.
      */
     private final IMPL_TYPE base;
 
@@ -38,8 +42,8 @@ public final class ClientView<IFACE_TYPE extends HierarchicalTable<IFACE_TYPE>, 
     private final Table keyTable;
 
     /**
-     * Name for an optional column of booleans that specify whether descendants of an expanded node should be
-     * expanded by default instead of the typical contracted by default.
+     * Name for an optional column of booleans that specify whether descendants of an expanded node should be expanded
+     * by default instead of the typical contracted by default.
      */
     private final ColumnName keyTableExpandDescendantsColumn;
 
@@ -69,8 +73,8 @@ public final class ClientView<IFACE_TYPE extends HierarchicalTable<IFACE_TYPE>, 
         this.effective = effective;
         this.keyTable = keyTable;
         this.keyTableExpandDescendantsColumn = keyTableExpandDescendantsColumn;
-        final Class<?> expandDescendantsDataType = keyTableExpandDescendantsColumn == null ? null :
-                keyTable.getDefinition().<Boolean>getColumn(keyTableExpandDescendantsColumn.name()).getDataType();
+        final Class<?> expandDescendantsDataType = keyTableExpandDescendantsColumn == null ? null
+                : keyTable.getDefinition().<Boolean>getColumn(keyTableExpandDescendantsColumn.name()).getDataType();
         if (expandDescendantsDataType != null && expandDescendantsDataType != Boolean.class) {
             throw new IllegalArgumentException("Invalid data type " + expandDescendantsDataType
                     + " for expand descendants column " + keyTableExpandDescendantsColumn.name()
@@ -88,27 +92,6 @@ public final class ClientView<IFACE_TYPE extends HierarchicalTable<IFACE_TYPE>, 
         }
     }
 
-    private static final class NodeState {
-
-        private final long id;
-        private final Table processed;
-        private final Object lookup;
-
-        private NodeState(final long id, final Table processed, final Object lookup) {
-            this.id = id;
-            this.processed = processed;
-            this.lookup = lookup;
-            if (processed.isRefreshing()) {
-                processed.retainReference();
-            }
-        }
-
-        private void release() {
-            if (processed.isRefreshing()) {
-                processed.dropReference();
-            }
-        }
-    }
 
     @Override
     public MemoizedOperationKey getMemoKey() {
