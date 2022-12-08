9fd1a57c0aa986eb39fdf15f8376ba7e2d4acd18
Colin Alworth <colinalworth@deephaven.io>
2022-12-08 15:53:58 -0600

    Attempt at correctly setting up debugging in current thread

--- py/server/deephaven_internal/java_threads.py
+++ py/server/deephaven_internal/java_threads.py
@@ -12,21 +12,22 @@ def create_thread_entry(thread_name):
     # Assign the java thread name to the python thread
     thread.name = 'java-' + thread_name
     # Then, if pydevd has already been initialized, we should attempt to make ourselves known to it.
-    try:
-        # Test each of our known
-        for name in ['pydevd', 'pydevd_pycharm']:
-            debugger = __import__(name)
-
-            # We don't want to be the first one to call settrace(), so check to see if setup completed on another
-            # thread before attempting it here
-            if debugger.SetupHolder.setup is not None:
-                debugger.settrace(suspend=False)
-    except ImportError:
-        # Debugger hasn't started yet (or we don't know which one is in use), so registering our thread
-        # above should be sufficient
-        pass
 
     # Return a def to Java with a particular name that will call back into the Java stack
     def JavaThread(runnable):
+        try:
+            # Test each of our known debugger impls
+            for name in ['pydevd', 'pydevd_pycharm']:
+                debugger = __import__(name)
+
+                # We don't want to be the first one to call settrace(), so check to see if setup completed on another
+                # thread before attempting it here
+                if debugger.SetupHolder.setup is not None:
+                    debugger.settrace(suspend=False)
+        except ImportError:
+        # Debugger hasn't started yet (or we don't know which one is in use), so registering our thread
+        # above should be sufficient
+          pass
+
         runnable.run()
     return JavaThread
