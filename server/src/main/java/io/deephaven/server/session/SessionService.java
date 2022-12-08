3672c0096e9c97afb1e0f5aeb6177efc446b94dc
Colin Alworth <colinalworth@deephaven.io>
2022-12-08 13:35:07 -0600

    Shut down LogBuffer listeners when the client stream has closed

--- server/src/main/java/io/deephaven/server/session/SessionService.java
+++ server/src/main/java/io/deephaven/server/session/SessionService.java
@@ -380,7 +380,7 @@ public class SessionService {
         }
 
         @Override
-        void onClose() {
+        public void onClose() {
             GrpcUtil.safelyExecuteLocked(responseObserver, () -> {
                 responseObserver.onError(GrpcUtil.statusRuntimeException(Code.UNAUTHENTICATED, "Session has ended"));
             });
