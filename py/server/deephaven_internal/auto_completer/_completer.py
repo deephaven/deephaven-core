17033c023f3cb7838c678f0e67aaf0fa1ff0f296
Devin Smith <devinsmith@deephaven.io>
2022-12-12 10:17:42 -0800

    Fix jedi SAFE mode (#3176)
    Fixes #3177

--- py/server/deephaven_internal/auto_completer/_completer.py
+++ py/server/deephaven_internal/auto_completer/_completer.py
@@ -79,11 +79,15 @@ class Completer(object):
 
         # run jedi
         txt = self.get_doc(uri)
-        # The Script completer is static analysis only, so we should actually be feeding it a whole document at once.
 
-        completer = Script if self.__mode == Mode.SAFE else Interpreter
+        completer = (
+            # The Script completer is static analysis only, so we should actually be feeding it a whole document at once.
+            Script(txt)
+            if self.__mode == Mode.SAFE
+            else Interpreter(txt, [self.__scope])
+        )
+        completions = completer.complete(line, col)
 
-        completions = completer(txt, [self.__scope]).complete(line, col)
         # for now, a simple sorting based on number of preceding _
         # we may want to apply additional sorting to each list before combining
         results: list = []
