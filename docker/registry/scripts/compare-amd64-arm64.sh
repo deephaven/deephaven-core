db0de5b58006723ebc2e57be94cefacedbb78af5
Devin Smith <devinsmith@deephaven.io>
2023-01-19 15:03:26 -0800

    Remove tensorflow-base, server-tensorflow-netty

--- docker/registry/scripts/compare-amd64-arm64.sh
+++ docker/registry/scripts/compare-amd64-arm64.sh
@@ -12,12 +12,7 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __gradlew="${__dir}/../../../gradlew"
 
 server_base_image_id="$(${__gradlew} -q docker-server-base:showImageId)"
-tensorflow_base_image_id="$(${__gradlew} -q docker-tensorflow-base:showImageId)"
 
 podman run -q --rm --override-arch arm64 "${server_base_image_id}" pip freeze > build/server-base.arm64.txt
 podman run -q --rm --override-arch amd64 "${server_base_image_id}" pip freeze > build/server-base.amd64.txt
 diff -q build/server-base.amd64.txt build/server-base.arm64.txt
-
-podman run -q --rm --override-arch arm64 "${tensorflow_base_image_id}" pip freeze > build/tensorflow-base.arm64.txt
-podman run -q --rm --override-arch amd64 "${tensorflow_base_image_id}" pip freeze > build/tensorflow-base.amd64.txt
-diff -q build/tensorflow-base.amd64.txt build/tensorflow-base.arm64.txt
