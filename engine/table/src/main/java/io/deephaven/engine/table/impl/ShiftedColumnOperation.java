f3fcb3e14ecc73839b1c73995a05169a9af46c6a
Nathaniel Bauernfeind <natebauernfeind@deephaven.io>
2023-01-20 22:05:19 -0700

    rewrite shifted column operation logic

--- engine/table/src/main/java/io/deephaven/engine/table/impl/ShiftedColumnOperation.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/ShiftedColumnOperation.java
@@ -3,10 +3,10 @@
  */
 package io.deephaven.engine.table.impl;
 
+import io.deephaven.base.verify.Assert;
 import io.deephaven.engine.rowset.RowSet;
-import io.deephaven.engine.rowset.RowSetBuilderRandom;
+import io.deephaven.engine.rowset.RowSetBuilderSequential;
 import io.deephaven.engine.rowset.RowSetFactory;
-import io.deephaven.engine.rowset.TrackingRowSet;
 import io.deephaven.engine.rowset.WritableRowSet;
 import io.deephaven.engine.table.ColumnSource;
 import io.deephaven.engine.table.MatchPair;
@@ -16,6 +16,7 @@ import io.deephaven.engine.table.TableUpdate;
 import io.deephaven.engine.table.impl.perf.QueryPerformanceRecorder;
 import io.deephaven.engine.table.impl.select.MatchPairFactory;
 import io.deephaven.engine.table.impl.sources.ShiftedColumnSource;
+import org.apache.commons.lang3.mutable.MutableLong;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
@@ -82,21 +83,23 @@ public class ShiftedColumnOperation {
      * additional shifted column(s) that is based on the given source column(s) and the shift.
      *
      * @param source the source table, used to create new table with the shifted column
-     * @param shift the constant shift value
+     * @param shift the constant shift value, must be non-zero
      * @param pairs the source and shifted column pair(s) as shifted=source match pairs
      * @return a new Table that has all columns from input table plus additional shifted column(s) that are created
      *         using the shift from their source column(s)
      */
     @NotNull
     private static Table getShiftedColumns(@NotNull Table source, long shift, @NotNull MatchPair... pairs) {
+        Assert.neqZero(shift, "shift");
+
         final Map<String, ColumnSource<?>> columnSourceMap = new LinkedHashMap<>(source.getColumnSourceMap());
         final Map<String, Set<String>> sourceToShiftModColSetMap = new LinkedHashMap<>();
         final Set<String> sourceColumns = new LinkedHashSet<>();
         final Set<String> shiftedColumns = new LinkedHashSet<>();
 
         Arrays.stream(pairs).forEach(pair -> {
-            columnSourceMap.put(pair.leftColumn,
-                    new ShiftedColumnSource<>(source.getRowSet(), source.getColumnSource(pair.rightColumn), shift));
+            columnSourceMap.put(pair.leftColumn, new ShiftedColumnSource<>(
+                    source.getRowSet(), source.getColumnSource(pair.rightColumn), shift));
             Set<String> set = sourceToShiftModColSetMap.computeIfAbsent(pair.rightColumn, s -> new LinkedHashSet<>());
             set.add(pair.rightColumn);
             set.add(pair.leftColumn);
@@ -109,18 +112,19 @@ public class ShiftedColumnOperation {
 
         Arrays.stream(source.getDefinition().getColumnNamesArray()).forEach(colName -> {
             if (sourceToShiftModColSetMap.containsKey(colName)) {
-                resultTableMCSs.add(
-                        result.newModifiedColumnSet(sourceToShiftModColSetMap.get(colName).toArray(new String[0])));
+                resultTableMCSs.add(result.newModifiedColumnSet(
+                        sourceToShiftModColSetMap.get(colName).toArray(String[]::new)));
             } else {
+                // add identity mapping for all other columns
                 resultTableMCSs.add(result.newModifiedColumnSet(colName));
             }
         });
 
         final QueryTable sourceAsQueryTable = (QueryTable) source;
         final ModifiedColumnSet sourceColumnSet =
-                sourceAsQueryTable.newModifiedColumnSet(sourceColumns.toArray(new String[0]));
-        final ModifiedColumnSet resultColumnSet = result.newModifiedColumnSet(shiftedColumns.toArray(new String[0]));
-        final ModifiedColumnSet columnSetForDownstream = result.getModifiedColumnSetForUpdates();
+                sourceAsQueryTable.newModifiedColumnSet(sourceColumns.toArray(String[]::new));
+        final ModifiedColumnSet dirtyColumnSet = result.newModifiedColumnSet(shiftedColumns.toArray(String[]::new));
+        final ModifiedColumnSet downstreamColumnSet = result.getModifiedColumnSetForUpdates();
         final ModifiedColumnSet.Transformer mcsTransformer = sourceAsQueryTable.newModifiedColumnSetTransformer(
                 source.getDefinition().getColumnNamesArray(), resultTableMCSs.toArray(ModifiedColumnSet[]::new));
 
@@ -129,49 +133,82 @@ public class ShiftedColumnOperation {
                 @Override
                 public void onUpdate(TableUpdate upstream) {
                     final TableUpdateImpl downstream = TableUpdateImpl.copy(upstream);
-                    downstream.modifiedColumnSet = columnSetForDownstream;
+                    downstream.modifiedColumnSet = downstreamColumnSet;
                     mcsTransformer.clearAndTransform(
                             upstream.modifiedColumnSet(), downstream.modifiedColumnSet);
 
-                    boolean updateColSetDownStream = upstream.modifiedColumnSet().containsAll(sourceColumnSet);
+                    WritableRowSet modifiedRows = null;
+                    boolean dirtyModifiedColumnSet = false;
 
-                    final RowSetBuilderRandom redirectedKeysBuilder = RowSetFactory.builderRandom();
-                    final RowSetBuilderRandom previousRedirectedKeysBuilder = RowSetFactory.builderRandom();
-                    final TrackingRowSet index = source.getRowSet();
+                    if (upstream.removed().isNonempty()) {
+                        try (final RowSet prevRowSet = source.getRowSet().copyPrev();
+                                final WritableRowSet dirtyRowSet = computeDirtyModifiedRowSetInPositionSpace(
+                                        prevRowSet, upstream.removed(), shift)) {
+                            if (dirtyRowSet.isNonempty()) {
+                                modifiedRows = prevRowSet.subSetForPositions(dirtyRowSet);
+                                dirtyModifiedColumnSet = true;
 
-                    try (final RowSet prevIndex = index.copyPrev()) {
-                        if (upstream.removed().isNonempty()) {
-                            updateColSetDownStream = true;
-                            updateRedirectedKeys(prevIndex, upstream.removed(), shift, previousRedirectedKeysBuilder);
-                        }
-
-                        if (upstream.modified().isNonempty()
-                                && upstream.modifiedColumnSet().containsAny(sourceColumnSet)) {
-                            if (upstream.modifiedColumnSet().containsAll(sourceColumnSet)) {
-                                updateColSetDownStream = true;
+                                // move the dirty rows into current keyspace
+                                if (upstream.shifted().nonempty()) {
+                                    upstream.shifted().apply(modifiedRows);
+                                }
                             }
-                            updateRedirectedKeys(index, upstream.modified(), shift, redirectedKeysBuilder);
-                        }
-
-                        if (upstream.added().isNonempty()) {
-                            updateColSetDownStream = true;
-                            updateRedirectedKeys(index, upstream.added(), shift, redirectedKeysBuilder);
-                        }
-
-                        if (updateColSetDownStream) {
-                            columnSetForDownstream.setAll(resultColumnSet);
                         }
                     }
 
-                    WritableRowSet modified = redirectedKeysBuilder.build();
-                    downstream.modified = modified;
-                    modified.insert(upstream.modified());
-                    try (final WritableRowSet prevModified = previousRedirectedKeysBuilder.build()) {
-                        prevModified.remove(upstream.removed());
-                        upstream.shifted().apply(prevModified);
-                        modified.insert(prevModified);
+                    final boolean haveAdds = upstream.added().isNonempty();
+                    final boolean haveMods = upstream.modified().isNonempty()
+                            && upstream.modifiedColumnSet().containsAny(sourceColumnSet);
+
+                    // we'll combine adds and mods to coalesce ranges prior to subSetForPositions
+                    if (haveAdds || haveMods) {
+                        try (final WritableRowSet dirtyFromAdds = haveAdds ? computeDirtyModifiedRowSetInPositionSpace(
+                                source.getRowSet(), upstream.added(), shift) : null;
+                                final WritableRowSet dirtyFromMods =
+                                        haveMods ? computeDirtyModifiedRowSetInPositionSpace(
+                                                source.getRowSet(), upstream.modified(), shift) : null) {
+
+                            if (haveAdds && dirtyFromAdds.isNonempty()) {
+                                dirtyModifiedColumnSet = true;
+                            }
+                            // note dirtyFromMods is propagated by the mcsTransformer
+
+                            final RowSet dirtyPositions = haveAdds ? dirtyFromAdds : dirtyFromMods;
+                            if (haveAdds && haveMods) {
+                                dirtyFromAdds.insert(dirtyFromMods);
+                            }
+
+                            if (dirtyPositions.isNonempty()) {
+                                final WritableRowSet dirtyRowSet =
+                                        source.getRowSet().subSetForPositions(dirtyPositions);
+
+                                if (haveAdds && haveMods) {
+                                    // modifications might dirty added rows
+                                    dirtyRowSet.remove(upstream.added());
+                                }
+
+                                if (dirtyRowSet.isEmpty()) {
+                                    dirtyRowSet.close();
+                                } else if (modifiedRows == null) {
+                                    modifiedRows = dirtyRowSet;
+                                } else {
+                                    modifiedRows.insert(dirtyRowSet);
+                                    dirtyRowSet.close();
+                                }
+                            }
+                        }
                     }
-                    modified.remove(upstream.added());
+
+                    if (dirtyModifiedColumnSet) {
+                        downstreamColumnSet.setAll(dirtyColumnSet);
+                    }
+
+                    if (modifiedRows != null) {
+                        // close the copied upstream set
+                        downstream.modified.close();
+                        modifiedRows.insert(upstream.modified());
+                        downstream.modified = modifiedRows;
+                    } // else we can use the existing upstream modified row set
 
                     result.notifyListeners(downstream);
                 }
@@ -182,43 +219,56 @@ public class ShiftedColumnOperation {
     }
 
     /**
-     * The method invokes an inPlaceShift of 1 or -1 repeatedly until all redirected keys are collected in to the passed
-     * in builder.
+     * Computes the modified rowSet based on the sourceRowSet and the updatedRowSet. The provided rowSets must be in the
+     * same keyspace (previous, or current). The resulting rowSet is in position space and will not include any rows
+     * that are in the updatedRowSet.
      *
-     * @param rowSet Index to compute modified keys, prevIndex expected in case of removed and currentIndex otherwise
-     * @param upstreamIndex the upstream rowSet (one of upstream.removed, upstream.modified or upstream.added depending
-     *        on call)
-     * @param shift the constant shift value
-     * @param redirectedKeysBuilder the builder used to collect redirected keys
+     * @param sourceRowSet the source's row set
+     * @param updatedRowSet the updated row set (removed, updated, or modified)
+     * @param shift the shift value
+     * @return the effective modified row set in position space of the same keyspace (previous, or current)
      */
-    private static void updateRedirectedKeys(
-            @NotNull RowSet rowSet,
-            @NotNull RowSet upstreamIndex,
-            long shift,
-            @NotNull RowSetBuilderRandom redirectedKeysBuilder) {
-        final WritableRowSet posRowSet = rowSet.invert(upstreamIndex);
-        final long lastKeyPos = rowSet.size() - 1;
-        for (long i = 0; i < Math.abs(shift); ++i) {
-            if (shift < 0) {
-                // the last position rowSet is removed to avoid any overflow errors.
-                // In this case the shiftInPlace as of now handles the overflow without any errors
-                // however, having the check eliminates this implicit dependency
-                if (posRowSet.lastRowKey() == lastKeyPos) {
-                    posRowSet.remove(lastKeyPos);
+    private static WritableRowSet computeDirtyModifiedRowSetInPositionSpace(
+            @NotNull RowSet sourceRowSet, @NotNull RowSet updatedRowSet, long shift) {
+        final MutableLong minKeyAllowed = new MutableLong(0);
+        final RowSetBuilderSequential rowSetBuilder = RowSetFactory.builderSequential();
+
+        // when shift < 0, we need to look-ahead to avoid including rows that are part of the updatedRowSet
+        final MutableLong prevRangeEnd = new MutableLong(-1);
+        try (final WritableRowSet posRowSet = sourceRowSet.invert(updatedRowSet)) {
+            posRowSet.forAllRowKeyRanges((s, e) -> {
+                final long dirtyStart;
+                final long dirtyEnd;
+
+                if (shift > 0) {
+                    dirtyStart = Math.max(minKeyAllowed.longValue(), s - shift);
+                    dirtyEnd = s - 1;
+                    minKeyAllowed.setValue(e + 1);
+                } else {
+                    final long pEnd = prevRangeEnd.longValue();
+                    prevRangeEnd.setValue(e);
+                    if (pEnd == -1) {
+                        return;
+                    }
+
+                    dirtyStart = pEnd + 1;
+                    dirtyEnd = Math.min(s - 1, pEnd - shift);
                 }
-                posRowSet.shiftInPlace(1);
-            } else {
-                // inPlaceShift of -1 is not allowed on key 0
-                if (posRowSet.firstRowKey() == 0) {
-                    posRowSet.remove(0);
+
+                if (dirtyStart <= dirtyEnd) {
+                    rowSetBuilder.appendRange(dirtyStart, dirtyEnd);
                 }
-                posRowSet.shiftInPlace(-1);
-            }
-            if (posRowSet.isNonempty()) {
-                try (final RowSet keysModified = rowSet.subSetForPositions(posRowSet)) {
-                    redirectedKeysBuilder.addRowSet(keysModified);
-                }
-            }
+            });
         }
+
+        // if we were looking ahead, we need to process the last range
+        final long pEnd = prevRangeEnd.longValue();
+        if (shift < 0 && pEnd != -1 && pEnd + 1 < sourceRowSet.size()) {
+            final long dirtyStart = pEnd + 1;
+            final long dirtyEnd = pEnd - shift;
+            rowSetBuilder.appendRange(dirtyStart, dirtyEnd);
+        }
+
+        return rowSetBuilder.build();
     }
 }
