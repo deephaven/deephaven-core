3243767781ac1d1033aeebefe2f7bc529cdb6e59
jianfengmao <jianfengmao@deephaven.io>
2023-01-31 09:41:32 -0700

    Reduce the test data size in repo

--- py/server/tests/test_dbc.py
+++ py/server/tests/test_dbc.py
@@ -16,10 +16,9 @@ class DbcTestCase(BaseTestCase):
         connection_string = 'Driver={PostgreSQL};Server=postgres;Port=5432;Database=test;Uid=test;Pwd=test;'
         with turbodbc.connect(connection_string=connection_string) as conn:
             with conn.cursor() as cursor:
-                cursor.execute("SELECT t_ts, t_id, t_instrument, t_exchange, t_price, t_size FROM CRYPTO_TRADES"
-                               " LIMIT 1000")
+                cursor.execute("SELECT t_ts, t_id, t_instrument, t_exchange, t_price, t_size FROM CRYPTO_TRADES")
                 table = dhodbc.read_cursor(cursor)
-                self.assertEqual(table.size, 1000)
+                self.assertEqual(table.size, cursor.rowcount)
 
     def test_read_adbc(self):
         import adbc_driver_postgresql.dbapi
@@ -27,10 +26,11 @@ class DbcTestCase(BaseTestCase):
         uri = "postgresql://postgres:5432/test?user=test&password=test"
         with adbc_driver_postgresql.dbapi.connect(uri) as conn:
             with conn.cursor() as cursor:
-                cursor.execute("SELECT t_ts, t_exchange, t_price, t_size FROM CRYPTO_TRADES "
-                               "LIMIT 1000")
+                cursor.execute("SELECT t_ts, t_exchange, t_price, t_size FROM CRYPTO_TRADES LIMIT 10")
                 table = dhadbc.read_cursor(cursor)
-                self.assertEqual(table.size, 1000)
+                # This is not ideal but ADBC might have a bug regarding cursor.rowcount it currently returns -1
+                # instead of the actual size
+                self.assertEqual(table.size, 10)
 
 
 if __name__ == '__main__':
