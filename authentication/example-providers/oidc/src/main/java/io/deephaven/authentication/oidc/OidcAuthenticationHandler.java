d2c05dd34c7ef18b3459b8bd4d18c87577e5108a
Colin Alworth <colinalworth@deephaven.io>
2023-02-03 07:47:12 -0600

    Simple oidc+keycloak demo

--- authentication/example-providers/oidc/src/main/java/io/deephaven/authentication/oidc/OidcAuthenticationHandler.java
+++ authentication/example-providers/oidc/src/main/java/io/deephaven/authentication/oidc/OidcAuthenticationHandler.java
@@ -3,18 +3,28 @@ package io.deephaven.authentication.oidc;
 import io.deephaven.auth.AuthContext;
 import io.deephaven.auth.AuthenticationException;
 import io.deephaven.auth.AuthenticationRequestHandler;
+import org.pac4j.core.client.Client;
 import org.pac4j.core.config.Config;
+import org.pac4j.core.context.Cookie;
 import org.pac4j.core.context.WebContext;
 import org.pac4j.core.context.session.SessionStore;
+import org.pac4j.core.credentials.Credentials;
 import org.pac4j.core.engine.DefaultSecurityLogic;
+import org.pac4j.core.engine.SecurityGrantedAccessAdapter;
 import org.pac4j.core.profile.ProfileManager;
+import org.pac4j.core.profile.UserProfile;
 import org.pac4j.http.client.direct.HeaderClient;
 import org.pac4j.oidc.client.KeycloakOidcClient;
 import org.pac4j.oidc.config.KeycloakOidcConfiguration;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
+
+import static io.deephaven.authentication.oidc.FlightTokenClient.FLIGHT_TOKEN_ATTRIBUTE_NAME;
 
 /**
  * Functionally behaves as a pac4j DirectClient, but can lean on a profile creator as BearerClient does
@@ -41,14 +51,116 @@ public class OidcAuthenticationHandler implements AuthenticationRequestHandler {
         DefaultSecurityLogic logic = new DefaultSecurityLogic();
 
         logic.setProfileManagerFactory(DeephavenProfileManager::new);
-        WebContext context = null;
+        WebContext context = new WebContext() {
+            @Override
+            public Optional<String> getRequestParameter(String name) {
+                return Optional.empty();
+            }
+
+            @Override
+            public Map<String, String[]> getRequestParameters() {
+                return Collections.emptyMap();
+            }
+
+            @Override
+            public Optional getRequestAttribute(String name) {
+                if (name.equals(FLIGHT_TOKEN_ATTRIBUTE_NAME)) {
+                    return Optional.of(stringToken);
+                }
+                return Optional.empty();
+            }
+
+            @Override
+            public void setRequestAttribute(String name, Object value) {
+
+            }
+
+            @Override
+            public Optional<String> getRequestHeader(String name) {
+                return Optional.empty();
+            }
+
+            @Override
+            public String getRequestMethod() {
+                return null;
+            }
+
+            @Override
+            public String getRemoteAddr() {
+                return null;
+            }
+
+            @Override
+            public void setResponseHeader(String name, String value) {
+
+            }
+
+            @Override
+            public Optional<String> getResponseHeader(String name) {
+                return Optional.empty();
+            }
+
+            @Override
+            public void setResponseContentType(String content) {
+
+            }
+
+            @Override
+            public String getServerName() {
+                return null;
+            }
+
+            @Override
+            public int getServerPort() {
+                return 0;
+            }
+
+            @Override
+            public String getScheme() {
+                return null;
+            }
+
+            @Override
+            public boolean isSecure() {
+                return false;
+            }
+
+            @Override
+            public String getFullRequestURL() {
+                return null;
+            }
+
+            @Override
+            public Collection<Cookie> getRequestCookies() {
+                return Collections.emptySet();
+            }
+
+            @Override
+            public void addResponseCookie(Cookie cookie) {
+
+            }
+
+            @Override
+            public String getPath() {
+                return null;
+            }
+        };
         SessionStore sessionStore = null;
-        logic.perform(context, sessionStore, cfg, new AccessGrantedOutcome(), (action, context) -> null, "", "", "", false);
+//        logic.perform(context, sessionStore, cfg, new AccessGrantedOutcome(), (action, ctx) -> null, "", "", "", false);
 
 
-        cfg.getClients().getClients().get(0).getCredentials()
+        Client client = cfg.getClients().getClients().get(0);
+        return client.getCredentials(context, sessionStore)
+                .map(credentials -> client.getUserProfile(credentials, context, sessionStore))
+                .map(profile -> new AuthContext.SuperUser());
+    }
 
-        return Optional.empty();
+    public static class AccessGrantedOutcome implements SecurityGrantedAccessAdapter {
+
+        @Override
+        public Object adapt(WebContext context, SessionStore sessionStore, Collection<UserProfile> profiles, Object... parameters) throws Exception {
+            return null;
+        }
     }
 
     public static class DeephavenProfileManager extends ProfileManager {
@@ -63,7 +175,7 @@ public class OidcAuthenticationHandler implements AuthenticationRequestHandler {
     public void initialize(String targetUrl) {
         KeycloakOidcConfiguration config = new KeycloakOidcConfiguration();
         config.setClientId("app");
-        config.setRealm("deephaven_realm");
+        config.setRealm("deephaven_core");
         config.setBaseUri("http://localhost:6060"); // TODO parameterize this
         config.setScope("openid email profile");
 
@@ -73,10 +185,8 @@ public class OidcAuthenticationHandler implements AuthenticationRequestHandler {
         client.setCallbackUrl("unused");
         client.init();
 
-        //TODO probably will use a custom client here
-        HeaderClient bearerClient =
-                new HeaderClient("Authorization", getAuthType() + " ", client.getProfileCreator());
+        FlightTokenClient flightTokenClient = new FlightTokenClient(client.getProfileCreator());
 
-        cfg = new Config("/unused", bearerClient);
+        cfg = new Config("/unused", flightTokenClient);
     }
 }
