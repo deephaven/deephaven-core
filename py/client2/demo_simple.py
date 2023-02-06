5db374ad5194c0c543e1bb1bc8e7d47269e7ca5e
Corey Kosak <kosak@users.noreply.github.com>
2023-02-06 17:14:02 -0500

    Python client: rename identifiers to be Python style-compliant (#3411)

--- py/client2/demo_simple.py
+++ py/client2/demo_simple.py
@@ -1,6 +1,6 @@
 import deephaven_client as dh
 
 client = dh.Client.connect("localhost:10000")
-manager = client.getManager()
-handle = manager.emptyTable(10).update(["II = ii"])
+manager = client.get_manager()
+handle = manager.empty_table(10).update(["II = ii"])
 print(handle.toString(True))
