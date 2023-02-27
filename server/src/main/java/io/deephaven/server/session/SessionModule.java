5fa82030b59ea06ed4682eac3a42aff85bfb638e
Colin Alworth <colinalworth@deephaven.io>
2023-02-26 21:18:46 -0600

    Provide a session listener callback interface (#3460)
    Fixes #3457

--- server/src/main/java/io/deephaven/server/session/SessionModule.java
+++ server/src/main/java/io/deephaven/server/session/SessionModule.java
@@ -6,11 +6,15 @@ package io.deephaven.server.session;
 import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
+import dagger.multibindings.ElementsIntoSet;
 import dagger.multibindings.IntoSet;
 import io.deephaven.server.auth.AuthorizationProvider;
 import io.deephaven.server.util.AuthorizationWrappedGrpcBinding;
 import io.grpc.BindableService;
 import io.grpc.ServerInterceptor;
+
+import java.util.Collections;
+import java.util.Set;
 
 @Module
 public interface SessionModule {
@@ -30,4 +34,10 @@ public interface SessionModule {
     @Binds
     @IntoSet
     TicketResolver bindSessionTicketResolverServerSideExports(ExportTicketResolver resolver);
+
+    @Provides
+    @ElementsIntoSet
+    static Set<SessionListener> primeSessionListeners() {
+        return Collections.emptySet();
+    }
 }
