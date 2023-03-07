8f0bbee2f3cc40c26d7a4a97d5ebac43506be81e
Nate Bauernfeind <natebauernfeind@deephaven.io>
2023-03-07 10:31:14 -0700

    Restore  Which Was Removed by #3385 (#3497)

---
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/locations/impl/CompositeTableDataServiceConsistencyMonitor.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2016-2022 Deephaven Data Labs and Patent Pending
+ */
+package io.deephaven.engine.table.impl.locations.impl;
+
+import java.util.function.Supplier;
+
+/**
+ * A consistency monitor for use in the CompositeTableDataService.
+ */
+public class CompositeTableDataServiceConsistencyMonitor {
+    /**
+     * The default instance used by the CompositeTableDataServices.
+     */
+    static final FunctionConsistencyMonitor INSTANCE = new FunctionConsistencyMonitor();
+
+    public static class ConsistentSupplier<T> extends FunctionConsistencyMonitor.ConsistentSupplier<T> {
+        public ConsistentSupplier(Supplier<T> underlyingSupplier) {
+            super(CompositeTableDataServiceConsistencyMonitor.INSTANCE, underlyingSupplier);
+        }
+    }
+}
