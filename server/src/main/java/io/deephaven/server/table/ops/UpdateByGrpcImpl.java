3288e416c89e98faeed6c324bd52b2b48dd51095
Nathaniel Bauernfeind <natebauernfeind@deephaven.io>
2023-03-10 14:06:30 -0700

    gRPC UpdateBy: Open Shared Lock if Source is Refreshing

--- server/src/main/java/io/deephaven/server/table/ops/UpdateByGrpcImpl.java
+++ server/src/main/java/io/deephaven/server/table/ops/UpdateByGrpcImpl.java
@@ -13,6 +13,7 @@ import io.deephaven.api.updateby.spec.WindowScale;
 import io.deephaven.auth.codegen.impl.TableServiceContextualAuthWiring;
 import io.deephaven.base.verify.Assert;
 import io.deephaven.engine.table.Table;
+import io.deephaven.engine.updategraph.UpdateGraphProcessor;
 import io.deephaven.proto.backplane.grpc.BatchTableRequest;
 import io.deephaven.proto.backplane.grpc.UpdateByRequest;
 import io.deephaven.proto.backplane.grpc.UpdateByRequest.UpdateByOperation.UpdateByColumn;
@@ -76,6 +77,13 @@ public final class UpdateByGrpcImpl extends GrpcTableOperation<UpdateByRequest> 
                 request.getOperationsList().stream().map(UpdateByGrpcImpl::adaptOperation).collect(Collectors.toList());
         final List<ColumnName> groupByColumns =
                 request.getGroupByColumnsList().stream().map(ColumnName::of).collect(Collectors.toList());
+
+        if (parent.isRefreshing()) {
+            return UpdateGraphProcessor.DEFAULT.sharedLock().computeLocked(() -> control == null
+                    ? parent.updateBy(operations, groupByColumns)
+                    : parent.updateBy(control, operations, groupByColumns));
+        }
+
         return control == null ? parent.updateBy(operations, groupByColumns)
                 : parent.updateBy(control, operations, groupByColumns);
     }
