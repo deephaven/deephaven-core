5ca63e82ffea5559236545107ad111a9db9f0673
Devin Smith <devinsmith@deephaven.io>
2023-03-27 11:05:17 -0700

    Improve flat partitioned walking

--- extensions/parquet/table/src/main/java/io/deephaven/parquet/table/layout/ParquetFlatPartitionedLayout.java
+++ extensions/parquet/table/src/main/java/io/deephaven/parquet/table/layout/ParquetFlatPartitionedLayout.java
@@ -11,8 +11,9 @@ import org.jetbrains.annotations.NotNull;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.util.function.Consumer;
 
 import static io.deephaven.parquet.table.ParquetTableWriter.PARQUET_FILE_EXTENSION;
@@ -21,6 +22,15 @@ import static io.deephaven.parquet.table.ParquetTableWriter.PARQUET_FILE_EXTENSI
  * Parquet {@link TableLocationKeyFinder location finder} that will discover multiple files in a single directory.
  */
 public final class ParquetFlatPartitionedLayout implements TableLocationKeyFinder<ParquetTableLocationKey> {
+    private static final PathMatcher MATCHER;
+
+    static {
+        MATCHER = FileSystems.getDefault().getPathMatcher(String.format("glob:*%s", PARQUET_FILE_EXTENSION));
+    }
+
+    private static boolean fileNameMatches(Path path) {
+        return MATCHER.matches(path.getFileName());
+    }
 
     private final File tableRootDirectory;
 
@@ -37,8 +47,8 @@ public final class ParquetFlatPartitionedLayout implements TableLocationKeyFinde
 
     @Override
     public void findKeys(@NotNull final Consumer<ParquetTableLocationKey> locationKeyObserver) {
-        try (final DirectoryStream<Path> parquetFileStream =
-                Files.newDirectoryStream(tableRootDirectory.toPath(), "*" + PARQUET_FILE_EXTENSION)) {
+        try (final DirectoryStream<Path> parquetFileStream = FileSystems.getDefault().provider()
+                .newDirectoryStream(tableRootDirectory.toPath(), ParquetFlatPartitionedLayout::fileNameMatches)) {
             for (final Path parquetFilePath : parquetFileStream) {
                 locationKeyObserver.accept(new ParquetTableLocationKey(parquetFilePath.toFile(), 0, null));
             }
