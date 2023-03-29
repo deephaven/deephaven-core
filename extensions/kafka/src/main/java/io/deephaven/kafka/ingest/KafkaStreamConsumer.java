6c841edfc24698ed8e50ec9a3808b61f90907aa5
Cristian Ferretti <cristianferretti@deephaven.io>
2023-03-29 19:26:22 -0400

    Remove unused include.

--- extensions/kafka/src/main/java/io/deephaven/kafka/ingest/KafkaStreamConsumer.java
+++ extensions/kafka/src/main/java/io/deephaven/kafka/ingest/KafkaStreamConsumer.java
@@ -7,7 +7,6 @@ import io.deephaven.stream.StreamFailureConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 /**
  * Consumer for lists of ConsumerRecords coming from Kafka. The StreamFailureConsumer is extended so that we can report
