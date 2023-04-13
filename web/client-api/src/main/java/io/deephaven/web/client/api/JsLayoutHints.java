deephaven/deephaven-core

commit 069b5431e94aa3617437d115818d6e70efd63b9a

Add SearchDisplayMode layout hints (#3512)
- Ported over from Enterprise, where these are already implemented
- Use Enum for python API
- Added more tests to verify layout hints

web/client-api/src/main/java/io/deephaven/web/client/api/JsLayoutHints.java
@@ -65,6 +65,8 @@ public class JsLayoutHints {
     }
 
     private boolean savedLayoutsAllowed = true;
+
+    private String searchDisplayMode = JsLayoutHints.SEARCH_DISPLAY_DEFAULT;
     private String[] frontColumns;
     private String[] backColumns;
     private String[] hiddenColumns;
@@ -107,6 +109,11 @@ public class JsLayoutHints {
             frozenColumns = JsObject.freeze(freezeStr.split(","));
         }
 
+        final String searchableStr = options.get("searchable");
+        if (searchableStr != null && !searchableStr.isEmpty()) {
+            searchDisplayMode = searchableStr;
+        }
+
         final String groupsStr = options.get("columnGroups");
         if (groupsStr != null && !groupsStr.isEmpty()) {
             ColumnGroup[] groups =
@@ -119,12 +126,22 @@ public class JsLayoutHints {
         return this;
     }
 
+    @JsProperty(namespace = "dh.SearchDisplayMode")
+    public static final String SEARCH_DISPLAY_DEFAULT = "Default",
+            SEARCH_DISPLAY_HIDE = "Hide",
+            SEARCH_DISPLAY_SHOW = "Show";
+
     @JsProperty
     public boolean getAreSavedLayoutsAllowed() {
         return savedLayoutsAllowed;
     }
 
     @JsNullable
+    @JsProperty
+    public String getSearchDisplayMode() {
+        return searchDisplayMode;
+    }
+
     @JsProperty
     public String[] getFrontColumns() {
         return frontColumns;

engine/table/src/main/java/io/deephaven/engine/util/LayoutHintBuilder.java
@@ -33,6 +33,27 @@ public class LayoutHintBuilder {
     private Map<String, ColumnGroup> columnGroups;
 
     /**
+     * The display mode for the search bar.
+     */
+    public enum SearchDisplayModes {
+        /**
+         * Use the system default
+         */
+        Default,
+        /**
+         * Permit the search bar to be displayed, regardless of system settings
+         */
+        Show,
+        /**
+         * Hide the search bar, regardless of system settings.
+         */
+        Hide
+    }
+
+    private SearchDisplayModes searchDisplayMode = SearchDisplayModes.Default;
+
+
+    /**
      * Helper class to maintain sub-properties for auto filter columns
      */
     private static class AutoFilterData {
@@ -229,6 +250,11 @@ public class LayoutHintBuilder {
         if (groupableStr != null && !groupableStr.isEmpty()) {
             final String[] groupableColumns = groupableStr.split(",");
             lhb.groupableColumns(groupableColumns);
+        }
+
+        final String searchableStr = options.get("searchable");
+        if (searchableStr != null && !searchableStr.isEmpty()) {
+            lhb.setSearchBarAccess(SearchDisplayModes.valueOf(searchableStr));
         }
 
         final String columnGroupsStr = options.get("columnGroups");
@@ -555,6 +581,30 @@ public class LayoutHintBuilder {
         return this;
     }
 
+    /**
+     * Set the search bar to explicitly be accessible or inaccessible, or use system default.
+     *
+     * @param searchable The display mode to use
+     * @return This LayoutHintBuilder
+     */
+    @ScriptApi
+    public LayoutHintBuilder setSearchBarAccess(final SearchDisplayModes searchable) {
+        searchDisplayMode = searchable;
+        return this;
+    }
+
+    /**
+     * Set the search bar to explicitly be accessible or inaccessible, or use system default.
+     *
+     * @param searchable The display mode to use
+     * @return This LayoutHintBuilder
+     */
+    @ScriptApi
+    public LayoutHintBuilder setSearchBarAccess(final String searchable) {
+        searchDisplayMode = SearchDisplayModes.valueOf(searchable);
+        return this;
+    }
+
     // endregion
 
     /**
@@ -599,6 +649,10 @@ public class LayoutHintBuilder {
 
         if (groupableColumns != null && !groupableColumns.isEmpty()) {
             sb.append("groupable=").append(String.join(",", groupableColumns)).append(';');
+        }
+
+        if (searchDisplayMode != SearchDisplayModes.Default) {
+            sb.append("searchable=").append(searchDisplayMode.toString()).append(';');
         }
 
         if (columnGroups != null && !columnGroups.isEmpty()) {
@@ -710,6 +764,13 @@ public class LayoutHintBuilder {
      */
     public @NotNull Set<String> getGroupableColumns() {
         return groupableColumns == null ? Collections.emptySet() : Collections.unmodifiableSet(groupableColumns);
+    }
+
+    public @NotNull SearchDisplayModes getSearchDisplayMode() {
+        if (searchDisplayMode == null) {
+            searchDisplayMode = SearchDisplayModes.Default;
+        }
+        return searchDisplayMode;
     }
 
     /**

py/server/deephaven/table.py
@@ -41,6 +41,7 @@ _JFilterOr = jpy.get_type("io.deephaven.api.filter.FilterOr")
 _JPair = jpy.get_type("io.deephaven.api.agg.Pair")
 _JMatchPair = jpy.get_type("io.deephaven.engine.table.MatchPair")
 _JLayoutHintBuilder = jpy.get_type("io.deephaven.engine.util.LayoutHintBuilder")
+_JSearchDisplayMode = jpy.get_type("io.deephaven.engine.util.LayoutHintBuilder$SearchDisplayModes")
 _JSnapshotWhenOptions = jpy.get_type("io.deephaven.api.snapshot.SnapshotWhenOptions")
 
 # PartitionedTable
@@ -87,6 +88,14 @@ class NodeType(Enum):
     include_constituent=True. The constituent level is the lowest in a rollup table. These nodes have column names 
     and types from the source table of the RollupTable. """
 
+class SearchDisplayMode(Enum):
+    """An enum of search display modes for layout hints"""
+    DEFAULT = _JSearchDisplayMode.Default
+    """Use the system default. This may depend on your user and/or system settings."""
+    SHOW = _JSearchDisplayMode.Show
+    """Permit the search bar to be displayed, regardless of user or system settings."""
+    HIDE = _JSearchDisplayMode.Hide
+    """Hide the search bar, regardless of user or system settings."""
 
 class _FormatOperationsRecorder(Protocol):
     """A mixin for creating format operations to be applied to individual nodes of either RollupTable or TreeTable."""
@@ -1844,7 +1853,7 @@ class Table(JObjectWrapper):
 
     def layout_hints(self, front: Union[str, List[str]] = None, back: Union[str, List[str]] = None,
                      freeze: Union[str, List[str]] = None, hide: Union[str, List[str]] = None,
-                     column_groups: List[dict] = None) -> Table:
+                     column_groups: List[dict] = None, search_display_mode: SearchDisplayMode = None) -> Table:
         """ Sets layout hints on the Table
 
         Args:
@@ -1859,6 +1868,9 @@ class Table(JObjectWrapper):
                 name (str): The group name
                 children (List[str]): The
                 color (Optional[str]): The hex color string or Deephaven color name
+            search_display_mode (SearchDisplayMode): set the search bar to explicitly be accessible or inaccessible, or use the system default.
+                :attr:`SearchDisplayMode.Show` will show the search bar, :attr:`SearchDisplayMode.Hide` will hide the search bar, and
+                :attr:`SearchDisplayMode.Default` will use the default value configured by the user and system settings.
 
         Returns:
             a new table with the layout hints set
@@ -1885,6 +1897,10 @@ class Table(JObjectWrapper):
                 for group in column_groups:
                     _j_layout_hint_builder.columnGroup(group.get("name"), j_array_list(group.get("children")),
                                                        group.get("color", ""))
+
+            if search_display_mode is not None:
+                _j_layout_hint_builder.setSearchBarAccess(search_display_mode.value)
+
         except Exception as e:
             raise DHError(e, "failed to create layout hints") from e
 

py/server/tests/test_table.py
@@ -13,7 +13,7 @@ from deephaven.execution_context import make_user_exec_ctx
 from deephaven.html import to_html
 from deephaven.jcompat import j_hashmap
 from deephaven.pandas import to_pandas
-from deephaven.table import Table
+from deephaven.table import Table, SearchDisplayMode
 from tests.testbase import BaseTestCase
 
 
@@ -619,6 +619,12 @@ class TableTestCase(BaseTestCase):
         self.assertIsNotNone(t)
 
     def test_layout_hints(self):
+        def verify_layout_hint(t: Table, layout_hint_str: str):
+            attrs = self.test_table.attributes()
+            attrs["LayoutHints"] = layout_hint_str
+            self.assertIsNotNone(t)
+            self.assertEquals(attrs, t.attributes())
+
         t = self.test_table.layout_hints(front="d", back="b", freeze="c", hide="d", column_groups=[
             {
                 "name": "Group1",
@@ -635,19 +641,28 @@ class TableTestCase(BaseTestCase):
                 "color": "RED"
             }
         ])
-        self.assertIsNotNone(t)
+        verify_layout_hint(t, "front=d;back=b;hide=d;freeze=c;columnGroups=name:Group1::children:a,b|name:Group2::children:c,d::color:#123456|name:Group3::children:e,f::color:#ff0000;")
 
         t = self.test_table.layout_hints(front=["d", "e"], back=["a", "b"], freeze=["c"], hide=["d"])
-        self.assertIsNotNone(t)
+        verify_layout_hint(t, "front=d,e;back=a,b;hide=d;freeze=c;")
 
         t = self.test_table.layout_hints(front="e")
-        self.assertIsNotNone(t)
+        verify_layout_hint(t, "front=e;")
 
         t = self.test_table.layout_hints(front=["e"])
-        self.assertIsNotNone(t)
+        verify_layout_hint(t, "front=e;")
+
+        t = self.test_table.layout_hints(search_display_mode=SearchDisplayMode.SHOW)
+        verify_layout_hint(t, "searchable=Show;")
+
+        t = self.test_table.layout_hints(search_display_mode=SearchDisplayMode.HIDE)
+        verify_layout_hint(t, "searchable=Hide;")
+
+        t = self.test_table.layout_hints(search_display_mode=SearchDisplayMode.DEFAULT)
+        verify_layout_hint(t, "")
 
         t = self.test_table.layout_hints()
-        self.assertIsNotNone(t)
+        verify_layout_hint(t, "")
 
         with self.assertRaises(DHError) as cm:
             t = self.test_table.layout_hints(front=["e"], back=True)

commit b07fcb33f644752f52171ae78b9d9107dae6320e

Fix layouthints TS for SearchDisplayMode (#3691)
- Fixes error introduced by conflicts from #3512 and #3567 
- Move SearchDisplayMode to it's own class, specify a type for it (which just maps to `string`)
  - `string` literal types (e.g. `type SearchDisplayModeType = 'default' | 'hide' | 'show';`) are not possible yet with GWT, so it's just type `string` for now

web/client-api/src/main/java/io/deephaven/web/client/api/JsLayoutHints.java
@@ -5,6 +5,7 @@ package io.deephaven.web.client.api;
 
 import com.vertispan.tsdefs.annotations.TsInterface;
 import com.vertispan.tsdefs.annotations.TsName;
+import com.vertispan.tsdefs.annotations.TsTypeRef;
 import elemental2.core.JsObject;
 import jsinterop.annotations.JsIgnore;
 import jsinterop.annotations.JsNullable;
@@ -16,7 +17,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @TsInterface
-@JsType(name = "LayoutHints", namespace = "dh")
+@JsType(namespace = "dh", name = "LayoutHints")
 public class JsLayoutHints {
     @TsInterface
     @JsType(namespace = "dh")
@@ -66,7 +67,7 @@ public class JsLayoutHints {
 
     private boolean savedLayoutsAllowed = true;
 
-    private String searchDisplayMode = JsLayoutHints.SEARCH_DISPLAY_DEFAULT;
+    private String searchDisplayMode = SearchDisplayMode.SEARCH_DISPLAY_DEFAULT;
     private String[] frontColumns;
     private String[] backColumns;
     private String[] hiddenColumns;
@@ -126,11 +127,6 @@ public class JsLayoutHints {
         return this;
     }
 
-    @JsProperty(namespace = "dh.SearchDisplayMode")
-    public static final String SEARCH_DISPLAY_DEFAULT = "Default",
-            SEARCH_DISPLAY_HIDE = "Hide",
-            SEARCH_DISPLAY_SHOW = "Show";
-
     @JsProperty
     public boolean getAreSavedLayoutsAllowed() {
         return savedLayoutsAllowed;
@@ -138,6 +134,7 @@ public class JsLayoutHints {
 
     @JsNullable
     @JsProperty
+    @TsTypeRef(SearchDisplayMode.class)
     public String getSearchDisplayMode() {
         return searchDisplayMode;
     }

web/client-api/src/main/java/io/deephaven/web/client/api/JsTable.java
@@ -60,7 +60,6 @@ import io.deephaven.web.shared.fu.JsConsumer;
 import io.deephaven.web.shared.fu.JsProvider;
 import io.deephaven.web.shared.fu.JsRunnable;
 import io.deephaven.web.shared.fu.RemoverFn;
-import jsinterop.annotations.JsIgnore;
 import jsinterop.annotations.JsMethod;
 import jsinterop.annotations.JsNullable;
 import jsinterop.annotations.JsOptional;

web/client-api/src/main/java/io/deephaven/web/client/api/SearchDisplayMode.java
@@ -1,0 +1,13 @@
+package io.deephaven.web.client.api;
+
+import com.vertispan.tsdefs.annotations.TsTypeDef;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+
+@TsTypeDef(tsType = "string")
+@JsType(namespace = "dh")
+public class SearchDisplayMode {
+    public static final String SEARCH_DISPLAY_DEFAULT = "Default",
+            SEARCH_DISPLAY_HIDE = "Hide",
+            SEARCH_DISPLAY_SHOW = "Show";
+}