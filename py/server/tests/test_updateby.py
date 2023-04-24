7bdb6e5b352409540576f4bb7f754ce55b02c7a3
Larry Booker <larrybooker@deephaven.io>
2023-04-24 15:28:23 -0700

    Refactored python tests to use setUpClass & tearDownClass.

--- py/server/tests/test_updateby.py
+++ py/server/tests/test_updateby.py
@@ -27,36 +27,128 @@ class UpdateByTestCase(BaseTestCase):
         self.ticking_table = None
         super().tearDown()
 
-    em_op_ctrl = OperationControl(on_null=BadDataBehavior.THROW,
-                               on_nan=BadDataBehavior.RESET,
-                               big_value_context=MathContext.UNLIMITED)
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.em_op_ctrl = OperationControl(on_null=BadDataBehavior.THROW,
+                                      on_nan=BadDataBehavior.RESET,
+                                      big_value_context=MathContext.UNLIMITED)
 
-    em_ops = [
-        # exponential moving average
-        ema_tick_decay(time_scale_ticks=100, cols="ema_a = a"),
-        ema_tick_decay(time_scale_ticks=100, cols="ema_a = a", op_control=em_op_ctrl),
-        ema_time_decay(ts_col="Timestamp", time_scale=10, cols="ema_a = a"),
-        ema_time_decay(ts_col="Timestamp", time_scale="00:00:00.001", cols="ema_c = c",
-                       op_control=em_op_ctrl),
-        # exponential moving sum
-        ems_tick_decay(time_scale_ticks=100, cols="ema_a = a"),
-        ems_tick_decay(time_scale_ticks=100, cols="ema_a = a", op_control=em_op_ctrl),
-        ems_time_decay(ts_col="Timestamp", time_scale=10, cols="ema_a = a"),
-        ems_time_decay(ts_col="Timestamp", time_scale="00:00:00.001", cols="ema_c = c",
-                       op_control=em_op_ctrl),
-        # exponential moving minimum
-        emmin_tick_decay(time_scale_ticks=100, cols="ema_a = a"),
-        emmin_tick_decay(time_scale_ticks=100, cols="ema_a = a", op_control=em_op_ctrl),
-        emmin_time_decay(ts_col="Timestamp", time_scale=10, cols="ema_a = a"),
-        emmin_time_decay(ts_col="Timestamp", time_scale="00:00:00.001", cols="ema_c = c",
-                       op_control=em_op_ctrl),
-        # exponential moving maximum
-        emmax_tick_decay(time_scale_ticks=100, cols="ema_a = a"),
-        emmax_tick_decay(time_scale_ticks=100, cols="ema_a = a", op_control=em_op_ctrl),
-        emmax_time_decay(ts_col="Timestamp", time_scale=10, cols="ema_a = a"),
-        emmax_time_decay(ts_col="Timestamp", time_scale="00:00:00.001", cols="ema_c = c",
-                       op_control=em_op_ctrl),
-    ]
+        cls.em_ops = [
+            # exponential moving average
+            ema_tick_decay(time_scale_ticks=100, cols="ema_a = a"),
+            ema_tick_decay(time_scale_ticks=100, cols="ema_a = a", op_control=cls.em_op_ctrl),
+            ema_time_decay(ts_col="Timestamp", time_scale=10, cols="ema_a = a"),
+            ema_time_decay(ts_col="Timestamp", time_scale="00:00:00.001", cols="ema_c = c",
+                           op_control=cls.em_op_ctrl),
+            # exponential moving sum
+            ems_tick_decay(time_scale_ticks=100, cols="ema_a = a"),
+            ems_tick_decay(time_scale_ticks=100, cols="ema_a = a", op_control=cls.em_op_ctrl),
+            ems_time_decay(ts_col="Timestamp", time_scale=10, cols="ema_a = a"),
+            ems_time_decay(ts_col="Timestamp", time_scale="00:00:00.001", cols="ema_c = c",
+                           op_control=cls.em_op_ctrl),
+            # exponential moving minimum
+            emmin_tick_decay(time_scale_ticks=100, cols="ema_a = a"),
+            emmin_tick_decay(time_scale_ticks=100, cols="ema_a = a", op_control=cls.em_op_ctrl),
+            emmin_time_decay(ts_col="Timestamp", time_scale=10, cols="ema_a = a"),
+            emmin_time_decay(ts_col="Timestamp", time_scale="00:00:00.001", cols="ema_c = c",
+                             op_control=cls.em_op_ctrl),
+            # exponential moving maximum
+            emmax_tick_decay(time_scale_ticks=100, cols="ema_a = a"),
+            emmax_tick_decay(time_scale_ticks=100, cols="ema_a = a", op_control=cls.em_op_ctrl),
+            emmax_time_decay(ts_col="Timestamp", time_scale=10, cols="ema_a = a"),
+            emmax_time_decay(ts_col="Timestamp", time_scale="00:00:00.001", cols="ema_c = c",
+                             op_control=cls.em_op_ctrl),
+        ]
+
+        cls.simple_ops = [
+            cum_sum,
+            cum_prod,
+            cum_min,
+            cum_max,
+            forward_fill,
+            delta
+        ]
+
+        # Rolling Operators list shared with test_rolling_ops / test_rolling_ops_proxy
+        cls.rolling_ops = [
+            # rolling sum
+            rolling_sum_tick(cols=["rsum_a = a", "rsum_d = d"], rev_ticks=10),
+            rolling_sum_tick(cols=["rsum_a = a", "rsum_d = d"], rev_ticks=10, fwd_ticks=10),
+            rolling_sum_time(ts_col="Timestamp", cols=["rsum_b = b", "rsum_e = e"], rev_time="00:00:10"),
+            rolling_sum_time(ts_col="Timestamp", cols=["rsum_b = b", "rsum_e = e"], rev_time=10_000_000_000,
+                             fwd_time=-10_000_000_00),
+            rolling_sum_time(ts_col="Timestamp", cols=["rsum_b = b", "rsum_e = e"], rev_time="00:00:30",
+                             fwd_time="-00:00:20"),
+            # rolling group
+            rolling_group_tick(cols=["rgroup_a = a", "rgroup_d = d"], rev_ticks=10),
+            rolling_group_tick(cols=["rgroup_a = a", "rgroup_d = d"], rev_ticks=10, fwd_ticks=10),
+            rolling_group_time(ts_col="Timestamp", cols=["rgroup_b = b", "rgroup_e = e"], rev_time="00:00:10"),
+            rolling_group_time(ts_col="Timestamp", cols=["rgroup_b = b", "rgroup_e = e"], rev_time=10_000_000_000,
+                               fwd_time=-10_000_000_00),
+            rolling_group_time(ts_col="Timestamp", cols=["rgroup_b = b", "rgroup_e = e"], rev_time="00:00:30",
+                               fwd_time="-00:00:20"),
+            # rolling average
+            rolling_avg_tick(cols=["ravg_a = a", "ravg_d = d"], rev_ticks=10),
+            rolling_avg_tick(cols=["ravg_a = a", "ravg_d = d"], rev_ticks=10, fwd_ticks=10),
+            rolling_avg_time(ts_col="Timestamp", cols=["ravg_b = b", "ravg_e = e"], rev_time="00:00:10"),
+            rolling_avg_time(ts_col="Timestamp", cols=["ravg_b = b", "ravg_e = e"], rev_time=10_000_000_000,
+                             fwd_time=-10_000_000_00),
+            rolling_avg_time(ts_col="Timestamp", cols=["ravg_b = b", "ravg_e = e"], rev_time="00:00:30",
+                             fwd_time="-00:00:20"),
+            # rolling minimum
+            rolling_min_tick(cols=["rmin_a = a", "rmin_d = d"], rev_ticks=10),
+            rolling_min_tick(cols=["rmin_a = a", "rmin_d = d"], rev_ticks=10, fwd_ticks=10),
+            rolling_min_time(ts_col="Timestamp", cols=["rmin_b = b", "rmin_e = e"], rev_time="00:00:10"),
+            rolling_min_time(ts_col="Timestamp", cols=["rmin_b = b", "rmin_e = e"], rev_time=10_000_000_000,
+                             fwd_time=-10_000_000_00),
+            rolling_min_time(ts_col="Timestamp", cols=["rmin_b = b", "rmin_e = e"], rev_time="00:00:30",
+                             fwd_time="-00:00:20"),
+            # rolling maximum
+            rolling_max_tick(cols=["rmax_a = a", "rmax_d = d"], rev_ticks=10),
+            rolling_max_tick(cols=["rmax_a = a", "rmax_d = d"], rev_ticks=10, fwd_ticks=10),
+            rolling_max_time(ts_col="Timestamp", cols=["rmax_b = b", "rmax_e = e"], rev_time="00:00:10"),
+            rolling_max_time(ts_col="Timestamp", cols=["rmax_b = b", "rmax_e = e"], rev_time=10_000_000_000,
+                             fwd_time=-10_000_000_00),
+            rolling_max_time(ts_col="Timestamp", cols=["rmax_b = b", "rmax_e = e"], rev_time="00:00:30",
+                             fwd_time="-00:00:20"),
+            # rolling product
+            rolling_prod_tick(cols=["rprod_a = a", "rprod_d = d"], rev_ticks=10),
+            rolling_prod_tick(cols=["rprod_a = a", "rprod_d = d"], rev_ticks=10, fwd_ticks=10),
+            rolling_prod_time(ts_col="Timestamp", cols=["rprod_b = b", "rprod_e = e"], rev_time="00:00:10"),
+            rolling_prod_time(ts_col="Timestamp", cols=["rprod_b = b", "rprod_e = e"], rev_time=10_000_000_000,
+                              fwd_time=-10_000_000_00),
+            rolling_prod_time(ts_col="Timestamp", cols=["rprod_b = b", "rprod_e = e"], rev_time="00:00:30",
+                              fwd_time="-00:00:20"),
+            # rolling count
+            rolling_count_tick(cols=["rcount_a = a", "rcount_d = d"], rev_ticks=10),
+            rolling_count_tick(cols=["rcount_a = a", "rcount_d = d"], rev_ticks=10, fwd_ticks=10),
+            rolling_count_time(ts_col="Timestamp", cols=["rcount_b = b", "rcount_e = e"], rev_time="00:00:10"),
+            rolling_count_time(ts_col="Timestamp", cols=["rcount_b = b", "rcount_e = e"], rev_time=10_000_000_000,
+                               fwd_time=-10_000_000_00),
+            rolling_count_time(ts_col="Timestamp", cols=["rcount_b = b", "rcount_e = e"], rev_time="00:00:30",
+                               fwd_time="-00:00:20"),
+            # rolling standard deviation
+            rolling_std_tick(cols=["rstd_a = a", "rstd_d = d"], rev_ticks=10),
+            rolling_std_tick(cols=["rstd_a = a", "rstd_d = d"], rev_ticks=10, fwd_ticks=10),
+            rolling_std_time(ts_col="Timestamp", cols=["rstd_b = b", "rstd_e = e"], rev_time="00:00:10"),
+            rolling_std_time(ts_col="Timestamp", cols=["rstd_b = b", "rstd_e = e"], rev_time=10_000_000_000,
+                             fwd_time=-10_000_000_00),
+            rolling_std_time(ts_col="Timestamp", cols=["rstd_b = b", "rstd_e = e"], rev_time="00:00:30",
+                             fwd_time="-00:00:20"),
+            # rolling weighted average (using "b" as the weight column)
+            rolling_wavg_tick(weight_col="b", cols=["rwavg_a = a", "rwavg_d = d"], rev_ticks=10),
+            rolling_wavg_tick(weight_col="b", cols=["rwavg_a = a", "rwavg_d = d"], rev_ticks=10, fwd_ticks=10),
+            rolling_wavg_time(ts_col="Timestamp", weight_col="b", cols=["rwavg_b = b", "rwavg_e = e"], rev_time="00:00:10"),
+            rolling_wavg_time(ts_col="Timestamp", weight_col="b", cols=["rwavg_b = b", "rwavg_e = e"], rev_time=10_000_000_000, fwd_time=-10_000_000_00),
+            rolling_wavg_time(ts_col="Timestamp", weight_col="b", cols=["rwavg_b = b", "rwavg_e = e"], rev_time="00:00:30", fwd_time="-00:00:20"),
+        ]
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        del cls.em_op_ctrl
+        del cls.em_ops
+        del cls.simple_ops
+        del cls.rolling_ops
 
     def test_em(self):
         for op in self.em_ops:
@@ -82,15 +174,6 @@ class UpdateByTestCase(BaseTestCase):
                         self.assertEqual(len(rct.columns), 1 + len(ct.columns))
                         with ugp.exclusive_lock():
                             self.assertEqual(ct.size, rct.size)                        
-
-    simple_ops = [
-        cum_sum,
-        cum_prod,
-        cum_min,
-        cum_max,
-        forward_fill,
-        delta
-    ]
 
     def test_simple_ops(self):
         pairs = ["UA=a", "UB=b"]
@@ -122,88 +205,6 @@ class UpdateByTestCase(BaseTestCase):
                     for ct, rct in zip(pt_proxy.target.constituent_tables, rt_proxy.target.constituent_tables):
                         with ugp.exclusive_lock():
                             self.assertEqual(ct.size, rct.size)
-
-    # Rolling Operators list shared with test_rolling_ops / test_rolling_ops_proxy
-    rolling_ops = [
-        # rolling sum
-        rolling_sum_tick(cols=["rsum_a = a", "rsum_d = d"], rev_ticks=10),
-        rolling_sum_tick(cols=["rsum_a = a", "rsum_d = d"], rev_ticks=10, fwd_ticks=10),
-        rolling_sum_time(ts_col="Timestamp", cols=["rsum_b = b", "rsum_e = e"], rev_time="00:00:10"),
-        rolling_sum_time(ts_col="Timestamp", cols=["rsum_b = b", "rsum_e = e"], rev_time=10_000_000_000,
-                         fwd_time=-10_000_000_00),
-        rolling_sum_time(ts_col="Timestamp", cols=["rsum_b = b", "rsum_e = e"], rev_time="00:00:30",
-                         fwd_time="-00:00:20"),
-
-        # rolling group
-        rolling_group_tick(cols=["rgroup_a = a", "rgroup_d = d"], rev_ticks=10),
-        rolling_group_tick(cols=["rgroup_a = a", "rgroup_d = d"], rev_ticks=10, fwd_ticks=10),
-        rolling_group_time(ts_col="Timestamp", cols=["rgroup_b = b", "rgroup_e = e"], rev_time="00:00:10"),
-        rolling_group_time(ts_col="Timestamp", cols=["rgroup_b = b", "rgroup_e = e"], rev_time=10_000_000_000,
-                           fwd_time=-10_000_000_00),
-        rolling_group_time(ts_col="Timestamp", cols=["rgroup_b = b", "rgroup_e = e"], rev_time="00:00:30",
-                           fwd_time="-00:00:20"),
-
-        # rolling average
-        rolling_avg_tick(cols=["ravg_a = a", "ravg_d = d"], rev_ticks=10),
-        rolling_avg_tick(cols=["ravg_a = a", "ravg_d = d"], rev_ticks=10, fwd_ticks=10),
-        rolling_avg_time(ts_col="Timestamp", cols=["ravg_b = b", "ravg_e = e"], rev_time="00:00:10"),
-        rolling_avg_time(ts_col="Timestamp", cols=["ravg_b = b", "ravg_e = e"], rev_time=10_000_000_000,
-                         fwd_time=-10_000_000_00),
-        rolling_avg_time(ts_col="Timestamp", cols=["ravg_b = b", "ravg_e = e"], rev_time="00:00:30",
-                         fwd_time="-00:00:20"),
-
-        # rolling minimum
-        rolling_min_tick(cols=["rmin_a = a", "rmin_d = d"], rev_ticks=10),
-        rolling_min_tick(cols=["rmin_a = a", "rmin_d = d"], rev_ticks=10, fwd_ticks=10),
-        rolling_min_time(ts_col="Timestamp", cols=["rmin_b = b", "rmin_e = e"], rev_time="00:00:10"),
-        rolling_min_time(ts_col="Timestamp", cols=["rmin_b = b", "rmin_e = e"], rev_time=10_000_000_000,
-                         fwd_time=-10_000_000_00),
-        rolling_min_time(ts_col="Timestamp", cols=["rmin_b = b", "rmin_e = e"], rev_time="00:00:30",
-                         fwd_time="-00:00:20"),
-
-        # rolling maximum
-        rolling_max_tick(cols=["rmax_a = a", "rmax_d = d"], rev_ticks=10),
-        rolling_max_tick(cols=["rmax_a = a", "rmax_d = d"], rev_ticks=10, fwd_ticks=10),
-        rolling_max_time(ts_col="Timestamp", cols=["rmax_b = b", "rmax_e = e"], rev_time="00:00:10"),
-        rolling_max_time(ts_col="Timestamp", cols=["rmax_b = b", "rmax_e = e"], rev_time=10_000_000_000,
-                         fwd_time=-10_000_000_00),
-        rolling_max_time(ts_col="Timestamp", cols=["rmax_b = b", "rmax_e = e"], rev_time="00:00:30",
-                         fwd_time="-00:00:20"),
-
-        # rolling product
-        rolling_prod_tick(cols=["rprod_a = a", "rprod_d = d"], rev_ticks=10),
-        rolling_prod_tick(cols=["rprod_a = a", "rprod_d = d"], rev_ticks=10, fwd_ticks=10),
-        rolling_prod_time(ts_col="Timestamp", cols=["rprod_b = b", "rprod_e = e"], rev_time="00:00:10"),
-        rolling_prod_time(ts_col="Timestamp", cols=["rprod_b = b", "rprod_e = e"], rev_time=10_000_000_000,
-                          fwd_time=-10_000_000_00),
-        rolling_prod_time(ts_col="Timestamp", cols=["rprod_b = b", "rprod_e = e"], rev_time="00:00:30",
-                          fwd_time="-00:00:20"),
-
-        # rolling count
-        rolling_count_tick(cols=["rcount_a = a", "rcount_d = d"], rev_ticks=10),
-        rolling_count_tick(cols=["rcount_a = a", "rcount_d = d"], rev_ticks=10, fwd_ticks=10),
-        rolling_count_time(ts_col="Timestamp", cols=["rcount_b = b", "rcount_e = e"], rev_time="00:00:10"),
-        rolling_count_time(ts_col="Timestamp", cols=["rcount_b = b", "rcount_e = e"], rev_time=10_000_000_000,
-                          fwd_time=-10_000_000_00),
-        rolling_count_time(ts_col="Timestamp", cols=["rcount_b = b", "rcount_e = e"], rev_time="00:00:30",
-                          fwd_time="-00:00:20"),
-
-        # rolling standard deviation
-        rolling_std_tick(cols=["rstd_a = a", "rstd_d = d"], rev_ticks=10),
-        rolling_std_tick(cols=["rstd_a = a", "rstd_d = d"], rev_ticks=10, fwd_ticks=10),
-        rolling_std_time(ts_col="Timestamp", cols=["rstd_b = b", "rstd_e = e"], rev_time="00:00:10"),
-        rolling_std_time(ts_col="Timestamp", cols=["rstd_b = b", "rstd_e = e"], rev_time=10_000_000_000,
-                          fwd_time=-10_000_000_00),
-        rolling_std_time(ts_col="Timestamp", cols=["rstd_b = b", "rstd_e = e"], rev_time="00:00:30",
-                          fwd_time="-00:00:20"),
-
-        # rolling weighted average (using "b" as the weight column)
-        rolling_wavg_tick(weight_col="b", cols=["rwavg_a = a", "rwavg_d = d"], rev_ticks=10),
-        rolling_wavg_tick(weight_col="b", cols=["rwavg_a = a", "rwavg_d = d"], rev_ticks=10, fwd_ticks=10),
-        rolling_wavg_time(ts_col="Timestamp", weight_col="b", cols=["rwavg_b = b", "rwavg_e = e"], rev_time="00:00:10"),
-        rolling_wavg_time(ts_col="Timestamp", weight_col="b", cols=["rwavg_b = b", "rwavg_e = e"], rev_time=10_000_000_000, fwd_time=-10_000_000_00),
-        rolling_wavg_time(ts_col="Timestamp", weight_col="b", cols=["rwavg_b = b", "rwavg_e = e"], rev_time="00:00:30", fwd_time="-00:00:20"),
-    ]
 
     def test_rolling_ops(self):
         for op in self.rolling_ops:
