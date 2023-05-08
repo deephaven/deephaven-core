90825979b033e9eaa66256f007ea5c07b4457f33
Devin Smith <devinsmith@deephaven.io>
2023-05-08 09:55:27 -0700

    Consolidate NULL_X paths
    Also brings io.deephaven.util.BooleanUtils into engine-query-constants.
    
    Fixes #895

--- qst/src/test/java/io/deephaven/qst/array/ShortArrayTest.java
+++ qst/src/test/java/io/deephaven/qst/array/ShortArrayTest.java
@@ -3,6 +3,7 @@
  */
 package io.deephaven.qst.array;
 
+import io.deephaven.util.QueryConstants;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,12 +13,12 @@ public class ShortArrayTest {
     @Test
     void boxInRawOut() {
         assertThat(ShortArray.of((short) 1, null, (short) 3).values()).containsExactly((short) 1,
-                Util.NULL_SHORT, (short) 3);
+                QueryConstants.NULL_SHORT, (short) 3);
     }
 
     @Test
     void rawInRawOut() {
-        assertThat(ShortArray.ofUnsafe((short) 1, Util.NULL_SHORT, (short) 3).values())
-                .containsExactly((short) 1, Util.NULL_SHORT, (short) 3);
+        assertThat(ShortArray.ofUnsafe((short) 1, QueryConstants.NULL_SHORT, (short) 3).values())
+                .containsExactly((short) 1, QueryConstants.NULL_SHORT, (short) 3);
     }
 }
