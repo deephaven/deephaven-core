7344116fcc3533a53c445041f0681942091d6d55
Chip Kent <chipkent@deephaven.io>
2023-05-09 14:03:58 -0600

    * DateTimeFormatter

--- engine/time/src/main/java/io/deephaven/time/DateTimeFormatter.java
+++ engine/time/src/main/java/io/deephaven/time/DateTimeFormatter.java
@@ -4,31 +4,46 @@
 package io.deephaven.time;
 
 
+import org.jetbrains.annotations.NotNull;
+
 import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Formatter for DateTimes.
+ * String formatter for {@link DateTime}.
  */
 public class DateTimeFormatter {
-    //TODO: remove deprecated
-    //TODO: document
-    //TODO: remove Joda exposure
-    //TODO: curate API
-    //TODO: review public vs private
     //TODO: test coverage
 
     private final String pattern;
-    private final Map<TimeZone, java.time.format.DateTimeFormatter> formatCache = new HashMap<>(3);
+    private final Map<ZoneId, java.time.format.DateTimeFormatter> formatCache = new HashMap<>(3);
 
+    /**
+     * Creates a new date time formatter using the default time zone.
+     *
+     * See {@link java.time.format.DateTimeFormatter} for valid format strings.
+     *
+     * @param pattern format pattern.
+     * @see TimeZone#TZ_DEFAULT
+     * @see java.time.format.DateTimeFormatter
+     */
     public DateTimeFormatter(String pattern) {
         this.pattern = pattern;
 
         // Do this here so we fail fast if there's a problem with the format string
-        getFormatter(TimeZone.TZ_DEFAULT);
+        getFormatter(TimeZone.TZ_DEFAULT.getZoneId());
     }
 
+    /**
+     * Creates a new date time formatter.
+     *
+     * @param isISO ISO 8601 format
+     * @param hasDate include date
+     * @param hasTime include time
+     * @param subsecondDigits include subsecond digits
+     * @param hasTZ include time zone
+     */
     public DateTimeFormatter(final boolean isISO, final boolean hasDate, final boolean hasTime,
             final int subsecondDigits, final boolean hasTZ) {
         this((hasDate ? "yyyy-MM-dd" : "") + (!hasDate || !hasTime ? "" : isISO ? "'T'" : " ") +
@@ -36,17 +51,44 @@ public class DateTimeFormatter {
                 (hasTime ? "S".repeat(subsecondDigits) : "") + (hasTZ ? " %t" : ""));
     }
 
-    private java.time.format.DateTimeFormatter getFormatter(TimeZone tz) {
+    private java.time.format.DateTimeFormatter getFormatter(ZoneId tz) {
         return formatCache.computeIfAbsent(tz, newTz -> java.time.format.DateTimeFormatter
                 .ofPattern(pattern.replaceAll("%t", '\'' + tz.toString().substring(3) + '\'')));
     }
 
-    public String format(DateTime dateTime, TimeZone tz) {
-        final ZoneId zone = tz.getTimeZone().toTimeZone().toZoneId();
-        return dateTime.toInstant().atZone(zone).format(getFormatter(tz));
+    /**
+     * Returns a DateTime formatted as a string.
+     *
+     * @param dateTime time to format as a string.
+     * @param timeZone time zone to use when formatting the string.
+     * @return date time formatted as a string.
+     */
+    @NotNull
+    public String format(@NotNull final DateTime dateTime, @NotNull final ZoneId timeZone) {
+        return dateTime.toInstant().atZone(timeZone).format(getFormatter(timeZone));
     }
 
-    public String format(DateTime dateTime) {
+    /**
+     * Returns a DateTime formatted as a string.
+     *
+     * @param dateTime time to format as a string.
+     * @param timeZone time zone to use when formatting the string.
+     * @return date time formatted as a string.
+     */
+    @NotNull
+    public String format(@NotNull final DateTime dateTime, @NotNull final TimeZone timeZone) {
+        return format(dateTime, timeZone.getZoneId());
+    }
+
+    /**
+     * Returns a DateTime formatted as a string using the default time zone.
+     *
+     * @param dateTime time to format as a string.
+     * @return date time formatted as a string.
+     * @see TimeZone#TZ_DEFAULT
+     */
+    @NotNull
+    public String format(@NotNull final DateTime dateTime) {
         return format(dateTime, TimeZone.TZ_DEFAULT);
     }
 
@@ -55,6 +97,11 @@ public class DateTimeFormatter {
         return format(DateTime.now());
     }
 
+    /**
+     * Gets the format pattern.
+     *
+     * @return format pattern.
+     */
     public String getPattern() {
         return pattern;
     }
