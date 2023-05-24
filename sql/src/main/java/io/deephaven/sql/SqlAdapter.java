5bdf9a3974bb2a7063a6c0e29a1e3bb7816a33a7
Devin Smith <devinsmith@deephaven.io>
2023-05-24 10:23:42 -0700

    Move to LexConfig

--- sql/src/main/java/io/deephaven/sql/SqlAdapter.java
+++ sql/src/main/java/io/deephaven/sql/SqlAdapter.java
@@ -4,13 +4,7 @@
 package io.deephaven.sql;
 
 import io.deephaven.qst.table.TableSpec;
-import org.apache.calcite.avatica.util.Casing;
-import org.apache.calcite.avatica.util.Quoting;
-import org.apache.calcite.config.CalciteConnectionConfig;
 import org.apache.calcite.config.CalciteConnectionConfigImpl;
-import org.apache.calcite.config.CalciteConnectionProperty;
-import org.apache.calcite.config.CharLiteralStyle;
-import org.apache.calcite.config.Lex;
 import org.apache.calcite.config.NullCollation;
 import org.apache.calcite.jdbc.CalciteSchema;
 import org.apache.calcite.jdbc.JavaTypeFactoryImpl;
@@ -30,7 +24,6 @@ import org.apache.calcite.sql.SqlExplainLevel;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.parser.SqlParser;
-import org.apache.calcite.sql.parser.SqlParser.Config;
 import org.apache.calcite.sql.validate.SqlValidator;
 import org.apache.calcite.sql.validate.SqlValidatorUtil;
 import org.apache.calcite.sql2rel.SqlToRelConverter;
@@ -38,9 +31,8 @@ import org.apache.calcite.sql2rel.StandardConvertletTable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 
 /**
  * This is the main public entrypoint for converting a SQL query into {@link TableSpec}.
@@ -48,33 +40,6 @@ import java.util.Set;
 public final class SqlAdapter {
 
     private static final Logger log = LoggerFactory.getLogger(SqlAdapter.class);
-
-    /**
-     * By default, calcite operates with the equivalent of {@link Lex#ORACLE} (via the defaults exposed through
-     * {@link SqlParser#config()}, and the default value for {@link CalciteConnectionProperty#LEX}). This has the
-     * side-effect of tokenizing unquoted identifiers into {@link Casing#TO_UPPER upper-case}, which is not what one
-     * might expect by default. Additionally, there is no {@link Lex} that has all of the defaults we want
-     * ({@link Lex#JAVA} comes close, but uses {@link Quoting#BACK_TICK} instead of {@link Quoting#DOUBLE_QUOTE}). We
-     * can be explicit with {@link Config#withQuoting(Quoting)}, {@link Config#withUnquotedCasing(Casing)},
-     * {@link Config#withQuotedCasing(Casing)}, {@link Config#withCaseSensitive(boolean)},
-     * {@link Config#withCharLiteralStyles(Iterable)}, {@link CalciteConnectionProperty#QUOTING},
-     * {@link CalciteConnectionProperty#UNQUOTED_CASING}, {@link CalciteConnectionProperty#QUOTED_CASING}, and
-     * {@link CalciteConnectionProperty#CASE_SENSITIVE}.
-     *
-     * <p>
-     * If a new {@link Lex} that has our defaults is introduced, or an interface is added, we'd be able to simplify how
-     * these configurations get built via {@link Config#withLex(Lex)} and {@link CalciteConnectionProperty#LEX}.
-     *
-     * @see #parserConfig()
-     * @see #calciteConnectionConfig()
-     */
-    @SuppressWarnings("unused")
-    private static final Lex _LEX = null; // leaving for the javadoc note
-    private static final Quoting QUOTING = Quoting.DOUBLE_QUOTE;
-    private static final Casing UNQUOTED_CASING = Casing.UNCHANGED;
-    private static final Casing QUOTED_CASING = Casing.UNCHANGED;
-    private static final boolean CASE_SENSITIVE = true;
-    private static final Set<CharLiteralStyle> CHAR_LITERAL_STYLES = Set.of(CharLiteralStyle.STANDARD);
 
     /**
      * Parses the {@code sql} query into a {@link TableSpec}.
@@ -142,7 +107,7 @@ public final class SqlAdapter {
     }
 
     private static SqlNode parse(String sql) {
-        final SqlParser parser = SqlParser.create(sql, parserConfig());
+        final SqlParser parser = SqlParser.create(sql, LexConfig.withLexConfig(SqlParser.config()));
         final SqlNode sqlNode;
         try {
             sqlNode = parser.parseQuery();
@@ -176,28 +141,9 @@ public final class SqlAdapter {
             }
             schema.add(info.qualifiedName().get(0), new DeephavenTable(TypeAdapter.of(info.header(), typeFactory)));
         }
-        return new CalciteCatalogReader(schema, Collections.emptyList(), typeFactory, calciteConnectionConfig());
-    }
-
-    private static SqlParser.Config parserConfig() {
-        // return SqlParser.config().withLex(_LEX);
-        return SqlParser.config()
-                .withQuoting(QUOTING)
-                .withUnquotedCasing(UNQUOTED_CASING)
-                .withQuotedCasing(QUOTED_CASING)
-                .withCaseSensitive(CASE_SENSITIVE)
-                .withCharLiteralStyles(CHAR_LITERAL_STYLES);
-    }
-
-    private static CalciteConnectionConfig calciteConnectionConfig() {
         final Properties props = new Properties();
-        // props.setProperty(CalciteConnectionProperty.LEX.camelName(), _LEX.name());
-        props.setProperty(CalciteConnectionProperty.QUOTING.camelName(), QUOTING.name());
-        props.setProperty(CalciteConnectionProperty.UNQUOTED_CASING.camelName(), UNQUOTED_CASING.name());
-        props.setProperty(CalciteConnectionProperty.QUOTED_CASING.camelName(), QUOTED_CASING.name());
-        props.setProperty(CalciteConnectionProperty.CASE_SENSITIVE.camelName(), String.valueOf(CASE_SENSITIVE));
-        // Note: there is no CalciteConnectionProperty.CHAR_LITERAL_STYLES or equivalent
-        return new CalciteConnectionConfigImpl(props);
+        LexConfig.setLexProperties(props);
+        return new CalciteCatalogReader(schema, List.of(), typeFactory, new CalciteConnectionConfigImpl(props));
     }
 
     private static SqlValidator validator(
