6b7c0e678c8822f0759b4892466b71ffd29ec8f0
Ryan Caudy <rcaudy@gmail.com>
2023-05-26 13:31:13 -0400

    Fix two stray usages of io.deephaven.time.DateTime

--- web/client-api/src/main/java/io/deephaven/web/client/api/subscription/ViewportData.java
+++ web/client-api/src/main/java/io/deephaven/web/client/api/subscription/ViewportData.java
@@ -185,7 +185,6 @@ public class ViewportData implements TableData {
             }
             case "java.time.Instant":
             case "java.time.ZonedDateTime":
-            case "io.deephaven.time.DateTime": {
                 JsArray<Any> values = Js.uncheckedCast(dataColumn);
                 DateWrapper[] cleanData = new DateWrapper[values.length];
                 for (int i = 0; i < values.length; i++) {
