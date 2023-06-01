9832eb04ecc5152bada01a14634b6fc820f4fd64
Nathaniel Bauernfeind <natebauernfeind@deephaven.io>
2023-06-01 07:37:13 -0600

    temporarily commit the extra controlled update graph for compilation

---
+++ engine/updategraph/src/main/java/io/deephaven/engine/testutil/ControlledUpdateGraph.java
@@ -0,0 +1,10 @@
+package io.deephaven.engine.testutil;
+
+import io.deephaven.engine.updategraph.UpdateGraphProcessor;
+
+// TODO (deephaven-core#3886): Extract test functionality from UpdateGraphProcessor
+public class ControlledUpdateGraph extends UpdateGraphProcessor {
+    public ControlledUpdateGraph() {
+        super("TEST", true, 1000, 25, -1);
+    }
+}
