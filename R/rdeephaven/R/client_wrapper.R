695897d83822e9dcc55a9aac5e92c5ba1545d951
alexpeters1208 <alexscottpeters1208@gmail.com>
2023-06-03 01:48:30 +0000

    Update client_wrapper doc

--- R/rdeephaven/R/client_wrapper.R
+++ R/rdeephaven/R/client_wrapper.R
@@ -1,39 +1,47 @@
 ######################################## INCOMPLETE DOCS ########################################
 
 #' @title The Deephaven Client
-#' @description The DeephavenClient class is responsible for establishing and maintaining
+#' @description The Deephaven Client class is responsible for establishing and maintaining
 #' a connection to a running Deephaven server and facilitating basic server requests. This is
 #' the primary interface for interacting with the Deephaven server from R.
 #' 
 #' @section Connecting to the Server via Client
 #' 
-#' To connect to a Deephaven server, you must create a new DeephavenClient object via `DeephavenClient$new(...)`,
+#' To connect to a Deephaven server, you must create a new DeephavenClient object via `Client$new(...)`,
 #' with the following arguments:
 #' 
-#' - `target`: The URL that the Deephaven server is running on. This argument is NOT OPTIONAL.
-#' - `session_type`: If you started a Python or Groovy server, you can start up a corresponding
-#'    console here with "python" or "groovy". Defaults to Python.
-#' - `auth_type`: The type of authentication your server is using. Can be "default", "basic", or "custom".
-#'    Defaults to "default".
-#' - `key`: The key credential in a key/value pair. For basic authentication, this is a username.
-#'    For custom authentication, it is a general key. For default (anonymous) authentication, use "".
-#'    Defaults to "".
-#' - `value`: The value credential in a key/value pair. For basic authentication, this is a password.
-#'    For custom authentication, it is a  general value. For default (anonymous) authentication, use "".
-#' 
-#' It is important that every one of these arguments be provided explicitly. We will provide optional and
-#' default arguments in a coming release, but for the moment we require the user to provide all arguments.
-#' Once the server connection has been established, you have a handful of key methods to handle basic server
-#' requests.
+#' - `target`: The URL that the Deephaven server is running on.
+#' - `client_options`: A `ClientOptions` instance that contains the relevant information for connecting to your server.
+#'   Check the documentation on client options via `?ClientOptions`
 #' 
 #' @section Methods
 #' 
-#' - `$open_table(name)`: Looks for a table named 'name' on the server, and returns a Deephaven TableHandle
-#'    reference to that table if it exists.
-#' - `$delete_table(name)`: Looks for a table named 'name' on the server, and deletes it if it exists.
-#'    Importantly, this is only effective 
-#' - `check_for_table`: 
-#' - `run_script`:
+#' - `$open_table(name)`: Looks for a table named 'name' on the server, and returns a Deephaven TableHandle reference
+#'    reference to that table if it exists. See the documentation on table handles via `?TableHandle` for more information.
+#' - `$import_table(table_object)`: Imports a new table to the Deephaven server and returns a Deephaven TableHandle
+#'    reference to the new table. Here, `table_object` can be an R Data Frame, an R Tibble, an Arrow Table, or an Arrow
+#'    RecordBatchReader. Note that this new table is not automatically bound to a variable name on the server.
+#'    See `?TableHandle` for more information.
+#' - `run_script(script)`: Runs a script on the server. The script must be in the language that the server console was
+#'    started with, and should be passed as a string to this method.
+#' 
+#' @examples
+#' 
+#' client_options <- ClientOptions$new()
+#' client_options$set_default_authentication()
+#' 
+#' # connect to the Deephaven server running on "localhost:10000" with anonymous 'default' authentication
+#' client <- Client$new(target="localhost:10000", client_options=client_options)
+#' 
+#' # open a table that already exists on the server
+#' new_table_handle1 <- client$open_table("table_on_the_server")
+#' 
+#' # create a new dataframe, import onto the server, and retrieve a reference
+#' new_data_frame <- data.frame(matrix(rnorm(10 * 1000), nrow = 10))
+#' new_table_handle2 <- client$import_table(new_data_frame)
+#' 
+#' # run a python script, since default client options specify a Python console
+#' client$run_script("print([i for i in range(10)])")
 
 #################################################################################################
 
@@ -53,7 +61,7 @@ Client <- R6Class("Client",
             }
             return(TableHandle$new(private$internal_client$open_table(name)))
         },
-        
+
         import_table = function(table_object) {
             table_object_class = class(table_object)
             if (table_object_class[[1]] == "data.frame") {
