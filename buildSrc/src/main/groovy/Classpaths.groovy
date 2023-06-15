530a6fcd49355b23b05644cfee5b71e8b7e5e7d7
Devin Smith <devinsmith@deephaven.io>
2023-06-15 07:33:00 -0700

    Bump sslcontext-kickstart to 8.1.1
    https://github.com/Hakky54/sslcontext-kickstart/blob/v8.1.1/CHANGELOG.md

--- buildSrc/src/main/groovy/Classpaths.groovy
+++ buildSrc/src/main/groovy/Classpaths.groovy
@@ -114,7 +114,7 @@ class Classpaths {
     static final String JACKSON_VERSION = '2.14.1'
 
     static final String SSLCONTEXT_GROUP = 'io.github.hakky54'
-    static final String SSLCONTEXT_VERSION = '7.4.8'
+    static final String SSLCONTEXT_VERSION = '8.1.1'
 
     static final String JETTY11_GROUP = 'org.eclipse.jetty'
     static final String JETTY11_NAME = 'jetty-bom'
