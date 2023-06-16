c7f90b14e5bc08da44d53ebea8fc27d9c333f6b7
Colin Alworth <colinalworth@deephaven.io>
2023-06-16 13:56:15 -0500

    Emit name instead of wire value

--- server/src/main/java/io/deephaven/server/arrow/ArrowFlightUtil.java
+++ server/src/main/java/io/deephaven/server/arrow/ArrowFlightUtil.java
@@ -171,7 +171,7 @@ public class ArrowFlightUtil {
 
             if (mi.header.headerType() != MessageHeader.RecordBatch) {
                 throw Exceptions.statusRuntimeException(Code.INVALID_ARGUMENT,
-                        "Only schema/record-batch messages supported, but I got " + mi.header.headerType());
+                        "Only schema/record-batch messages supported, instead got " + MessageHeader.name(mi.header.headerType()));
             }
 
             final int numColumns = resultTable.getColumnSources().size();
