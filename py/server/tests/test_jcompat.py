2fe6de1276fac26c32db21793316fa0c4d9523ca
Colin Alworth <colinalworth@deephaven.io>
2023-06-20 18:37:17 -0500

    Add a simple test converting a py func into a java lambda

--- py/server/tests/test_jcompat.py
+++ py/server/tests/test_jcompat.py
@@ -5,8 +5,10 @@
 import unittest
 
 from deephaven import dtypes
-from deephaven.jcompat import j_function
+from deephaven.jcompat import j_function, j_lambda
 from tests.testbase import BaseTestCase
+
+import jpy
 
 
 class JCompatTestCase(BaseTestCase):
@@ -15,6 +17,14 @@ class JCompatTestCase(BaseTestCase):
             return str(v)
 
         j_func = j_function(int_to_str, dtypes.string)
+
+        r = j_func.apply(10)
+        self.assertEqual(r, "10")
+
+    def test_j_lambda(self):
+        def int_to_str(v: int) -> str:
+            return str(v)
+        j_func = j_lambda(int_to_str, jpy.get_type('java.util.function.Function'), dtypes.string)
 
         r = j_func.apply(10)
         self.assertEqual(r, "10")
