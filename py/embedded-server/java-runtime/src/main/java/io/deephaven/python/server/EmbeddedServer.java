deephaven/deephaven-core

commit 8a5b4947fc29da0edfcdf4ab25afe8e3fd0e3dfe

ExecutionContext: record state of globals before deferring work (#2539)
The QueryScope, QueryLibrary, CompilerTools were merged into a new package `io.deephaven.engine.context` and are bundled as a distinct artifact `engine-context`. By default the ThreadLocal is "poisoned" to prevent accidental use of this state in unexpected contexts. The session's execution context will be installed from all entry points to the gRPC API as well as scripts run in a deephaven script session. To use elsewhere it is encouraged to capture the least state needed and pass the new ExecutionContext to use on another thread or during a deferred task. 

WhereFilters and SelectColumns will be initialized immediately and copies will retain details instead of needing to reevaluate in deferred contexts.

This commit introduces `TableOperationsDefaults` which lifts many default `TableOperations` implementations from `TableDefaults`. This helps reinforce consistency in other implementations of TableOperations.

The QueryLanguageParser can now deduce the return type of a generic method if it is fixed due to inheritance rules (both via interfaces and parent classes).

py/embedded-server/java-runtime/src/main/java/io/deephaven/python/server/EmbeddedServer.java
@@ -7,6 +7,8 @@ import dagger.BindsInstance;
 import dagger.Component;
 import io.deephaven.configuration.Configuration;
 import io.deephaven.engine.util.ScriptSession;
+import io.deephaven.server.console.groovy.GroovyConsoleSessionModule;
+import io.deephaven.server.console.python.PythonConsoleSessionModule;
 import io.deephaven.server.console.python.PythonGlobalScopeModule;
 import io.deephaven.server.healthcheck.HealthCheckModule;
 import io.deephaven.server.jetty.JettyConfig;
@@ -35,7 +37,9 @@ public class EmbeddedServer {
             PythonGlobalScopeModule.class,
             HealthCheckModule.class,
             PythonPluginsRegistration.Module.class,
-            JettyServerModule.class
+            JettyServerModule.class,
+            PythonConsoleSessionModule.class,
+            GroovyConsoleSessionModule.class,
     })
     public interface PythonServerComponent extends DeephavenApiServerComponent {
         @Component.Builder
@@ -76,8 +80,13 @@ public class EmbeddedServer {
 
     public void start() throws Exception {
         server.run();
-        checkGlobals(scriptSession.get(), null);
+
+        final ScriptSession scriptSession = this.scriptSession.get();
+        checkGlobals(scriptSession, null);
         System.out.println("Server started on port " + server.server().getPort());
+
+        // We need to open the systemic execution context to permanently install the contexts for this thread.
+        scriptSession.getExecutionContext().open();
     }
 
     private void checkGlobals(ScriptSession scriptSession, @Nullable ScriptSession.SnapshotScope lastSnapshot) {

commit c77110e6dd7bd9ee06313c039a52d76db6e1c313

Move global scan and exec context scope to Server constructor (#3996)
This makes it possible to use the deephaven_server.Server without
calling start() - no http server will have started, but the engine is
entirely usable from the main python thread. Then, if start() is
later called, any tables created will be correctly exposed.

Fixes a regression from #2539.

Partial #3765

py/embedded-server/java-runtime/src/main/java/io/deephaven/python/server/EmbeddedServer.java
@@ -100,17 +100,17 @@ public class EmbeddedServer {
                 .withErr(null)
                 .build()
                 .injectFields(this);
+
+        checkGlobals(scriptSession.get(), null);
+
+        // We need to open the systemic execution context to permanently install the contexts for this thread.
+        scriptSession.get().getExecutionContext().open();
     }
 
     public void start() throws Exception {
         server.run();
 
-        final ScriptSession scriptSession = this.scriptSession.get();
-        checkGlobals(scriptSession, null);
-        Bootstrap.printf("Server started on port %d%n", server.server().getPort());
-
-        // We need to open the systemic execution context to permanently install the contexts for this thread.
-        scriptSession.getExecutionContext().open();
+        Bootstrap.printf("Server started on port %d%n", getPort());
     }
 
     private void checkGlobals(ScriptSession scriptSession, @Nullable ScriptSession.SnapshotScope lastSnapshot) {