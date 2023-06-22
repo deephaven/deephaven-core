a5c4aca85df283f53bae793ce8553d296a6d55fc
Devin Smith <devinsmith@deephaven.io>
2023-06-22 10:49:54 -0700

    Break out DeephavenApiServerModule implementation Modules (#3954)

---
+++ server/src/main/java/io/deephaven/server/session/ScriptSessionModule.java
@@ -0,0 +1,30 @@
+package io.deephaven.server.session;
+
+import dagger.Module;
+import dagger.Provides;
+import io.deephaven.configuration.Configuration;
+import io.deephaven.engine.util.ScriptSession;
+
+import javax.inject.Provider;
+import javax.inject.Singleton;
+import java.util.Map;
+
+/**
+ * Provides a {@link ScriptSession}.
+ */
+@Module
+public class ScriptSessionModule {
+
+    @Provides
+    @Singleton
+    public static ScriptSession provideScriptSession(Map<String, Provider<ScriptSession>> scriptTypes) {
+        // Check which script language is configured
+        String scriptSessionType = Configuration.getInstance().getProperty("deephaven.console.type");
+
+        // Emit an error if the selected language isn't provided
+        if (!scriptTypes.containsKey(scriptSessionType)) {
+            throw new IllegalArgumentException("Console type not found: " + scriptSessionType);
+        }
+        return scriptTypes.get(scriptSessionType).get();
+    }
+}
