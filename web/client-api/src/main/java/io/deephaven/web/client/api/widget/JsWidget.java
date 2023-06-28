2c34658fceba45693928f107f3d1c9e1e18cae73
Matthew Runyon <matthewrunyon@deephaven.io>
2023-06-28 01:08:31 -0500

    WIP JsWidget Bidir stream

--- web/client-api/src/main/java/io/deephaven/web/client/api/widget/JsWidget.java
+++ web/client-api/src/main/java/io/deephaven/web/client/api/widget/JsWidget.java
@@ -6,13 +6,19 @@ package io.deephaven.web.client.api.widget;
 import com.vertispan.tsdefs.annotations.TsInterface;
 import com.vertispan.tsdefs.annotations.TsName;
 import elemental2.core.Uint8Array;
+import elemental2.dom.CustomEventInit;
 import elemental2.promise.Promise;
-import io.deephaven.javascript.proto.dhinternal.io.deephaven.proto.object_pb.FetchObjectResponse;
+import io.deephaven.javascript.proto.dhinternal.io.deephaven.proto.console_pb.AutoCompleteRequest;
+import io.deephaven.javascript.proto.dhinternal.io.deephaven.proto.console_pb.AutoCompleteResponse;
+import io.deephaven.javascript.proto.dhinternal.io.deephaven.proto.object_pb.*;
 import io.deephaven.javascript.proto.dhinternal.io.deephaven.proto.ticket_pb.Ticket;
 import io.deephaven.web.client.api.WorkerConnection;
+import io.deephaven.web.client.api.barrage.stream.BiDiStream;
 import jsinterop.annotations.JsFunction;
 import jsinterop.annotations.JsMethod;
 import jsinterop.annotations.JsProperty;
+
+import java.util.function.Supplier;
 
 @TsInterface
 @TsName(namespace = "dh", name = "Widget")
@@ -20,6 +26,9 @@ public class JsWidget {
     private final WorkerConnection connection;
     private final WidgetFetch fetch;
     private Ticket ticket;
+
+    private final Supplier<BiDiStream<MessageRequest, MessageResponse>> streamFactory;
+    private BiDiStream<MessageRequest, MessageResponse> messageStream;
 
     @JsFunction
     public interface WidgetFetchCallback {
@@ -37,6 +46,12 @@ public class JsWidget {
     public JsWidget(WorkerConnection connection, WidgetFetch fetch) {
         this.connection = connection;
         this.fetch = fetch;
+        BiDiStream.Factory<MessageRequest, MessageResponse> factory = connection.streamFactory();
+        streamFactory = () -> factory.create(
+                connection.objectServiceClient()::messageStream,
+                (first, headers) -> connection.objectServiceClient().openMessageStream(first, headers),
+                (next, headers, c) -> connection.objectServiceClient().nextMessageStream(next, headers, c::apply),
+                new MessageRequest());
     }
 
     public Promise<JsWidget> refetch() {
@@ -47,6 +62,28 @@ public class JsWidget {
                 } else {
                     this.response = response;
                     this.ticket = ticket;
+
+                    messageStream = streamFactory.get();
+                    messageStream.onData(res -> {
+                        // Pass the data to any attached message handler
+                        // res.getData();
+                    });
+                    messageStream.onStatus(status -> {
+                        if (!status.isOk()) {
+                            // Do something if request failed
+                        }
+                    });
+                    messageStream.onEnd(status -> {
+                        messageStream = null;
+                    });
+
+                    // First message establishes a connection w/ the plugin object instance we're talking to
+                    MessageRequest req = new MessageRequest();
+                    ConnectRequest data = new ConnectRequest();
+                    data.setTypedTicket(ticket);
+                    req.setSourceId(data);
+                    messageStream.send(req);
+
                     resolve.onInvoke(this);
                 }
             });
@@ -81,5 +118,18 @@ public class JsWidget {
         }
 
         return this.exportedObjects;
+    }
+
+    @JsMethod
+    public void sendMessage(String msg) {
+        if (messageStream == null) {
+            // Should we warn the user they tried to send a message to an unfetched widget?
+            return;
+        }
+        MessageRequest req = new MessageRequest();
+        DataRequest data = new DataRequest();
+        data.setData(msg);
+        req.setData(data);
+        messageStream.send(req);
     }
 }
