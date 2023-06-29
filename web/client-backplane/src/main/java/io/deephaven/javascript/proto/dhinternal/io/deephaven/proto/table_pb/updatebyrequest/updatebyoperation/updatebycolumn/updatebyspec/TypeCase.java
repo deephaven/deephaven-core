2d26060297e17f08663a53dc00f4ceb9fc7d1297
Nathaniel Bauernfeind <natebauernfeind@deephaven.io>
2023-06-29 11:00:14 -0600

    ff

---
+++ web/client-backplane/src/main/java/io/deephaven/javascript/proto/dhinternal/io/deephaven/proto/table_pb/updatebyrequest/updatebyoperation/updatebycolumn/updatebyspec/TypeCase.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2016-2022 Deephaven Data Labs and Patent Pending
+ */
+package io.deephaven.javascript.proto.dhinternal.io.deephaven.proto.table_pb.updatebyrequest.updatebyoperation.updatebycolumn.updatebyspec;
+
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+@JsType(
+        isNative = true,
+        name = "dhinternal.io.deephaven.proto.table_pb.UpdateByRequest.UpdateByOperation.UpdateByColumn.UpdateBySpec.TypeCase",
+        namespace = JsPackage.GLOBAL)
+public class TypeCase {
+    public static int EMA, FILL, MAX, MIN, PRODUCT, SUM, TYPE_NOT_SET;
+}
