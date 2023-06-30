f5a3b187d3e8a2109d6b145acfd9555b181578f0
Alex Peters <80283343+alexpeters1208@users.noreply.github.com>
2023-06-29 21:33:53 -0500

    Update R/rdeephaven/R/helper_functions.R
    Co-authored-by: Chip Kent <5250374+chipkent@users.noreply.github.com>

--- R/rdeephaven/R/helper_functions.R
+++ R/rdeephaven/R/helper_functions.R
@@ -9,7 +9,8 @@
 .verify_int <- function(arg_name, int_candidate) {
     if (class(int_candidate)[[1]] != "numeric") {
         stop(paste0("'", arg_name, "' must be an integer. Got an object of class ", class(int_candidate)[[1]], " instead."))
-    } else if (all.equal(int_candidate, as.integer(int_candidate)) != TRUE) { # must use != TRUE as the result of all.equal() is not strictly boolean
+    } else if (all.equal(int_candidate, as.integer(int_candidate)) != TRUE) { 
+        # must use != TRUE as the result of all.equal() is not strictly boolean
         stop(paste0("'", arg_name, "' must be an integer. Got a non-integer numeric type instead."))
     } else if (length(int_candidate) != 1) {
         stop(paste0("'", arg_name, "' must be an integer. Got a numeric vector of length ", length(int_candidate), " instead."))
