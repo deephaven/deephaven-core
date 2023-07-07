ceb54c97b8550fd733172b785cfbd95ef7c14d8a
Ryan Caudy <ryan@deephaven.io>
2023-07-07 16:06:18 -0400

    KafkaTools refactor to allow for alternative StreamConsumers and fix error propagation (#4142)

--- engine/table/src/main/java/io/deephaven/stream/StreamConsumer.java
+++ engine/table/src/main/java/io/deephaven/stream/StreamConsumer.java
@@ -11,14 +11,12 @@ import org.jetbrains.annotations.NotNull;
  * Chunk-oriented consumer for streams of data.
  */
 public interface StreamConsumer extends StreamFailureConsumer {
+
     /**
-     * <p>
      * Accept a batch of rows splayed across per-column {@link WritableChunk chunks} of {@link Values values}.
-     *
      * <p>
      * Ownership of {@code data} passes to the consumer, which must be sure to
      * {@link io.deephaven.chunk.util.pools.PoolableChunk#close close} each chunk when it's no longer needed.
-     *
      * <p>
      * Implementations will generally have a mechanism for determining the expected number and type of input chunks, but
      * this is not dictated at the interface level.
