ff88960b7ed7343fdc2bb5a0b9897c28743218d6
Colin Alworth <colinalworth@deephaven.io>
2023-07-13 11:12:49 -0500

    Remove unused groovy init script, move py script to module

--- py/server/deephaven_internal/jvm/__init__.py
+++ py/server/deephaven_internal/jvm/__init__.py
@@ -66,3 +66,25 @@ def init_jvm(*args, **kwargs):
             "Unable to initialize JVM, try setting the environment variable JAVA_HOME (JDK 11+ required)") from e
     ready()
     return result
+
+def init_py():
+    """Finishes starting Python to be usable from inside of a Java process. Not intended to be called in cases
+    where there process was start as Python, and Java was started from inside Python.
+    """
+
+    import jpy
+    import os
+    import sys
+    from deephaven_internal.stream import TeeStream
+
+    # Set stdin to /dev/null to prevent functions (like help()) that attempt to read from stdin from hanging python
+    # execution from within Java.
+    os.dup2(os.open("/dev/null", os.O_RDONLY), 0)
+
+    jpy.VerboseExceptions.enabled = True
+    # If you want jpy to tell you about all that it is doing, change this
+    # jpy.diag.flags = jpy.diag.F_ALL
+
+    j_sys = jpy.get_type('java.lang.System')
+    sys.stdout = TeeStream.redirect(sys.stdout, j_sys.out)
+    sys.stderr = TeeStream.redirect(sys.stderr, j_sys.err)
