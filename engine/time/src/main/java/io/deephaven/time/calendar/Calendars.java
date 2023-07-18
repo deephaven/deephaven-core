fdaef8d153baeeb392fa15cbe99ef365ad99a972
Chip Kent <chipkent@deephaven.io>
2023-07-18 14:16:37 -0600

    Committing to rebase

--- engine/time/src/main/java/io/deephaven/time/calendar/Calendars.java
+++ engine/time/src/main/java/io/deephaven/time/calendar/Calendars.java
@@ -13,77 +13,30 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.nio.file.NoSuchFileException;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Consumer;
 
 /**
  * A collection of business calendars.
  */
-public class Calendars implements Map<String, BusinessCalendar> {
+public class Calendars {
 
     private static final Logger logger = LoggerFactory.getLogger(Calendars.class);
     private static final String BUSINESS_CALENDAR_PROP_INTERNAL = "Calendar.importPath";
     private static final String BUSINESS_CALENDAR_PROP_USER = "Calendar.userImportPath";
-    private static final Calendars instance = new Calendars();
-    private static final String defaultName = Configuration.getInstance().getProperty("Calendar.default");
+    private static String defaultName = Configuration.getInstance().getProperty("Calendar.default");
 
-    /**
-     * Gets the singleton map of business calendars.
-     *
-     * @return singleton map of calendars
-     */
-    static Calendars getInstance() {
-        return instance;
-    }
+    private static final Map<String, BusinessCalendar> map = new HashMap<>();
+    private static String[] names = new String[0];
 
-    /**
-     * Returns a business calendar.
-     *
-     * @param name name of the calendar
-     * @return business calendar
-     * @throws IllegalArgumentException no calendar matching {@code name}
-     * @throws RequirementFailure       if the input is null
-     */
-    public static BusinessCalendar calendar(final String name) {
-        Require.neqNull(name, "name");
+    private Calendars() {}
 
-        final String n = name.toUpperCase();
+    // region Load
 
-        if (!instance.containsKey(n)) {
-            throw new IllegalArgumentException("No such calendar: " + name);
-        }
-
-        return instance.get(n);
-    }
-
-    /**
-     * Returns the default business calendar.
-     *
-     * @return default business calendar. The default is specified by the {@code Calendar.default} property.
-     */
-    public static BusinessCalendar calendar() {
-        return calendar(defaultName);
-    }
-
-    /**
-     * Returns the names of all available calendars
-     *
-     * @return names of all available calendars
-     */
-    public static String[] calendarNames() {
-        return instance.keySet().toArray(String[]::new);
-    }
-
-
-    private final Map<String, BusinessCalendar> calendars = new HashMap<>();
-
-
-    private Calendars() {
+    static {
         final Configuration configuration = Configuration.getInstance();
-
 
         loadProperty(configuration, BUSINESS_CALENDAR_PROP_INTERNAL);
 
@@ -92,7 +45,7 @@ public class Calendars implements Map<String, BusinessCalendar> {
         }
     }
 
-    private void loadProperty(final Configuration configuration, final String property) {
+    private static void loadProperty(final Configuration configuration, final String property) {
         final String location = configuration.getProperty(property);
         try {
             load(location);
@@ -101,9 +54,8 @@ public class Calendars implements Map<String, BusinessCalendar> {
         }
     }
 
-    private void load(final String businessCalendarConfig)
-            throws NoSuchFileException {
-        final InputStream configToLoad = this.getClass().getResourceAsStream(businessCalendarConfig);
+    private static void load(final String businessCalendarConfig) throws NoSuchFileException {
+        final InputStream configToLoad = Calendars.class.getResourceAsStream(businessCalendarConfig);
 
         if (configToLoad == null) {
             logger.warn("Could not find " + businessCalendarConfig + " on classpath");
@@ -112,7 +64,7 @@ public class Calendars implements Map<String, BusinessCalendar> {
 
         final Consumer<String> consumer = (filePath) -> {
             try {
-                final InputStream inputStream = this.getClass().getResourceAsStream(filePath);
+                final InputStream inputStream = Calendars.class.getResourceAsStream(filePath);
                 if (inputStream != null) {
                     final File calendarFile = inputStreamToFile(inputStream);
                     final BusinessCalendar businessCalendar = BusinessCalendarParser.loadBusinessCalendar(calendarFile);
@@ -140,21 +92,24 @@ public class Calendars implements Map<String, BusinessCalendar> {
         }
     }
 
-    private void addCalendar(final BusinessCalendar cal) {
+    private synchronized static void addCalendar(final BusinessCalendar cal) {
         final String name = cal.name().toUpperCase();
         if (!NameValidator.isValidQueryParameterName(name)) {
             throw new IllegalArgumentException("Invalid name for calendar: name='" + name + "'");
         }
 
-        if (containsKey(name)) {
-            final Calendar oldCalendar = get(name);
+        if (map.containsKey(name)) {
+            final Calendar oldCalendar = map.get(name);
             if (oldCalendar.equals(cal)) {
                 return;
             }
             throw new IllegalArgumentException("Multiple calendars have the same name: name='" + name + "'");
         }
 
-        put(name, cal);
+        map.put(name, cal);
+
+        names = map.keySet().toArray(String[]::new);
+        Arrays.sort(names);
     }
 
     /**
@@ -162,7 +117,7 @@ public class Calendars implements Map<String, BusinessCalendar> {
      *
      * @param file business calendar file
      */
-    public void addCalendarFromFile(final String file) {
+    public static void addCalendarFromFile(final String file) {
         addCalendarFromFile(new File(file));
     }
 
@@ -171,80 +126,13 @@ public class Calendars implements Map<String, BusinessCalendar> {
      *
      * @param file business calendar file
      */
-    public void addCalendarFromFile(final File file) {
+    public static void addCalendarFromFile(final File file) {
         if (file.getAbsolutePath().endsWith(".calendar")) {
             final BusinessCalendar cal = BusinessCalendarParser.loadBusinessCalendar(file);
             addCalendar(cal);
         } else {
             throw new UnsupportedOperationException("Calendar file must be in .calendar format");
         }
-    }
-
-    @Override
-    public int size() {
-        return calendars.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return calendars.isEmpty();
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        return !(key == null || !key.getClass().isAssignableFrom(String.class))
-                && calendars.containsKey(((String) key).toUpperCase());
-
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        return calendars.containsValue(value);
-    }
-
-    @Override
-    public BusinessCalendar get(Object key) {
-        return calendars.get(key);
-    }
-
-    @Override
-    public BusinessCalendar put(String key, BusinessCalendar value) {
-        Require.neqNull(key, "key");
-        key = key.toUpperCase();
-        return calendars.put(key, value);
-    }
-
-    @Override
-    public BusinessCalendar remove(Object key) {
-        return calendars.remove(key);
-    }
-
-    @Override
-    public void putAll(@NotNull Map<? extends String, ? extends BusinessCalendar> m) {
-        calendars.putAll(m);
-    }
-
-    @Override
-    public void clear() {
-        calendars.clear();
-    }
-
-    @NotNull
-    @Override
-    public Set<String> keySet() {
-        return calendars.keySet();
-    }
-
-    @NotNull
-    @Override
-    public Collection<BusinessCalendar> values() {
-        return calendars.values();
-    }
-
-    @NotNull
-    @Override
-    public Set<Entry<String, BusinessCalendar>> entrySet() {
-        return calendars.entrySet();
     }
 
     private static File inputStreamToFile(@NotNull InputStream inputStream) throws IOException {
@@ -262,4 +150,58 @@ public class Calendars implements Map<String, BusinessCalendar> {
         outputStream.close();
         return calendarFile;
     }
+
+    // end region
+
+    // region Methods
+
+    /**
+     * Sets the default calendar name.
+     *
+     * @param name calendar name
+     */
+    public synchronized static void setDefaultCalendar(final String name) {
+        Require.neqNull(name, "name");
+        defaultName = name;
+    }
+
+    /**
+     * Returns a business calendar.
+     *
+     * @param name name of the calendar
+     * @return business calendar
+     * @throws IllegalArgumentException no calendar matching {@code name}
+     * @throws RequirementFailure       if the input is null
+     */
+    public synchronized static BusinessCalendar calendar(final String name) {
+        Require.neqNull(name, "name");
+
+        final String n = name.toUpperCase();
+
+        if (!map.containsKey(n)) {
+            throw new IllegalArgumentException("No such calendar: " + name);
+        }
+
+        return map.get(n);
+    }
+
+    /**
+     * Returns the default business calendar.
+     *
+     * @return default business calendar. The default is specified by the {@code Calendar.default} property.
+     */
+    public synchronized static BusinessCalendar calendar() {
+        return calendar(defaultName);
+    }
+
+    /**
+     * Returns the names of all available calendars
+     *
+     * @return names of all available calendars
+     */
+    public synchronized static String[] calendarNames() {
+        return names;
+    }
+
+    // endregion
 }
