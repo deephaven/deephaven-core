626a019d1674a6c0ea87cebc43ac5bfe6fcce441
Ryan Caudy <rcaudy@gmail.com>
2023-07-26 10:57:05 -0400

    Update extensions/parquet/table/src/main/java/io/deephaven/parquet/table/ParquetTableWriter.java

--- extensions/parquet/table/src/main/java/io/deephaven/parquet/table/ParquetTableWriter.java
+++ extensions/parquet/table/src/main/java/io/deephaven/parquet/table/ParquetTableWriter.java
@@ -571,8 +571,8 @@ public class ParquetTableWriter {
             final long tableSize = tableRowSet.size();
             final int targetPageSize = getTargetRowsPerPage(
                     valueSource.getType(), writeInstructions.getTargetPageSize());
-            helper = new FlatColumnWriterHelper(targetPageSize);
             maxValuesPerPage = maxRowsPerPage = (int) Math.min(tableSize, targetPageSize);
+            helper = new FlatColumnWriterHelper(maxValuesPerPage);
             pageCount = Math.toIntExact((tableSize + targetPageSize - 1) / targetPageSize);
         }
 
