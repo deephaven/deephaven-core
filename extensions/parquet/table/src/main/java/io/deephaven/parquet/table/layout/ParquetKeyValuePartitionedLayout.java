a7b3103c3b6aa0f56d95ab484818723f5d864325
Ryan Caudy <rcaudy@gmail.com>
2023-08-07 19:42:26 -0400

    Introduce LocationTableBuilderDefinition, and a constructor for ParquetKeyValuePartitionedLayout that uses it

--- extensions/parquet/table/src/main/java/io/deephaven/parquet/table/layout/ParquetKeyValuePartitionedLayout.java
+++ extensions/parquet/table/src/main/java/io/deephaven/parquet/table/layout/ParquetKeyValuePartitionedLayout.java
@@ -4,7 +4,10 @@
 package io.deephaven.parquet.table.layout;
 
 import io.deephaven.csv.CsvTools;
+import io.deephaven.engine.table.ColumnDefinition;
+import io.deephaven.engine.table.TableDefinition;
 import io.deephaven.engine.table.impl.locations.local.KeyValuePartitionLayout;
+import io.deephaven.engine.table.impl.locations.local.LocationTableBuilderDefinition;
 import io.deephaven.parquet.table.location.ParquetTableLocationKey;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,6 +20,16 @@ import java.io.File;
  *           the same rules.
  */
 public class ParquetKeyValuePartitionedLayout extends KeyValuePartitionLayout<ParquetTableLocationKey> {
+
+    public ParquetKeyValuePartitionedLayout(
+            @NotNull final File tableRootDirectory,
+            @NotNull final TableDefinition tableDefinition) {
+        super(tableRootDirectory,
+                ParquetFileHelper::fileNameMatches,
+                new LocationTableBuilderDefinition(tableDefinition),
+                (path, partitions) -> new ParquetTableLocationKey(path.toFile(), 0, partitions),
+                Math.toIntExact(tableDefinition.getColumnStream().filter(ColumnDefinition::isPartitioning).count()));
+    }
 
     public ParquetKeyValuePartitionedLayout(
             @NotNull final File tableRootDirectory,
