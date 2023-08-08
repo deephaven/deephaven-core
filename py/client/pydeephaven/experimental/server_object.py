088e7eca6fa3621b629a746eae35a2a82f1db879
Colin Alworth <colinalworth@deephaven.io>
2023-08-08 11:12:08 -0500

    Add clarifying documentation

--- py/client/pydeephaven/experimental/server_object.py
+++ py/client/pydeephaven/experimental/server_object.py
@@ -6,6 +6,12 @@ from pydeephaven.proto import ticket_pb2
 
 @dataclasses.dataclass
 class ServerObject:
+    """
+    Implementations of this type are client-side representations of objects that actually exist on the Deephaven server.
+    Presently used to enable the client API users to send and receive references to server-side plugins.
+
+    Marked as "experimental" for now, as we work to improve the client plugin experience.
+    """
     type_: str
     ticket: bytes
     def typed_ticket(self) -> ticket_pb2.TypedTicket:
