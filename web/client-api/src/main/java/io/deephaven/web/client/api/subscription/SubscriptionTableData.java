a4aacb779f72ef992e47a6534e17cd1a9eea04f8
Ethan Alvizo <ethanalvizo@gmail.com>
2023-08-18 04:06:48 -0400

    linting fix

--- web/client-api/src/main/java/io/deephaven/web/client/api/subscription/SubscriptionTableData.java
+++ web/client-api/src/main/java/io/deephaven/web/client/api/subscription/SubscriptionTableData.java
@@ -509,20 +509,20 @@ public class SubscriptionTableData {
         }
     }
 
-        /**
-         * Event data, describing the indexes that were added/removed/updated, and providing access to Rows (and thus data
-         * in columns) either by index, or scanning the complete present index.
-         * <p>
-         * This class supports two ways of reading the table - checking the changes made since the last update, and reading
-         * all data currently in the table. While it is more expensive to always iterate over every single row in the table,
-         * it may in some cases actually be cheaper than maintaining state separately and updating only the changes, though
-         * both options should be considered.
-         * <p>
-         * The RangeSet objects allow iterating over the LongWrapper indexes in the table. Note that these "indexes" are not
-         * necessarily contiguous and may be negative, and represent some internal state on the server, allowing it to keep
-         * track of data efficiently. Those LongWrapper objects can be passed to the various methods on this instance to
-         * read specific rows or cells out of the table.
-         */
+    /**
+     * Event data, describing the indexes that were added/removed/updated, and providing access to Rows (and thus data
+     * in columns) either by index, or scanning the complete present index.
+     * <p>
+     * This class supports two ways of reading the table - checking the changes made since the last update, and reading
+     * all data currently in the table. While it is more expensive to always iterate over every single row in the table,
+     * it may in some cases actually be cheaper than maintaining state separately and updating only the changes, though
+     * both options should be considered.
+     * <p>
+     * The RangeSet objects allow iterating over the LongWrapper indexes in the table. Note that these "indexes" are not
+     * necessarily contiguous and may be negative, and represent some internal state on the server, allowing it to keep
+     * track of data efficiently. Those LongWrapper objects can be passed to the various methods on this instance to
+     * read specific rows or cells out of the table.
+     */
     @TsInterface
     @TsName(name = "SubscriptionTableData", namespace = "dh")
     public class UpdateEventData implements TableData {
