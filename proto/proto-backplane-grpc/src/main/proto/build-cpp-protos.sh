efa8c1690888ecae3ba2076e5ea994b86aef4a26
Cristian Ferretti <cristianferretti@deephaven.io>
2023-08-20 12:09:47 -0400

    Reflect change of dirname in build-cpp-protos.sh.

--- proto/proto-backplane-grpc/src/main/proto/build-cpp-protos.sh
+++ proto/proto-backplane-grpc/src/main/proto/build-cpp-protos.sh
@@ -7,4 +7,4 @@ set -euxo pipefail
 
 mkdir -p "${CPP_PROTO_BUILD_DIR}"
 $DHCPP_LOCAL/protobuf/bin/protoc `find . -name \*.proto` --cpp_out=${CPP_PROTO_BUILD_DIR} --grpc_out=${CPP_PROTO_BUILD_DIR} --plugin=protoc-gen-grpc=$DHCPP_LOCAL/grpc/bin/grpc_cpp_plugin
-mv -f ${CPP_PROTO_BUILD_DIR}/deephaven/proto/*.{h,cc} ../../../../../cpp-client/deephaven/client/proto/deephaven/proto
+mv -f ${CPP_PROTO_BUILD_DIR}/deephaven/proto/*.{h,cc} ../../../../../cpp-client/deephaven/dhclient/proto/deephaven/proto
