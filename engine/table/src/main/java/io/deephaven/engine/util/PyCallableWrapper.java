b4c1830b30b98f54e40e9910cc3908ff38b82847
rbasralian <rbasralian@deephaven.io>
2023-08-31 09:09:42 -0700

    Modify the abstract syntax tree in language parser (#3157)
    * Port parser AST modification change from DHE, as well as additional tests. Works except perhaps implicit python calls. "Comments" on nodes are used as internal flags. Primitive booleans are boxed slightly less frequently.
    
    * spotless
    
    * Fix bugs where a child's new parent is lost when inserting a new node between the parent and child. Fix overaggressive assertion. Implement `replaceChildExpressions` with `Node.replace()`
    
    * Handle AST modification for implicit calls (including Python). Add tests for Python calls. Add idempotence check (as sanity check).
    
    * Handle Python field access. Add more PyObject tests.
    
    * tests for equals/not-equals null, by type
    
    * replace some new RuntimeExceptions with custom exceptions
    
    * fix python call vectorization
    
    * simplify some `MethodCallExpr` constructor calls
    
    * fix comments split up by spotless
    
    * remove some unnecessary `else`s
    
    * replace some RuntimeExceptions with UnsupportedOperationExceptions
    
    * replace RuntimeExceptions with more specific ones
    
    * Update engine/table/src/main/java/io/deephaven/engine/table/impl/lang/QueryLanguageParser.java
    
    Co-authored-by: Nate Bauernfeind <nate.bauernfeind@gmail.com>
    
    * comment out non-functional test
    
    * improve code coverage. don't replace primitive boolean BinaryExprs with method calls when we don't have to.
    
    * work around setArguments() bug more completely. add more tests.
    
    * regenerate sample files. (looks like a non-null-safe int-to-long conversion got fixed.)
    
    * Make more fields immutable. Don't auto-simplify (or expand) imported names in casts; keep user's original code. Don't include non-static members when processing static imports. Add tests for class imports and static imports. Clean up misc. warnings/typos.
    
    * fix capitalization typo
    
    * fix bug with generic array return types
    
    * update TODOs with tickets
    
    * update another TODO
    
    * cleanup after merge
    
    * fix parameter selection between boxed and primitive overloads
    
    * some changes from review. run spotless.
    
    * Apply suggestions from code review
    
    Co-authored-by: Colin Alworth <colin@vertispan.com>
    
    * add javadocs for lookupStaticImport. cache static import lookup results.
    
    * fix handling of null arguments
    
    * comment
    
    * remove a TODO
    
    * add a cache for resolved names, too
    
    * fix still-overly-complicated check
    
    * spotless
    
    * #3267 Support testing of Python function calls from TestQueryLanguageParser, without Python env. Also, fix a StackOverflow on explicit calls to PyCallableWrapper.getAttribute().
    
    * remove testOverrideClassLookups (not needed after PyCallableWrapper refactoring)
    
    * Replace magic Comments with DataKeys and setData() calls.
    
    * Fix return types for methods that return generic arrays
    
    * update Logic.or(boolean[]) to iterate with unboxed boolean
    
    ---------
    
    Co-authored-by: Nate Bauernfeind <nate.bauernfeind@gmail.com>
    Co-authored-by: Colin Alworth <colin@vertispan.com>

--- engine/table/src/main/java/io/deephaven/engine/util/PyCallableWrapper.java
+++ engine/table/src/main/java/io/deephaven/engine/util/PyCallableWrapper.java
@@ -1,80 +1,36 @@
 package io.deephaven.engine.util;
 
-import io.deephaven.engine.table.impl.select.python.ArgumentsChunked;
-import io.deephaven.internal.log.LoggerFactory;
-import io.deephaven.io.logger.Logger;
-import org.jpy.PyModule;
 import org.jpy.PyObject;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
- * When given a pyObject that is a callable, we stick it inside the callable wrapper, which implements a call() varargs
- * method, so that we can call it using __call__ without all of the JPy nastiness.
+ * Created by rbasralian on 8/12/23
  */
-public class PyCallableWrapper {
-    private static final Logger log = LoggerFactory.getLogger(PyCallableWrapper.class);
+public interface PyCallableWrapper {
+    PyObject getAttribute(String name);
 
-    private static final PyObject NUMBA_VECTORIZED_FUNC_TYPE = getNumbaVectorizedFuncType();
+    <T> T getAttribute(String name, Class<? extends T> valueType);
 
-    private static final PyModule dh_table_module = PyModule.importModule("deephaven.table");
+    void parseSignature();
 
-    private static final Map<Character, Class<?>> numpyType2JavaClass = new HashMap<>();
+    Object call(Object... args);
 
-    static {
-        numpyType2JavaClass.put('i', int.class);
-        numpyType2JavaClass.put('l', long.class);
-        numpyType2JavaClass.put('h', short.class);
-        numpyType2JavaClass.put('f', float.class);
-        numpyType2JavaClass.put('d', double.class);
-        numpyType2JavaClass.put('b', byte.class);
-        numpyType2JavaClass.put('?', boolean.class);
-        numpyType2JavaClass.put('U', String.class);
-        numpyType2JavaClass.put('O', Object.class);
-    }
+    List<Class<?>> getParamTypes();
 
-    private final PyObject pyCallable;
+    boolean isVectorized();
 
-    private String signature = null;
-    private List<Class<?>> paramTypes;
-    private Class<?> returnType;
-    private boolean vectorizable = false;
-    private boolean vectorized = false;
-    private Collection<ChunkArgument> chunkArguments;
-    private boolean numbaVectorized;
-    private PyObject unwrapped;
+    boolean isVectorizable();
 
-    public PyCallableWrapper(PyObject pyCallable) {
-        this.pyCallable = pyCallable;
-    }
+    void setVectorizable(boolean vectorizable);
 
-    public PyObject getAttribute(String name) {
-        return this.pyCallable.getAttribute(name);
-    }
+    void initializeChunkArguments();
 
-    public <T> T getAttribute(String name, Class<? extends T> valueType) {
-        return this.pyCallable.getAttribute(name, valueType);
-    }
+    void addChunkArgument(ChunkArgument chunkArgument);
 
-    public ArgumentsChunked buildArgumentsChunked(List<String> columnNames) {
-        for (ChunkArgument arg : chunkArguments) {
-            if (arg instanceof ColumnChunkArgument) {
-                String columnName = ((ColumnChunkArgument) arg).getColumnName();
-                int chunkSourceIndex = columnNames.indexOf(columnName);
-                if (chunkSourceIndex < 0) {
-                    throw new IllegalArgumentException("Column source not found: " + columnName);
-                }
-                ((ColumnChunkArgument) arg).setSourceChunkIndex(chunkSourceIndex);
-            }
-        }
-        return new ArgumentsChunked(chunkArguments, returnType, numbaVectorized);
-    }
+    Class<?> getReturnType();
 
-    public static abstract class ChunkArgument {
+    abstract class ChunkArgument {
         private final Class<?> type;
 
         public Class<?> getType() {
@@ -86,7 +42,8 @@ public class PyCallableWrapper {
         }
     }
 
-    public static class ColumnChunkArgument extends ChunkArgument {
+    class ColumnChunkArgument extends ChunkArgument {
+
         private final String columnName;
         private int sourceChunkIndex;
         private boolean resolved = false;
@@ -114,7 +71,7 @@ public class PyCallableWrapper {
         }
     }
 
-    public static class ConstantChunkArgument extends ChunkArgument {
+    class ConstantChunkArgument extends ChunkArgument {
         private final Object value;
 
         public ConstantChunkArgument(Object value, Class<?> type) {
@@ -125,137 +82,6 @@ public class PyCallableWrapper {
         public Object getValue() {
             return value;
         }
-    }
-
-    /**
-     * This assumes that the Python interpreter won't be re-initialized during a session, if this turns out to be a
-     * false assumption, then we'll need to make this initialization code 'python restart' proof.
-     */
-    private static PyObject getNumbaVectorizedFuncType() {
-        try {
-            return PyModule.importModule("numba.np.ufunc.dufunc").getAttribute("DUFunc");
-        } catch (Exception e) {
-            if (log.isDebugEnabled()) {
-                log.debug("Numba isn't installed in the Python environment.");
-            }
-            return null;
-        }
-    }
-
-    private void prepareSignature() {
-        if (pyCallable.getType().equals(NUMBA_VECTORIZED_FUNC_TYPE)) {
-            List<PyObject> params = pyCallable.getAttribute("types").asList();
-            if (params.isEmpty()) {
-                throw new IllegalArgumentException(
-                        "numba vectorized function must have an explicit signature: " + pyCallable);
-            }
-            // numba allows a vectorized function to have multiple signatures
-            if (params.size() > 1) {
-                throw new UnsupportedOperationException(
-                        pyCallable
-                                + " has multiple signatures; this is not currently supported for numba vectorized functions");
-            }
-            signature = params.get(0).getStringValue();
-            unwrapped = null;
-            numbaVectorized = true;
-            vectorized = true;
-        } else if (pyCallable.hasAttribute("dh_vectorized")) {
-            signature = pyCallable.getAttribute("signature").toString();
-            unwrapped = pyCallable.getAttribute("callable");
-            numbaVectorized = false;
-            vectorized = true;
-        } else {
-            signature = dh_table_module.call("_encode_signature", pyCallable).toString();
-            unwrapped = pyCallable;
-            numbaVectorized = false;
-            vectorized = false;
-        }
-    }
-
-    public void parseSignature() {
-        if (signature != null) {
-            return;
-        }
-
-        prepareSignature();
-
-        // the 'types' field of a vectorized function follows the pattern of '[ilhfdb?O]*->[ilhfdb?O]',
-        // eg. [ll->d] defines two int64 (long) arguments and a double return type.
-        if (signature == null || signature.isEmpty()) {
-            throw new IllegalStateException("Signature should always be available.");
-        }
-
-        char numpyTypeCode = signature.charAt(signature.length() - 1);
-        Class<?> returnType = numpyType2JavaClass.get(numpyTypeCode);
-        if (returnType == null) {
-            throw new IllegalStateException(
-                    "Vectorized functions should always have an integral, floating point, boolean, String, or Object return type: "
-                            + numpyTypeCode);
-        }
-
-        List<Class<?>> paramTypes = new ArrayList<>();
-        for (char numpyTypeChar : signature.toCharArray()) {
-            if (numpyTypeChar != '-') {
-                Class<?> paramType = numpyType2JavaClass.get(numpyTypeChar);
-                if (paramType == null) {
-                    throw new IllegalStateException(
-                            "Parameters of vectorized functions should always be of integral, floating point, boolean, String, or Object type: "
-                                    + numpyTypeChar + " of " + signature);
-                }
-                paramTypes.add(paramType);
-            } else {
-                break;
-            }
-        }
-
-        this.paramTypes = paramTypes;
-        if (returnType == Object.class) {
-            this.returnType = PyObject.class;
-        } else if (returnType == boolean.class) {
-            this.returnType = Boolean.class;
-        } else {
-            this.returnType = returnType;
-        }
-    }
-
-    public PyObject vectorizedCallable() {
-        if (numbaVectorized) {
-            return pyCallable;
-        } else {
-            return dh_table_module.call("dh_vectorize", unwrapped);
-        }
-    }
-
-    public Object call(Object... args) {
-        return PythonScopeJpyImpl.convert(pyCallable.callMethod("__call__", args));
-    }
-
-    public List<Class<?>> getParamTypes() {
-        return paramTypes;
-    }
-
-    public boolean isVectorized() {
-        return vectorized;
-    }
-
-    public boolean isVectorizable() {
-        return vectorizable;
-    }
-
-    public void setVectorizable(boolean vectorizable) {
-        this.vectorizable = vectorizable;
-    }
-
-    public void initializeChunkArguments() {
-        this.chunkArguments = new ArrayList<>();
-    }
-
-    public void addChunkArgument(ChunkArgument chunkArgument) {
-        this.chunkArguments.add(chunkArgument);
-    }
-
-    public Class<?> getReturnType() {
-        return returnType;
     }
 
 }
