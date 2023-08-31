0fdf7d0b25e8f2af7c258ce384c2a5dc3e37c056
Cristian Ferretti <cristianferretti@deephaven.io>
2023-08-30 23:31:32 -0400

    Followup to comment on small objects.

--- cpp-client/deephaven/dhclient/include/public/deephaven/client/utility/misc_types.h
+++ cpp-client/deephaven/dhclient/include/public/deephaven/client/utility/misc_types.h
@@ -28,7 +28,7 @@ using DurationSpecifier = std::variant<std::chrono::nanoseconds, int64_t, std::s
  */
 struct OnCloseCbId {
   std::uint32_t id;
-  friend bool operator<(const OnCloseCbId &lhs, const OnCloseCbId &rhs) {
+  friend bool operator<(const OnCloseCbId lhs, const OnCloseCbId rhs) {
     return lhs.id < rhs.id;
   }
 };
