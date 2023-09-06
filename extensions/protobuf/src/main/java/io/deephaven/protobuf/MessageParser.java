56b713d81edd9763b2a61b9db868a01fa3cb533f
Devin Smith <devinsmith@deephaven.io>
2023-09-06 15:07:00 -0700

    Add BytesValue javadoc about ByteString

--- extensions/protobuf/src/main/java/io/deephaven/protobuf/MessageParser.java
+++ extensions/protobuf/src/main/java/io/deephaven/protobuf/MessageParser.java
@@ -66,7 +66,7 @@ public interface MessageParser {
      * </tr>
      * <tr>
      * <td>{@link com.google.protobuf.BytesValue}</td>
-     * <td>{@code byte[]}</td>
+     * <td>{@code byte[]} (or {@link com.google.protobuf.ByteString}, see {@link FieldOptions#bytes()})</td>
      * </tr>
      * <tr>
      * <td>{@link com.google.protobuf.Any}</td>
