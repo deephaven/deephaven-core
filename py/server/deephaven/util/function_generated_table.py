a0ad88f95550d56257bccb507624dbf35e253173
rbasralian <rbasralian@deephaven.io>
2023-09-06 21:30:03 -0400

    python documentation update

--- py/server/deephaven/util/function_generated_table.py
+++ py/server/deephaven/util/function_generated_table.py
@@ -22,15 +22,25 @@ def create(table_generator: Callable[[], Table],
     'source_tables' ticks or (b) after refresh_interval_ms have elapsed. Either 'refresh_interval_ms' or
     'source_tables' must be set (but not both).
 
+    Function-generated tables can be used to produce dynamic tables from sources outside Deephaven. For example,
+    function-generated tables can create tables that are produced by arbitrary Python logic (including using Pandas or
+    numpy). They can also be used to retrieve data from external sources (such as files or websites).
+
     The table definition must not change between invocations of the 'table_generator' function.
 
     Note that any tables used by the 'table_generator' function *MUST* be specified in the 'source_tables'.
 
-    :param table_generator: The table generator function. This function must return a Table.
-    :param source_tables: Source tables used by the 'table_generator' function. The 'table_generator' is rerun when any of these tables tick.
-    :param refresh_interval_ms: Interval (in milliseconds) at which the 'table_generator' function is rerun.
-    :param exec_context: Optionally, a custom execution context. If 'None', the current execution context is used.
-    :return: A Table that is automatically regenerated when any of the source_tables tick. This table will be regenerated *after* any changes to the source_tables have been processed.
+    Args:
+        table_generator (Callable[[], Table]): The table generator function. This function must return a Table.
+        source_tables (Union[Table, List[Table]]): Source tables used by the 'table_generator' function. The
+            'table_generator' is rerun when any of these tables tick.
+        refresh_interval_ms (int): Interval (in milliseconds) at which the 'table_generator' function is rerun.
+        exec_context (ExecutionContext) -> Table): Optionally, a custom execution context. If 'None', the current
+            execution context is used.
+
+    Returns:
+        A Table that is automatically regenerated when any of the source_tables tick. This table will be regenerated
+        *after* any changes to the source_tables have been processed.
     """
 
     if refresh_interval_ms is None and source_tables is None:
