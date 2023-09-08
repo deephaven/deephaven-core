58aeb5f1cdb071c9a56402d1e139072b1981fddc
alexpeters1208 <alexscottpeters1208@gmail.com>
2023-09-08 16:52:09 -0500

    Remove RecordBatchStreamReader directive

--- R/rdeephaven/R/exports.R
+++ R/rdeephaven/R/exports.R
@@ -2,7 +2,7 @@
 #' @useDynLib rdeephaven, .registration = TRUE
 #' @importFrom Rcpp evalCpp
 #'
-#' @importFrom arrow RecordBatchStreamReader, arrow_table as_arrow_table as_record_batch_reader
+#' @importFrom arrow arrow_table as_arrow_table as_record_batch_reader
 #' @importFrom dplyr as_tibble as_data_frame
 
 loadModule("DeephavenInternalModule", TRUE)
