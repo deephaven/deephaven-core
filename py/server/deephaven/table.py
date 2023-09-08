e2883b7a3bce06a103007f14b35cd5f53b8adc2b
Ethan Alvizo <ethanalvizo@gmail.com>
2023-09-08 12:04:25 -0400

    undo rebase mistakes

--- py/server/deephaven/table.py
+++ py/server/deephaven/table.py
@@ -43,6 +43,7 @@ _JPair = jpy.get_type("io.deephaven.api.Pair")
 _JLayoutHintBuilder = jpy.get_type("io.deephaven.engine.util.LayoutHintBuilder")
 _JSearchDisplayMode = jpy.get_type("io.deephaven.engine.util.LayoutHintBuilder$SearchDisplayModes")
 _JSnapshotWhenOptions = jpy.get_type("io.deephaven.api.snapshot.SnapshotWhenOptions")
+_JBlinkTableTools = jpy.get_type("io.deephaven.engine.table.impl.BlinkTableTools")
 _JAxisOption = jpy.get_type("io.deephaven.engine.table.Table$DataBarAxisOptions")
 _JValuePlacementOption = jpy.get_type("io.deephaven.engine.table.Table$DataBarValuePlacementOptions")
 _JDirectionOption = jpy.get_type("io.deephaven.engine.table.Table$DataBarDirectionOptions")
@@ -73,6 +74,11 @@ _JNodeType = jpy.get_type("io.deephaven.engine.table.hierarchical.RollupTable$No
 _JFormatOperationsRecorder = jpy.get_type("io.deephaven.engine.table.hierarchical.FormatOperationsRecorder")
 _JSortOperationsRecorder = jpy.get_type("io.deephaven.engine.table.hierarchical.SortOperationsRecorder")
 _JFilterOperationsRecorder = jpy.get_type("io.deephaven.engine.table.hierarchical.FilterOperationsRecorder")
+
+# MultiJoin Table and input
+_JMultiJoinInput = jpy.get_type("io.deephaven.engine.table.MultiJoinInput")
+_JMultiJoinTable = jpy.get_type("io.deephaven.engine.table.MultiJoinTable")
+_JMultiJoinFactory = jpy.get_type("io.deephaven.engine.table.MultiJoinFactory")
 
 # For unittest vectorization
 _test_vectorization = False
@@ -569,6 +575,11 @@ class Table(JObjectWrapper):
         if self._is_refreshing is None:
             self._is_refreshing = self.j_table.isRefreshing()
         return self._is_refreshing
+
+    @property
+    def is_blink(self) -> bool:
+        """Whether this table is a blink table."""
+        return _JBlinkTableTools.isBlink(self.j_table)
 
     @property
     def update_graph(self) -> UpdateGraph:
