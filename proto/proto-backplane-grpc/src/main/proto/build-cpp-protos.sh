d208c704d54ef80c0ad21f70710dd8c8a7b2e299
Cristian Ferretti <cristianferretti@deephaven.io>
2023-09-15 18:03:05 -0400

    Followup to review comment: build-cpp-protos.sh.

--- proto/proto-backplane-grpc/src/main/proto/build-cpp-protos.sh
+++ proto/proto-backplane-grpc/src/main/proto/build-cpp-protos.sh
@@ -2,14 +2,15 @@
 
 set -euxo pipefail
 
-: ${DHCPP_LOCAL:=$HOME/dhcpp}
+: ${DHCPP:=$HOME/dhcpp}
+: ${PROTOC_BIN:=$DHCPP/bin}
 : ${CPP_PROTO_BUILD_DIR:=build}
 
 mkdir -p "${CPP_PROTO_BUILD_DIR}"
-$DHCPP_LOCAL/bin/protoc \
+$DHCPP/bin/protoc \
     $(find . -name '*.proto') \
     --cpp_out=${CPP_PROTO_BUILD_DIR} \
     --grpc_out=${CPP_PROTO_BUILD_DIR} \
-    --plugin=protoc-gen-grpc=$DHCPP_LOCAL/bin/grpc_cpp_plugin
+    --plugin=protoc-gen-grpc=${PROTOC_BIN}/grpc_cpp_plugin
 mv -f ${CPP_PROTO_BUILD_DIR}/deephaven/proto/*.{h,cc} \
    ../../../../../cpp-client/deephaven/dhclient/proto/deephaven/proto
