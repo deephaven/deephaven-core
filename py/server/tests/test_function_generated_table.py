43efedead978827d9f043891c82b5ed0ecf3448c
rbasralian <rbasralian@deephaven.io>
2023-09-15 10:14:42 -0400

    Look up row keys properly instead of using `.getColumnSource().get(0)`. Include `function_generated_table` in `__all__`.

--- py/server/tests/test_function_generated_table.py
+++ py/server/tests/test_function_generated_table.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+from typing import Any
 
 import deephaven.dtypes as dht
 from deephaven import empty_table, input_table, new_table, update_graph, function_generated_table
 from deephaven.column import string_col, int_col
 from deephaven.execution_context import get_exec_ctx
+from deephaven.table import Table
 from tests.testbase import BaseTestCase
 
 
@@ -23,12 +25,14 @@ class TableTestCase(BaseTestCase):
         with update_graph.exclusive_lock(self.test_update_graph):
             result_table = function_generated_table(table_generator_function, refresh_interval_ms=2000)
             self.assertEqual(result_table.size, 1)
-            initial_time = result_table.j_table.getColumnSource("Timestamp").get(0)
+            first_row_key = get_row_key(0, result_table)
+            initial_time = result_table.j_table.getColumnSource("Timestamp").get(first_row_key)
 
             if not result_table.await_update(5_000):
                 raise RuntimeError("Result table did not update within 5 seconds")
 
-            later_time = result_table.j_table.getColumnSource("Timestamp").get(0)
+            first_row_key = get_row_key(0, result_table)
+            later_time = result_table.j_table.getColumnSource("Timestamp").get(first_row_key)
 
         # Make sure it ticked at least once within 5 seconds. It should have ticked twice,
         # but leaving a wider margin to ensure the test passes -- as long as it ticks at all
@@ -55,7 +59,8 @@ class TableTestCase(BaseTestCase):
         self.wait_ticking_table_update(result_table, row_count=1, timeout=30)
         print("result_table has 1 row at time: " + str(datetime.now()))
 
-        result_str = result_table.j_table.getColumnSource("ResultStr").get(0)
+        first_row_key = get_row_key(0, result_table)
+        result_str = result_table.j_table.getColumnSource("ResultStr").get(first_row_key)
         self.assertEqual(result_str, 'test string')
 
     def test_generated_table_2triggers(self):
@@ -66,8 +71,11 @@ class TableTestCase(BaseTestCase):
         append_only_input_table2 = input_table(col_defs={"MyInt": dht.int32})
 
         def table_generator_function():
-            my_str = append_only_input_table1.last_by().j_table.getColumnSource('MyStr').get(0)
-            my_int = append_only_input_table2.last_by().j_table.getColumnSource('MyInt').getInt(0)
+            t1 = append_only_input_table1.last_by()
+            t2 = append_only_input_table2.last_by()
+
+            my_str = t1.j_table.getColumnSource('MyStr').get(get_row_key(0, t1))
+            my_int = t2.j_table.getColumnSource('MyInt').getInt(get_row_key(0, t2))
 
             return new_table([
                 string_col('ResultStr', [my_str]),
@@ -78,8 +86,9 @@ class TableTestCase(BaseTestCase):
                                                 source_tables=[append_only_input_table1, append_only_input_table2])
 
         self.assertEqual(result_table.size, 1)
-        result_str = result_table.j_table.getColumnSource("ResultStr").get(0)
-        result_int = result_table.j_table.getColumnSource("ResultInt").get(0)
+        first_row_key = get_row_key(0, result_table)
+        result_str = result_table.j_table.getColumnSource("ResultStr").get(first_row_key)
+        result_int = result_table.j_table.getColumnSource("ResultInt").get(first_row_key)
         self.assertEqual(result_str, None)
         self.assertEqual(result_int, None)
 
@@ -87,15 +96,22 @@ class TableTestCase(BaseTestCase):
             append_only_input_table1.add(new_table([string_col(name='MyStr', data=['test string'])]))
 
             self.assertEqual(result_table.size, 1)
-            result_str = result_table.j_table.getColumnSource("ResultStr").get(0)
-            result_int = result_table.j_table.getColumnSource("ResultInt").get(0)
+            first_row_key = get_row_key(0, result_table)
+            result_str = result_table.j_table.getColumnSource("ResultStr").get(first_row_key)
+            result_int = result_table.j_table.getColumnSource("ResultInt").get(first_row_key)
             self.assertEqual(result_str, 'test string')
             self.assertEqual(result_int, None)
 
             append_only_input_table2.add(new_table([int_col(name='MyInt', data=[12345])]))
 
             self.assertEqual(result_table.size, 1)
-            result_str = result_table.j_table.getColumnSource("ResultStr").get(0)
-            result_int = result_table.j_table.getColumnSource("ResultInt").get(0)
+
+            first_row_key = get_row_key(0, result_table)
+            result_str = result_table.j_table.getColumnSource("ResultStr").get(first_row_key)
+            result_int = result_table.j_table.getColumnSource("ResultInt").get(first_row_key)
             self.assertEqual(result_str, 'test string')
             self.assertEqual(result_int, 12345)
+
+
+def get_row_key(row_position: int, t: Table) -> Any:
+    return t.j_table.getRowSet().get(row_position)
