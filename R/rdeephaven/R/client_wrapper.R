dc06185f4cf9c16bf57c9b095da31c0581499a41
alexpeters1208 <alexscottpeters1208@gmail.com>
2023-09-18 17:44:06 -0500

    Make minimal changes

--- R/rdeephaven/R/client_wrapper.R
+++ R/rdeephaven/R/client_wrapper.R
@@ -1,12 +1,7 @@
-#' @title The Deephaven R Client
 #' @description
-#' A Client is the entry point for interacting with the Deephaven server.
-#' It is used to create new tables, import data to and export data from the server,
-#' and run queries on the server.
-#'
-#' @usage NULL
-#' @format NULL
-#' @docType class
+#' A Client is the entry point for interacting with the Deephaven server. It is used to create new tables,
+#' import data to and export data from the server, and run queries on the server.
+#' @export
 Client <- R6Class("Client",
   cloneable = FALSE,
   public = list(
