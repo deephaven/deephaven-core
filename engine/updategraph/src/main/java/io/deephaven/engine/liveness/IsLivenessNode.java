219b6b3668e829fb215f093fbdbd172f5d07403d
Colin Alworth <colinalworth@deephaven.io>
2023-10-05 15:50:39 -0500

    Added javadoc

--- engine/updategraph/src/main/java/io/deephaven/engine/liveness/IsLivenessNode.java
+++ engine/updategraph/src/main/java/io/deephaven/engine/liveness/IsLivenessNode.java
@@ -5,7 +5,17 @@ import org.jetbrains.annotations.NotNull;
 import java.lang.ref.WeakReference;
 import java.util.stream.Stream;
 
+/**
+ * Indicates that this class implements LivenessNode via a member rather than implementing it directly. The real
+ * LivenessNode is exposed via {@link #asLivenessNode()}, all other methods delegate to this instance.
+ */
 public interface IsLivenessNode extends LivenessNode {
+    /**
+     * Returns the "real" {@link LivenessNode} instance. When implementing this, care should be taken
+     * to match lifecycle of the {@code IsLivenessNode} instance with this instance, as the returned
+     * {@code LivenessNode} is behaves as a proxy for {@code this}.
+     * @return a LivenessNode to use to manage this object's liveness.
+     */
     LivenessNode asLivenessNode();
 
     @Override
