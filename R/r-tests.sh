ef1351c3d3e88f842787665bfa3399786879cfa1
Cristian Ferretti <cristianferretti@deephaven.io>
2023-10-12 21:10:36 -0400

    Now it works

--- R/r-tests.sh
+++ R/r-tests.sh
@@ -17,23 +17,18 @@ cd $DH_PREFIX/src/rdeephaven
 OUT_XML="$1"
 OUT_LOG="$2"
 
-R --no-save <<"EOF" >& "${OUT_LOG}"
+R --no-save --no-restore <<EOF >& "${OUT_LOG}"
 library('testthat')
 options(testthat.output_file = '${OUT_XML}')
-test_package('rdeephaven', reporter = 'junit')
+status = tryCatch(
+  {
+     test_package('rdeephaven', reporter = 'junit')
+     0
+  },
+  error=function(e) 1
+)
+print(paste0('status=', status))
+quit(save='no', status=status)
 EOF
-
-if [ ! -f "${OUT_XML}" ]; then
-    echo "$0: No test result was produced, failed." 1>&2
-    exit 1
-fi
-
-NFAILURES=$(grep -o 'failures="[0-9]*"' | grep -v 'failures="0"' | wc -l)
-NERRORS=$(grep -o 'errors="[0-9]*' | grep -v 'errors="0"' | wc -l)
-
-if [ "${NFAILURES}" -ne 0 ] || [ "${NERRORS}" -ne 0 ]; then
-    echo "$0: Test run had ${NFAILURES} failures and ${NERRORS} errors, failed." 1>&2
-    exit 1
-fi
 
 exit 0
