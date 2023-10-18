e0e968e01f69c426788be26ebd76395c147aba8b
Devin Smith <devinsmith@deephaven.io>
2023-10-18 16:05:10 -0700

    Add KafkaTools.Consume#objectProcessorSpec for custom KeyOrValueSpecs (#4648)
    This introduces a new public interface, ObjectProcessor, that provides a means for end-users to create custom KeyOrValueSpec for use with Kafka ingestion. The interface isn't specific to kafka though, and should be extensible to other domains in the future.
    
    The existing KeyOrValue specs are left unchanged; but arguably, there may be value in porting our existing implementations over to it in the future.
    
    Fixes #4346

---
+++ extensions/kafka/src/main/java/io/deephaven/processor/ObjectProcessor.java
@@ -0,0 +1,155 @@
+/**
+ * Copyright (c) 2016-2023 Deephaven Data Labs and Patent Pending
+ */
+package io.deephaven.processor;
+
+import io.deephaven.chunk.ChunkType;
+import io.deephaven.chunk.ObjectChunk;
+import io.deephaven.chunk.WritableChunk;
+import io.deephaven.qst.type.BooleanType;
+import io.deephaven.qst.type.BoxedType;
+import io.deephaven.qst.type.ByteType;
+import io.deephaven.qst.type.CharType;
+import io.deephaven.qst.type.DoubleType;
+import io.deephaven.qst.type.FloatType;
+import io.deephaven.qst.type.GenericType;
+import io.deephaven.qst.type.InstantType;
+import io.deephaven.qst.type.IntType;
+import io.deephaven.qst.type.LongType;
+import io.deephaven.qst.type.ShortType;
+import io.deephaven.qst.type.Type;
+
+import java.time.Instant;
+import java.util.List;
+
+/**
+ * An interface for processing data from one or more input objects into output chunks on a 1-to-1 input record to output
+ * row basis.
+ *
+ * @param <T> the object type
+ */
+public interface ObjectProcessor<T> {
+
+    /**
+     * Creates or returns an implementation that adds strict safety checks around {@code delegate}
+     * {@link #processAll(ObjectChunk, List)}. The may be useful for development or debugging purposes.
+     *
+     * @param delegate the delegate
+     * @return the strict implementation
+     * @param <T> the object type
+     */
+    static <T> ObjectProcessor<T> strict(ObjectProcessor<T> delegate) {
+        return ObjectProcessorStrict.create(delegate);
+    }
+
+    /**
+     * Creates or returns a row-limited implementation. If {@code delegate} has already been limited more than
+     * {@code rowLimit}, {@code delegate} is returned. Otherwise, a row-limited implementation is created that wraps
+     * {@code delegate} and invokes {@link #processAll(ObjectChunk, List) delegate#processAll} with {@code rowLimit}
+     * sized {@code out} chunks, except for the last invocation which may have size less-than {@code rowLimit}.
+     *
+     * <p>
+     * Adding a row-limit may be useful in cases where the input objects are "wide". By limiting the number of rows
+     * considered at any given time, there may be better opportunity for read caching.
+     *
+     * <p>
+     * Callers should typically have knowledge of the {@code delegate} implementation; for example, in cases where the
+     * {@code delegate} implementation already processes data in a row-oriented fashion, adding a row-limit here
+     * introduces extraneous overhead.
+     *
+     * @param delegate the delegate
+     * @param rowLimit the row limit
+     * @return the row-limited processor
+     * @param <T> the object type
+     */
+    static <T> ObjectProcessor<T> rowLimited(ObjectProcessor<T> delegate, int rowLimit) {
+        return ObjectProcessorRowLimited.of(delegate, rowLimit);
+    }
+
+    /**
+     * The relationship between {@link #outputTypes() output types} and the {@link #processAll(ObjectChunk, List)
+     * processAll out param} {@link WritableChunk#getChunkType()}.
+     *
+     * <table>
+     * <tr>
+     * <th>{@link Type}</th>
+     * <th>{@link ChunkType}</th>
+     * </tr>
+     * <tr>
+     * <td>{@link ByteType}</td>
+     * <td>{@link ChunkType#Byte}</td>
+     * </tr>
+     * <tr>
+     * <td>{@link ShortType}</td>
+     * <td>{@link ChunkType#Short}</td>
+     * </tr>
+     * <tr>
+     * <td>{@link IntType}</td>
+     * <td>{@link ChunkType#Int}</td>
+     * </tr>
+     * <tr>
+     * <td>{@link LongType}</td>
+     * <td>{@link ChunkType#Long}</td>
+     * </tr>
+     * <tr>
+     * <td>{@link FloatType}</td>
+     * <td>{@link ChunkType#Float}</td>
+     * </tr>
+     * <tr>
+     * <td>{@link DoubleType}</td>
+     * <td>{@link ChunkType#Double}</td>
+     * </tr>
+     * <tr>
+     * <td>{@link CharType}</td>
+     * <td>{@link ChunkType#Char}</td>
+     * </tr>
+     * <tr>
+     * <td>{@link BooleanType}</td>
+     * <td>{@link ChunkType#Byte} (<b>not</b> {@link ChunkType#Boolean})</td>
+     * </tr>
+     * <tr>
+     * <td>{@link BoxedType}</td>
+     * <td>Same as {@link BoxedType#primitiveType()} would yield.</td>
+     * </tr>
+     * <tr>
+     * <td>{@link InstantType}</td>
+     * <td>{@link ChunkType#Long} ({@link io.deephaven.time.DateTimeUtils#epochNanos(Instant)})</td>
+     * </tr>
+     * <tr>
+     * <td>All other {@link GenericType}</td>
+     * <td>{@link ChunkType#Object}</td>
+     * </tr>
+     * </table>
+     */
+    static ChunkType chunkType(Type<?> type) {
+        return ObjectProcessorTypes.of(type);
+    }
+
+    /**
+     * The logical output types {@code this} instance processes. The size and types correspond to the expected size and
+     * {@link io.deephaven.chunk.ChunkType chunk types} for {@link #processAll(ObjectChunk, List)} as specified by
+     * {@link #chunkType(Type)}.
+     *
+     * @return the output types
+     */
+    List<Type<?>> outputTypes();
+
+    /**
+     * Processes {@code in} into {@code out} by appending {@code in.size()} values to each chunk. The size of each
+     * {@code out} chunk will be incremented by {@code in.size()}. Implementations are free to process the data in a
+     * row-oriented, column-oriented, or mix-oriented fashion. {@code in}, {@code out}, and the elements of {@code out}
+     * are owned by the caller; implementations should not keep references to these. Implementations may copy references
+     * from {@code in} into the elements of {@code out}; for example, if the input type {@code T} is {@code byte[]},
+     * implementations may copy that reference into {@code out}. In general, implementations should document the
+     * references they may copy from {@code in} into {@code out}.
+     *
+     * <p>
+     * If an exception is thrown the output chunks will be in an unspecified state for all rows after their initial
+     * size.
+     *
+     * @param in the input objects
+     * @param out the output chunks as specified by {@link #outputTypes()}; each chunk must have remaining capacity of
+     *        at least {@code in.size()}
+     */
+    void processAll(ObjectChunk<? extends T, ?> in, List<WritableChunk<?>> out);
+}
---
+++ extensions/kafka/src/main/java/io/deephaven/processor/ObjectProcessorStrict.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2016-2023 Deephaven Data Labs and Patent Pending
+ */
+package io.deephaven.processor;
+
+import io.deephaven.UncheckedDeephavenException;
+import io.deephaven.chunk.ChunkType;
+import io.deephaven.chunk.ObjectChunk;
+import io.deephaven.chunk.WritableChunk;
+import io.deephaven.qst.type.Type;
+
+import java.util.List;
+import java.util.Objects;
+
+class ObjectProcessorStrict<T> implements ObjectProcessor<T> {
+
+    static <T> ObjectProcessor<T> create(ObjectProcessor<T> delegate) {
+        if (delegate instanceof ObjectProcessorStrict) {
+            return delegate;
+        }
+        return new ObjectProcessorStrict<>(delegate);
+    }
+
+    private final ObjectProcessor<T> delegate;
+
+    ObjectProcessorStrict(ObjectProcessor<T> delegate) {
+        this.delegate = Objects.requireNonNull(delegate);
+    }
+
+    @Override
+    public List<Type<?>> outputTypes() {
+        return delegate.outputTypes();
+    }
+
+    @Override
+    public void processAll(ObjectChunk<? extends T, ?> in, List<WritableChunk<?>> out) {
+        final int numColumns = delegate.outputTypes().size();
+        if (numColumns != out.size()) {
+            throw new IllegalArgumentException(String.format(
+                    "Expected delegate.outputTypes().size() == out.size(). delegate.outputTypes().size()=%d, out.size()=%d",
+                    numColumns, out.size()));
+        }
+        final int[] originalSizes = new int[numColumns];
+        for (int chunkIx = 0; chunkIx < numColumns; ++chunkIx) {
+            final WritableChunk<?> chunk = out.get(chunkIx);
+            if (chunk.capacity() - chunk.size() < in.size()) {
+                throw new IllegalArgumentException(String.format(
+                        "out chunk does not have enough remaining capacity. chunkIx=%d, in.size()=%d, chunk.size()=%d, chunk.capacity()=%d",
+                        chunkIx, in.size(), chunk.size(), chunk.capacity()));
+            }
+            final Type<?> type = delegate.outputTypes().get(chunkIx);
+            final ChunkType expectedChunkType = ObjectProcessor.chunkType(type);
+            final ChunkType actualChunkType = chunk.getChunkType();
+            if (expectedChunkType != actualChunkType) {
+                throw new IllegalArgumentException(String.format(
+                        "Improper ChunkType. chunkIx=%d, outputType=%s, expectedChunkType=%s, actualChunkType=%s",
+                        chunkIx, type, expectedChunkType, actualChunkType));
+            }
+            originalSizes[chunkIx] = chunk.size();
+        }
+        delegate.processAll(in, out);
+        for (int chunkIx = 0; chunkIx < numColumns; ++chunkIx) {
+            final WritableChunk<?> chunk = out.get(chunkIx);
+            final int expectedSize = originalSizes[chunkIx] + in.size();
+            if (chunk.size() != expectedSize) {
+                throw new UncheckedDeephavenException(String.format(
+                        "Implementation did not increment chunk size correctly. chunkIx=%d, (before) chunk.size()=%d, (after) chunk.size()=%d, in.size()=%d",
+                        chunkIx, originalSizes[chunkIx], chunk.size(), in.size()));
+            }
+        }
+    }
+}
