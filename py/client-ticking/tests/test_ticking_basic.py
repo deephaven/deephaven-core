a48f291dd9f5be126efbc2d8edfe0c9fa8a9233d
Cristian Ferretti <cristianferretti@deephaven.io>
2023-10-23 15:54:42 -0400

    Followup to Jianfeng and Corey's comments.

--- py/client-ticking/tests/test_ticking_basic.py
+++ py/client-ticking/tests/test_ticking_basic.py
@@ -6,18 +6,20 @@ import unittest
 
 import pydeephaven as dh
 import time
+import sys
 
 class TickingBasicTestCase(unittest.TestCase):
     def test_ticking_basic_time_table(self):
         session = dh.Session()
-        half_second_in_nanos = 500 * 1000 * 1000
+        half_second_in_nanos = 200 * 1000 * 1000
         table = session.time_table(period=half_second_in_nanos).update(formulas=["Col1 = i"])
+        session.bind_table(name="my_ticking_table", table=table)
         table_added_last_col1_seen = -1
         table_added_update_count = 0
         def update_table_added(added):
             nonlocal table_added_update_count
             nonlocal table_added_last_col1_seen
-            for value in added.to_pylist():
+            for value in added['Col1'].to_pylist():
                 prev = table_added_last_col1_seen
                 table_added_last_col1_seen = value
                 if prev != -1:
@@ -25,8 +27,19 @@ class TickingBasicTestCase(unittest.TestCase):
             table_added_update_count += 1
         listener_handle = dh.listen(table, lambda update : update_table_added(update.added('Col1')))
         listener_handle.start()
-        time.sleep(3)
-        self.assertTrue(2 >= table_added_update_count)
+        seen_rows = 0
+        start_seconds = time.time()
+        # Wait until we see a given number of updates or timeout.  Note the callback for the updates
+        # is already checking they are of the right form.
+        col1_target = 10
+        timeout_seconds = 10
+        while True:
+            time.sleep(1)
+            if table_added_last_col1_seen >= col1_target:
+                break
+            now_seconds = time.time()
+            self.assertTrue(now_seconds - start_seconds < timeout_seconds)  # eventually fail
+        self.assertTrue(4 >= table_added_update_count)
         listener_handle.stop()
         session.close()
 
