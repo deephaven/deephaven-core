f6dc16c76a3d2a2aea04845a747390b9df5521d2
Nate Bauernfeind <natebauernfeind@deephaven.io>
2023-10-25 17:45:18 -0600

    Vector Expansion Kernel O(n^2) -> O(n) (#4722)

--- extensions/barrage/src/main/java/io/deephaven/extensions/barrage/chunk/vector/ByteVectorExpansionKernel.java
+++ extensions/barrage/src/main/java/io/deephaven/extensions/barrage/chunk/vector/ByteVectorExpansionKernel.java
@@ -18,7 +18,9 @@ import io.deephaven.chunk.WritableIntChunk;
 import io.deephaven.chunk.WritableObjectChunk;
 import io.deephaven.chunk.attributes.Any;
 import io.deephaven.chunk.attributes.ChunkPositions;
-import io.deephaven.chunk.sized.SizedByteChunk;
+import io.deephaven.engine.primitive.function.ByteConsumer;
+import io.deephaven.engine.primitive.iterator.CloseablePrimitiveIteratorOfByte;
+import io.deephaven.util.datastructures.LongSizedDataStructure;
 import io.deephaven.vector.ByteVector;
 import io.deephaven.vector.ByteVectorDirect;
 import io.deephaven.vector.Vector;
@@ -37,24 +39,31 @@ public class ByteVectorExpansionKernel implements VectorExpansionKernel {
         }
 
         final ObjectChunk<ByteVector, A> typedSource = source.asObjectChunk();
-        final SizedByteChunk<A> resultWrapper = new SizedByteChunk<>();
 
-        int lenWritten = 0;
+        long totalSize = 0;
+        for (int i = 0; i < typedSource.size(); ++i) {
+            final ByteVector row = typedSource.get(i);
+            totalSize += row == null ? 0 : row.size();
+        }
+        final WritableByteChunk<A> result = WritableByteChunk.makeWritableChunk(
+                LongSizedDataStructure.intSize("ExpansionKernel", totalSize));
+        result.setSize(0);
+
         perElementLengthDest.setSize(source.size() + 1);
         for (int i = 0; i < typedSource.size(); ++i) {
             final ByteVector row = typedSource.get(i);
-            final int len = row == null ? 0 : row.intSize("ByteVectorExpansionKernel");
-            perElementLengthDest.set(i, lenWritten);
-            final WritableByteChunk<A> result = resultWrapper.ensureCapacityPreserve(lenWritten + len);
-            for (int j = 0; j < len; ++j) {
-                result.set(lenWritten + j, row.get(j));
+            perElementLengthDest.set(i, result.size());
+            if (row == null) {
+                continue;
             }
-            lenWritten += len;
-            result.setSize(lenWritten);
+            final ByteConsumer consumer = result::add;
+            try (final CloseablePrimitiveIteratorOfByte iter = row.iterator()) {
+                iter.forEachRemaining(consumer);
+            }
         }
-        perElementLengthDest.set(typedSource.size(), lenWritten);
+        perElementLengthDest.set(typedSource.size(), result.size());
 
-        return resultWrapper.get();
+        return result;
     }
 
     @Override
@@ -81,15 +90,13 @@ public class ByteVectorExpansionKernel implements VectorExpansionKernel {
 
         int lenRead = 0;
         for (int i = 0; i < itemsInBatch; ++i) {
-            final int ROW_LEN = perElementLengthDest.get(i + 1) - perElementLengthDest.get(i);
-            if (ROW_LEN == 0) {
+            final int rowLen = perElementLengthDest.get(i + 1) - perElementLengthDest.get(i);
+            if (rowLen == 0) {
                 result.set(outOffset + i, ZERO_LENGTH_VECTOR);
             } else {
-                final byte[] row = new byte[ROW_LEN];
-                for (int j = 0; j < ROW_LEN; ++j) {
-                    row[j] = typedSource.get(lenRead + j);
-                }
-                lenRead += ROW_LEN;
+                final byte[] row = new byte[rowLen];
+                typedSource.copyToArray(lenRead, row, 0, rowLen);
+                lenRead += rowLen;
                 result.set(outOffset + i, new ByteVectorDirect(row));
             }
         }
