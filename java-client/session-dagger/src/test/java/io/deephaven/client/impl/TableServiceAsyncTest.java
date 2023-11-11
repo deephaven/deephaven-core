66946684e0981db5f399bf638fe4390c1d7a03f5
Nate Bauernfeind <natebauernfeind@deephaven.io>
2023-11-10 17:45:52 -0700

    `TableServiceAsyncTest`: Eliminate Compiler Overhead Variance; Close `QueryCompiler`'s `JavaFileManager` (#4808)

--- java-client/session-dagger/src/test/java/io/deephaven/client/impl/TableServiceAsyncTest.java
+++ java-client/session-dagger/src/test/java/io/deephaven/client/impl/TableServiceAsyncTest.java
@@ -5,7 +5,12 @@ package io.deephaven.client.impl;
 
 import io.deephaven.client.DeephavenSessionTestBase;
 import io.deephaven.client.impl.TableService.TableHandleFuture;
+import io.deephaven.engine.context.ExecutionContext;
+import io.deephaven.engine.table.Table;
+import io.deephaven.engine.testutil.TstUtils;
+import io.deephaven.engine.util.TableTools;
 import io.deephaven.qst.table.TableSpec;
+import io.deephaven.util.SafeCloseable;
 import org.junit.Test;
 
 import java.time.Duration;
@@ -19,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TableServiceAsyncTest extends DeephavenSessionTestBase {
 
     private static final Duration GETTIME = Duration.ofSeconds(15);
-    private static final int CHAIN_OPS = 50;
+    private static final int CHAIN_OPS = 250;
     private static final int CHAIN_ROWS = 1000;
 
     @Test(timeout = 20000)
@@ -27,7 +32,7 @@ public class TableServiceAsyncTest extends DeephavenSessionTestBase {
         final List<TableSpec> longChain = createLongChain();
         final TableSpec longChainLast = longChain.get(longChain.size() - 1);
         try (final TableHandle handle = get(session.executeAsync(longChainLast))) {
-            checkSucceeded(handle);
+            checkSucceeded(handle, CHAIN_OPS);
         }
     }
 
@@ -36,9 +41,10 @@ public class TableServiceAsyncTest extends DeephavenSessionTestBase {
         final List<TableSpec> longChain = createLongChain();
         final List<? extends TableHandleFuture> futures = session.executeAsync(longChain);
         try {
+            int chainLength = 0;
             for (final TableHandleFuture future : futures) {
                 try (final TableHandle handle = get(future)) {
-                    checkSucceeded(handle);
+                    checkSucceeded(handle, ++chainLength);
                 }
             }
         } catch (final Throwable t) {
@@ -55,7 +61,7 @@ public class TableServiceAsyncTest extends DeephavenSessionTestBase {
         // Cancel or close all but the last one
         TableService.TableHandleFuture.cancelOrClose(futures.subList(0, futures.size() - 1), true);
         try (final TableHandle lastHandle = get(futures.get(futures.size() - 1))) {
-            checkSucceeded(lastHandle);
+            checkSucceeded(lastHandle, CHAIN_OPS);
         }
     }
 
@@ -68,7 +74,7 @@ public class TableServiceAsyncTest extends DeephavenSessionTestBase {
         try (final TableHandle ignored = get(tableService.executeAsync(longChainLast))) {
             for (int i = 0; i < 1000; ++i) {
                 try (final TableHandle handle = get(tableService.executeAsync(longChainLast))) {
-                    checkSucceeded(handle);
+                    checkSucceeded(handle, CHAIN_OPS);
                 }
             }
         }
@@ -79,22 +85,30 @@ public class TableServiceAsyncTest extends DeephavenSessionTestBase {
         return future.getOrCancel(GETTIME);
     }
 
-    private static void checkSucceeded(TableHandle x) {
+    private void checkSucceeded(TableHandle x, int chainLength) {
         assertThat(x.isSuccessful()).isTrue();
+        try (final SafeCloseable ignored = getExecutionContext().open()) {
+            final Table result = serverSessionState.<Table>getExport(x.exportId().id()).get();
+            ExecutionContext.getContext().getQueryScope().putParam("ChainLength", chainLength);
+            final Table expected = TableTools.emptyTable(CHAIN_ROWS).update("Current = ii - 1 + ChainLength");
+            TstUtils.assertTableEquals(expected, result);
+        }
     }
 
     private static List<TableSpec> createLongChain() {
         return createLongChain(CHAIN_OPS, CHAIN_ROWS);
     }
 
-    private static List<TableSpec> createLongChain(int numColumns, int numRows) {
-        final List<TableSpec> longChain = new ArrayList<>(numColumns);
-        for (int i = 0; i < numColumns; ++i) {
+    private static List<TableSpec> createLongChain(int chainLength, int numRows) {
+        final List<TableSpec> longChain = new ArrayList<>(chainLength);
+        for (int i = 0; i < chainLength; ++i) {
             if (i == 0) {
-                longChain.add(TableSpec.empty(numRows).view("I_0=ii"));
+                longChain.add(TableSpec.empty(numRows).view("Current = ii"));
             } else {
                 final TableSpec prev = longChain.get(i - 1);
-                longChain.add(prev.updateView("I_" + i + " = 1 + I_" + (i - 1)));
+                // Note: it's important that this formula is constant with respect to "i", otherwise we'll spend a lot
+                // of time compiling formulas
+                longChain.add(prev.updateView("Current = 1 + Current"));
             }
         }
         return longChain;
