040028cc9f163ca9033141f272c92a10e44a9aaa
jianfengmao <jianfengmao@deephaven.io>
2023-11-14 12:38:13 -0700

    Accept suggested changes to docstrings

--- py/server/deephaven/pandas.py
+++ py/server/deephaven/pandas.py
@@ -127,8 +127,9 @@ def to_pandas(table: Table, cols: List[str] = None,
         dtype_backend (str): which dtype_backend to use, e.g. whether a DataFrame should have NumPy arrays,
             nullable dtypes are used for all dtypes that have a nullable implementation when “numpy_nullable” is set,
             pyarrow is used for all dtypes if “pyarrow” is set. None means Numpy backed DataFrames with no nullable
-            dtypes. Both "numpy_nullable" and "pyarrow" automatically converts Deephaven nulls and enable the correct
-            mapping of Java String. default is "numpy_nullable".
+            dtypes. Both "numpy_nullable" and "pyarrow" automatically convert Deephaven nulls to Pandas NA and enable
+            Pandas extension types.  Extension types are needed to support types beyond NumPy's type system.  Extension
+            types support operations such as properly mapping Java Strings to Python strings. default is "numpy_nullable".
         conv_null (bool): when dtype_backend is not set, whether to check for Deephaven nulls in the data and
             automatically replace them with pd.NA. default is True.
 
