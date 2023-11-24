8cfe8a84d9cf6cd45db653f6e3abce7de4f2b9e9
Nate Bauernfeind <natebauernfeind@deephaven.io>
2023-11-24 14:54:32 -0700

    Introduce Blink-To-Append-Only Memoization (#4880)
    Co-authored-by: Ryan Caudy <rcaudy@gmail.com>

--- engine/table/src/main/java/io/deephaven/engine/table/impl/QueryTable.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/QueryTable.java
@@ -116,15 +116,19 @@ public class QueryTable extends BaseTable<QueryTable> {
          */
         class Result<T extends DynamicNode & NotificationStepReceiver> {
             public final T resultNode;
-            public final TableUpdateListener resultListener; // may be null if parent is non-ticking
+            /**
+             * The listener that should be attached to the parent. The listener may be null if the table does not need
+             * to respond to ticks from other sources (e.g. the parent is non-refreshing).
+             */
+            public final TableUpdateListener resultListener;
 
             public Result(@NotNull final T resultNode) {
                 this(resultNode, null);
             }
 
             /**
-             * Construct the result of an operation. The listener may be null if the parent is non-ticking and the table
-             * does not need to respond to ticks from other sources.
+             * Construct the result of an operation. The listener may be null if the table does not need to respond to
+             * ticks from other sources (e.g. the parent is non-refreshing).
              *
              * @param resultNode the result of the operation
              * @param resultListener the listener that should be attached to the parent (or null)
@@ -3537,8 +3541,7 @@ public class QueryTable extends BaseTable<QueryTable> {
 
                 resultTable.setValue(result.resultNode);
                 if (snapshotControl != null) {
-                    snapshotControl.setListenerAndResult(Require.neqNull(result.resultListener, "resultListener"),
-                            result.resultNode);
+                    snapshotControl.setListenerAndResult(result.resultListener, result.resultNode);
                 }
 
                 return true;
