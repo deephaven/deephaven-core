5dad9fec373228f119cc380b48bebb185a0de86d
Ryan Caudy <ryan@deephaven.io>
2023-11-28 20:42:33 -0500

    Add optional extra dependencies to PartitionedTable.transform and PartitionedTable.p (#4889)

--- engine/table/src/main/java/io/deephaven/engine/table/impl/by/ChunkedOperatorAggregationHelper.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/by/ChunkedOperatorAggregationHelper.java
@@ -1705,6 +1705,10 @@ public class ChunkedOperatorAggregationHelper {
         outputPosition.setValue(0);
         final OperatorAggregationStateManager stateManager = stateManagerSupplier.get();
 
+        if (initialKeys.isEmpty()) {
+            return stateManager;
+        }
+
         final ColumnSource<?>[] keyColumnsToInsert;
         final boolean closeRowsToInsert;
         final RowSequence rowsToInsert;
