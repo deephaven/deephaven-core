0fabbd5bdde39fe7a8a3385385bfd3a52a733071
Colin Alworth <colinalworth@deephaven.io>
2023-12-05 12:42:12 -0600

    Force singleton oitp for tests

--- engine/test-utils/src/main/java/io/deephaven/engine/context/TestExecutionContext.java
+++ engine/test-utils/src/main/java/io/deephaven/engine/context/TestExecutionContext.java
@@ -3,9 +3,12 @@ package io.deephaven.engine.context;
 import io.deephaven.auth.AuthContext;
 import io.deephaven.engine.table.impl.OperationInitializationThreadPool;
 import io.deephaven.engine.testutil.ControlledUpdateGraph;
+import io.deephaven.engine.updategraph.impl.PeriodicUpdateGraph;
 import io.deephaven.util.thread.ThreadInitializationFactory;
 
 public class TestExecutionContext {
+    private static final PeriodicUpdateGraph UPDATE_GRAPH = new PeriodicUpdateGraph("TEST", true, 1000, 25, -1, ThreadInitializationFactory.NO_OP);
+    private static final OperationInitializationThreadPool OPERATION_INITIALIZATION = new OperationInitializationThreadPool(ThreadInitializationFactory.NO_OP);
 
     public static ExecutionContext createForUnitTests() {
         return new ExecutionContext.Builder(new AuthContext.SuperUser())
@@ -13,8 +16,8 @@ public class TestExecutionContext {
                 .newQueryScope()
                 .newQueryLibrary()
                 .setQueryCompiler(QueryCompiler.createForUnitTests())
-                .setUpdateGraph(ControlledUpdateGraph.INSTANCE)
-                .setOperationInitializer(new OperationInitializationThreadPool(ThreadInitializationFactory.NO_OP))
+                .setUpdateGraph(UPDATE_GRAPH)
+                .setOperationInitializer(OPERATION_INITIALIZATION)
                 .build();
     }
 }
