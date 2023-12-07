3abdb9ddf26b4b1938e859e48973a2374c163e98
Charles P. Wright <charleswright@deephaven.io>
2023-12-07 09:58:11 -0500

    remove tests for deleted code.

--- engine/table/src/test/java/io/deephaven/engine/table/impl/util/TestKeyedArrayBackedMutableTable.java
+++ engine/table/src/test/java/io/deephaven/engine/table/impl/util/TestKeyedArrayBackedMutableTable.java
@@ -4,8 +4,6 @@
 package io.deephaven.engine.table.impl.util;
 
 import io.deephaven.UncheckedDeephavenException;
-import io.deephaven.base.SleepUtil;
-import io.deephaven.datastructures.util.CollectionUtil;
 import io.deephaven.engine.context.ExecutionContext;
 import io.deephaven.engine.table.Table;
 import io.deephaven.engine.table.impl.FailureListener;
@@ -13,18 +11,14 @@ import io.deephaven.engine.table.impl.TableUpdateValidator;
 import io.deephaven.engine.testutil.ControlledUpdateGraph;
 import io.deephaven.engine.testutil.junit4.EngineCleanup;
 import io.deephaven.engine.util.TableTools;
-import io.deephaven.engine.util.config.InputTableStatusListener;
 import io.deephaven.engine.util.config.MutableInputTable;
 import io.deephaven.util.function.ThrowingRunnable;
 import junit.framework.TestCase;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 import static io.deephaven.engine.testutil.TstUtils.assertTableEquals;
@@ -142,49 +136,6 @@ public class TestKeyedArrayBackedMutableTable {
         assertTableEquals(input.where("Name != `Fred`"), kabut);
     }
 
-    @Test
-    public void testAddRows() throws Throwable {
-        final Table input = TableTools.newTable(stringCol("Name", "Fred", "George", "Earl"),
-                stringCol("Employer", "Slate Rock and Gravel", "Spacely Sprockets", "Wesayso"));
-
-        final KeyedArrayBackedMutableTable kabut = KeyedArrayBackedMutableTable.make(input, "Name");
-        final TableUpdateValidator validator = TableUpdateValidator.make("kabut", kabut);
-        final Table validatorResult = validator.getResultTable();
-        final FailureListener failureListener = new FailureListener();
-        validatorResult.addUpdateListener(failureListener);
-
-        assertTableEquals(input, kabut);
-
-        final MutableInputTable mutableInputTable = (MutableInputTable) kabut.getAttribute(Table.INPUT_TABLE_ATTRIBUTE);
-        TestCase.assertNotNull(mutableInputTable);
-
-        final Table input2 = TableTools.newTable(stringCol("Name", "Randy"), stringCol("Employer", "USGS"));
-
-        final Map<String, Object> randyMap =
-                CollectionUtil.mapFromArray(String.class, Object.class, "Name", "Randy", "Employer", "USGS");
-        final TestStatusListener listener = new TestStatusListener();
-        mutableInputTable.addRow(randyMap, true, listener);
-        SleepUtil.sleep(100);
-        listener.assertIncomplete();
-        final ControlledUpdateGraph updateGraph = ExecutionContext.getContext().getUpdateGraph().cast();
-        updateGraph.runWithinUnitTestCycle(kabut::run);
-        assertTableEquals(TableTools.merge(input, input2), kabut);
-        listener.waitForCompletion();
-        listener.assertSuccess();
-
-        // TODO: should we throw the exception from the initial palce, should we defer edit checking to the UGP which
-        // would make it consistent, but also slower to produce errors and uglier for reporting?
-        final TestStatusListener listener2 = new TestStatusListener();
-        final Map<String, Object> randyMap2 =
-                CollectionUtil.mapFromArray(String.class, Object.class, "Name", "Randy", "Employer", "Tegridy");
-        mutableInputTable.addRow(randyMap2, false, listener2);
-        SleepUtil.sleep(100);
-        listener2.assertIncomplete();
-        updateGraph.runWithinUnitTestCycle(kabut::run);
-        assertTableEquals(TableTools.merge(input, input2), kabut);
-        listener2.waitForCompletion();
-        listener2.assertFailure(IllegalArgumentException.class, "Can not edit keys Randy");
-    }
 
     @Test
     public void testAddBack() throws Exception {
@@ -212,88 +163,6 @@ public class TestKeyedArrayBackedMutableTable {
 
         handleDelayedRefresh(() -> mutableInputTable.add(input2), kabut);
         assertTableEquals(input2, kabut);
-    }
-
-    @Test
-    public void testSetRows() {
-        final Table input = TableTools.newTable(stringCol("Name", "Fred", "George", "Earl"),
-                stringCol("Employer", "Slate Rock and Gravel", "Spacely Sprockets", "Wesayso"),
-                stringCol("Spouse", "Wilma", "Jane", "Fran"));
-
-        final KeyedArrayBackedMutableTable kabut = KeyedArrayBackedMutableTable.make(input, "Name");
-        final TableUpdateValidator validator = TableUpdateValidator.make("kabut", kabut);
-        final Table validatorResult = validator.getResultTable();
-        final FailureListener failureListener = new FailureListener();
-        validatorResult.addUpdateListener(failureListener);
-
-        assertTableEquals(input, kabut);
-
-        final MutableInputTable mutableInputTable = (MutableInputTable) kabut.getAttribute(Table.INPUT_TABLE_ATTRIBUTE);
-        TestCase.assertNotNull(mutableInputTable);
-
-        final Table defaultValues = input.where("Name=`George`");
-        final Table ex2 = TableTools.newTable(stringCol("Name", "George"), stringCol("Employer", "Cogswell"),
-                stringCol("Spouse", "Jane"));
-
-        final Map<String, Object> cogMap =
-                CollectionUtil.mapFromArray(String.class, Object.class, "Name", "George", "Employer", "Cogswell");
-        mutableInputTable.setRow(defaultValues, 0, cogMap);
-        SleepUtil.sleep(100);
-        final ControlledUpdateGraph updateGraph = ExecutionContext.getContext().getUpdateGraph().cast();
-        updateGraph.runWithinUnitTestCycle(kabut::run);
-        assertTableEquals(TableTools.merge(input, ex2).lastBy("Name"), kabut);
-    }
-
-    private static class TestStatusListener implements InputTableStatusListener {
-        boolean success = false;
-        Throwable error = null;
-
-        @Override
-        public synchronized void onError(Throwable t) {
-            if (success || error != null) {
-                throw new IllegalStateException("Can not complete listener twice!");
-            }
-            error = t;
-            notifyAll();
-        }
-
-        @Override
-        public synchronized void onSuccess() {
-            if (success || error != null) {
-                throw new IllegalStateException("Can not complete listener twice!");
-            }
-            success = true;
-            notifyAll();
-        }
-
-        private synchronized void assertIncomplete() {
-            TestCase.assertFalse(success);
-            TestCase.assertNull(error);
-        }
-
-        private void waitForCompletion() throws InterruptedException {
-            synchronized (this) {
-                while (!success && error == null) {
-                    wait();
-                }
-            }
-        }
-
-        private synchronized void assertSuccess() throws Throwable {
-            if (!success) {
-                throw error;
-            }
-        }
-
-        private synchronized void assertFailure(@NotNull final Class<? extends Throwable> errorClass,
-                @Nullable final String errorMessage) {
-            TestCase.assertFalse(success);
-            TestCase.assertNotNull(error);
-            TestCase.assertTrue(errorClass.isAssignableFrom(error.getClass()));
-            if (errorMessage != null) {
-                TestCase.assertEquals(errorMessage, error.getMessage());
-            }
-        }
     }
 
     public static void handleDelayedRefresh(final ThrowingRunnable<IOException> action,
