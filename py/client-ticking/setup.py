ac9a33665ee901afb337084b7d9387a1420ed01c
Devin Smith <devinsmith@deephaven.io>
2023-12-11 15:46:28 -0800

    Bump to 0.30.4

--- py/client-ticking/setup.py
+++ py/client-ticking/setup.py
@@ -13,7 +13,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='pydeephaven-ticking',
-    version='0.30.3.dev0',
+    version='0.30.4.dev0',
     description='The Deephaven Python Client for Ticking Tables',
     long_description=README,
     long_description_content_type="text/markdown",
@@ -44,5 +44,5 @@ setup(
                    libraries=["dhcore_static"]
         )]),
     python_requires='>=3.8',
-    install_requires=['pydeephaven==0.30.3']
+    install_requires=['pydeephaven==0.30.4']
 )
--- py/server/deephaven/__init__.py
+++ py/server/deephaven/__init__.py
@@ -7,7 +7,7 @@ unlocks the unique power of Deephaven to the Python community.
 
 """
 
-__version__ = "0.30.3"
+__version__ = "0.30.4"
 
 from deephaven_internal import jvm
 
