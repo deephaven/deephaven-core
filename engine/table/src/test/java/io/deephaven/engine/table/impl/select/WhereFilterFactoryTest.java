f811e63008e8fabb278205641c7f893e82f62d45
George Wan <gw9999@outlook.com>
2023-12-22 08:53:47 -0800

    Update engine/table/src/test/java/io/deephaven/engine/table/impl/select/WhereFilterFactoryTest.java
    Co-authored-by: Colin Alworth <colin@vertispan.com>

--- engine/table/src/test/java/io/deephaven/engine/table/impl/select/WhereFilterFactoryTest.java
+++ engine/table/src/test/java/io/deephaven/engine/table/impl/select/WhereFilterFactoryTest.java
@@ -254,7 +254,7 @@ public class WhereFilterFactoryTest extends RefreshingTableTestCase {
         assertEquals(1, idx.size());
         assertEquals(b, DataAccessHelpers.getColumn(t, 0).get(idx.firstRowKey()));
         // match one of two items
-        f = WhereFilterFactory.getExpression("BigDecimal in " + a + ", " + b + "");
+        f = WhereFilterFactory.getExpression("BigDecimal in " + a + ", " + b);
         f.init(t.getDefinition());
         assertEquals(MatchFilter.class, f.getClass());
         idx = f.filter(t.getRowSet().copy(), t.getRowSet(), t, false);
