90c2cb9585d3a2ed60a8555da1151941b131b9ca
Ryan Caudy <ryan@deephaven.io>
2024-01-04 20:47:24 -0500

    PagingChunkSource context protocol documentation, many silly cleanups, TODOs for channel context integration (#5000)
    * Solidify the "contract" re: GetContext and FillContext passed from PagingChunkSources to Pages. Add an API to ColumnChunkPageStore that will allow different contexts to be used.
    
    * Fix JavaDoc issues in DateTimeUtils
    
    * Random formatting, JavaDoc, inheritance, and variable name changes
    
    ---------
    
    Co-authored-by: Shivam Malhotra <malhotraashivam@gmail.com>

--- engine/table/src/main/java/io/deephaven/engine/table/impl/sources/regioned/ColumnRegionObject.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/sources/regioned/ColumnRegionObject.java
@@ -4,6 +4,7 @@
 package io.deephaven.engine.table.impl.sources.regioned;
 
 import io.deephaven.chunk.attributes.Any;
+import io.deephaven.engine.page.PagingContextHolder;
 import io.deephaven.engine.table.impl.chunkattributes.DictionaryKeys;
 import io.deephaven.chunk.ChunkType;
 import io.deephaven.chunk.WritableChunk;
@@ -35,7 +36,7 @@ public interface ColumnRegionObject<DATA_TYPE, ATTR extends Any> extends ColumnR
     /**
      * Get a single object from this region.
      *
-     * @param context      A {@link RegionContextHolder} to enable resource caching where suitable, with current
+     * @param context      A {@link PagingContextHolder} to enable resource caching where suitable, with current
      *                     region index pointing to this region
      * @param elementIndex Element row key in the table's address space
      * @return The object value at the specified element row key
