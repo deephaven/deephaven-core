d7cca33318cef10b9dd480628452b268b134eda4
Nathaniel Bauernfeind <natebauernfeind@deephaven.io>
2024-01-09 15:02:51 -0700

    Use WeakReference

--- engine/table/src/main/java/io/deephaven/engine/table/impl/InstrumentedTableUpdateSource.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/InstrumentedTableUpdateSource.java
@@ -7,29 +7,36 @@ import io.deephaven.engine.table.impl.util.DelayedErrorNotifier;
 import io.deephaven.util.annotations.ReferentialIntegrity;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.ref.WeakReference;
+
 public abstract class InstrumentedTableUpdateSource extends InstrumentedUpdateSource {
 
-    private final BaseTable<?> result;
+    private final WeakReference<BaseTable<?>> tableReference;
 
     @ReferentialIntegrity
     private Runnable delayedErrorReference;
 
-    public InstrumentedTableUpdateSource(final BaseTable<?> result, final String description) {
-        super(result.getUpdateGraph(), description);
-        this.result = result;
+    public InstrumentedTableUpdateSource(final BaseTable<?> table, final String description) {
+        super(table.getUpdateGraph(), description);
+        tableReference = new WeakReference<>(table);
     }
 
     @Override
     protected final void onRefreshError(@NotNull final Exception error) {
-        if (result.satisfied(updateGraph.clock().currentStep())) {
+        final BaseTable<?> table = tableReference.get();
+        if (table == null) {
+            return;
+        }
+
+        if (table.satisfied(updateGraph.clock().currentStep())) {
             // If the result is already satisfied (because it managed to send its notification, or was otherwise
             // satisfied) we should not send our error notification on this cycle.
-            if (!result.isFailed()) {
+            if (!table.isFailed()) {
                 // If the result isn't failed, we need to mark it as such on the next cycle.
-                delayedErrorReference = new DelayedErrorNotifier(error, entry, result);
+                delayedErrorReference = new DelayedErrorNotifier(error, entry, table);
             }
         } else {
-            result.notifyListenersOnError(error, entry);
+            table.notifyListenersOnError(error, entry);
         }
     }
 
