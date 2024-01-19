76f3e1cefdb253faac4083e0a3ae36514ab42941
Devin Smith <devinsmith@deephaven.io>
2024-01-19 13:01:47 -0800

    Remove unused imports/dependency

--- extensions/kafka/src/main/java/io/deephaven/kafka/ingest/FieldCopierAdapter.java
+++ extensions/kafka/src/main/java/io/deephaven/kafka/ingest/FieldCopierAdapter.java
@@ -4,17 +4,6 @@
 package io.deephaven.kafka.ingest;
 
 import io.deephaven.processor.functions.ObjectProcessorFunctions;
-import io.deephaven.qst.type.BoxedBooleanType;
-import io.deephaven.function.ToBooleanFunction;
-import io.deephaven.function.ToByteFunction;
-import io.deephaven.function.ToCharFunction;
-import io.deephaven.function.ToDoubleFunction;
-import io.deephaven.function.ToFloatFunction;
-import io.deephaven.function.ToIntFunction;
-import io.deephaven.function.ToLongFunction;
-import io.deephaven.function.ToObjectFunction;
-import io.deephaven.function.ToPrimitiveFunction;
-import io.deephaven.function.ToShortFunction;
 import io.deephaven.function.TypedFunction;
 
 import java.util.List;
