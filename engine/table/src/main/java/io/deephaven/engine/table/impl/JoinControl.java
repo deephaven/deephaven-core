4724b6f7b746e453be7f911a8d42e97799ec6f0c
Ryan Caudy <rcaudy@gmail.com>
2024-01-30 10:03:07 -0500

    Codify updated JoinControl rules.

--- engine/table/src/main/java/io/deephaven/engine/table/impl/JoinControl.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/JoinControl.java
@@ -3,6 +3,7 @@
  */
 package io.deephaven.engine.table.impl;
 
+import io.deephaven.engine.table.DataIndex;
 import io.deephaven.engine.table.Table;
 import io.deephaven.engine.table.ColumnSource;
 import io.deephaven.engine.table.impl.indexer.DataIndexer;
@@ -10,6 +11,7 @@ import io.deephaven.engine.table.impl.sources.regioned.SymbolTableSource;
 import io.deephaven.engine.table.impl.sources.sparse.SparseConstants;
 import io.deephaven.util.annotations.VisibleForTesting;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @VisibleForTesting
 public class JoinControl {
@@ -33,14 +35,6 @@ public class JoinControl {
                         Math.min(MAX_TABLE_SIZE, Long.highestOneBit(initialCapacity) * 2)));
     }
 
-    int tableSizeForRightBuild(Table rightTable) {
-        return tableSize(rightTable.size());
-    }
-
-    int tableSizeForLeftBuild(Table leftTable) {
-        return tableSize(leftTable.size());
-    }
-
     double getMaximumLoadFactor() {
         return DEFAULT_MAX_LOAD_FACTOR;
     }
@@ -49,17 +43,157 @@ public class JoinControl {
         return DEFAULT_TARGET_LOAD_FACTOR;
     }
 
-    static boolean useDataIndex(Table table, ColumnSource<?>[] sources) {
-        return DataIndexer.of(table.getRowSet()).hasDataIndex(sources);
+    @Nullable
+    DataIndex dataIndexToUse(Table table, ColumnSource<?>[] sources) {
+        final DataIndexer indexer = DataIndexer.existingOf(table.getRowSet());
+        return indexer == null ? null : indexer.getDataIndex(sources);
     }
 
-    boolean buildLeft(QueryTable leftTable, Table rightTable) {
-        return leftTable.size() <= rightTable.size();
+    static final class BuildParameters {
+
+        enum From {
+            LeftInput, LeftDataIndex, RightInput, RightDataIndex
+        }
+
+        private final From firstBuildFrom;
+        private final int hashTableSize;
+
+        private BuildParameters(
+                @NotNull final JoinControl.BuildParameters.From firstBuildFrom,
+                final int hashTableSize) {
+            this.firstBuildFrom = firstBuildFrom;
+            this.hashTableSize = hashTableSize;
+        }
+
+        From firstBuildFrom() {
+            return firstBuildFrom;
+        }
+
+        int hashTableSize() {
+            return hashTableSize;
+        }
     }
 
-    boolean considerSymbolTables(QueryTable leftTable, @SuppressWarnings("unused") QueryTable rightTable,
-            boolean useLeftGrouping, boolean useRightGrouping, ColumnSource<?> leftSource,
-            ColumnSource<?> rightSource) {
+    /**
+     * Join Control Goals:
+     * <ol>
+     * <li>Keep the hash table small</li>
+     * <li>Avoid rehashing</li>
+     * </ol>
+     * To meet these goals, we:
+     * <dl>
+     * <dt>Both sides refreshing</dt>
+     * <dd>Build from a data index if available. Prefer building from the expected highest cardinality side first to try
+     * to minimize rehashing.</dd>
+     * <dt>One side refreshing</dt>
+     * <dd>Always build from the static side. Use a data index if that side has one.</dd>
+     * <dt>Both sides static</dt>
+     * <dd>Build from the expected lowest cardinality side to avoid creating unnecessary states. Use a data index if
+     * available on expected lowest cardinality side.</dd>
+     * </dl>
+     */
+    BuildParameters buildParameters(
+            @NotNull final Table leftTable, @Nullable Table leftDataIndexTable,
+            @NotNull final Table rightTable, @Nullable Table rightDataIndexTable) {
+        final BuildParameters.From firstBuildFrom;
+        if (leftTable.isRefreshing()) {
+            if (rightTable.isRefreshing()) {
+                // Both refreshing: build from largest available data index, or largest table if no indexes
+                if (leftDataIndexTable != null) {
+                    if (rightDataIndexTable != null) {
+                        if (leftDataIndexTable.size() >= rightDataIndexTable.size()) {
+                            firstBuildFrom = BuildParameters.From.LeftDataIndex;
+                        } else {
+                            firstBuildFrom = BuildParameters.From.RightDataIndex;
+                        }
+                    } else {
+                        firstBuildFrom = BuildParameters.From.LeftDataIndex;
+                    }
+                } else if (rightDataIndexTable != null) {
+                    firstBuildFrom = BuildParameters.From.RightDataIndex;
+                } else if (leftTable.size() >= rightTable.size()) {
+                    firstBuildFrom = BuildParameters.From.LeftInput;
+                } else {
+                    firstBuildFrom = BuildParameters.From.RightInput;
+                }
+            } else {
+                // Left refreshing, right static: build from right data index if available, else right table
+                if (rightDataIndexTable != null) {
+                    firstBuildFrom = BuildParameters.From.RightDataIndex;
+                } else {
+                    firstBuildFrom = BuildParameters.From.RightInput;
+                }
+            }
+        } else if (rightTable.isRefreshing()) {
+            // Left static, right refreshing: build from left data index if available, else left table
+            if (leftDataIndexTable != null) {
+                firstBuildFrom = BuildParameters.From.LeftDataIndex;
+            } else {
+                firstBuildFrom = BuildParameters.From.LeftInput;
+            }
+        } else {
+            // Both static: build from smallest available data index or smallest table; ties go to smallest table
+            if (leftDataIndexTable != null) {
+                if (rightDataIndexTable != null) {
+                    if (leftDataIndexTable.size() <= rightDataIndexTable.size()) {
+                        firstBuildFrom = BuildParameters.From.LeftDataIndex;
+                    } else {
+                        firstBuildFrom = BuildParameters.From.RightDataIndex;
+                    }
+                } else if (leftDataIndexTable.size() < rightTable.size()) {
+                    firstBuildFrom = BuildParameters.From.LeftDataIndex;
+                } else {
+                    firstBuildFrom = BuildParameters.From.RightInput;
+                }
+            } else if (rightDataIndexTable != null) {
+                if (leftTable.size() <= rightDataIndexTable.size()) {
+                    firstBuildFrom = BuildParameters.From.LeftInput;
+                } else {
+                    firstBuildFrom = BuildParameters.From.RightDataIndex;
+                }
+            } else if (leftTable.size() <= rightTable.size()) {
+                firstBuildFrom = BuildParameters.From.LeftInput;
+            } else {
+                firstBuildFrom = BuildParameters.From.RightInput;
+            }
+        }
+        // If we're going to do our initial build from a data index, choose the hash table size accordingly.
+        // Else, choose our default initial size.
+        final int hashTableSize;
+        switch (firstBuildFrom) {
+            case LeftDataIndex:
+                hashTableSize = tableSize(leftDataIndexTable.size());
+                break;
+            case RightDataIndex:
+                hashTableSize = tableSize(rightDataIndexTable.size());
+                break;
+            case LeftInput:
+            case RightInput:
+                hashTableSize = initialBuildSize();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected first build from " + firstBuildFrom);
+        }
+        return new BuildParameters(firstBuildFrom, hashTableSize);
+    }
+
+    /**
+     * Same as {@link #buildParameters(Table, Table, Table, Table)}, but it's assumed that all RHS rows are unique.
+     * That, is the RHS table is treated like its own data index table in some respects.
+     */
+    BuildParameters buildParametersForUniqueRights(
+            @NotNull final Table leftTable, @Nullable Table leftDataIndexTable, @NotNull final Table rightTable) {
+        final BuildParameters result = buildParameters(leftTable, leftDataIndexTable, rightTable, rightTable);
+        if (result.firstBuildFrom() == BuildParameters.From.RightDataIndex) {
+            return new BuildParameters(BuildParameters.From.RightInput, result.hashTableSize());
+        }
+        return result;
+    }
+
+    boolean considerSymbolTables(
+            Table leftTable, Table rightTable,
+            boolean useLeftGrouping, boolean useRightGrouping,
+            ColumnSource<?> leftSource, ColumnSource<?> rightSource) {
         return !leftTable.isRefreshing() && !useLeftGrouping && leftSource.getType() == String.class
                 && !rightTable.isRefreshing() && !useRightGrouping && rightSource.getType() == String.class
                 && leftSource instanceof SymbolTableSource && rightSource instanceof SymbolTableSource
