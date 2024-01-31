0ef7f79fcb2abc21fa874ac1c8224d2f74af90e8
Shivam Malhotra <malhotraashivam@gmail.com>
2024-01-31 18:14:15 +0530

    Calling methods from inside ParquetHadoop for writing metadata files

--- extensions/parquet/base/src/main/java/io/deephaven/parquet/base/ParquetFileWriter.java
+++ extensions/parquet/base/src/main/java/io/deephaven/parquet/base/ParquetFileWriter.java
@@ -3,8 +3,9 @@
  */
 package io.deephaven.parquet.base;
 
+import org.apache.hadoop.conf.Configuration;
+
 import org.apache.hadoop.fs.Path;
-import org.apache.parquet.crypto.InternalFileEncryptor;
 import org.apache.parquet.format.converter.ParquetMetadataConverter;
 import io.deephaven.util.channel.SeekableChannelsProvider;
 import io.deephaven.parquet.compress.CompressorAdapter;
@@ -18,22 +19,16 @@ import org.apache.parquet.hadoop.Footer;
 import org.apache.parquet.hadoop.metadata.*;
 import org.apache.parquet.internal.column.columnindex.OffsetIndex;
 import org.apache.parquet.internal.hadoop.metadata.IndexReference;
-import org.apache.parquet.io.ParquetEncodingException;
 import org.apache.parquet.schema.MessageType;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import static org.apache.parquet.format.Util.writeFileMetaData;
-import static org.apache.parquet.hadoop.ParquetFileWriter.CURRENT_VERSION;
-import static org.apache.parquet.hadoop.ParquetFileWriter.MAGIC;
 
 public final class ParquetFileWriter {
     private static final ParquetMetadataConverter metadataConverter = new ParquetMetadataConverter();
@@ -49,9 +44,6 @@ public final class ParquetFileWriter {
     private final List<BlockMetaData> blocks = new ArrayList<>();
     private final List<List<OffsetIndex>> offsetIndexes = new ArrayList<>();
     private final String filePath;
-    private final SeekableChannelsProvider channelsProvider;
-    private static final String PARQUET_METADATA_FILE = "_metadata";
-    private static final String PARQUET_COMMON_METADATA_FILE = "_common_metadata";
 
     public ParquetFileWriter(
             final String filePath,
@@ -70,7 +62,6 @@ public final class ParquetFileWriter {
         this.type = type;
         this.compressorAdapter = DeephavenCompressorAdapterFactory.getInstance().getByName(codecName);
         this.filePath = filePath;
-        this.channelsProvider = channelsProvider;
     }
 
     public RowGroupWriter addRowGroup(final long size) {
@@ -91,135 +82,27 @@ public final class ParquetFileWriter {
         bufferedOutput.close();
         compressorAdapter.close();
         // Write the metadata file
-        final Path metadataDirPath = new Path(new File(filePath).getParent());
-        final String actualFilePath = filePath.replace(".NEW_", "");
+        final File pqFile = new File(filePath);
+        final Path metadataDirPath = new Path(pqFile.getParent());
+        final String actualFilePath = pqFile.getAbsolutePath().replace(".NEW_", "");
         final Footer fsFooter = new Footer(new Path(actualFilePath), footer);
         final ArrayList<Footer> footers = new ArrayList<>();
         footers.add(fsFooter);
-        writeMetadataFile(metadataDirPath, footers);
+        final Configuration conf = new Configuration();
+        // Not setting it requires adding dependency on hadoop-commons and commons-configuration2
+        // Setting it means we will not cache the filesystem instance which leads to a small performance hit but that's
+        // okay since this is only used when writing the metadata files
+        // But this leads to additional warnings like
+        // Jan 31, 2024 6:31:23 PM org.apache.hadoop.fs.FileSystem loadFileSystems
+        // WARNING: Cannot load filesystem: java.util.ServiceConfigurationError: org.apache.hadoop.fs.FileSystem:
+        // Provider org.apache.hadoop.fs.viewfs.ViewFileSystem could not be instantiated
+        // Jan 31, 2024 6:31:23 PM org.apache.hadoop.fs.FileSystem loadFileSystems
+        // WARNING: java.lang.NoClassDefFoundError: org/apache/commons/configuration2/Configuration
+        // Jan 31, 2024 6:31:23 PM org.apache.hadoop.fs.FileSystem loadFileSystems
+        // WARNING: java.lang.ClassNotFoundException: org.apache.commons.configuration2.Configuration
+        conf.setBoolean("fs.file.impl.disable.cache", true);
+        org.apache.parquet.hadoop.ParquetFileWriter.writeMetadataFile(conf, metadataDirPath, footers);
     }
-
-    @Deprecated
-    public void writeMetadataFile(Path outputPath, List<Footer> footers) throws IOException {
-        ParquetMetadata metadataFooter = mergeFooters(outputPath, footers);
-
-        writeMetadataFile(outputPath, metadataFooter, PARQUET_METADATA_FILE);
-
-        metadataFooter.getBlocks().clear();
-        writeMetadataFile(outputPath, metadataFooter, PARQUET_COMMON_METADATA_FILE);
-    }
-
-    static GlobalMetaData mergeInto(
-            FileMetaData toMerge,
-            GlobalMetaData mergedMetadata) {
-        return mergeInto(toMerge, mergedMetadata, true);
-    }
-
-    static GlobalMetaData mergeInto(
-            FileMetaData toMerge,
-            GlobalMetaData mergedMetadata,
-            boolean strict) {
-        MessageType schema = null;
-        Map<String, Set<String>> newKeyValues = new HashMap<String, Set<String>>();
-        Set<String> createdBy = new HashSet<String>();
-        if (mergedMetadata != null) {
-            schema = mergedMetadata.getSchema();
-            newKeyValues.putAll(mergedMetadata.getKeyValueMetaData());
-            createdBy.addAll(mergedMetadata.getCreatedBy());
-        }
-        if ((schema == null && toMerge.getSchema() != null)
-                || (schema != null && !schema.equals(toMerge.getSchema()))) {
-            schema = mergeInto(toMerge.getSchema(), schema, strict);
-        }
-        for (Map.Entry<String, String> entry : toMerge.getKeyValueMetaData().entrySet()) {
-            Set<String> values = newKeyValues.get(entry.getKey());
-            if (values == null) {
-                values = new LinkedHashSet<String>();
-                newKeyValues.put(entry.getKey(), values);
-            }
-            values.add(entry.getValue());
-        }
-        createdBy.add(toMerge.getCreatedBy());
-        return new GlobalMetaData(
-                schema,
-                newKeyValues,
-                createdBy);
-    }
-
-    static MessageType mergeInto(MessageType toMerge, MessageType mergedSchema, boolean strict) {
-        if (mergedSchema == null) {
-            return toMerge;
-        }
-
-        return mergedSchema.union(toMerge, strict);
-    }
-
-    static ParquetMetadata mergeFooters(Path root, List<Footer> footers) {
-        return mergeFooters(root, footers, new StrictKeyValueMetadataMergeStrategy());
-    }
-
-    static ParquetMetadata mergeFooters(Path root, List<Footer> footers,
-            KeyValueMetadataMergeStrategy keyValueMergeStrategy) {
-        String rootPath = root.toUri().getPath();
-        GlobalMetaData fileMetaData = null;
-        List<BlockMetaData> blocks = new ArrayList<BlockMetaData>();
-        for (Footer footer : footers) {
-            String footerPath = footer.getFile().toUri().getPath();
-            if (!footerPath.startsWith(rootPath)) {
-                throw new ParquetEncodingException(
-                        footerPath + " invalid: all the files must be contained in the root " + root);
-            }
-            footerPath = footerPath.substring(rootPath.length());
-            while (footerPath.startsWith("/")) {
-                footerPath = footerPath.substring(1);
-            }
-            fileMetaData = mergeInto(footer.getParquetMetadata().getFileMetaData(), fileMetaData);
-            for (BlockMetaData block : footer.getParquetMetadata().getBlocks()) {
-                block.setPath(footerPath);
-                blocks.add(block);
-            }
-        }
-        return new ParquetMetadata(fileMetaData.merge(keyValueMergeStrategy), blocks);
-    }
-
-    /**
-     * @deprecated metadata files are not recommended and will be removed in 2.0.0
-     */
-    @Deprecated
-    private void writeMetadataFile(Path outputPathRoot, ParquetMetadata metadataFooter, String parquetMetadataFile)
-            throws IOException {
-        Path metaDataPath = new Path(outputPathRoot, parquetMetadataFile);
-        writeMetadataFile(metaDataPath, metadataFooter);
-    }
-
-    /**
-     * @deprecated metadata files are not recommended and will be removed in 2.0.0
-     */
-    @Deprecated
-    private void writeMetadataFile(Path outputPath, ParquetMetadata metadataFooter)
-            throws IOException {
-        final PositionedBufferedOutputStream metadataOutputStream =
-                new PositionedBufferedOutputStream(channelsProvider.getWriteChannel(outputPath.toString(), false),
-                        OUTPUT_BUFFER_SIZE);
-        metadataOutputStream.write(MAGIC);
-        serializeMetadataFooter(metadataFooter, metadataOutputStream, null, new ParquetMetadataConverter());
-        metadataOutputStream.close();
-    }
-
-    private static void serializeMetadataFooter(ParquetMetadata footer, PositionedBufferedOutputStream out,
-            InternalFileEncryptor fileEncryptor, ParquetMetadataConverter metadataConverter) throws IOException {
-
-        // Unencrypted file
-        if (null == fileEncryptor) {
-            long footerIndex = out.position();
-            org.apache.parquet.format.FileMetaData parquetMetadata =
-                    metadataConverter.toParquetMetadata(CURRENT_VERSION, footer);
-            writeFileMetaData(parquetMetadata, out);
-            BytesUtils.writeIntLittleEndian(out, (int) (out.position() - footerIndex));
-            out.write(MAGIC);
-        }
-    }
-
 
     private void serializeFooter(final ParquetMetadata footer) throws IOException {
         final long footerIndex = bufferedOutput.position();
