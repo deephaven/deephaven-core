f1059234509350e6b5d8f7bf6671b7a6f91820aa
Jianfeng Mao <4297243+jmao-denver@users.noreply.github.com>
2024-02-20 23:51:42 -0700

    Apply suggestions from code review
    Co-authored-by: Chip Kent <5250374+chipkent@users.noreply.github.com>

--- py/server/deephaven/_dep.py
+++ py/server/deephaven/_dep.py
@@ -1,14 +1,14 @@
 #
 #     Copyright (c) 2016-2023 Deephaven Data Labs and Patent Pending
 #
-"""This module is an internal module to support dependencies management"""
+"""This module is an internal module to support dependency management"""
 
 import importlib
 from types import ModuleType
 from typing import Optional
 
 
-def soft_dependency(module_name) -> Optional[ModuleType]:
+def soft_dependency(module_name: str) -> Optional[ModuleType]:
     """Attempt to import a module and return it if it exists, otherwise return None.
     Args:
         module_name (str): the name of the module to import
