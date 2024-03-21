dd6ace7a87dfde5c1ab50d7c530e87d5b980cc95
Ryan Caudy <ryan@deephaven.io>
2024-03-21 12:13:30 -0400

    Use AutoCloseable in some places where we previously used SafeCloseable, and standardize some SafeCloseable utilities (#5274)

--- Util/src/main/java/io/deephaven/util/SafeCloseable.java
+++ Util/src/main/java/io/deephaven/util/SafeCloseable.java
@@ -18,40 +18,43 @@ import java.util.stream.Stream;
  */
 public interface SafeCloseable extends AutoCloseable {
 
+    @Override
+    void close();
+
     /**
-     * {@link #close() Close} all non-{@code null} SafeCloseable arguments.
+     * {@link #close() Close} all non-{@code null} {@link AutoCloseable} arguments.
      *
-     * @param safeCloseables SafeCloseables to {@link #close() close}
+     * @param autoCloseables {@link AutoCloseable AutoCloseables} to {@link #close() close}
      */
-    static void closeAll(@NotNull final SafeCloseable... safeCloseables) {
-        closeAll(Arrays.asList(safeCloseables).iterator());
+    static void closeAll(@NotNull final AutoCloseable... autoCloseables) {
+        closeAll(Arrays.asList(autoCloseables).iterator());
     }
 
     /**
-     * {@link #close() Close} all non-{@code null} SafeCloseable elements. Terminates the {@code stream}.
+     * {@link #close() Close} all non-{@code null} {@link AutoCloseable} elements. Terminates the {@code stream}.
      *
-     * @param stream the stream of SafeCloseables to {@link #close() close}
-     * @param <SCT> the safe closable type
+     * @param stream The stream of {@link AutoCloseable AutoCloseables} to {@link #close() close}
+     * @param <ACT> the auto closable type
      */
-    static <SCT extends SafeCloseable> void closeAll(@NotNull final Stream<SCT> stream) {
+    static <ACT extends AutoCloseable> void closeAll(@NotNull final Stream<ACT> stream) {
         closeAll(stream.iterator());
     }
 
     /**
-     * {@link #close() Close} all non-{@code null} SafeCloseable elements. Consumes the {@code iterator}.
+     * {@link #close() Close} all non-{@code null} {@link AutoCloseable} elements. Consumes the {@code iterator}.
      *
-     * @param iterator the iterator of SafeCloseables to {@link #close() close}
-     * @param <SCT> the safe closable type
+     * @param iterator The iterator of {@link AutoCloseable AutoCloseables} to {@link #close() close}
+     * @param <ACT> the auto closable type
      */
-    static <SCT extends SafeCloseable> void closeAll(@NotNull final Iterator<SCT> iterator) {
+    static <ACT extends AutoCloseable> void closeAll(@NotNull final Iterator<ACT> iterator) {
         List<Exception> exceptions = null;
         while (iterator.hasNext()) {
-            final SafeCloseable safeCloseable = iterator.next();
-            if (safeCloseable == null) {
+            final AutoCloseable autoCloseable = iterator.next();
+            if (autoCloseable == null) {
                 continue;
             }
             try {
-                safeCloseable.close();
+                autoCloseable.close();
             } catch (Exception e) {
                 if (exceptions == null) {
                     exceptions = new ArrayList<>();
@@ -67,16 +70,17 @@ public interface SafeCloseable extends AutoCloseable {
     }
 
     /**
-     * {@link #close() Close} a single SafeCloseable argument if it is non-{@code null}.
+     * {@link #close() Close} a single {@link AutoCloseable} argument if it is non-{@code null}.
      *
-     * @param safeCloseable The SafeCloseable to {@link #close() close}
+     * @param autoCloseable The {@link AutoCloseable} to {@link #close() close}
      */
-    static void closeIfNonNull(@Nullable final SafeCloseable safeCloseable) {
-        if (safeCloseable != null) {
-            safeCloseable.close();
+    static void closeIfNonNull(@Nullable final AutoCloseable autoCloseable) {
+        if (autoCloseable != null) {
+            try {
+                autoCloseable.close();
+            } catch (Exception e) {
+                throw new UncheckedDeephavenException("Exception while closing resource", e);
+            }
         }
     }
-
-    @Override
-    void close();
 }
