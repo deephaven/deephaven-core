7a2b0edd51347fbb98a160d61538243c23b5b0f4
Cristian Ferretti <jcferretti2020@gmail.com>
2024-03-22 19:00:18 -0400

    Add python 3.12 classifier to setup.py.

--- py/client-ticking/setup.py
+++ py/client-ticking/setup.py
@@ -54,6 +54,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 
     ext_modules = cythonize(
