4916a57bbfdc0224c5655417ff6ac716b94193b2
rbasralian <rbasralian@deephaven.io>
2024-03-22 00:49:15 +0000

    spotless (copyright)

--- data-adapter/src/main/java/io/deephaven/dataadapter/consumers/ObjDoubleConsumer.java
+++ data-adapter/src/main/java/io/deephaven/dataadapter/consumers/ObjDoubleConsumer.java
@@ -1,3 +1,6 @@
+//
+// Copyright (c) 2016-2024 Deephaven Data Labs and Patent Pending
+//
 package io.deephaven.dataadapter.consumers;
 
 public interface ObjDoubleConsumer<R> extends java.util.function.ObjDoubleConsumer<R> {
--- data-adapter/src/main/java/io/deephaven/dataadapter/rec/updaters/FloatRecordUpdater.java
+++ data-adapter/src/main/java/io/deephaven/dataadapter/rec/updaters/FloatRecordUpdater.java
@@ -1,3 +1,6 @@
+//
+// Copyright (c) 2016-2024 Deephaven Data Labs and Patent Pending
+//
 package io.deephaven.dataadapter.rec.updaters;
 
 import io.deephaven.dataadapter.consumers.ObjFloatConsumer;
