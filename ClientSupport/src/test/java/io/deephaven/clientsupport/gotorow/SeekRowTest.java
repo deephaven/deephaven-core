df5b61801c3887c461f20caa7123ec90042d77c3
Steven Wu <wusteven815@gmail.com>
2024-03-25 16:09:20 -0400

    update unsorted test

--- ClientSupport/src/test/java/io/deephaven/clientsupport/gotorow/SeekRowTest.java
+++ ClientSupport/src/test/java/io/deephaven/clientsupport/gotorow/SeekRowTest.java
@@ -299,9 +299,14 @@ public class SeekRowTest {
 
     @Test
     public void unsorted() {
-        final Table t = newTable(intCol("num", 3, 1, 2, 1, 3, 2));
+        final Table t = newTable(intCol("num", 3, 1, 1, 2, 3, 1, 1, 2));
+        assertSeekPositionAllRows(t, 1,
+                new int[] {1, 2, 5, 5, 5, 6, 1, 1},
+                new int[] {6, 6, 1, 2, 2, 2, 5, 6});
         assertSeekPositionAllRows(t, 2,
-                new int[] {2, 2, 5, 5, 5, 2},
-                new int[] {5, 5, 5, 2, 2, 2});
-    }
+                new int[] {3, 3, 3, 7, 7, 7, 7, 3},
+                new int[] {6, 6, 1, 2, 2, 2, 5, 6});
+        assertSeekPositionAllRows(t, 3,
+                new int[] {4, 4, 4, 4, 0, 0, 0, 0},
+                new int[] {6, 6, 1, 2, 2, 2, 5, 6});
 }
