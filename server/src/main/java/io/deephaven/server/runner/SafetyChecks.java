7988383658af0026824e4374c846f8e3144d7a68
Devin Smith <devinsmith@deephaven.io>
2024-03-27 11:14:42 -0700

    Add EOL safety check

--- server/src/main/java/io/deephaven/server/runner/SafetyChecks.java
+++ server/src/main/java/io/deephaven/server/runner/SafetyChecks.java
@@ -5,7 +5,10 @@ package io.deephaven.server.runner;
 
 import java.lang.Runtime.Version;
 import java.lang.management.ManagementFactory;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Deephaven safety checks.
@@ -28,12 +31,14 @@ public final class SafetyChecks {
     /**
      * Applies a set of safety checks if {@link #isEnabled()}.
      *
+     * @see JDK_END_OF_LIFE
      * @see JDK_8287432
      */
     public static void check() {
         if (!isEnabled()) {
             return;
         }
+        JDK_END_OF_LIFE.check();
         JDK_8287432.check();
     }
 
@@ -82,7 +87,7 @@ public final class SafetyChecks {
     }
 
     /**
-     * See <a href="https://bugs.openjdk.org/browse/JDK-8287432">JDK-8287432</a>.
+     * A safety check for <a href="https://bugs.openjdk.org/browse/JDK-8287432">JDK-8287432</a>.
      */
     public static final class JDK_8287432 {
 
@@ -92,7 +97,7 @@ public final class SafetyChecks {
             }
             if (isVulnerableVersion() && !hasWorkaround()) {
                 throw exception(JDK_8287432.class, String.format(
-                        "The current JVM is vulnerable to the bug https://bugs.openjdk.org/browse/JDK-8287432. Please update to 11.0.17+, 17.0.5+, or 21+. If that is not possible, you can apply a workaround '%s'.",
+                        "The current JDK is vulnerable to the bug https://bugs.openjdk.org/browse/JDK-8287432. We recommend updating to 11.0.17+, 17.0.5+, or 21+. If that is not possible, you can apply a workaround '%s'.",
                         String.join(" ", workaroundJvmArguments())));
             }
         }
@@ -119,6 +124,47 @@ public final class SafetyChecks {
 
         private static List<String> workaroundJvmArguments() {
             return List.of("-XX:+UnlockDiagnosticVMOptions", "-XX:DisableIntrinsic=_currentThread");
+        }
+    }
+
+    /**
+     * A safety check for end-of-life JDKs, see
+     * <a href="https://www.oracle.com/java/technologies/java-se-support-roadmap.html">Oracle Java SE Support
+     * Roadmap</a>.
+     */
+    public static final class JDK_END_OF_LIFE {
+
+        private static final Map<Integer, LocalDate> EOL_DATES = Map.ofEntries(
+                Map.entry(11, LocalDate.of(2032, Month.JANUARY, 31)),
+                Map.entry(12, LocalDate.of(2019, Month.SEPTEMBER, 30)),
+                Map.entry(13, LocalDate.of(2020, Month.MARCH, 31)),
+                Map.entry(14, LocalDate.of(2020, Month.SEPTEMBER, 30)),
+                Map.entry(15, LocalDate.of(2021, Month.MARCH, 31)),
+                Map.entry(16, LocalDate.of(2021, Month.SEPTEMBER, 30)),
+                Map.entry(17, LocalDate.of(2029, Month.SEPTEMBER, 30)),
+                Map.entry(18, LocalDate.of(2022, Month.SEPTEMBER, 30)),
+                Map.entry(19, LocalDate.of(2023, Month.MARCH, 31)),
+                Map.entry(20, LocalDate.of(2023, Month.SEPTEMBER, 30)),
+                Map.entry(21, LocalDate.of(2031, Month.SEPTEMBER, 30)),
+                Map.entry(22, LocalDate.of(2024, Month.SEPTEMBER, 30)),
+                Map.entry(23, LocalDate.of(2025, Month.MARCH, 31)),
+                Map.entry(24, LocalDate.of(2025, Month.SEPTEMBER, 30)),
+                Map.entry(25, LocalDate.of(2033, Month.SEPTEMBER, 30)));
+
+        private static void check() {
+            if (!isEnabled(JDK_END_OF_LIFE.class)) {
+                return;
+            }
+            final Version version = Runtime.version();
+            final LocalDate eolDate = EOL_DATES.get(version.feature());
+            if (eolDate == null) {
+                return;
+            }
+            if (LocalDate.now().isAfter(eolDate)) {
+                throw exception(JDK_END_OF_LIFE.class, String.format(
+                        "The current JDK feature version %d is end-of-life as of %s. We recommend updating to a non end-of-life JDK.",
+                        version.feature(), eolDate));
+            }
         }
     }
 }
