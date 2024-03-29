55ef75998ce7ef84066b87b2ce7d79db563c2a8a
Mike Bender <mikebender@deephaven.io>
2024-03-29 01:01:08 -0400

    Add ticking table tests

--- web/client-api/src/test/java/io/deephaven/web/client/api/PartitionedTableTestGwt.java
+++ web/client-api/src/test/java/io/deephaven/web/client/api/PartitionedTableTestGwt.java
@@ -3,6 +3,11 @@
 //
 package io.deephaven.web.client.api;
 
+import elemental2.core.JsArray;
+import elemental2.dom.CustomEvent;
+import io.deephaven.web.client.api.subscription.ViewportData;
+import io.deephaven.web.client.api.tree.JsTreeTable;
+
 public class PartitionedTableTestGwt extends AbstractAsyncGwtTestCase {
     @Override
     public String getModuleName() {
@@ -15,6 +20,12 @@ public class PartitionedTableTestGwt extends AbstractAsyncGwtTestCase {
             .script("partitioned_source = source.partition_by(by=['MyKey'])")
             .script("partitioned_result = partitioned_source.transform(func=lambda t: t.drop_columns('MyKey'))")
             .script("constituent_result = partitioned_result.get_constituent(['0'])");
+
+    private final TableSourceBuilder tickingTables = new TableSourceBuilder()
+            .script("from deephaven import time_table")
+            .script("source = time_table('PT0.1s').update(['MyKey=``+i%5', 'x=i'])")
+            .script("partitioned_source = source.partition_by(by=['MyKey'])")
+            .script("partitioned_result = partitioned_source.transform(func=lambda t: t.drop_columns('MyKey'))");
 
 
     public void testPartitionedTable() {
@@ -65,9 +76,96 @@ public class PartitionedTableTestGwt extends AbstractAsyncGwtTestCase {
                         return partitionedTable.getTable("2");
                     }).then(constituentTable -> {
                         assertEquals(20d, constituentTable.getSize());
+                        constituentTable.close();
+                        partitionedTable.close();
+                        return null;
+                    });
+                })
+                .then(this::finish).catch_(this::report);
+    }
+
+    public void testConstituentResult() {
+        connect(tables)
+                .then(table("constituent_result"))
+                .then(table -> {
+                    delayTestFinish(1500);
+
+                    JsArray<Column> columns = table.getColumns();
+                    assertEquals(1, columns.length);
+                    assertEquals("x", columns.getAt(0).getName());
+
+                    table.close();
+                    return null;
+                })
+                .then(this::finish).catch_(this::report);
+    }
+
+    public void testTickingPartitionedTable() {
+        connect(tickingTables)
+                .then(partitionedTable("partitioned_source"))
+                .then(partitionedTable -> {
+                    delayTestFinish(20_000);
+                    Column[] keyColumns = partitionedTable.getKeyColumns();
+                    assertEquals(1, keyColumns.length);
+                    assertEquals("MyKey", keyColumns[0].getName());
+
+                    Column[] columns = partitionedTable.getColumns();
+                    assertEquals(3, columns.length);
+                    assertEquals("MyKey", columns[1].getName());
+                    assertEquals("x", columns[2].getName());
+
+                    return partitionedTable.getKeyTable().then(keyTable -> {
+                        keyTable.setViewport(0, 99, keyTable.getColumns(), null);
+                        return keyTable.getViewportData().then(data -> {
+                            assertEquals(0d, keyTable.getSize());
+
+                            return waitForEventWhere(keyTable, JsTable.EVENT_UPDATED,
+                                    (CustomEvent<ViewportData> d) -> d.detail.getRows().length == 5, 20004);
+                        });
+                    }).then(event -> partitionedTable.getTable("2")).then(constituentTable -> {
+                        assertEquals(3, constituentTable.getColumns().length);
+                        assertEquals(2d, constituentTable.getSize());
+
+                        constituentTable.close();
                         partitionedTable.close();
 
                         return null;
+                    });
+                })
+                .then(this::finish).catch_(this::report);
+    }
+
+    public void testTickingTransformedPartitionedTable() {
+        connect(tickingTables)
+                .then(partitionedTable("partitioned_result"))
+                .then(partitionedTable -> {
+                    delayTestFinish(20_000);
+                    Column[] keyColumns = partitionedTable.getKeyColumns();
+                    assertEquals(1, keyColumns.length);
+                    assertEquals("MyKey", keyColumns[0].getName());
+
+                    Column[] columns = partitionedTable.getColumns();
+                    assertEquals(2, columns.length);
+                    assertEquals("Timestamp", columns[0].getName());
+                    assertEquals("x", columns[1].getName());
+
+                    return partitionedTable.getKeyTable().then(keyTable -> {
+                        keyTable.setViewport(0, 99, keyTable.getColumns(), null);
+                        return keyTable.getViewportData().then(data -> {
+                            assertEquals(0d, keyTable.getSize());
+
+                            return waitForEventWhere(keyTable, JsTable.EVENT_UPDATED,
+                                    (CustomEvent<ViewportData> d) -> d.detail.getRows().length == 5, 20004);
+                        }).then(event -> partitionedTable.getTable("2")).then(constituentTable -> {
+                            assertEquals(2, constituentTable.getColumns().length);
+                            assertEquals(2d, constituentTable.getSize());
+
+                            keyTable.close();
+                            constituentTable.close();
+                            partitionedTable.close();
+
+                            return null;
+                        });
                     });
                 })
                 .then(this::finish).catch_(this::report);
