61b70ba326097ac1416d01e9ece884931b128e04
Shivam Malhotra <malhotraashivam@gmail.com>
2024-04-04 16:01:14 -0500

    Moved to a soft reference based cache

--- extensions/s3/src/main/java/io/deephaven/extensions/s3/ModuloBasedRequestCache.java
+++ extensions/s3/src/main/java/io/deephaven/extensions/s3/ModuloBasedRequestCache.java
@@ -7,18 +7,20 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import software.amazon.awssdk.services.s3.S3Uri;
 
+import java.lang.ref.SoftReference;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static java.lang.Math.abs;
 
 /**
- * This class uses a modulo-based mapping function with a {@link ConcurrentHashMap} to cache requests.
+ * This class uses a modulo-based mapping function with a {@link ConcurrentHashMap} to cache {@link SoftReference} to
+ * requests.
  */
 final class ModuloBasedRequestCache implements S3RequestCache {
 
     private final int maxSize;
-    private final Map<Integer, S3ChannelContext.Request> requests;
+    private final Map<Integer, SoftReference<S3ChannelContext.Request>> requests;
 
     ModuloBasedRequestCache(final int maxSize) {
         this.maxSize = maxSize;
@@ -29,7 +31,11 @@ final class ModuloBasedRequestCache implements S3RequestCache {
     @Nullable
     public S3ChannelContext.Request getRequest(@NotNull final S3Uri uri, final long fragmentIndex) {
         final int cacheIdx = cacheIndex(uri, fragmentIndex);
-        final S3ChannelContext.Request request = requests.get(cacheIdx);
+        final SoftReference<S3ChannelContext.Request> requestRef = requests.get(cacheIdx);
+        if (requestRef == null) {
+            return null;
+        }
+        final S3ChannelContext.Request request = requestRef.get();
         return request == null || !request.isFragment(uri, fragmentIndex) ? null : request;
     }
 
@@ -38,19 +44,25 @@ final class ModuloBasedRequestCache implements S3RequestCache {
     public S3ChannelContext.Request getOrCreateRequest(@NotNull final S3Uri uri, final long fragmentIndex,
             @NotNull final S3ChannelContext context) {
         final int cacheIdx = cacheIndex(uri, fragmentIndex);
-        return requests.compute(cacheIdx, (key, existingRequest) -> {
-            if (existingRequest != null && existingRequest.isFragment(uri, fragmentIndex)) {
-                return existingRequest;
-            } else {
-                // TODO Discuss with Ryan and Devin where this should happen
-                // if (existingRequest != null) {
-                // existingRequest.release();
-                // }
-                final S3ChannelContext.Request newRequest = new S3ChannelContext.Request(fragmentIndex, context);
-                newRequest.init();
-                return newRequest;
-            }
-        });
+        S3ChannelContext.Request ret;
+        do {
+            final SoftReference<S3ChannelContext.Request> requestRef =
+                    requests.compute(cacheIdx, (key, existingRequestRef) -> {
+                        if (existingRequestRef != null) {
+                            final S3ChannelContext.Request existingRequest = existingRequestRef.get();
+                            if (existingRequest != null && existingRequest.isFragment(uri, fragmentIndex)) {
+                                return existingRequestRef;
+                            }
+                        }
+                        // TODO Discuss with Ryan and Devin where we should release
+                        final S3ChannelContext.Request newRequest =
+                                new S3ChannelContext.Request(fragmentIndex, context);
+                        newRequest.init();
+                        return new SoftReference<>(newRequest);
+                    });
+            ret = requestRef.get();
+        } while (ret == null);
+        return ret;
     }
 
     public int maxSize() {
@@ -59,7 +71,14 @@ final class ModuloBasedRequestCache implements S3RequestCache {
 
     @Override
     public void cancelAllAndRelease() {
-        requests.values().forEach(S3ChannelContext.Request::release);
+        requests.values().forEach(requestRef -> {
+            if (requestRef != null) {
+                final S3ChannelContext.Request request = requestRef.get();
+                if (request != null) {
+                    request.release();
+                }
+            }
+        });
         requests.clear();
     }
 
