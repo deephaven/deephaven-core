6c7240093877ddff48b28f20d67c81e921abf73c
Devin Smith <devinsmith@deephaven.io>
2024-04-05 07:55:36 -0700

    Update entrypoint

--- py/client-ticking/pyClientTickingWheels/entrypoint.sh
+++ py/client-ticking/pyClientTickingWheels/entrypoint.sh
@@ -10,14 +10,16 @@ ORIG_PATH="$PATH"
 for spec in ${WHEELS_SET}; do
   tag=$(echo "$spec" | cut -d: -f 2)
   rm -f ./*.cpp ./*.so
+  # Note: using PATH b/c these do not have venv activates
   PATH="/opt/python/${tag}/bin:$ORIG_PATH"
-  pip3 install cython
+  pip install cython
   MAKEFLAGS="-j${NCPUS}" \
     CFLAGS="-I${DHCPP}/include" \
     LDFLAGS="-L${DHCPP}/lib" \
     DEEPHAVEN_VERSION="${DEEPHAVEN_VERSION}" \
-    python3 setup.py build_ext -i
-    DEEPHAVEN_VERSION="${DEEPHAVEN_VERSION}" python3 setup.py bdist_wheel
-    auditwheel repair --wheel-dir /out dist/pydeephaven_ticking*"${tag}"*.whl
-    rm -f dist/pydeephaven_ticking*"${tag}"*.whl
+    python setup.py build_ext -i
+  DEEPHAVEN_VERSION="${DEEPHAVEN_VERSION}" \
+    python setup.py bdist_wheel
+  auditwheel repair --wheel-dir /out dist/pydeephaven_ticking*"${tag}"*.whl
+  rm -f dist/pydeephaven_ticking*"${tag}"*.whl
 done
