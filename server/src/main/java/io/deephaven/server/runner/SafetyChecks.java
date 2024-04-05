2c8dbcb320ebffab3a545858213b72a1e3a68743
Devin Smith <devinsmith@deephaven.io>
2024-04-05 08:39:17 -0700

    Removed - adding ticket to potentially add back in future #5334

--- server/src/main/java/io/deephaven/server/runner/SafetyChecks.java
+++ server/src/main/java/io/deephaven/server/runner/SafetyChecks.java
@@ -5,10 +5,7 @@ package io.deephaven.server.runner;
 
 import java.lang.Runtime.Version;
 import java.lang.management.ManagementFactory;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Deephaven safety checks.
@@ -31,14 +28,12 @@ public final class SafetyChecks {
     /**
      * Applies a set of safety checks if {@link #isEnabled()}.
      *
-     * @see JDK_END_OF_LIFE
      * @see JDK_8287432
      */
     public static void check() {
         if (!isEnabled()) {
             return;
         }
-        JDK_END_OF_LIFE.check();
         JDK_8287432.check();
     }
 
@@ -124,48 +119,6 @@ public final class SafetyChecks {
 
         private static List<String> workaroundJvmArguments() {
             return List.of("-XX:+UnlockDiagnosticVMOptions", "-XX:DisableIntrinsic=_currentThread");
-        }
-    }
-
-    /**
-     * A safety check for end-of-life JDKs, see
-     * <a href="https://www.oracle.com/java/technologies/java-se-support-roadmap.html">Oracle Java SE Support
-     * Roadmap</a>. This will throw an exception when the {@link Runtime#version()} is 1 year past the EOL date.
-     */
-    public static final class JDK_END_OF_LIFE {
-
-        private static final Map<Integer, LocalDate> EOL_DATES = Map.ofEntries(
-                Map.entry(11, LocalDate.of(2032, Month.JANUARY, 31)),
-                Map.entry(12, LocalDate.of(2019, Month.SEPTEMBER, 30)),
-                Map.entry(13, LocalDate.of(2020, Month.MARCH, 31)),
-                Map.entry(14, LocalDate.of(2020, Month.SEPTEMBER, 30)),
-                Map.entry(15, LocalDate.of(2021, Month.MARCH, 31)),
-                Map.entry(16, LocalDate.of(2021, Month.SEPTEMBER, 30)),
-                Map.entry(17, LocalDate.of(2029, Month.SEPTEMBER, 30)),
-                Map.entry(18, LocalDate.of(2022, Month.SEPTEMBER, 30)),
-                Map.entry(19, LocalDate.of(2023, Month.MARCH, 31)),
-                Map.entry(20, LocalDate.of(2023, Month.SEPTEMBER, 30)),
-                Map.entry(21, LocalDate.of(2031, Month.SEPTEMBER, 30)),
-                Map.entry(22, LocalDate.of(2024, Month.SEPTEMBER, 30)),
-                Map.entry(23, LocalDate.of(2025, Month.MARCH, 31)),
-                Map.entry(24, LocalDate.of(2025, Month.SEPTEMBER, 30)),
-                Map.entry(25, LocalDate.of(2033, Month.SEPTEMBER, 30)));
-
-
-        private static void check() {
-            if (!isEnabled(JDK_END_OF_LIFE.class)) {
-                return;
-            }
-            final Version version = Runtime.version();
-            final LocalDate eolDate = EOL_DATES.get(version.feature());
-            if (eolDate == null) {
-                return;
-            }
-            if (LocalDate.now().isAfter(eolDate.plusYears(1))) {
-                throw exception(JDK_END_OF_LIFE.class, String.format(
-                        "The current JDK feature version %d is end-of-life as of %s. We recommend updating to a non end-of-life JDK.",
-                        version.feature(), eolDate));
-            }
         }
     }
 }
