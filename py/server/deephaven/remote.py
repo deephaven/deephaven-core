43e16d5e86c0d704ed9227d454e2d5e70a1d457b
jianfengmao <jianfengmao@deephaven.io>
2024-04-18 15:56:06 -0600

    Use the new BarrageSessionFactoryClient

--- py/server/deephaven/remote.py
+++ py/server/deephaven/remote.py
@@ -7,7 +7,7 @@ from typing import Dict
 
 import jpy
 
-from deephaven import DHError
+from deephaven import DHError, uri
 from deephaven._wrapper import JObjectWrapper
 from deephaven.table import Table
 
@@ -24,20 +24,19 @@ _JRootAllocator = jpy.get_type("org.apache.arrow.memory.RootAllocator")
 _JSharedId = jpy.get_type("io.deephaven.client.impl.SharedId")
 _JBarrageTableResolver = jpy.get_type("io.deephaven.server.uri.BarrageTableResolver")
 
-_session_cache: Dict[str, RemoteSession] = {}  # use WeakValueDictionary to avoid memory leak?
+_session_cache: Dict[str, BarrageSession] = {}  # use WeakValueDictionary to avoid memory leak?
 _remote_session_lock = threading.Lock()
 
 
-def remote_session(host: str,
-                   port: int = 10000,
-                   auth_type: str = "Anonymous",
-                   auth_token: str = "",
-                   # never_timeout: bool = True,
-                   # session_type: str = 'python',
-                   # use_tls: bool = False,
-                   # tls_root_certs: bytes = None,
-                   # client_cert_chain: bytes = None,
-                   ) -> RemoteSession:
+def barrage_session(host: str,
+                    port: int = 10000,
+                    auth_type: str = "Anonymous",
+                    auth_token: str = "",
+                    use_tls: bool = False,
+                    tls_root_certs: bytes = None,
+                    client_cert_chain: bytes = None,
+                    client_private_key: bytes = None,
+                    ) -> BarrageSession:
     """Returns a Deephaven gRPC session to a remote server if a cached session is available; otherwise, creates a new
     session.
 
@@ -50,120 +49,78 @@ def remote_session(host: str,
         auth_token (str): the authentication token string. When auth_type is 'Basic', it must be
             "user:password"; when auth_type is "Anonymous', it will be ignored; when auth_type is a custom-built
             authenticator, it must conform to the specific requirement of the authenticator
+        use_tls (bool): if True, use a TLS connection.  Defaults to False
+        tls_root_certs (bytes): PEM encoded root certificates to use for TLS connection, or None to use system defaults.
+             If not None implies use a TLS connection and the use_tls argument should have been passed
+             as True. Defaults to None
+        client_cert_chain (bytes): PEM encoded client certificate if using mutual TLS.  Defaults to None,
+             which implies not using mutual TLS.
+        client_private_key (bytes): PEM encoded client private key for client_cert_chain if using mutual TLS.
+             Defaults to None, which implies not using mutual TLS.
+
     Returns:
         a Deephaven gRPC session
 
     Raises:
         DHError
     """
-    with _remote_session_lock:
-        uri = f"dh+plain://{host}:{port}"
-        session = _session_cache.get(uri)
-        if session:
-            if session.is_alive:  # doesn't guarantee the session is still alive, just that it hasn't been explicitly
-                # closed
-                return session
-            else:
-                del _session_cache[uri]
-        session = RemoteSession(host, port, auth_type, auth_token)
-        _session_cache[uri] = session
-        return session
+    try:
+        j_barrage_session_factory_client = uri.resolve(
+            "dh:///app/io.deephaven.server.barrage.BarrageSessionFactoryClient/field/instance")
+
+        target_uri = f"{host}:{port}"
+        if use_tls:
+            target_uri = f"dh://{target_uri}"
+        else:
+            target_uri = f"dh+plain://{target_uri}"
+
+        _j_client_config = (_JClientConfig.builder()
+                          .target(_JDeephavenTarget.of(_JURI(target_uri)))
+                          .build())
+        auth = f"{auth_type} {auth_token}"
+        _j_barrage_session_factory = j_barrage_session_factory_client.factory(_j_client_config, auth)
+        return BarrageSession(_j_barrage_session_factory.newBarrageSession())
+    except Exception as e:
+        raise DHError("failed to get a barrage session to the target remote Deephaven server.") from e
 
 
-class RemoteSession (JObjectWrapper):
+class BarrageSession (JObjectWrapper):
     """ A Deephaven gRPC session to a remote server."""
-    # def __init__(self, j_barrage_session):
-    #     self.j_barrage_session = j_barrage_session
-    #
-    # def subscribe(self, ticket: bytes):
-    #     j_table_handle = self._j_session.of(_JSharedId(ticket).ticketId().table())
-    #     j_barrage_subscription = self._j_barrage_session.subscribe(j_table_handle,
-    #                                                                _JBarrageTableResolver.SUB_OPTIONS)
-    #     j_table = j_barrage_subscription.entireTable().get()
-    #     return Table(j_table)
-    #
-    # def snapshot(self):
-    #     return self.j_barrage_session.snapshot()
-    def __init__(self, host: str,
-                 port: int = 10000,
-                 auth_type: str = "Anonymous",
-                 auth_token: str = "",
-                 # never_timeout: bool = True,
-                 # session_type: str = 'python',
-                 # use_tls: bool = False,
-                 # tls_root_certs: bytes = None,
-                 # client_cert_chain: bytes = None,
-                 ):
-        """Creates a Deephaven gRPC session to a remote server.
-
-        Args:
-            host (str): the host name or IP address of the Deephaven server.
-            port (int): the port number that the remote Deephaven server is listening on, default is 10000.
-            auth_type (str): the authentication type string, can be "Anonymous', 'Basic", or any custom-built
-                authenticator in the server, such as "io.deephaven.authentication.psk.PskAuthenticationHandler",
-                default is 'Anonymous'.
-            auth_token (str): the authentication token string. When auth_type is 'Basic', it must be
-                "user:password"; when auth_type is "Anonymous', it will be ignored; when auth_type is a custom-built
-                authenticator, it must conform to the specific requirement of the authenticator
-        """
-        self.host = host
-        self.port = port
-        self._auth_type = auth_type
-        self._auth_token = auth_token
-        self.grpc_channel = None
-        self._r_lock = threading.RLock()
-        self._is_alive = False
-        self._uri = f"dh+plain://{host}:{port}"
-        self._connect()
+    j_object_type = _JBarrageSession
 
     @property
-    def is_alive(self) -> bool:
-        """if the session is alive."""
-        return self._is_alive
+    def j_object(self) -> jpy.JType:
+        return self.j_barrage_session
 
-    def _connect(self) -> None:
-        try:
-            _j_host_config = (_JClientConfig.builder()
-                              .target(_JDeephavenTarget.of(_JURI(self._uri)))
-                              .build())
-            self._j_channel = _JChannelHelper.channel(_j_host_config)
-            _j_dh_channel = _JDeephavenChannelImpl(self._j_channel)
+    def __init__(self, j_barrage_session):
+        self.j_barrage_session = j_barrage_session
+        self.j_session = j_barrage_session.session()
 
-            _j_session_config = (_JSessionImplConfig.builder()
-                                 .executor(_JExecutors.newScheduledThreadPool(4))
-                                 .authenticationTypeAndValue(f"{self._auth_type} {self._auth_token}")
-                                 .channel(_j_dh_channel)
-                                 .build())
-            self._j_session = _JSessionImpl.create(_j_session_config)
-            self._j_barrage_session = _JBarrageSession.create(self._j_session, _JRootAllocator(), self._j_channel)
-            self._is_alive = True
-        except Exception as e:
-            raise DHError("failed to create a session to a remote Deephaven server.") from e
+    def subscribe(self, ticket: bytes) -> Table:
+        """ TODO
 
-    def close(self):
-        """Closes the gRPC connection."""
-        if not self._is_alive:
-            return
+        Args:
+            ticket:
 
-        with self._r_lock:
-            if not self._is_alive:
-                return
-            try:
-                self._j_session.close()
-            except Exception as e:
-                raise DHError("failed to close the session.") from e
-            finally:
-                self._is_alive = False
+        Returns:
 
-    def fetch(self, shared_ticket: bytes) -> Table:
-        """Fetches data from the Deephaven server."""
-        if not self._is_alive:
-            raise DHError("the session is not alive.")
-        try:
-            j_table_handle = self._j_session.of(_JSharedId(shared_ticket).ticketId().table())
-            j_barrage_subscription = self._j_barrage_session.subscribe(j_table_handle,
-                                                                       _JBarrageTableResolver.SUB_OPTIONS)
-            j_table = j_barrage_subscription.entireTable().get()
-            return Table(j_table)
-        except Exception as e:
-            raise DHError("failed to fetch data from the server.") from e
+        """
+        j_table_handle = self.j_session.of(_JSharedId(ticket).ticketId().table())
+        j_barrage_subscription = self.j_barrage_session.subscribe(j_table_handle,
+                                                                   _JBarrageTableResolver.SUB_OPTIONS)
+        return Table(j_barrage_subscription.entireTable().get())
+
+    def snapshot(self, ticket: bytes) -> Table:
+        """ TODO
+
+        Args:
+            ticket:
+
+        Returns:
+
+        """
+        j_table_handle = self.j_session.of(_JSharedId(ticket).ticketId().table())
+        j_barrage_snapshot = self.j_barrage_session.snapshot(j_table_handle, _JBarrageTableResolver.SNAP_OPTIONS)
+        return Table(j_barrage_snapshot.entireTable().get())
+
+
--- py/server/tests/test_remote.py
+++ py/server/tests/test_remote.py
@@ -3,14 +3,14 @@
 #
 
 import unittest
-from deephaven.remote import remote_session
+from deephaven.remote import barrage_session
 
 from tests.testbase import BaseTestCase
 
 
 class RemoteTestCase(BaseTestCase):
     def test_session(self):
-        session = remote_session(host="core-server-2-1", port=10000, auth_type="Anonymous")
+        session = barrage_session(host="core-server-2-1", port=10000, auth_type="Anonymous")
         t = session.fetch(b'hgd\xc6\xf3\xea\xd3\x15\xbabB#\x1e-\x94\xfcI')
         self.assertEqual(t.size, 1000)
         self.assertEqual(len(t.columns), 2)
