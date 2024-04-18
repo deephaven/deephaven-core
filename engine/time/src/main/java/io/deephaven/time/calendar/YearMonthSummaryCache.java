f3fadc9107d9fd0809bb9d2fe5f35206fd50d44a
Chip Kent <chipkent@deephaven.io>
2024-04-18 14:01:22 -0600

    Added a thread-safe lazily initialized summary data cache by month and year.

---
+++ engine/time/src/main/java/io/deephaven/time/calendar/YearMonthSummaryCache.java
@@ -0,0 +1,170 @@
+package io.deephaven.time.calendar;
+
+import java.time.LocalDate;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+/**
+ * A thread-safe lazily initialized cache for year and month summaries.
+ *
+ * @param <T> the type of the summary
+ */
+class YearMonthSummaryCache<T> {
+
+    private final Map<Integer, T> yearCache = new ConcurrentHashMap<>();
+    private final Map<Integer, T> monthCache = new ConcurrentHashMap<>();
+
+    private final Function<Integer, T> computeMonthSummary;
+    private final Function<Integer, T> computeYearSummary;
+
+    /**
+     * Creates a new cache.
+     *
+     * @param computeMonthSummary the function to compute a month summary
+     * @param computeYearSummary  the function to compute a year summary
+     */
+    YearMonthSummaryCache(Function<Integer, T> computeMonthSummary, Function<Integer, T> computeYearSummary) {
+        this.computeMonthSummary = computeMonthSummary;
+        this.computeYearSummary = computeYearSummary;
+    }
+
+    /**
+     * Clears the cache.
+     */
+    void clear() {
+        yearCache.clear();
+        monthCache.clear();
+    }
+
+    /**
+     * Gets the month summary for the specified year and month.
+     *
+     * @param yearMonth the year and month
+     * @return the month summary
+     */
+    T getMonthSummary(int yearMonth) {
+        return monthCache.computeIfAbsent(yearMonth, computeMonthSummary);
+    }
+
+    /**
+     * Gets the month summary for the specified year and month.
+     *
+     * @param year  the year
+     * @param month the month
+     * @return the month summary
+     */
+    T getMonthSummary(int year, int month) {
+        return getMonthSummary(year * 100 + month);
+    }
+
+    /**
+     * Gets the year summary for the specified year.
+     *
+     * @param year the year
+     * @return the year summary
+     */
+    T getYearSummary(int year) {
+        return yearCache.computeIfAbsent(year, computeYearSummary);
+    }
+
+    private class YearMonthSummaryIterator implements Iterator<T> {
+
+        private int currentYear;
+        private int currentMonth;
+        private int currentYearMonth;
+        private int finalYear;
+        private int finalMonth;
+        final private int finalYearMonth;
+
+        YearMonthSummaryIterator(LocalDate start, LocalDate end) {
+            int startYear = start.getYear();
+            int startMonth = start.getMonthValue();
+            int endYear = end.getYear();
+            int endMonth = end.getMonthValue();
+
+            currentMonth = startMonth;
+            currentYear = startYear;
+
+            if(start.getDayOfMonth() != 1) {
+                incrementCurrentByMonth();
+            }
+
+            currentYearMonth = currentYear * 100 + currentMonth;
+
+            final LocalDate endPlus1 = end.plusDays(1);
+            final int endPlus1Month = endPlus1.getMonthValue();
+
+            finalMonth = endMonth;
+            finalYear = endYear;
+
+            if(endPlus1Month == endMonth) {
+                if(finalMonth == 1) {
+                    finalMonth = 12;
+                    finalYear = finalYear - 1;
+                } else {
+                    finalMonth = finalMonth - 1;
+                }
+            }
+
+            finalYearMonth = finalYear * 100 + finalMonth;
+        }
+
+        private void incrementCurrentByMonth() {
+            if(currentMonth == 12) {
+                currentMonth = 1;
+                currentYear += 1;
+            } else {
+                currentMonth = currentMonth + 1;
+            }
+
+            currentYearMonth = currentYear * 100 + currentMonth;
+        }
+
+        private void incrementCurrentByYear() {
+            currentYear++;
+            currentYearMonth = currentYear * 100 + currentMonth;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentYearMonth <= finalYearMonth;
+        }
+
+        @Override
+        public T next() {
+            final T val;
+
+            if(currentMonth == 1 && (currentYear != finalYear || finalMonth == 12)) {
+                val = getYearSummary(currentYear);
+                incrementCurrentByYear();
+            } else {
+                val = getMonthSummary(currentYear * 100 + currentMonth);
+                incrementCurrentByMonth();
+            }
+
+            return val;
+        }
+    }
+
+    /**
+     * Gets an iterator over the summaries for the specified range.  The returned iterator will include the start
+     * date if {@code startInclusive} is true, and the end date if {@code endInclusive} is true.
+     * If the start date is after the end date, the iterator will be empty.
+     * <p>
+     * The iterator will return summaries in chronological order,
+     * and these summaries can be a mix of month and year summaries.
+     * Dates not represented by complete summaries will be skipped (e.g. partial months).
+     *
+     * @param start          the start date
+     * @param end            the end date
+     * @param startInclusive whether the start date is inclusive
+     * @param endInclusive   whether the end date is inclusive
+     * @return the iterator
+     */
+    Iterator<T> iterator(final LocalDate start, final LocalDate end,
+                         final boolean startInclusive, final boolean endInclusive) {
+        return new YearMonthSummaryIterator(startInclusive ? start : start.plusDays(1), endInclusive ? end : end.minusDays(1));
+    }
+}
---
+++ engine/time/src/test/java/io/deephaven/time/calendar/TestYearMonthSummaryCache.java
@@ -0,0 +1,284 @@
+package io.deephaven.time.calendar;
+
+import io.deephaven.base.testing.BaseArrayTestCase;
+
+import java.time.LocalDate;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+@SuppressWarnings({"DataFlowIssue", "ConstantValue"})
+public class TestYearMonthSummaryCache extends BaseArrayTestCase {
+
+    public void testGetters() {
+        final int[] monthCount = new int[]{0};
+        final int[] yearCount = new int[]{0};
+
+        final Function<Integer, String> monthSummary = i -> {
+            monthCount[0]++;
+            return "month" + i;
+        };
+
+        final Function<Integer, String> yearSummary = i -> {
+            yearCount[0]++;
+            return "year" + i;
+        };
+
+        final YearMonthSummaryCache<String> cache = new YearMonthSummaryCache<>(monthSummary, yearSummary);
+        assertEquals("month202101", cache.getMonthSummary(202101));
+        assertEquals(1, monthCount[0]);
+        assertEquals(0, yearCount[0]);
+        assertEquals("year2021", cache.getYearSummary(2021));
+        assertEquals(1, monthCount[0]);
+        assertEquals(1, yearCount[0]);
+        assertEquals("month202101", cache.getMonthSummary(202101));
+        assertEquals(1, monthCount[0]);
+        assertEquals(1, yearCount[0]);
+        assertEquals("year2021", cache.getYearSummary(2021));
+        assertEquals(1, monthCount[0]);
+        assertEquals(1, yearCount[0]);
+
+        assertEquals("month202102", cache.getMonthSummary(202102));
+        assertEquals(2, monthCount[0]);
+        assertEquals(1, yearCount[0]);
+        assertEquals("year2022", cache.getYearSummary(2022));
+        assertEquals(2, monthCount[0]);
+        assertEquals(2, yearCount[0]);
+
+        cache.clear();
+
+        assertEquals("month202101", cache.getMonthSummary(202101));
+        assertEquals(3, monthCount[0]);
+        assertEquals(2, yearCount[0]);
+        assertEquals("year2021", cache.getYearSummary(2021));
+        assertEquals(3, monthCount[0]);
+        assertEquals(3, yearCount[0]);
+        assertEquals("month202101", cache.getMonthSummary(202101));
+        assertEquals(3, monthCount[0]);
+        assertEquals(3, yearCount[0]);
+        assertEquals("year2021", cache.getYearSummary(2021));
+        assertEquals(3, monthCount[0]);
+        assertEquals(3, yearCount[0]);
+
+        assertEquals("month202102", cache.getMonthSummary(202102));
+        assertEquals(4, monthCount[0]);
+        assertEquals(3, yearCount[0]);
+        assertEquals("year2022", cache.getYearSummary(2022));
+        assertEquals(4, monthCount[0]);
+        assertEquals(4, yearCount[0]);
+
+        assertEquals(cache.getMonthSummary(202101), cache.getMonthSummary(2021, 1));
+    }
+
+    private static <T> Stream<T>
+    iteratorToStream(Iterator<T> iterator) {
+        Spliterator<T> spliterator = Spliterators.spliteratorUnknownSize(iterator, 0);
+        return StreamSupport.stream(spliterator, false);
+    }
+
+    public void testIteratorInclusive() {
+        final YearMonthSummaryCache<String> cache = new YearMonthSummaryCache<>(i -> "month" + i, i -> "year" + i);
+        final boolean startInclusive = true;
+        final boolean endInclusive = true;
+
+        // end before start
+        LocalDate start = LocalDate.of(2021, 1, 2);
+        LocalDate end = LocalDate.of(2021, 1, 1);
+        String[] target = {};
+        String[] actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // same month
+        start = LocalDate.of(2021, 1, 1);
+        end = LocalDate.of(2021, 1, 11);
+        target = new String[]{};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // adjacent partial months
+        start = LocalDate.of(2021, 1, 3);
+        end = LocalDate.of(2021, 2, 11);
+        target = new String[]{};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // full month + partial month
+        start = LocalDate.of(2021, 1, 1);
+        end = LocalDate.of(2021, 2, 11);
+        target = new String[]{"month202101"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // full month + few days
+        start = LocalDate.of(2020, 12, 12);
+        end = LocalDate.of(2021, 2, 11);
+        target = new String[]{"month202101"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // multiple months + few days
+        start = LocalDate.of(2020, 11, 12);
+        end = LocalDate.of(2021, 4, 11);
+        target = new String[]{"month202012", "month202101", "month202102", "month202103"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // partial month + full month
+        start = LocalDate.of(2021, 1, 3);
+        end = LocalDate.of(2021, 2, 28);
+        target = new String[]{"month202102"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // full year
+        start = LocalDate.of(2021, 1, 1);
+        end = LocalDate.of(2021, 12, 31);
+        target = new String[]{"year2021"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // full year + few days
+        start = LocalDate.of(2020, 12, 11);
+        end = LocalDate.of(2022, 1, 3);
+        target = new String[]{"year2021"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // multiple years + few days
+        start = LocalDate.of(2018, 12, 11);
+        end = LocalDate.of(2022, 1, 3);
+        target = new String[]{"year2019", "year2020", "year2021"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // mixed
+        start = LocalDate.of(2018, 10, 11);
+        end = LocalDate.of(2022, 3, 3);
+        target = new String[]{"month201811", "month201812", "year2019", "year2020", "year2021", "month202201", "month202202"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+    }
+
+    public void testIteratorExclusiveInclusive() {
+        final YearMonthSummaryCache<String> cache = new YearMonthSummaryCache<>(i -> "month" + i, i -> "year" + i);
+
+        // start and end of month
+
+        LocalDate start = LocalDate.of(2021, 12, 1);
+        LocalDate end = LocalDate.of(2021, 12, 31);
+
+        boolean startInclusive = true;
+        boolean endInclusive = true;
+        String[] target = new String[]{"month202112"};
+        String[] actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = false;
+        endInclusive = true;
+        target = new String[]{};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = true;
+        endInclusive = false;
+        target = new String[]{};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = false;
+        endInclusive = false;
+        target = new String[]{};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // day before start of month
+
+        start = LocalDate.of(2021, 11, 30);
+        end = LocalDate.of(2021, 12, 31);
+
+        startInclusive = true;
+        endInclusive = true;
+        target = new String[]{"month202112"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = false;
+        endInclusive = true;
+        target = new String[]{"month202112"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = true;
+        endInclusive = false;
+        target = new String[]{};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = false;
+        endInclusive = false;
+        target = new String[]{};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // day after end of month
+
+        start = LocalDate.of(2021, 12, 1);
+        end = LocalDate.of(2022, 1, 1);
+
+        startInclusive = true;
+        endInclusive = true;
+        target = new String[]{"month202112"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = false;
+        endInclusive = true;
+        target = new String[]{};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = true;
+        endInclusive = false;
+        target = new String[]{"month202112"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = false;
+        endInclusive = false;
+        target = new String[]{};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        // day before and after end of month
+
+        start = LocalDate.of(2021, 11, 30);
+        end = LocalDate.of(2022, 1, 1);
+
+        startInclusive = true;
+        endInclusive = true;
+        target = new String[]{"month202112"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = false;
+        endInclusive = true;
+        target = new String[]{"month202112"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = true;
+        endInclusive = false;
+        target = new String[]{"month202112"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+
+        startInclusive = false;
+        endInclusive = false;
+        target = new String[]{"month202112"};
+        actual = iteratorToStream(cache.iterator(start, end, startInclusive, endInclusive)).toArray(String[]::new);
+        assertEquals(target, actual);
+    }
+}
