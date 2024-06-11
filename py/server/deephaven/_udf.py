30140d27d4186e1e515ec88830d421d968585354
jianfengmao <jianfengmao@deephaven.io>
2024-06-11 09:14:46 -0600

    Fix an issue with boxed type arg and np scaar wrn

--- py/server/deephaven/_udf.py
+++ py/server/deephaven/_udf.py
@@ -200,6 +200,11 @@ class _ParsedSignature:
     def prepare_auto_arg_conv(self, encoded_arg_types: str) -> bool:
         """ Determine whether the auto argument conversion should be used and set the converter functions for the
         parameters."""
+        # numba and numpy ufuncs don't need auto argument conversion as they handle the conversion themselves and the
+        # arg types are already verified by the query engine
+        if isinstance(self.fn, (numba.np.ufunc.gufunc.GUFunc, numba.np.ufunc.dufunc.DUFunc)) or isinstance(self.fn, numpy.ufunc):
+            return False
+
         if not self.params or not encoded_arg_types:
             return False
 
@@ -210,6 +215,9 @@ class _ParsedSignature:
 
         for arg_type_str, param in zip(arg_type_strs, self.params):
             param.setup_arg_converter(arg_type_str)
+
+        if isinstance(self.fn, (numba.np.ufunc.gufunc.GUFunc, numba.np.ufunc.dufunc.DUFunc)) or isinstance(self.fn, numpy.ufunc):
+            warnings.filterwarnings("default", category=UserWarning)
 
         if all([param.arg_converter is None for param in self.params]):
             arg_conv_needed = False
