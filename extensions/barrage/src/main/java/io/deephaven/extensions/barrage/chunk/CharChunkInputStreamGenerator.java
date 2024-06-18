8eea3a395ab4b553244c3d941683f196f1ab872e
Colin Alworth <colinalworth@deephaven.io>
2024-06-18 15:25:46 -0500

    Make assertion provide more info

--- extensions/barrage/src/main/java/io/deephaven/extensions/barrage/chunk/CharChunkInputStreamGenerator.java
+++ extensions/barrage/src/main/java/io/deephaven/extensions/barrage/chunk/CharChunkInputStreamGenerator.java
@@ -264,9 +264,7 @@ public class CharChunkInputStreamGenerator extends BaseChunkInputStreamGenerator
             // consumed entire validity buffer by here
 
             final long payloadRead = (long) nodeInfo.numElements * elementSize;
-            if (payloadBuffer < payloadRead) {
-                throw new IllegalStateException("payload buffer is too short for expected number of elements");
-            }
+            Assert.geq(payloadBuffer, "payloadBuffer", payloadRead, "payloadRead");
 
             if (options.useDeephavenNulls()) {
                 useDeephavenNulls(conversion, is, nodeInfo, chunk, outOffset);
