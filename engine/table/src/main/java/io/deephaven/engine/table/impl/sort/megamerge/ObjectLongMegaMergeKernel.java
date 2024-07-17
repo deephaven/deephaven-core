c947b3163d2bd8748ef7364cef7843b70d9cef46
Devin Smith <devinsmith@deephaven.io>
2024-07-17 08:59:32 -0700

    perf: MegaMerge, use explicit comparison operator

--- engine/table/src/main/java/io/deephaven/engine/table/impl/sort/megamerge/ObjectLongMegaMergeKernel.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/sort/megamerge/ObjectLongMegaMergeKernel.java
@@ -179,14 +179,18 @@ public class ObjectLongMegaMergeKernel {
     }
 
     // region comparison functions
-    private static int doComparison(Object lhs, Object rhs) {
-        return ObjectComparisons.compare(lhs, rhs);
+    private static boolean lt(Object lhs, Object rhs) {
+        return ObjectComparisons.lt(lhs, rhs);
     }
-    // endregion comparison functions
+
+    private static boolean leq(Object lhs, Object rhs) {
+        return ObjectComparisons.leq(lhs, rhs);
+    }
 
     private static boolean geq(Object lhs, Object rhs) {
-        return doComparison(lhs, rhs) >= 0;
+        return ObjectComparisons.geq(lhs, rhs);
     }
+    // endregion comparison functions
 
     // when we binary search in 1, we must identify a position for search value that is *after* our test values;
     // because the values from run 2 may never be inserted before an equal value from run 1
@@ -199,13 +203,11 @@ public class ObjectLongMegaMergeKernel {
     }
 
     private static long bound(ObjectArraySource valuesToSort, long lo, long hi, Object searchValue,
-            @SuppressWarnings("SameParameterValue") final boolean lower) {
-        final int compareLimit = lower ? -1 : 0; // lt or leq
-
+            @SuppressWarnings("SameParameterValue") final boolean strict) {
         while (lo < hi) {
             final long mid = (lo + hi) >>> 1;
             final Object testValue = valuesToSort.getUnsafe(mid);
-            final boolean moveLo = doComparison(testValue, searchValue) <= compareLimit;
+            final boolean moveLo = strict ? lt(testValue, searchValue) : leq(testValue, searchValue);
             if (moveLo) {
                 // For bound, (testValue OP searchValue) means that the result somewhere later than 'mid' [OP=lt or leq]
                 lo = mid + 1;
@@ -225,13 +227,11 @@ public class ObjectLongMegaMergeKernel {
     }
 
     private static int bound(ObjectChunk<Object, ?> valuesToSort, int lo, int hi, Object searchValue,
-            @SuppressWarnings("SameParameterValue") final boolean lower) {
-        final int compareLimit = lower ? -1 : 0; // lt or leq
-
+            @SuppressWarnings("SameParameterValue") final boolean strict) {
         while (lo < hi) {
             final int mid = (lo + hi) >>> 1;
             final Object testValue = valuesToSort.get(mid);
-            final boolean moveLo = doComparison(testValue, searchValue) <= compareLimit;
+            final boolean moveLo = strict ? lt(testValue, searchValue) : leq(testValue, searchValue);
             if (moveLo) {
                 // For bound, (testValue OP searchValue) means that the result somewhere later than 'mid' [OP=lt or leq]
                 lo = mid + 1;
