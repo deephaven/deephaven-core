5dd41bccaa9d60af5fe7fb60f0e18919831e5651
Colin Alworth <colinalworth@deephaven.io>
2024-07-19 08:21:48 -0500

    Restore missing import

--- extensions/barrage/src/main/java/io/deephaven/extensions/barrage/chunk/BaseChunkInputStreamGenerator.java
+++ extensions/barrage/src/main/java/io/deephaven/extensions/barrage/chunk/BaseChunkInputStreamGenerator.java
@@ -4,6 +4,7 @@
 package io.deephaven.extensions.barrage.chunk;
 
 import io.deephaven.chunk.attributes.Values;
+import io.deephaven.chunk.util.pools.PoolableChunk;
 import io.deephaven.engine.rowset.RowSequence;
 import io.deephaven.engine.rowset.RowSequenceFactory;
 import io.deephaven.engine.rowset.RowSet;
