1aa2c1a37683d89e190c2b68efe1a604c94af83c
Colin Alworth <colin@vertispan.com>
2024-07-31 14:13:01 -0500

    Update extensions/barrage/src/main/java/io/deephaven/extensions/barrage/chunk/array/ArrayReflectUtil.java
    Co-authored-by: Ryan Caudy <rcaudy@gmail.com>

--- extensions/barrage/src/main/java/io/deephaven/extensions/barrage/chunk/array/ArrayReflectUtil.java
+++ extensions/barrage/src/main/java/io/deephaven/extensions/barrage/chunk/array/ArrayReflectUtil.java
@@ -6,12 +6,11 @@ package io.deephaven.extensions.barrage.chunk.array;
 import java.lang.reflect.Array;
 
 /**
- * Holds a reflective call that the JS API cannot make. Instead, the JS API will return an {@code Object[]} instance,
- * which will not bother JS clients, but wouldn't make sense for the JVM-based engine.
+ * This utility class exists to isolate wrappers around reflective calls that may need to have their implementations replaced when emulated in the JS API.
  */
 class ArrayReflectUtil {
     /**
-     * Delegates to {@link Array#newInstance(Class, int)} in the JVM.
+     * Allocate an array to hold {@code length} elements of type {@code componentType}. The standard (Java) implementation delegates to {@link Array#newInstance(Class, int)}.
      *
      * @param componentType the component type in the array to be created
      * @param length the length of the array to create
