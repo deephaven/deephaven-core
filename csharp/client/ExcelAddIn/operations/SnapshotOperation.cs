1e991f7bdab015b1644a60140b109ec7ae13463b
Corey Kosak <kosak@kosak.com>
2024-08-11 16:25:43 -0400

    blah

--- csharp/client/ExcelAddIn/operations/SnapshotOperation.cs
+++ csharp/client/ExcelAddIn/operations/SnapshotOperation.cs
@@ -1,20 +1,58 @@
 ﻿using Deephaven.DeephavenClient.ExcelAddIn.ExcelDna;
 using Deephaven.DeephavenClient.ExcelAddIn.Util;
+using ExcelDna.Integration;
 
 namespace Deephaven.DeephavenClient.ExcelAddIn.Operations;
 
-internal class SnapshotOperation : IOperation {
-  private readonly string _tableName;
+internal class SnapshotOperation : IExcelObservable {
+  private readonly string _tableDescriptor;
   private readonly string _filter;
   private readonly bool _wantHeaders;
-  private readonly IDataListener _dataListener;
+  private readonly OperationManager _operationManager;
 
-  public SnapshotOperation(string tableName, string filter, bool wantHeaders, IDataListener dataListener) {
-    _tableName = tableName;
+  public SnapshotOperation(string tableDescriptor, string filter, bool wantHeaders, OperationManager operationManager) {
+    _tableDescriptor = tableDescriptor;
     _filter = filter;
     _wantHeaders = wantHeaders;
-    _dataListener = dataListener;
+    _operationManager = operationManager;
   }
+
+  IDisposable IExcelObservable.Subscribe(IExcelObserver observer) {
+    _observerCollection.Add(observer, out var isFirst);
+
+    if (isFirst) {
+      _operationManagerDisposer = _operationManager.SubscribeToFilteredTable(this, _tableDescriptor, _filter);
+    }
+
+    return new ActionAsDisposable(() => IExcelObservableDispose(observer));
+  }
+
+  public void ExcelObservableDispose(IExcelObserver observer) {
+    _observerCollection.Remove(observer, out var wasLast);
+    if (!wasLast) {
+      return;
+    }
+
+    var temp = _operationManagerDisposer;
+    if (temp == null) {
+      return;
+    }
+    _operationManagerDisposer = null;
+    temp.Dispose();
+  }
+
+
+
+  _observerCollection.Remove(observer, out var wasLast);
+      if (wasLast) {
+        
+      }
+
+
+      return IExcelObservableRemove(observer);
+    });
+  }
+
 
   public void NewClientState(Client? client, string? message) {
     if (message != null) {
