993fc90c209a9aa99a184e7f25e46d51a927b118
Shivam Malhotra <malhotraashivam@gmail.com>
2024-08-28 12:27:41 -0500

    Junit5 -> Junit4 + some minor testing changes

--- extensions/iceberg/s3/src/test/java/io/deephaven/iceberg/util/IcebergLocalStackTest.java
+++ extensions/iceberg/s3/src/test/java/io/deephaven/iceberg/util/IcebergLocalStackTest.java
@@ -3,24 +3,21 @@
 //
 package io.deephaven.iceberg.util;
 
-
 import io.deephaven.extensions.s3.S3Instructions.Builder;
 import io.deephaven.extensions.s3.testlib.SingletonContainers;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
+import org.junit.BeforeClass;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 
-@Tag("testcontainers")
 public class IcebergLocalStackTest extends IcebergToolsTest {
 
-    @BeforeAll
-    static void initContainer() {
+    @BeforeClass
+    public static void initContainer() {
         // ensure container is started so container startup time isn't associated with a specific test
         SingletonContainers.LocalStack.init();
     }
 
     @Override
-    public Builder s3Instructions(Builder builder) {
+    public Builder s3Instructions(final Builder builder) {
         return SingletonContainers.LocalStack.s3Instructions(builder);
     }
 
