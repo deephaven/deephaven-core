4e16aa4f0717773efcc4db5122d929be405e3223
alexpeters1208 <alexscottpeters1208@gmail.com>
2024-09-06 15:54:10 -0500

    Modify script to split docs

--- proto/proto-backplane-grpc/docs/generate_docs.sh
+++ proto/proto-backplane-grpc/docs/generate_docs.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-protoc -I/usr/include -I/protos --doc_out=/out `find /protos/ -name \*.proto`
+# Find all .proto files in the /protos directory
+for proto_file in `find /protos/ -name \*.proto`; do
+  # Extract the base name of the .proto file (e.g., myfile.proto -> myfile)
+  base_name=$(basename "$proto_file" .proto)
+
+  # Generate documentation for each .proto file
+  protoc \
+    -I/usr/include -I/protos \
+    --doc_out=/out --doc_opt=markdown,"${base_name}.md" \
+    "$proto_file"
+done
