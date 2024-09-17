34c3fb3e9ea6708679e36f2a27cb80ff327b39e3
Chip Kent <chipkent@deephaven.io>
2024-09-17 12:29:43 -0600

    v2

--- py/server/deephaven/experimental/pivot.py
+++ py/server/deephaven/experimental/pivot.py
@@ -1,24 +1,23 @@
-import jpy
-import random
-from deephaven.table import Table
-from deephaven import time_table, empty_table
-import deephaven.table
-from deephaven.table import multi_join
-from deephaven import numpy as dhnp
-import re
-from deephaven.server.executors import submit_task
 from typing import Sequence, List, Union, Protocol
+import random
+import re
+import jpy
+from deephaven import time_table, empty_table, numpy as dhnp
+from deephaven.table import Table, multi_join, MultiJoinTable
+
 
 # Instead of legalizing the column name, it would be preferable to assign known names, and index them by the names table; using some kind of display name.
 # We don't know that we'll actually replace stuff to be unique here, which means this is not actually correct.
-def legalize_column(s : str):
+def _legalize_column(s: str) -> str:
     if re.match("^[_a-zA-Z][_a-zA-Z0-9]*$", s):
         return s
     if re.match("^[_a-zA-Z].*$", s):
         return re.sub("[^_a-zA-Z0-9]", "_", s)
     return "_" + re.sub("[^_a-zA-Z0-9]", "_", s)
 
-def do_multijoin(partitions_table, row_column_names : list[str], col_column_name : str, value_column_name : str):
+
+def _do_multijoin(partitions_table, row_column_names: list[str], col_column_name: str,
+                  value_column_name: str) -> MultiJoinTable:
     # Define the columns for a multi-join
 
     keys = partitions_table.keys()
@@ -32,28 +31,29 @@ def do_multijoin(partitions_table, row_column_names : list[str], col_column_name
     ki = 0
 
     for con in partitions_table.constituent_tables:
-        tables.append(con.view(row_column_names + ["%s=%s" % (legalize_column(str(key_values[ki][0])), value_column_name)]))
+        tables.append(
+            con.view(row_column_names + ["%s=%s" % (_legalize_column(str(key_values[ki][0])), value_column_name)]))
         ki = ki + 1
 
     return multi_join(input=tables, on=row_column_names).table()
 
-def pivot(source, row_column_names : list[str], col_column_name : str, value_column_name : str):
+
+def pivot(source: Table, row_column_names: list[str], col_column_name: str, value_column_name: str) -> MultiJoinTable:
     # Partition the source by column
-    partitioned_source=source.partition_by(col_column_name)
-    pvt = do_multijoin(partitioned_source, row_column_names, col_column_name, value_column_name)
+    partitioned_source = source.partition_by(col_column_name)
+    pvt = _do_multijoin(partitioned_source, row_column_names, col_column_name, value_column_name)
     return pvt
 
-
 # # Java wrappers
-# random_class = jpy.get_type("java.util.Random")
-# random_inst = random_class(0)
-# 
-# y=empty_table(1000).select(["Row=ii%10", "Col=(int)((ii/10) % 30)", "Sentinel=random_inst.nextDouble()"]).where("Sentinel > 0.3")
-# 
-# # first part of the pivot is getting unique row and column values
-# ys = y.sum_by(["Row", "Col"])
-# 
-# pvt=pivot(ys, ["Row"], "Col", "Sentinel")
+random_class = jpy.get_type("java.util.Random")
+random_inst = random_class(0)
+
+y=empty_table(1000).select(["Row=ii%10", "Col=(int)((ii/10) % 30)", "Sentinel=random_inst.nextDouble()"]).where("Sentinel > 0.3")
+
+# first part of the pivot is getting unique row and column values
+ys = y.sum_by(["Row", "Col"])
+
+pvt=pivot(ys, ["Row"], "Col", "Sentinel")
 # 
 # # we have no real sector data, but it is nice for an example
 # #sectors = ["Apples", "Bananas", "Carrots", "Eggplant", "Fig"]
