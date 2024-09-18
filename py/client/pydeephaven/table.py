46f54962b6a76bc0834379b9778ce4248859bbae
jianfengmao <jianfengmao@deephaven.io>
2024-09-18 15:31:53 -0600

    Move the new interface to table.py

--- py/client/pydeephaven/table.py
+++ py/client/pydeephaven/table.py
@@ -6,13 +6,13 @@ data."""
 
 from __future__ import annotations
 
-from typing import List, Union
+from typing import List, Union, Sequence
 
 import pyarrow as pa
 
 from pydeephaven._utils import to_list
 
-from pydeephaven._table_ops import MetaTableOp, SortDirection
+from pydeephaven._table_ops import MetaTableOp, SortDirection, MultijoinTablesOp
 from pydeephaven.agg import Aggregation
 from pydeephaven.dherror import DHError
 from pydeephaven._table_interface import TableInterface
@@ -804,3 +804,67 @@ class InputTable(Table):
             self.session.input_table_service.delete(self, table)
         except Exception as e:
             raise DHError("delete data in the InputTable failed.") from e
+
+
+class MultiJoinInput:
+    """A MultiJoinInput represents the input tables, key columns and additional columns to be used in the multi-table
+    natural join.
+    """
+    table: Table
+    on: Union[str, Sequence[str]]
+    joins: Union[str, Sequence[str]] = None
+
+    def __init__(self, table: Table, on: Union[str, Sequence[str]], joins: Union[str, Sequence[str]] = None):
+        """Initializes a MultiJoinInput object.
+
+        Args:
+            table (Table): the right table to include in the join
+            on (Union[str, Sequence[str]]): the column(s) to match, can be a common name or an equality expression that
+                matches every input table, i.e. "col_a = col_b" to rename output column names.
+            joins (Union[str, Sequence[str]], optional): the column(s) to be added from the table to the result
+                table, can be renaming expressions, i.e. "new_col = col"; default is None
+        """
+        self.table = table
+        self.on = to_list(on)
+        self.joins = to_list(joins)
+
+
+def multi_join(input: Union[Table, Sequence[Table], MultiJoinInput, Sequence[MultiJoinInput]],
+               on: Union[str, Sequence[str]] = None) -> Table:
+    """ The multi_join method creates a new table by performing a multi-table natural join on the input tables. The
+    result consists of the set of distinct keys from the input tables natural joined to each input table. Input
+    tables need not have a matching row for each key, but they may not have multiple matching rows for a given key.
+
+    Args:
+        input (Union[Table, Sequence[Table], MultiJoinInput, Sequence[MultiJoinInput]]): the input objects specifying the
+            tables and columns to include in the join.
+        on (Union[str, Sequence[str]], optional): the column(s) to match, can be a common name or an equality expression
+            that matches every input table, i.e. "col_a = col_b" to rename output column names. Note: When
+            MultiJoinInput objects are supplied, this parameter must be omitted.
+
+    Returns:
+        Table: the result of the multi-table natural join operation.
+
+    Raises:
+        DHError
+    """
+    if isinstance(input, Table) or (isinstance(input, Sequence) and all(isinstance(t, Table) for t in input)):
+        tables = to_list(input)
+        session = tables[0].session
+        if not all([t.session == session for t in tables]):
+            raise DHError(message="all tables must be from the same session.")
+        multi_join_inputs = [MultiJoinInput(table=t, on=on) for t in tables]
+    elif isinstance(input, MultiJoinInput) or (
+            isinstance(input, Sequence) and all(isinstance(ji, MultiJoinInput) for ji in input)):
+        if on is not None:
+            raise DHError(message="on parameter is not permitted when MultiJoinInput objects are provided.")
+        multi_join_inputs = to_list(input)
+        session = multi_join_inputs[0].table.session
+        if not all([mji.table.session == session for mji in multi_join_inputs]):
+            raise DHError(message="all tables must be from the same session.")
+    else:
+        raise DHError(
+            message="input must be a Table, a sequence of Tables, a MultiJoinInput, or a sequence of MultiJoinInputs.")
+
+    table_op = MultijoinTablesOp(multi_join_inputs=multi_join_inputs)
+    return session.table_service.grpc_table_op(None, table_op, table_class=Table)
--- py/client/tests/test_multijoin.py
+++ py/client/tests/test_multijoin.py
@@ -5,8 +5,8 @@ import unittest
 
 from pyarrow import csv
 
-from pydeephaven import DHError
-from pydeephaven.session import MultiJoinInput
+from pydeephaven import DHError, Session
+from pydeephaven.table import MultiJoinInput, multi_join
 from tests.testbase import BaseTestCase
 
 
@@ -31,7 +31,7 @@ class MultiJoinTestCase(BaseTestCase):
 
     def test_static_simple(self):
         # Test with multiple input tables
-        mj_table = self.session.multi_join(input=[self.static_tableA, self.static_tableB], on=["a", "b"])
+        mj_table = multi_join(input=[self.static_tableA, self.static_tableB], on=["a", "b"])
 
         # Output table is static
         self.assertFalse(mj_table.is_refreshing)
@@ -40,7 +40,7 @@ class MultiJoinTestCase(BaseTestCase):
         self.assertEqual(mj_table.size, self.static_tableB.size)
 
         # Test with a single input table
-        mj_table = self.session.multi_join(self.static_tableA, ["a", "b"])
+        mj_table = multi_join(self.static_tableA, ["a", "b"])
 
         # Output table is static
         self.assertFalse(mj_table.is_refreshing)
@@ -49,13 +49,13 @@ class MultiJoinTestCase(BaseTestCase):
 
     def test_ticking_simple(self):
         # Test with multiple input tables
-        mj_table = self.session.multi_join(input=[self.ticking_tableA, self.ticking_tableB], on=["a", "b"])
+        mj_table = multi_join(input=[self.ticking_tableA, self.ticking_tableB], on=["a", "b"])
 
         # Output table is refreshing
         self.assertTrue(mj_table.is_refreshing)
 
         # Test with a single input table
-        mj_table = self.session.multi_join(input=self.ticking_tableA, on=["a", "b"])
+        mj_table = multi_join(input=self.ticking_tableA, on=["a", "b"])
 
         # Output table is refreshing
         self.assertTrue(mj_table.is_refreshing)
@@ -66,7 +66,7 @@ class MultiJoinTestCase(BaseTestCase):
             MultiJoinInput(table=self.static_tableA, on=["key1=a", "key2=b"], joins=["c1", "e1"]),
             MultiJoinInput(table=self.static_tableB, on=["key1=a", "key2=b"], joins=["d2"])
         ]
-        mj_table = self.session.multi_join(mj_input)
+        mj_table = multi_join(mj_input)
 
         # Output table is static
         self.assertFalse(mj_table.is_refreshing)
@@ -75,7 +75,7 @@ class MultiJoinTestCase(BaseTestCase):
         self.assertEqual(mj_table.size, self.static_tableB.size)
 
         # Test with a single input
-        mj_table = self.session.multi_join(MultiJoinInput(table=self.static_tableA, on=["key1=a", "key2=b"], joins="c1"))
+        mj_table = multi_join(MultiJoinInput(table=self.static_tableA, on=["key1=a", "key2=b"], joins="c1"))
 
         # Output table is static
         self.assertFalse(mj_table.is_refreshing)
@@ -88,13 +88,13 @@ class MultiJoinTestCase(BaseTestCase):
             MultiJoinInput(table=self.ticking_tableA, on=["key1=a", "key2=b"], joins=["c1", "e1"]),
             MultiJoinInput(table=self.ticking_tableB, on=["key1=a", "key2=b"], joins=["d2"])
         ]
-        mj_table = self.session.multi_join(mj_input)
+        mj_table = multi_join(mj_input)
 
         # Output table is refreshing
         self.assertTrue(mj_table.is_refreshing)
 
         # Test with a single input
-        mj_table = self.session.multi_join(input=MultiJoinInput(table=self.ticking_tableA, on=["key1=a", "key2=b"], joins="c1"))
+        mj_table = multi_join(input=MultiJoinInput(table=self.ticking_tableA, on=["key1=a", "key2=b"], joins="c1"))
 
         # Output table is refreshing
         self.assertTrue(mj_table.is_refreshing)
@@ -106,7 +106,20 @@ class MultiJoinTestCase(BaseTestCase):
             MultiJoinInput(table=self.ticking_tableB, on=["key1=a", "key2=b"], joins=["d2"])
         ]
         with self.assertRaises(DHError):
-            mj_table = self.session.multi_join(mj_input, on=["key1=a", "key2=b"])
+            mj_table = multi_join(mj_input, on=["key1=a", "key2=b"])
+
+        session = Session()
+        t = session.time_table("PT00:00:00.001").update(
+            ["a = i", "b = i*i % 13", "c1 = i * 13 % 23", "d1 = a + b", "e1 = a - b"]).drop_columns(["Timestamp"])
+
+        # Assert the exception is raised when to-be-joined tables are not from the same session.
+        mj_input = [
+            MultiJoinInput(table=self.ticking_tableA, on=["key1=a", "key2=b"], joins=["c1", "e1"]),
+            MultiJoinInput(table=t, on=["key1=a", "key2=b"], joins=["d2"])
+        ]
+        with self.assertRaises(DHError) as cm:
+            mj_table = multi_join(mj_input)
+        self.assertIn("all tables must be from the same session", str(cm.exception))
 
 
 if __name__ == '__main__':
