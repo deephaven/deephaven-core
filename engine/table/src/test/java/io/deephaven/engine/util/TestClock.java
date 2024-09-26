967b2033e2b9f81fb4bcf6d371b8f162a7f8c6f1
Charles P. Wright <charleswright@deephaven.io>
2024-09-26 11:40:57 -0400

    perf: Embed WindowCheck in TimeSeriesFilter. (#6081)
    Fixes #6062.

--- engine/table/src/test/java/io/deephaven/engine/util/TestClock.java
+++ engine/table/src/test/java/io/deephaven/engine/util/TestClock.java
@@ -4,6 +4,9 @@
 package io.deephaven.engine.util;
 
 import io.deephaven.base.clock.ClockNanoBase;
+import io.deephaven.time.DateTimeUtils;
+
+import java.time.ZoneId;
 
 public final class TestClock extends ClockNanoBase {
     public long now;
@@ -19,5 +22,43 @@ public final class TestClock extends ClockNanoBase {
     @Override
     public long currentTimeNanos() {
         return now;
+    }
+
+    /**
+     * Set the time.
+     *
+     * @param epochNanos the time to set in nanos since the epoch
+     * @return this clock
+     */
+    public TestClock setNanos(long epochNanos) {
+        this.now = epochNanos;
+        return this;
+    }
+
+    /**
+     * Set the time.
+     *
+     * @param epochMillis the time to set in millis since the epoch
+     * @return this clock
+     */
+    public TestClock setMillis(long epochMillis) {
+        this.now = DateTimeUtils.millisToNanos(epochMillis);
+        return this;
+    }
+
+    /**
+     * Add millis to the current time.
+     *
+     * @param millis the number of millis to add
+     * @return this clock
+     */
+    public TestClock addMillis(long millis) {
+        this.now += DateTimeUtils.millisToNanos(millis);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "TestClock{" + DateTimeUtils.epochNanosToInstant(now).atZone(ZoneId.systemDefault()) + "}";
     }
 }
