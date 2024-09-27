e5966892a2766a95d52cfa57dfda8701199a9ed3
Jianfeng Mao <4297243+jmao-denver@users.noreply.github.com>
2024-09-27 13:25:01 -0600

    test: Fix a potential leak or worse in the test (#6146)
    This came up as a suspect to a more serious test failure.

--- py/client/tests/test_plugin_client.py
+++ py/client/tests/test_plugin_client.py
@@ -4,7 +4,7 @@
 import unittest
 
 from pydeephaven.session import Session
-from pydeephaven.ticket import SharedTicket, ServerObject
+from pydeephaven.ticket import SharedTicket, ServerObject, ScopeTicket
 from tests.testbase import BaseTestCase
 
 
@@ -14,8 +14,11 @@ class PluginClientTestCase(BaseTestCase):
         server_script = '''
 from deephaven.plot.figure import Figure
 from deephaven import empty_table
+import deephaven.pandas as dhpd
 
 source = empty_table(20).update(["Letter = (i % 2 == 0) ? `A` : `B`", "X = 0.1 * i", "Y = randomDouble(0.0, 5.0)"])
+
+df = dhpd.to_pandas(source)
 
 plot = Figure().plot_xy(series_name="Random numbers", t=source, x="X", y="Y").show()
 
@@ -36,8 +39,10 @@ plot3 = Figure().plot_xy(series_name="Random numbers", t=empty_table(30).update(
         self.assertGreater(len(refs), 0)
         ref = refs[0]
         self.assertEqual(ref.type, "Table")
+        plugin_client.close()
 
-    def test_publish_fetch(self):
+    @unittest.skip("need to wait for https://github.com/deephaven/deephaven-core/issues/5996")
+    def test_publish_fetch_figure(self):
         plugin_client = self.session.plugin_client(self.session.exportable_objects["plot3"])
         self.assertIsNotNone(plugin_client)
 
@@ -63,7 +68,6 @@ plot3 = Figure().plot_xy(series_name="Random numbers", t=empty_table(30).update(
             sub_session.close()
 
         with self.subTest("Fetchable in the Plugin object"):
-            self.skipTest("need to wait for https://github.com/deephaven/deephaven-core/issues/5996")
             payload, refs = next(plugin_client.resp_stream)
             self.assertGreater(len(payload), 0)
             self.assertGreater(len(refs), 0)
@@ -85,7 +89,6 @@ plot3 = Figure().plot_xy(series_name="Random numbers", t=empty_table(30).update(
             sub_session.close()
 
         with self.subTest("released Plugin object"):
-            self.skipTest("need to wait for https://github.com/deephaven/deephaven-core/issues/5996")
             sub_session = Session()
             server_obj = ServerObject(type="Figure", ticket=shared_ticket)
             sub_plugin_client = sub_session.plugin_client(server_obj)
@@ -95,6 +98,52 @@ plot3 = Figure().plot_xy(series_name="Random numbers", t=empty_table(30).update(
             sub_session.close()
 
         plugin_client.close()
+
+    @unittest.skip("need to wait for https://github.com/deephaven/deephaven-core/issues/5996")
+    def test_publish_fetch_pandas(self):
+        plugin_client = self.session.plugin_client(self.session.exportable_objects["df"])
+        self.assertIsNotNone(plugin_client)
+
+        # First fetch the Plugin object, then publish it
+        export_plugin_client = self.session.fetch(plugin_client)
+        shared_ticket = SharedTicket.random_ticket()
+        self.session.publish(export_plugin_client, shared_ticket)
+
+        # Another session to use the shared Plugin object
+        sub_session = Session()
+        server_obj = ServerObject(type="pandas.DataFrame", ticket=shared_ticket)
+        sub_plugin_client = sub_session.plugin_client(server_obj)
+        payload, refs = next(sub_plugin_client.resp_stream)
+        self.assertEqual(len(payload), 0)
+        self.assertEqual(len(refs), 1)
+        ref = refs[0]
+        self.assertEqual(ref.type, "Table")
+        fetched = ref.fetch()
+        self.assertIsNotNone(fetched)
+        self.assertEqual(fetched.size, 20)
+        sub_plugin_client.close()
+        sub_session.close()
+
+        # continue with source plugin client
+        payload, refs = next(plugin_client.resp_stream)
+        self.assertEqual(len(payload), 0)
+        self.assertEqual(len(refs), 1)
+        ref = refs[0]
+        self.assertEqual(ref.type, "Table")
+        fetched = ref.fetch()
+        self.assertIsNotNone(fetched)
+        self.assertEqual(fetched.size, 20)
+        plugin_client.close()
+
+    def test_publish_fetch_table(self):
+        export_ticket = self.session.fetch(ScopeTicket.scope_ticket("source"))
+        shared_ticket = SharedTicket.random_ticket()
+        self.session.publish(export_ticket, shared_ticket)
+
+        sub_session = Session()
+        sub_export_ticket = sub_session.fetch(shared_ticket)
+        sub_table = sub_session.table_service.fetch_etcr(sub_export_ticket.pb_ticket)
+        self.assertEqual(sub_table.size, 20)
 
 
 if __name__ == "__main__":
