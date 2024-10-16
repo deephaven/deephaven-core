3692f7a882c0527316dad55d736b2ce5f9e4a7c9
Colin Alworth <colinalworth@deephaven.io>
2024-10-15 21:41:17 -0500

    fix: JS API rollups with unique aggs must specify null sentinel (#6202)
    Updated the HTML sample to support other missing aggregations.
    
    Fixes #6200

--- web/client-api/src/test/java/io/deephaven/web/client/api/HierarchicalTableTestGwt.java
+++ web/client-api/src/test/java/io/deephaven/web/client/api/HierarchicalTableTestGwt.java
@@ -3,10 +3,19 @@
 //
 package io.deephaven.web.client.api;
 
+import elemental2.core.JsArray;
 import elemental2.dom.CustomEvent;
 import elemental2.promise.Promise;
+import io.deephaven.web.client.api.tree.JsRollupConfig;
 import io.deephaven.web.client.api.tree.JsTreeTable;
 import io.deephaven.web.client.api.tree.TreeViewportData;
+import io.deephaven.web.client.api.tree.enums.JsAggregationOperation;
+import jsinterop.base.Js;
+import jsinterop.base.JsPropertyMap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
 
 public class HierarchicalTableTestGwt extends AbstractAsyncGwtTestCase {
     private static final Format red = new Format(0x1ff000001e0e0e0L, 0, null, null);
@@ -23,8 +32,10 @@ public class HierarchicalTableTestGwt extends AbstractAsyncGwtTestCase {
                     "empty_table(1000).update(['ID=i', 'Parent=i == 0 ? null : (int)(i/10)']).format_columns(['ID=ID>0 ? GREEN : RED']).tree('ID', 'Parent')")
             .script("ticking_tree",
                     "time_table('PT0.1s').update(['ID=i', 'Parent=i == 0 ? null : (int)(i/10)']).format_columns(['ID=ID>0 ? GREEN : RED']).tree('ID', 'Parent')")
+            .script("table_to_rollup",
+                    "time_table('PT0.1s').update(['Y=Math.sin(i/3)', 'X=i%3']).format_columns(['Y=Y>0 ? GREEN : RED'])")
             .script("ticking_rollup",
-                    "time_table('PT0.1s').update(['Y=Math.sin(i/3)', 'X=i%3']).format_columns(['Y=Y>0 ? GREEN : RED']).rollup(aggs=[agg.first('Y')],by=['X'],include_constituents=True)");
+                    "table_to_rollup.rollup(aggs=[agg.first('Y')],by=['X'],include_constituents=True)");
 
     public void testStaticTreeTable() {
         connect(tables)
@@ -264,6 +275,50 @@ public class HierarchicalTableTestGwt extends AbstractAsyncGwtTestCase {
                                 assertTrue(rollup.isClosed());
                                 return null;
                             });
+                })
+                .then(this::finish).catch_(this::report);
+    }
+
+    public void testCreateRollup() {
+        connect(tables)
+                .then(table("table_to_rollup"))
+                .then(table -> {
+                    List<Supplier<Promise<JsTreeTable>>> tests = new ArrayList<>();
+                    // For each supported operation, apply it to the numeric column
+                    // Then expand to verify data can load
+                    List<String> count = List.of(
+                            JsAggregationOperation.COUNT,
+                            JsAggregationOperation.COUNT_DISTINCT,
+                            // TODO(deephaven-core#6201) re-enable this line when fixed
+                            // JsAggregationOperation.DISTINCT,
+                            JsAggregationOperation.MIN,
+                            JsAggregationOperation.MAX,
+                            JsAggregationOperation.SUM,
+                            JsAggregationOperation.ABS_SUM,
+                            JsAggregationOperation.VAR,
+                            JsAggregationOperation.AVG,
+                            JsAggregationOperation.STD,
+                            JsAggregationOperation.FIRST,
+                            JsAggregationOperation.LAST,
+                            JsAggregationOperation.UNIQUE);
+                    for (int i = 0; i < count.size(); i++) {
+                        final int step = i;
+                        String operation = count.get(i);
+                        JsRollupConfig cfg = new JsRollupConfig();
+                        cfg.groupingColumns = Js.uncheckedCast(JsArray.of("X"));
+                        cfg.includeConstituents = true;
+                        cfg.aggregations = JsPropertyMap.of(operation, JsArray.of("Y"));
+                        tests.add(() -> table.rollup(cfg).then(r -> {
+                            r.setViewport(0, 99, null, null);
+                            delayTestFinish(15000 + step);
+
+                            return waitForEventWhere(r, JsTreeTable.EVENT_UPDATED,
+                                    (CustomEvent<TreeViewportData> d) -> r.getSize() == 4, 13000 + step)
+                                    .then(event -> Promise.resolve(r));
+                        }));
+                    }
+
+                    return tests.stream().reduce((p1, p2) -> () -> p1.get().then(result -> p2.get())).get().get();
                 })
                 .then(this::finish).catch_(this::report);
     }
