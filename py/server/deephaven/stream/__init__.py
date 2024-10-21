fbd63db192bc8c9ea54e12d39b0b973af2552091
jianfengmao <jianfengmao@deephaven.io>
2024-10-21 12:15:41 -0600

    Improve docstring

--- py/server/deephaven/stream/__init__.py
+++ py/server/deephaven/stream/__init__.py
@@ -14,10 +14,13 @@ _JBlinkTableTools = jpy.get_type("io.deephaven.engine.table.impl.BlinkTableTools
 _JAddOnlyToBlinkTableAdapter = jpy.get_type("io.deephaven.engine.table.impl.AddOnlyToBlinkTableAdapter")
 
 def add_only_to_blink(table: Table) -> Table:
-    """ Creates a blink table from the given add-only table.  The blink table contains the rows added in the latest update cycle.
+    """ Creates a blink table from the given add-only table.  The blink table contains the rows added in the latest
+    update cycle.
 
     Note that the use of this function should be limited to add-only tables that are not fully in-memory, or when
-    blink table specific aggregation semantics are desired.
+    blink table specific aggregation semantics are desired. If the table is fully in-memory, creating a downstream blink
+    table is not recommended because it doesn't achieve the main benefit of blink tables, which is to reduce memory usage
+    but instead increases memory usage.
 
     Args:
         table (Table): the source table
