58d899bbf3db60549e62f9ad3963749174dcfeae
Colin Alworth <colinalworth@deephaven.io>
2024-11-14 15:12:53 -0600

    HAck python client to still work

--- py/client/pydeephaven/proto/__init__.py
+++ py/client/pydeephaven/proto/__init__.py
@@ -2,4 +2,14 @@
 # Copyright (c) 2016-2024 Deephaven Data Labs and Patent Pending
 #
 
-from deephaven_core.proto import *
+from deephaven_core.proto import application_pb2, application_pb2_grpc
+from deephaven_core.proto import config_pb2, config_pb2_grpc
+from deephaven_core.proto import console_pb2, console_pb2_grpc
+from deephaven_core.proto import hierarchicaltable_pb2, hierarchicaltable_pb2_grpc
+from deephaven_core.proto import inputtable_pb2, inputtable_pb2_grpc
+from deephaven_core.proto import object_pb2, object_pb2_grpc
+from deephaven_core.proto import partitionedtable_pb2, partitionedtable_pb2_grpc
+from deephaven_core.proto import session_pb2, session_pb2_grpc
+from deephaven_core.proto import storage_pb2, storage_pb2_grpc
+from deephaven_core.proto import table_pb2, table_pb2_grpc
+from deephaven_core.proto import ticket_pb2
