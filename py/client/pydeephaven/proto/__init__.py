65cc2ef556438796f618b008c0528bb7b63abfa5
Colin Alworth <colinalworth@deephaven.io>
2024-11-15 10:51:39 -0600

    Warn about moved namespace

--- py/client/pydeephaven/proto/__init__.py
+++ py/client/pydeephaven/proto/__init__.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2016-2024 Deephaven Data Labs and Patent Pending
 #
 
+# Re-export the generated protobuf and gRPC modules in their old locations
 from deephaven_core.proto import application_pb2, application_pb2_grpc
 from deephaven_core.proto import config_pb2, config_pb2_grpc
 from deephaven_core.proto import console_pb2, console_pb2_grpc
@@ -13,3 +14,8 @@ from deephaven_core.proto import session_pb2, session_pb2_grpc
 from deephaven_core.proto import storage_pb2, storage_pb2_grpc
 from deephaven_core.proto import table_pb2, table_pb2_grpc
 from deephaven_core.proto import ticket_pb2
+
+# Log a warning that users should update imports
+from warnings import warn
+
+warn('The pydeephaven.proto namespace is deprecated, please use deephaven_core.proto instead. Deprecated in 0.37.0.', stacklevel=2)
