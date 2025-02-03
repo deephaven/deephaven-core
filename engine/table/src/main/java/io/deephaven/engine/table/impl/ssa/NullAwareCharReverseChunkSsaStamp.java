17f1c3497f6033b281870119c0553ff99b57b7af
Colin Alworth <colinalworth@deephaven.io>
2025-02-03 16:17:59 -0600

    all generated types

--- engine/table/src/main/java/io/deephaven/engine/table/impl/ssa/NullAwareCharReverseChunkSsaStamp.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/ssa/NullAwareCharReverseChunkSsaStamp.java
@@ -10,7 +10,7 @@
 
 
 //
-// Copyright (c) 2016-2024 Deephaven Data Labs and Patent Pending
+// Copyright (c) 2016-2025 Deephaven Data Labs and Patent Pending
 //
 package io.deephaven.engine.table.impl.ssa;
 
