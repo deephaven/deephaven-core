6252118ccb954998b3332bc56d031d12fa19da2d
Corey Kosak <kosak@users.noreply.github.com>
2025-02-07 20:21:42 -0500

    feat(cpp-client): Honor Arrow TimeUnit when converting Timestamp and Time64 to ColumnSource (#6609)
    This PR has adds two tests but leaves one disabled. The disabled one
    will fail on the main branch; that is why it is disabled.
    
    To run all the tests, execute this command.
    ```
    ./gradlew :cpp-client:testCppClient
    ```
    
    Then, if you rebase this branch onto your `nate/nightly/barrage_types`
    branch, you can re-enable the disabled test by editing the file
    `cpp-client/deephaven/tests/src/time_unit_test.cc` and removing the tag
    `[.hidden]`. Currently that tag is at line 75.
    
    You should then be able to run all the unit tests with the same gradlew
    command above and they should all pass.

---
+++ cpp-client/deephaven/dhclient/include/public/deephaven/client/utility/internal_types.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2016-2024 Deephaven Data Labs and Patent Pending
+ */
+#pragma once
+
+#include <arrow/flight/types.h>
+
+namespace deephaven::client::utility {
+/**
+ * For Deephaven use only
+ */
+namespace internal {
+/**
+ * This class exists only for the benefit of the unit tests. Our normal DateTime class has a
+ * native time resolution of nanoseconds. This class allows our unit tests to upload a
+ * DateTime having a different time unit so we can confirm that the client and server both handle
+ * it correctly.
+ */
+template<arrow::TimeUnit::type UNIT>
+struct InternalDateTime {
+  explicit InternalDateTime(int64_t value) : value_(value) {}
+
+  int64_t value_ = 0;
+};
+
+/**
+ * This class exists only for the benefit of the unit tests. Our normal LocalTime class has a
+ * native time resolution of nanoseconds. This class allows our unit tests to upload a
+ * LocalTime having a different time unit so we can confirm that the client and server both handle
+ * it correctly.
+ */
+template<arrow::TimeUnit::type UNIT>
+struct InternalLocalTime {
+  // Arrow Time64 only supports micro and nano units
+  static_assert(UNIT == arrow::TimeUnit::MICRO || UNIT == arrow::TimeUnit::NANO);
+
+  explicit InternalLocalTime(int64_t value) : value_(value) {}
+
+  int64_t value_ = 0;
+};
+}  // namespace internal
+} // namespace deephaven::client::utility
