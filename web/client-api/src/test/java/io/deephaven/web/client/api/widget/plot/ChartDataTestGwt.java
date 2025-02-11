deephaven/deephaven-core

commit ff5478dcb3ec1d1eb2d6fc43db0d7aa27da4a01c

fix: JS charts should handle shifts, read subscription data by key (#6076)
Only viewport subscriptions should read data (and formatting) by
position. Also fixes an error where -1 was not returned when an invalid
position was checked in a RangeSet.

Fixes #6074
Fixes #2435

web/client-api/src/main/java/io/deephaven/web/client/api/subscription/AbstractTableSubscription.java
@@ -298,7 +298,7 @@ public abstract class AbstractTableSubscription extends HasEventHandling {
     }
 
     /**
-     * TableData type for both viewports and full table subscriptions.
+     * TableData type for full table subscriptions.
      */
     @TsIgnore
     public static class SubscriptionEventData extends UpdateEventData implements ViewportData, SubscriptionTableData {
@@ -327,6 +327,28 @@ public abstract class AbstractTableSubscription extends HasEventHandling {
             return fullRowSet;
         }
     }
+
+    /**
+     * TableData type for viewport subscriptions.
+     */
+    @TsIgnore
+    public static class ViewportEventData extends SubscriptionEventData {
+        public ViewportEventData(WebBarrageSubscription subscription, int rowStyleColumn, JsArray<Column> columns,
+                RangeSet added, RangeSet removed, RangeSet modified, ShiftedRange[] shifted) {
+            super(subscription, rowStyleColumn, columns, added, removed, modified, shifted);
+        }
+
+        @Override
+        public Any getData(long key, Column column) {
+            return super.getData(fullRowSet.getRange().get(key), column);
+        }
+
+        @Override
+        public Format getFormat(long index, Column column) {
+            return super.getFormat(fullRowSet.getRange().get(index), column);
+        }
+    }
+
 
     /**
      * Base type to allow trees to extend from here separately from tables.
@@ -403,7 +425,7 @@ public abstract class AbstractTableSubscription extends HasEventHandling {
 
         @Override
         public Any getData(long key, Column column) {
-            return subscription.getData(fullRowSet.getRange().get(key), column.getIndex());
+            return subscription.getData(key, column.getIndex());
         }
 
         @Override
@@ -413,24 +435,23 @@ public abstract class AbstractTableSubscription extends HasEventHandling {
 
         @Override
         public Format getFormat(long index, Column column) {
-            long key = fullRowSet.getRange().get(index);
             long cellColors = 0;
             long rowColors = 0;
             String numberFormat = null;
             String formatString = null;
             if (column.getStyleColumnIndex() != null) {
-                LongWrapper wrapper = subscription.getData(key, column.getStyleColumnIndex()).uncheckedCast();
+                LongWrapper wrapper = subscription.getData(index, column.getStyleColumnIndex()).uncheckedCast();
                 cellColors = wrapper == null ? 0 : wrapper.getWrapped();
             }
             if (rowStyleColumn != NO_ROW_FORMAT_COLUMN) {
-                LongWrapper wrapper = subscription.getData(key, column.getStyleColumnIndex()).uncheckedCast();
+                LongWrapper wrapper = subscription.getData(index, column.getStyleColumnIndex()).uncheckedCast();
                 rowColors = wrapper == null ? 0 : wrapper.getWrapped();
             }
             if (column.getFormatStringColumnIndex() != null) {
-                numberFormat = subscription.getData(key, column.getFormatStringColumnIndex()).uncheckedCast();
+                numberFormat = subscription.getData(index, column.getFormatStringColumnIndex()).uncheckedCast();
             }
             if (column.getFormatStringColumnIndex() != null) {
-                formatString = subscription.getData(key, column.getFormatStringColumnIndex()).uncheckedCast();
+                formatString = subscription.getData(index, column.getFormatStringColumnIndex()).uncheckedCast();
             }
             return new Format(cellColors, rowColors, numberFormat, formatString);
         }

web/client-api/src/main/java/io/deephaven/web/client/api/subscription/TableViewportSubscription.java
@@ -139,7 +139,7 @@ public class TableViewportSubscription extends AbstractTableSubscription {
         if (rowsAdded.size() != rowsRemoved.size() && originalActive) {
             fireEventWithDetail(JsTable.EVENT_SIZECHANGED, size());
         }
-        UpdateEventData detail = new SubscriptionEventData(barrageSubscription, rowStyleColumn, getColumns(), rowsAdded,
+        UpdateEventData detail = new ViewportEventData(barrageSubscription, rowStyleColumn, getColumns(), rowsAdded,
                 rowsRemoved, totalMods, shifted);
 
         detail.setOffset(this.viewportRowSet.getFirstRow());

commit 3971fb9954e954cba50d3104a1b01ea5558780d6

fix: JS ChartData should write updates to the correct row
The provided mapping function was also not applied to modified rows,
also fixed in this patch.

Regression introduced by #6076.

Fixes #18632

web/client-api/src/test/java/io/deephaven/web/client/api/AbstractAsyncGwtTestCase.java
@@ -56,8 +56,8 @@ public abstract class AbstractAsyncGwtTestCase extends GWTTestCase {
     public static class TableSourceBuilder {
         private final List<String> pythonScripts = new ArrayList<>();
 
-        public TableSourceBuilder script(String script) {
-            pythonScripts.add(script);
+        public TableSourceBuilder script(String... script) {
+            pythonScripts.addAll(Arrays.asList(script));
             return this;
         }
 
@@ -156,7 +156,7 @@ public abstract class AbstractAsyncGwtTestCase extends GWTTestCase {
                             return ideSession.then(session -> {
 
                                 if (consoleTypes.includes("python")) {
-                                    return runAllScriptsInOrder(ideSession, session, tables.pythonScripts);
+                                    return runAllScriptsInOrder(session, tables.pythonScripts);
                                 }
                                 throw new IllegalStateException("Unsupported script type " + consoleTypes);
                             });
@@ -165,23 +165,10 @@ public abstract class AbstractAsyncGwtTestCase extends GWTTestCase {
         });
     }
 
-    private Promise<IdeSession> runAllScriptsInOrder(CancellablePromise<IdeSession> ideSession, IdeSession session,
-            List<String> code) {
-        Promise<IdeSession> result = ideSession;
-        for (int i = 0; i < code.size(); i++) {
-            final int index = i;
-            result = result.then(ignore -> {
-                delayTestFinish(4000 + index);
-
-                return session.runCode(code.get(index));
-            }).then(r -> {
-                if (r.getError() != null) {
-                    return Promise.reject(r.getError());
-                }
-                return ideSession;
-            });
-        }
-        return result;
+    private Promise<IdeSession> runAllScriptsInOrder(IdeSession session, List<String> code) {
+        String block = String.join("\n", code);
+        delayTestFinish(4000);
+        return session.runCode(block).then(ignore -> Promise.resolve(session));
     }
 
     public IThenable.ThenOnFulfilledCallbackFn<IdeSession, JsTable> table(String tableName) {

web/client-api/src/test/java/io/deephaven/web/client/api/widget/plot/ChartDataTestGwt.java
@@ -3,16 +3,39 @@
 //
 package io.deephaven.web.client.api.widget.plot;
 
+import elemental2.core.JsArray;
 import elemental2.promise.Promise;
 import io.deephaven.web.client.api.AbstractAsyncGwtTestCase;
+import io.deephaven.web.client.api.Column;
+import io.deephaven.web.client.api.JsTable;
 import io.deephaven.web.client.api.subscription.AbstractTableSubscription;
+import io.deephaven.web.client.api.subscription.SubscriptionTableData;
 import io.deephaven.web.client.api.subscription.TableSubscription;
+import io.deephaven.web.shared.fu.JsFunction;
+import jsinterop.base.Any;
+import jsinterop.base.Js;
+import jsinterop.base.JsPropertyMap;
+
+import java.io.Serializable;
+import java.util.function.BiConsumer;
 
 public class ChartDataTestGwt extends AbstractAsyncGwtTestCase {
     private final AbstractAsyncGwtTestCase.TableSourceBuilder tables = new AbstractAsyncGwtTestCase.TableSourceBuilder()
             .script("from deephaven import time_table")
             .script("appending_table", "time_table('PT0.1s').update([\"A = i % 2\", \"B = `` + A\"])")
             .script("updating_table", "appending_table.last_by('A')");
+
+    private final TableSourceBuilder tickingData = new TableSourceBuilder()
+            .script("from deephaven import input_table",
+                    "from deephaven import dtypes as dht",
+                    "import jpy",
+                    "from deephaven.execution_context import ExecutionContext, get_exec_ctx",
+                    "my_col_defs = {\"ID\": dht.int32, \"Value\": dht.string, \"Deleted\": dht.bool_}",
+                    "ug = jpy.get_type('io.deephaven.engine.updategraph.impl.EventDrivenUpdateGraph').newBuilder('test').existingOrBuild()",
+                    "exec_ctx = ExecutionContext(get_exec_ctx().j_object.withUpdateGraph(ug))",
+                    "with exec_ctx:",
+                    "    input = input_table(col_defs=my_col_defs, key_cols=\"ID\")",
+                    "    result = input.without_attributes('InputTable').where(\"!Deleted\").sort(\"ID\")");
 
     @Override
     public String getModuleName() {
@@ -88,5 +111,171 @@ public class ChartDataTestGwt extends AbstractAsyncGwtTestCase {
                             });
                 })
                 .then(this::finish).catch_(this::report);
+    }
+
+    public void testGeneratedChartData() {
+        connect(tickingData)
+                .then(ideSession -> {
+                    delayTestFinish(9870);
+                    Promise<JsTable> inputPromise = ideSession.getTable("input", false);
+                    Promise<JsTable> resultPromise = ideSession.getTable("result", false);
+                    return inputPromise.then(JsTable::inputTable).then(delayFinish(9871)).then(input -> {
+                        delayTestFinish(9872);
+                        return resultPromise.then(result -> {
+                            delayTestFinish(9873);
+                            ChartData chartData = new ChartData(result);
+
+                            // Keep the same function instance, as it is used for caching data
+                            JsFunction<Any, Any> dataFunction = arr -> Js.asAny("::" + arr);
+
+                            BiConsumer<SubscriptionTableData, SubscriptionTableData> consistencyCheck =
+                                    (subscriptionUpdate, snapshot) -> {
+                                        chartData.update(subscriptionUpdate);
+                                        // Verify the "Value" column, the only mutable column, has the same contents
+                                        // regardless of source
+                                        String[] expectedValues = new String[snapshot.getRows().length];
+                                        snapshot.getRows().forEach((row, index) -> {
+                                            expectedValues[index] = row.get(result.findColumn("Value")).asString();
+                                            return null;
+                                        });
+                                        JsArray<Any> values = chartData.getColumn("Value", null, subscriptionUpdate);
+                                        JsArray<Any> valuesWithPrefix =
+                                                chartData.getColumn("Value", dataFunction, subscriptionUpdate);
+                                        assertEquals(values.length, valuesWithPrefix.length);
+                                        assertEquals(expectedValues.length, values.length);
+                                        for (int i = 0; i < expectedValues.length; i++) {
+                                            assertEquals(expectedValues[i], values.getAt(i).asString());
+                                            assertEquals("::" + expectedValues[i],
+                                                    valuesWithPrefix.getAt(i).asString());
+                                        }
+                                    };
+                            return subscriptionValidator(result, result.getColumns())
+                                    // Check initial state (empty)
+                                    .check(consistencyCheck)
+                                    // Add three rows
+                                    .when(() -> {
+                                        input.addRows(new JsPropertyMap[] {
+                                                row(1, "A"),
+                                                row(2, "B"),
+                                                row(5, "E")
+                                        }, null);
+                                    })
+                                    .check(consistencyCheck)
+                                    // Add two more rows, causing a shift
+                                    .when(() -> {
+                                        input.addRows(new JsPropertyMap[] {
+                                                row(3, "C"),
+                                                row(4, "D")
+                                        }, null);
+                                    })
+                                    .check(consistencyCheck)
+                                    // Update an existing row
+                                    .when(() -> {
+                                        input.addRows(new JsPropertyMap[] {
+                                                row(3, "F")
+                                        }, null);
+                                    })
+                                    .check(consistencyCheck)
+                                    // Delete two rows
+                                    .when(() -> {
+                                        input.addRows(new JsPropertyMap[] {
+                                                delete(1),
+                                                delete(3)
+                                        }, null);
+                                    })
+                                    .check(consistencyCheck)
+                                    .cleanup();
+                        });
+                    });
+                })
+                .then(this::finish).catch_(this::report);
+    }
+
+    /**
+     * Helper to validate subscription results by comparing an updated subscription against a fresh snapshot. Intended
+     * to be generalized for use in viewport tests, but haven't yet done this.
+     */
+    private SubscriptionValidator subscriptionValidator(JsTable table, JsArray<Column> subscriptionColumns) {
+        TableSubscription subscription = table.subscribe(subscriptionColumns);
+
+        return new SubscriptionValidator() {
+            Promise<?> nextStep = Promise.resolve(subscription);
+
+            @Override
+            public SubscriptionValidator when(Runnable setup) {
+                // Run the step when the previous step completes
+                nextStep = nextStep.then(ignore -> {
+                    setup.run();
+                    return Promise.resolve(ignore);
+                });
+                return this;
+            }
+
+            @Override
+            public SubscriptionValidator check(BiConsumer<SubscriptionTableData, SubscriptionTableData> check) {
+                // Run this as a step once the previous step completes, by waiting for the original subscription to
+                // update, then creating a new subscription and waiting for that as well. Pass both updates to the
+                // provided check function.
+                nextStep = nextStep.then(ignore -> new Promise<>((resolve, reject) -> {
+                    try {
+                        subscription.addEventListenerOneShot(TableSubscription.EVENT_UPDATED, e1 -> {
+                            try {
+                                SubscriptionTableData data = (SubscriptionTableData) e1.getDetail();
+                                // Now that this update has happened, we make a new subscription to get a single
+                                // snapshot of the table
+                                TableSubscription checkSub = table.subscribe(subscriptionColumns);
+                                checkSub.addEventListenerOneShot(TableSubscription.EVENT_UPDATED, e2 -> {
+                                    try {
+                                        SubscriptionTableData checkData = (SubscriptionTableData) e2.getDetail();
+                                        check.accept(data, checkData);
+                                        resolve.onInvoke(checkData);
+                                    } catch (Throwable e) {
+                                        reject.onInvoke(e);
+                                    } finally {
+                                        checkSub.close();
+                                    }
+                                });
+                            } catch (Throwable e) {
+                                reject.onInvoke(e);
+                            }
+                        });
+                    } catch (Throwable e) {
+                        reject.onInvoke(e);
+                    }
+                }));
+                return this;
+            }
+
+            @Override
+            public Promise<?> cleanup() {
+                // Finished, clean up after ourselves, emit success/failure
+                return nextStep.finally_(subscription::close);
+            }
+        };
+    }
+
+    private interface SubscriptionValidator {
+        /**
+         * Performs some step that will lead to the table subscription being updated.
+         */
+        SubscriptionValidator when(Runnable setup);
+
+        /**
+         * Checks that the actual and expected data match. Must complete synchronously.
+         */
+        SubscriptionValidator check(BiConsumer<SubscriptionTableData, SubscriptionTableData> check);
+
+        /**
+         * Cleans up after validation work, returning a promise that the test can await.
+         */
+        Promise<?> cleanup();
+    }
+
+    private static JsPropertyMap<? extends Serializable> row(int id, String value) {
+        return JsPropertyMap.of("ID", (double) id, "Value", value, "Deleted", false);
+    }
+
+    private static JsPropertyMap<? extends Serializable> delete(int id) {
+        return JsPropertyMap.of("ID", (double) id, "Value", "deleted", "Deleted", true);
     }
 }