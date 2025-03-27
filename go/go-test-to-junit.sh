499e10969d2cb3d6795859b61a7037566f34c33c
Colin Alworth <colin@colinalworth.com>
2025-03-27 09:23:15 -0500

    back out tcpdump wrappers for client integration tests

--- go/go-test-to-junit.sh
+++ go/go-test-to-junit.sh
@@ -6,26 +6,4 @@ set -o xtrace
 
 XML=${1};
 
-# Start a tcpdump in the background
-tcpdump -C 10 -Z root -i any -U -w /out/tcpdump.pcap &
-
-# Pause for a moment to let the dump start
-sleep 1
-
-# Run the tests
-go test -vet=all -v ./... 2>&1 | go-junit-report -set-exit-code -iocopy -out "$XML" && ret=$? || ret=$?;
-
-# Pause another moment to ensure all packets were captured
-sleep 1
-
-# Stop tcpdump
-kill %1;
-
-# Check if the tests passed, emit correct exit code
-if [ $ret -eq 0 ] ; then
-    echo "Tests passed, deleting pcap"
-    rm /out/tcpdump*
-else
-    echo "Tests failed, emitting exit code $ret"
-    exit $ret
-fi
+go test -vet=all -v ./... 2>&1 | go-junit-report -set-exit-code -iocopy -out "$XML";
