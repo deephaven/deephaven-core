b1e12f430df9049bd537d76363ee7b2bf32a98df
Colin Alworth <colin@colinalworth.com>
2025-03-28 14:23:09 -0500

    Share more code, delete unused code

--- engine/base/src/main/java/io/deephaven/engine/table/impl/ContextWithChunk.java
+++ engine/base/src/main/java/io/deephaven/engine/table/impl/ContextWithChunk.java
@@ -7,7 +7,6 @@ import io.deephaven.base.verify.Assert;
 import io.deephaven.chunk.*;
 import io.deephaven.chunk.attributes.Any;
 import io.deephaven.engine.table.Context;
-import io.deephaven.util.SafeCloseable;
 import org.jetbrains.annotations.NotNull;
 
 public class ContextWithChunk<ATTR extends Any, CONTEXT extends Context> implements Context {
@@ -66,20 +65,6 @@ public class ContextWithChunk<ATTR extends Any, CONTEXT extends Context> impleme
     public static <CONTEXT extends Context> CONTEXT getContext(@NotNull Context context) {
         // noinspection unchecked,rawtypes
         return (CONTEXT) ((ContextWithChunk) context).context;
-    }
-
-    /**
-     * Makes sure that the internal array (and hence the writableChunk) is at least the specified size.
-     */
-    public void ensureSize(final int length) {
-        if (writableChunk.size() < length) {
-            if (writableChunk.capacity() < length) {
-                final SafeCloseable oldWritableChunk = writableChunk;
-                writableChunk = writableChunk.getChunkType().makeWritableChunk(length);
-                oldWritableChunk.close();
-            }
-            writableChunk.setSize(length);
-        }
     }
 
     /**
