28f5a07cc45e65e22b118b8bd437032c992e0f1f
Charles P. Wright <charleswright@deephaven.io>
2025-04-03 13:07:54 -0400

    feat: DH-19123: Improve Documentation around TableUpdate RowSet resource management. (#6754)
    Co-authored-by: Ryan Caudy <rcaudy@gmail.com>

--- engine/api/src/main/java/io/deephaven/engine/table/TableUpdate.java
+++ engine/api/src/main/java/io/deephaven/engine/table/TableUpdate.java
@@ -6,14 +6,32 @@ package io.deephaven.engine.table;
 import io.deephaven.base.log.LogOutput;
 import io.deephaven.base.log.LogOutputAppendable;
 import io.deephaven.base.verify.Assert;
+import io.deephaven.base.verify.AssertionFailure;
 import io.deephaven.engine.rowset.RowSet;
 import io.deephaven.engine.rowset.RowSetShiftData;
+import io.deephaven.engine.updategraph.NotificationQueue;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.BiConsumer;
 
 /**
  * Table update propagation structure, describing the rows and columns that were added, removed, modified, and shifted
  * on a given cycle.
+ * <p>
+ * {@link TableUpdateListener TableUpdateListeners} must not mutate TableUpdates they receive.
+ * <p>
+ * A TableUpdate received by a TableUpdateListener is only valid during the updating phase of the cycle that it was
+ * created on. Listeners should be very careful to ensure that any deferred or asynchronous usage respects this
+ * constraint.
+ * <p>
+ * All deferred or asynchronous usage must be guarded by a successful {@link #acquire()}, and terminated by a
+ * {@link #release()}. In the typical pattern, the TableUpdateListener acquires the TableUpdate when
+ * {@link TableUpdateListener#getNotification(TableUpdate) making a notification}, and releases it when the notification
+ * has been completely {@link NotificationQueue.Notification#run() run}. Users typically extend a base
+ * TableUpdateListener implementation that handles this on their behalf, but must be sure not to inadvertently use the
+ * TableUpdate outside its expected context without taking additional precautions.
+ * <p>
+ *
  */
 public interface TableUpdate extends LogOutputAppendable {
 
@@ -30,14 +48,17 @@ public interface TableUpdate extends LogOutputAppendable {
     }
 
     /**
-     * Increment the reference count on this object.
+     * Acquire a reference count for this TableUpdate that will keep it from being cleaned up until it is
+     * {@link #release() released}. Code that calls this method must be sure to call {@link #release()} when the
+     * TableUpdate is no longer needed. Acquiring a TableUpdate does not change the constraint that it must not be used
+     * outside the updating phase for which it was created.
      *
      * @return {@code this} for convenience
      */
     TableUpdate acquire();
 
     /**
-     * Decrement the reference count on this object.
+     * Release a previously-acquired reference count for this object.
      */
     void release();
 
@@ -52,25 +73,22 @@ public interface TableUpdate extends LogOutputAppendable {
     }
 
     /**
-     * @return true if all internal state is initialized
+     * Throws an {@link io.deephaven.base.verify.AssertionFailure} if this update is not structurally valid, i.e. its
+     * accessors will return non-{@code null}, usable data structures. This does not test for usage outside the
+     * appropriate updating phase.
      */
-    default boolean valid() {
-        return added() != null
-                && removed() != null
-                && modified() != null
-                && shifted() != null
-                && modifiedColumnSet() != null;
-    }
+    void validate() throws AssertionFailure;
 
     /**
      * @return a cached copy of the modified RowSet in pre-shift keyspace
      */
+    @NotNull
     RowSet getModifiedPreShift();
 
     /**
      * This helper iterates through the modified RowSet and supplies both the pre-shift and post-shift keys per row.
      *
-     * @param consumer a consumer to feed the modified pre-shift and post-shift key values to.
+     * @param consumer A consumer to feed the modified pre-shift and post-shift key values to
      */
     default void forAllModified(final BiConsumer<Long, Long> consumer) {
         final RowSet prevModified = getModifiedPreShift();
@@ -86,27 +104,53 @@ public interface TableUpdate extends LogOutputAppendable {
     }
 
     /**
-     * rows added (post-shift keyspace)
+     * Rows added (in post-shift keyspace).
+     * <p>
+     * A {@link #validate()} update never returns a null RowSet, but the returned RowSet may be {@link RowSet#isEmpty()
+     * empty}.
+     * <p>
+     * Note that the TableUpdate object still retains ownership of the returned {@link RowSet} object. The caller must
+     * not close the returned RowSet. To use the RowSet beyond the scope of a notification, the caller must
+     * {@link #acquire() acquire} the update or make a {@link RowSet#copy() copy} of the RowSet.
      */
+    @NotNull
     RowSet added();
 
     /**
-     * rows removed (pre-shift keyspace)
+     * Rows removed (in pre-shift keyspace).
+     * <p>
+     * A {@link #validate()} update never returns a null RowSet, but the returned RowSet may be {@link RowSet#isEmpty()
+     * empty}.
+     * <p>
+     * Note that the TableUpdate object still retains ownership of the returned {@link RowSet} object. The caller must
+     * not close the returned RowSet. To use the RowSet beyond the scope of a notification, the caller must
+     * {@link #acquire() acquire} the update or make a {@link RowSet#copy() copy} of the RowSet.
      */
+    @NotNull
     RowSet removed();
 
     /**
-     * rows modified (post-shift keyspace)
+     * Rows modified (in post-shift keyspace).
+     * <p>
+     * A {@link #validate()} update never returns a null RowSet, but the returned RowSet may be {@link RowSet#isEmpty()
+     * empty}.
+     * <p>
+     * Note that the TableUpdate object still retains ownership of the returned {@link RowSet} object. The caller must
+     * not close the returned RowSet. To use the RowSet beyond the scope of a notification, the caller must
+     * {@link #acquire() acquire} the update or make a {@link RowSet#copy() copy} of the RowSet.
      */
+    @NotNull
     RowSet modified();
 
     /**
-     * rows that shifted to new indices
+     * Rows that shifted to new row keys.
      */
+    @NotNull
     RowSetShiftData shifted();
 
     /**
-     * the set of columns that might have changed for rows in the {@code modified()} RowSet
+     * The set of columns that might have changed for rows in the {@link #modified()} RowSet.
      */
+    @NotNull
     ModifiedColumnSet modifiedColumnSet();
 }
