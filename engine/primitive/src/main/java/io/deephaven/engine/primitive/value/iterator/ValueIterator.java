f65fce053cd673fabdf0c6cee3535018de568c90
Nathaniel Bauernfeind <natebauernfeind@deephaven.io>
2025-04-29 12:33:32 -0600

    appease spotless...

--- engine/primitive/src/main/java/io/deephaven/engine/primitive/value/iterator/ValueIterator.java
+++ engine/primitive/src/main/java/io/deephaven/engine/primitive/value/iterator/ValueIterator.java
@@ -103,8 +103,8 @@ public interface ValueIterator<TYPE> extends CloseableIterator<TYPE> {
     }
 
     /**
-     * Wraps a ValueIterator with set number of prefix nulls, postfix nulls, or both. The result must be
-     * {@link #close() closed}.
+     * Wraps a ValueIterator with set number of prefix nulls, postfix nulls, or both. The result must be {@link #close()
+     * closed}.
      *
      * @param iterator The ValueIterator to wrap
      * @param prefixNulls The number of nulls to add to the beginning of the iterator
