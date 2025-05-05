47f20a1e5114b601042b372e2176d63fd58fc081
Charles P. Wright <charleswright@deephaven.io>
2025-05-05 19:12:20 -0400

    unused import.

--- web/client-api/src/test/java/io/deephaven/web/client/api/PartitionedTableTestGwt.java
+++ web/client-api/src/test/java/io/deephaven/web/client/api/PartitionedTableTestGwt.java
@@ -4,7 +4,6 @@
 package io.deephaven.web.client.api;
 
 import elemental2.core.JsArray;
-import elemental2.dom.DomGlobal;
 import io.deephaven.web.client.api.event.Event;
 
 public class PartitionedTableTestGwt extends AbstractAsyncGwtTestCase {
