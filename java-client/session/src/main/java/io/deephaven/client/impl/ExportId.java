a1c2c8ea7c671cff05c1361c871ebfd3804c3ae7
Charles P. Wright <charleswright@deephaven.io>
2025-05-08 12:56:12 -0400

    Revert ExportId for now; since it is the most controversial part.

--- java-client/session/src/main/java/io/deephaven/client/impl/ExportId.java
+++ java-client/session/src/main/java/io/deephaven/client/impl/ExportId.java
@@ -52,7 +52,7 @@ public final class ExportId implements HasExportId {
         return (type == null ? "?:" : type + ":") + ExportTicketHelper.toReadableString(exportId);
     }
 
-    public int id() {
+    int id() {
         return exportId;
     }
 
