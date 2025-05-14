7e16baea18288bd268490fff4147b2e6839eb4b3
Devin Smith <devinsmith@deephaven.io>
2025-05-14 08:17:54 -0700

    fix: remove usage of anonymous classes for NameMappingProvider, ResolverProvider
    This is necessary so that Enterprise can get access to the internal implementations to serialize / deserialize them for the upcoming Iceberg schema extended format.

--- extensions/iceberg/src/main/java/io/deephaven/iceberg/util/NameMappingProviderImpl.java
+++ extensions/iceberg/src/main/java/io/deephaven/iceberg/util/NameMappingProviderImpl.java
@@ -6,7 +6,40 @@ package io.deephaven.iceberg.util;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.mapping.NameMapping;
 
-abstract class NameMappingProviderImpl implements NameMappingProvider {
+import java.util.Objects;
 
-    abstract NameMapping create(Table table);
+interface NameMappingProviderImpl extends NameMappingProvider {
+
+    NameMapping create(Table table);
+
+    enum Empty implements NameMappingProviderImpl {
+        EMPTY;
+
+        @Override
+        public NameMapping create(Table table) {
+            return NameMapping.empty();
+        }
+    }
+
+    enum FromTable implements NameMappingProviderImpl {
+        FROM_TABLE;
+
+        @Override
+        public NameMapping create(Table table) {
+            return NameMappingUtil.readNameMappingDefault(table).orElse(NameMapping.empty());
+        }
+    }
+
+    final class Explicit implements NameMappingProviderImpl {
+        private final NameMapping impl;
+
+        public Explicit(NameMapping impl) {
+            this.impl = Objects.requireNonNull(impl);
+        }
+
+        @Override
+        public NameMapping create(Table table) {
+            return impl;
+        }
+    }
 }
