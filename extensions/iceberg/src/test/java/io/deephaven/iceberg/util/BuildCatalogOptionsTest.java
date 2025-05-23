980c79cd6ce93ddda2d54e3139c079b1b97d2c57
Shivam Malhotra <malhotraashivam@gmail.com>
2025-05-23 16:43:44 -0500

    Add tests for BuildCatalogOptions

--- extensions/iceberg/src/test/java/io/deephaven/iceberg/util/BuildCatalogOptionsTest.java
+++ extensions/iceberg/src/test/java/io/deephaven/iceberg/util/BuildCatalogOptionsTest.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
 
 class BuildCatalogOptionsTest {
 
-    public static final Map<String, String> MINIMAL_PROPS = Map.of(CatalogUtil.ICEBERG_CATALOG_TYPE, "minimal");
+    private static final Map<String, String> MINIMAL_PROPS = Map.of(CatalogUtil.ICEBERG_CATALOG_TYPE, "minimal");
 
     @Test
     void minimalOptions() {
@@ -68,5 +68,48 @@ class BuildCatalogOptionsTest {
         }
     }
 
+    @Test
+    void testBasicPropertyInjection() {
+        final BuildCatalogOptions options = BuildCatalogOptions.builder()
+                .putAllProperties(MINIMAL_PROPS)
+                .build();
+        assertThat(options.enablePropertyInjection()).isTrue();
+        assertThat(options.properties()).isEqualTo(MINIMAL_PROPS);
 
+        final Map<String, String> updatedProperties = options.updatedProperties();
+        assertThat(updatedProperties).containsAllEntriesOf(MINIMAL_PROPS);
+        assertThat(updatedProperties.get("s3.crt.enabled")).isEqualTo("false");
+        assertThat(updatedProperties.get("client.credentials-provider")).isEqualTo(
+                "io.deephaven.iceberg.util.DeephavenS3ClientCredentialsProvider");
+    }
+
+    @Test
+    void testPropertyForwardingWithInjection() {
+        final Map<String, String> props = Map.of(
+                CatalogUtil.ICEBERG_CATALOG_TYPE, "minimal",
+                "s3.access-key-id", "accessKey",
+                "s3.session-token", "sessionToken",
+                "s3.crt.enabled", "true");
+        final BuildCatalogOptions options = BuildCatalogOptions.builder()
+                .putAllProperties(props)
+                .build();
+        assertThat(options.properties()).isEqualTo(props);
+
+        final Map<String, String> updatedProperties = options.updatedProperties();
+        assertThat(updatedProperties).containsAllEntriesOf(props);
+        assertThat(updatedProperties.get("s3.crt.enabled")).isEqualTo("true");
+        assertThat(updatedProperties.get("client.credentials-provider.s3.access-key-id")).isEqualTo("accessKey");
+        assertThat(updatedProperties.get("client.credentials-provider.s3.session-token")).isEqualTo("sessionToken");
+    }
+
+    @Test
+    void disablePropertyInjection() {
+        final BuildCatalogOptions options = BuildCatalogOptions.builder()
+                .putAllProperties(MINIMAL_PROPS)
+                .enablePropertyInjection(false)
+                .build();
+        assertThat(options.properties()).isEqualTo(MINIMAL_PROPS);
+        assertThat(options.updatedProperties()).isEqualTo(MINIMAL_PROPS);
+        assertThat(options.enablePropertyInjection()).isFalse();
+    }
 }
