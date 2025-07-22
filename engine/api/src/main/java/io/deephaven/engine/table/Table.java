d98283bbe2e7524787f116775d5b6847744c63a8
Charles P. Wright <cpwright@gmail.com>
2025-07-21 21:25:53 -0400

    Update engine/api/src/main/java/io/deephaven/engine/table/Table.java
    Co-authored-by: Ryan Caudy <rcaudy@gmail.com>

--- engine/api/src/main/java/io/deephaven/engine/table/Table.java
+++ engine/api/src/main/java/io/deephaven/engine/table/Table.java
@@ -556,11 +556,11 @@ public interface Table extends
     Table removeBlink();
 
     /**
-     * Returns a child Table with {@link #BLINK_TABLE_ATTRIBUTE} set to {@code true}.
+     * Returns {@code this} or a child Table with {@link #BLINK_TABLE_ATTRIBUTE} set to {@code true}.
      *
      * <p>
-     * This table must already produce an update pattern that conforms to blink semantics. If the Table produces an
-     * update that does not conform to blink semantics, then the child table will notify of an error.
+     * This table must already produce an update pattern that conforms to blink semantics. If it produces an
+     * update that does not conform to blink semantics, then the returned table will notify of an error.
      * </p>
      *
      * @return A child table with the blink attribute set, or this table if already a blink table.
