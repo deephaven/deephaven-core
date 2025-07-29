cf18e8357b7da42cff040c90885cfc418f7231f6
Charles P. Wright <charleswright@deephaven.io>
2025-07-29 17:25:12 -0400

    compile fix

--- engine/table/src/test/java/io/deephaven/engine/table/impl/QueryTableWhereTest.java
+++ engine/table/src/test/java/io/deephaven/engine/table/impl/QueryTableWhereTest.java
@@ -61,6 +61,7 @@ import java.math.BigInteger;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
