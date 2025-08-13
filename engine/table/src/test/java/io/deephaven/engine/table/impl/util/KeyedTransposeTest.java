aa137cec1d1ec5b752cf00137cb012ff869b3f70
stanbrub <stanbrubaker@illumon.com>
2025-08-13 11:45:28 -0600

    Added more unit tests

--- engine/table/src/test/java/io/deephaven/engine/table/impl/util/KeyedTransposeTest.java
+++ engine/table/src/test/java/io/deephaven/engine/table/impl/util/KeyedTransposeTest.java
@@ -26,4 +26,33 @@ public class KeyedTransposeTest extends RefreshingTableTestCase {
                 new String[]{"Level"});
         TableTools.show(t);
     }
+
+    public void testKeyTranspose2() {
+        Table source = getStaticTable();
+        Table t = KeyedTranspose.keyedTranspose(source, List.of(AggCount("Count"), AggSum("Sum=Cat")),
+                new String[]{"Date","Host"}, new String[]{"Level"});
+        TableTools.show(t);
+    }
+
+    public void testKeyTranspose3() {
+        Table source = getStaticTable();
+        Table t = KeyedTranspose.keyedTranspose(source, List.of(AggCount("Count")),
+                new String[]{"Date","Host"}, new String[]{"Level", "Cat"});
+        TableTools.show(t);
+    }
+
+    public void testKeyTranspose4() {
+        Table source = getStaticTable();
+        Table t = KeyedTranspose.keyedTranspose(source, List.of(AggCount("Count"), AggSum("Sum=Cat")),
+                new String[]{"Date","Host"}, new String[]{"Level", "Cat"});
+        TableTools.show(t);
+    }
+
+    public void testKeyTransposeIncremental() {
+        Table source = getStaticTable();
+        Table t = KeyedTranspose.keyedTranspose(source, List.of(AggCount("Count"), AggSum("Sum=Cat")),
+                new String[]{"Date","Host"}, new String[]{});
+        TableTools.show(t);
+    }
+
 }
