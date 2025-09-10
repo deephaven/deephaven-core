a9c7463e260b4d0fc67a3b684da34a9603b24ed0
Corey Kosak <kosak@users.noreply.github.com>
2025-09-10 00:40:23 -0400

    feat(csharp/client): DH-20309: Simplify the SharableDict implementation. Add Unit Tests (#7153)
    Co-authored-by: Larry Booker <lbooker42@gmail.com>

---
+++ csharp/client/Dh_NetClient/sharables/NodeHelpers.cs
@@ -0,0 +1,13 @@
+﻿//
+// Copyright (c) 2016-2025 Deephaven Data Labs and Patent Pending
+//
+namespace Deephaven.Dh_NetClient;
+
+using System.Runtime.CompilerServices;
+
+[InlineArray(64)]
+public struct Array64<T> {
+  public T Item;
+
+  public int Length => 64;
+}
