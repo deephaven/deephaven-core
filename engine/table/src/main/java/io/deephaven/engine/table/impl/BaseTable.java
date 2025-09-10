ec17028262723f61af189f1b8cdd558ca5e42a8b
Charles P. Wright <charleswright@deephaven.io>
2025-09-10 14:54:52 -0400

    revert basetable change

--- engine/table/src/main/java/io/deephaven/engine/table/impl/BaseTable.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/BaseTable.java
@@ -425,21 +425,6 @@ public abstract class BaseTable<IMPL_TYPE extends BaseTable<IMPL_TYPE>> extends 
     }
 
     /**
-     * Create a copy of this table which reports itself as add-only.
-     *
-     * <p>
-     * If this table is already add-only, returns a copy of this. Otherwise, the {@link #ADD_ONLY_TABLE_ATTRIBUTE} is
-     * set and a new child table is returned. If this table does not conform to the add-only contract, then the child
-     * will produce an error notification.
-     * </p>
-     *
-     * @return a copy of this table with the {@link #ADD_ONLY_TABLE_ATTRIBUTE} set
-     */
-    public Table assertAddOnly() {
-        return withAttributes(Map.of(Table.ADD_ONLY_TABLE_ATTRIBUTE, Boolean.TRUE));
-    }
-
-    /**
      * Returns true if this table is append-only, or has an attribute asserting that no modifies, shifts, or removals
      * are generated and that all new rows are added to the end of the table.
      *
@@ -472,6 +457,11 @@ public abstract class BaseTable<IMPL_TYPE extends BaseTable<IMPL_TYPE>> extends 
     @Override
     public Table assertBlink() {
         return withAttributes(Map.of(BLINK_TABLE_ATTRIBUTE, true));
+    }
+
+    @Override
+    public Table assertAddOnly() {
+        return withAttributes(Map.of(ADD_ONLY_TABLE_ATTRIBUTE, true));
     }
 
     @Override
