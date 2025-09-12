7570ea3b3b61378158694261e1bc902cf232a597
Colin Alworth <colin@colinalworth.com>
2025-09-12 14:01:09 -0500

    Support sending real deephaven:type to the client, other cleanup

--- extensions/barrage/src/main/java/io/deephaven/extensions/barrage/util/BarrageUtil.java
+++ extensions/barrage/src/main/java/io/deephaven/extensions/barrage/util/BarrageUtil.java
@@ -31,6 +31,8 @@ import io.deephaven.engine.table.GridAttributes;
 import io.deephaven.engine.table.Table;
 import io.deephaven.engine.table.TableDefinition;
 import io.deephaven.engine.table.impl.BaseTable;
+import io.deephaven.engine.table.impl.preview.DisplayWrapper;
+import io.deephaven.engine.table.impl.preview.PreviewType;
 import io.deephaven.engine.table.impl.remote.ConstructSnapshot;
 import io.deephaven.engine.table.impl.sources.ReinterpretUtils;
 import io.deephaven.engine.table.impl.util.BarrageMessage;
@@ -619,9 +621,12 @@ public class BarrageUtil {
                         putMetadata(metadata, ATTR_COMPONENT_TYPE_TAG, String.class.getCanonicalName());
                     }
                 }
-            } else {
-                // Otherwise, data type will be converted to a String
+            } else if (dataType == PreviewType.class || dataType == DisplayWrapper.class) {
+                // Explicitly signal that this will be a string
                 putMetadata(metadata, ATTR_TYPE_TAG, String.class.getCanonicalName());
+            } else {
+                // Otherwise, send the data type as is, but we will serialize to a string
+                putMetadata(metadata, ATTR_TYPE_TAG, dataType.getCanonicalName());
             }
 
             // Only one of these will be true, if any are true the column will not be visible
