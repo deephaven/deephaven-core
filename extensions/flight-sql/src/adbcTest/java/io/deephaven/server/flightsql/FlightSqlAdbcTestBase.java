654683e6478c598ebc73fbe3059709ce5f1c68f8
Charles P. Wright <charleswright@deephaven.io>
2025-09-17 12:42:08 -0400

    more attribute fixes.

--- extensions/flight-sql/src/adbcTest/java/io/deephaven/server/flightsql/FlightSqlAdbcTestBase.java
+++ extensions/flight-sql/src/adbcTest/java/io/deephaven/server/flightsql/FlightSqlAdbcTestBase.java
@@ -34,12 +34,7 @@ public abstract class FlightSqlAdbcTestBase extends DeephavenServerTestBase {
 
     private static final Map<String, String> DEEPHAVEN_INT = Map.of(
             "deephaven:isSortable", "true",
-            "deephaven:isRowStyle", "false",
-            "deephaven:isPartitioning", "false",
-            "deephaven:type", "int",
-            "deephaven:isNumberFormat", "false",
-            "deephaven:isStyle", "false",
-            "deephaven:isDateFormat", "false");
+            "deephaven:type", "int");
 
     BufferAllocator allocator;
     AdbcDatabase database;
