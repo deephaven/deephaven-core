80e8f7681a0e0e5083bd41bc32d4fad50ee71b7e
Colin Alworth <colinalworth@deephaven.io>
2025-09-25 19:58:14 -0500

    fix: DH-20448, DH-20449: Better schema information for complex types (#7228)
    Enables JS and Arrow clients to handle any reasonable data, and provides
    a better error for BarrageTable clients when more serialization support
    needs to be added.
    
    Co-authored-by: Charles P. Wright <cpwright@gmail.com>

--- web/client-api/src/main/java/io/deephaven/web/client/api/barrage/WebChunkReaderFactory.java
+++ web/client-api/src/main/java/io/deephaven/web/client/api/barrage/WebChunkReaderFactory.java
@@ -267,7 +267,8 @@ public class WebChunkReaderFactory implements ChunkReader.Factory {
                     listMode = ListChunkReader.Mode.VARIABLE;
                 }
 
-                if (typeInfo.componentType() == byte.class && listMode == ListChunkReader.Mode.VARIABLE) {
+                Class<?> componentType = typeInfo.componentType();
+                if (componentType == byte.class && listMode == ListChunkReader.Mode.VARIABLE) {
                     // special case for byte[]
                     return (fieldNodeIter, bufferInfoIter, is, outChunk, outOffset,
                             totalRows) -> (T) extractChunkFromInputStream(
@@ -278,10 +279,10 @@ public class WebChunkReaderFactory implements ChunkReader.Factory {
                                     outChunk, outOffset, totalRows);
                 }
 
-                // noinspection DataFlowIssue
+                // noinspection DataFlowIssue,ConstantValue
                 final BarrageTypeInfo<Field> componentTypeInfo = new BarrageTypeInfo<>(
-                        typeInfo.componentType(),
-                        typeInfo.componentType().getComponentType(),
+                        componentType,
+                        componentType == null ? null : componentType.getComponentType(),
                         typeInfo.arrowField().children(0));
                 final ChunkType chunkType = ListChunkReader.getChunkTypeFor(componentTypeInfo.type());
                 final ExpansionKernel<?> kernel =
