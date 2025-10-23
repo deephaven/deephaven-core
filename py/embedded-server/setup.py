6d3e8e5bea90f7c6e0f7a29954da3ab1efa042bb
jianfengmao <jianfengmao@deephaven.io>
2025-10-23 17:14:57 -0600

    Modify required Py version

--- py/embedded-server/setup.py
+++ py/embedded-server/setup.py
@@ -56,7 +56,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -64,7 +63,7 @@ setup(
         "Programming Language :: Python :: 3.13",
     ],
     keywords="Deephaven Development",
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[
         "jpy>=1.1.0",
         "java-utilities",
