b3d2c461c62871540b7ba57ad7cbe9be470ac818
Corey Kosak <coreykosak@deephaven.io>
2025-11-01 18:50:58 -0400

    proposed rewrite of WritableChunkUtils

--- engine/chunk/src/main/java/io/deephaven/chunk/WritableChunkUtils.java
+++ engine/chunk/src/main/java/io/deephaven/chunk/WritableChunkUtils.java
@@ -15,13 +15,6 @@ import static io.deephaven.util.QueryConstants.NULL_DOUBLE;
 import static io.deephaven.util.QueryConstants.NULL_FLOAT;
 
 final class WritableChunkUtils {
-
-    private static final int NULL_FLOAT_BITS = Float.floatToIntBits(NULL_FLOAT);
-    private static final int MIN_FINITE_FLOAT_BITS = Float.floatToIntBits(QueryConstants.MIN_FINITE_FLOAT);
-
-    private static final long NULL_DOUBLE_BITS = Double.doubleToLongBits(NULL_DOUBLE);
-    private static final long MIN_FINITE_DOUBLE_BITS = Double.doubleToLongBits(QueryConstants.MIN_FINITE_DOUBLE);
-
     @SuppressWarnings("unused")
     public static void sort(boolean[] data, int fromIndexInclusive, int toIndexExclusive) {
         throw new UnsupportedOperationException();
@@ -36,12 +29,22 @@ final class WritableChunkUtils {
         Arrays.sort(data, fromIndexInclusive, toIndexExclusive);
     }
 
+    // These assertions reflect sorting assumptions made by this code. If these assertions
+    // change, this code will need to be carefully audited.
+    static {
+        assert(NULL_CHAR == Character.MAX_VALUE);
+        assert(NULL_FLOAT == -Float.MAX_VALUE);
+        assert(Float.NEGATIVE_INFINITY < NULL_FLOAT);
+    }
+
     public static void sort(char[] data, int fromIndexInclusive, int toIndexExclusive) {
         sortUnsafe(data, fromIndexInclusive, toIndexExclusive);
-        // We need to "fix-up" the nulls (Character.MAX_VALUE) from end of subset to beginning of subset.
-        // [ (...,) data_1, ..., data_m, NULL_CHAR_1, ..., NULL_CHAR_n (,...) ]
+
+        // sortUnsafe() places Deephaven NULL_CHAR (Character.MAX_VALUE) at the end of the slice. This code moves them to the
+        // beginning of the slice.
+        // [ data_1, ..., data_m, NULL_CHAR * n ]
         // ->
-        // [ (...,) NULL_CHAR_1, ..., NULL_CHAR_n, data_1, ..., data_m (,...) ]
+        // [ NULL_CHAR * n, data_1, ..., data_m ]
         if (toIndexExclusive - fromIndexInclusive <= 1) {
             // size 0 or 1, already sorted
             return;
@@ -54,9 +57,9 @@ final class WritableChunkUtils {
             // none are NULL_CHAR, already sorted
             return;
         }
-        // We know first position is not NULL_CHAR; we don't want to exclude the last position, b/c it may be the
-        // only NULL_CHAR.
-        final int firstNullIx = binarySearchFirst(data, fromIndexInclusive + 1, toIndexExclusive, NULL_CHAR);
+
+        // We can start with fromIndexInclusive + 1 because we know that data[fromIndexInclusive] is not NULL_CHAR
+        final int firstNullIx = lowerBound(data, fromIndexInclusive + 1, toIndexExclusive, NULL_CHAR);
         final int nullCount = toIndexExclusive - firstNullIx;
         System.arraycopy(data, fromIndexInclusive, data, fromIndexInclusive + nullCount,
                 toIndexExclusive - fromIndexInclusive - nullCount);
@@ -101,11 +104,11 @@ final class WritableChunkUtils {
 
     public static void sort(float[] data, int fromIndexInclusive, int toIndexExclusive) {
         sortUnsafe(data, fromIndexInclusive, toIndexExclusive);
-        // We need to "fix-up" null (-Float.MAX_VALUE) vs -Infinity, which we know will be at the beginning of the
-        // subset
-        // [ (...,) -Inf_1, ..., -Inf_m, NULL_FLOAT_1, ..., NULL_FLOAT_m, ..., (,...)]
+        // We need to fix up null (-Float.MAX_VALUE) vs -Infinity, which we know will be at the beginning of the
+        // slice
+        // [ -Inf * m, NULL_FLOAT * n, ...]
         // ->
-        // [ (...,) NULL_FLOAT_1, ..., NULL_FLOAT_m, -Inf_1, ..., -Inf_n, ..., (,...)]
+        // [ NULL_FLOAT * n, -Inf * m, ...]
         if (toIndexExclusive - fromIndexInclusive <= 1) {
             // size 0 or 1, already sorted
             return;
@@ -115,26 +118,25 @@ final class WritableChunkUtils {
             // when it's in the first position
             return;
         }
-        final int firstNullIx =
-                binarySearchFirst(data, fromIndexInclusive + 1, toIndexExclusive, NULL_FLOAT, NULL_FLOAT_BITS);
-        if (firstNullIx < 0) {
+        // We can start with fromIndexInclusive + 1 because we know that data[fromIndexInclusive] is not NULL_FLOAT
+        final int beginNullIx = lowerBound(data, fromIndexInclusive + 1, toIndexExclusive, NULL_FLOAT);
+        if (beginNullIx == toIndexExclusive || data[beginNullIx] != NULL_FLOAT) {
             // No NULL_FLOAT
             return;
         }
-        final int negInfCount = firstNullIx - fromIndexInclusive;
-        final int nullCount;
-        {
-            final int firstFloatAfterNull =
-                    binarySearchFirst(data, firstNullIx + 1, toIndexExclusive, MIN_FINITE_FLOAT, MIN_FINITE_FLOAT_BITS);
-            final int lastNullIx = (firstFloatAfterNull >= 0 ? firstFloatAfterNull : ~firstFloatAfterNull) - 1;
-            nullCount = lastNullIx - firstNullIx + 1;
-        }
-        final int size = Math.min(nullCount, negInfCount);
-        Arrays.fill(data, fromIndexInclusive, fromIndexInclusive + size, NULL_FLOAT);
-        {
-            final int negInftoIndexExclusive = fromIndexInclusive + nullCount + negInfCount;
-            Arrays.fill(data, negInftoIndexExclusive - size, negInftoIndexExclusive, Float.NEGATIVE_INFINITY);
-        }
+        final int endNullIx = upperBound(data, fromIndexInclusive + 1, toIndexExclusive, NULL_FLOAT);
+
+        final int negInfCount = beginNullIx - fromIndexInclusive;
+        final int nullCount = endNullIx - beginNullIx;
+
+        // Only some of the -Inf and some of the NULL_FLOATS are wrong. For example (with m = 3 and n = 5)
+        // [ -Inf, -Inf, -Inf, NULL_FLOAT, NULL_FLOAT, NULL_FLOAT, NULL_FLOAT, NULL_FLOAT, ...]
+        // ->
+        // [ NULL_FLOAT, NULL_FLOAT, NULL_FLOAT, unchanged, unchanged, -Inf, -Inf, -Inf, ...]
+
+        final int numWrong = Math.min(nullCount, negInfCount);
+        Arrays.fill(data, fromIndexInclusive, fromIndexInclusive + numWrong, NULL_FLOAT);
+        Arrays.fill(data, endNullIx - numWrong, endNullIx, Float.NEGATIVE_INFINITY);
     }
 
     public static void sortUnsafe(double[] data, int fromIndexInclusive, int toIndexExclusive) {
@@ -143,11 +145,11 @@ final class WritableChunkUtils {
 
     public static void sort(double[] data, int fromIndexInclusive, int toIndexExclusive) {
         sortUnsafe(data, fromIndexInclusive, toIndexExclusive);
-        // We need to "fix-up" null (-Double.MAX_VALUE) vs -Infinity, which we know will be at the beginning of the
-        // subset
-        // [ (...,) -Inf_1, ..., -Inf_m, NULL_DOUBLE_1, ..., NULL_DOUBLE_m, ..., (,...)]
+        // We need to fix up null (-Double.MAX_VALUE) vs -Infinity, which we know will be at the beginning of the
+        // slice
+        // [ -Inf * m, NULL_DOUBLE * n, ...]
         // ->
-        // [ (...,) NULL_DOUBLE_1, ..., NULL_DOUBLE_m, -Inf_1, ..., -Inf_n, ..., (,...)]
+        // [ NULL_DOUBLE * n, -Inf * m, ...]
         if (toIndexExclusive - fromIndexInclusive <= 1) {
             // size 0 or 1, already sorted
             return;
@@ -157,26 +159,25 @@ final class WritableChunkUtils {
             // when it's in the first position
             return;
         }
-        final int firstNullIx =
-                binarySearchFirst(data, fromIndexInclusive + 1, toIndexExclusive, NULL_DOUBLE, NULL_DOUBLE_BITS);
-        if (firstNullIx < 0) {
+        // We can start with fromIndexInclusive + 1 because we know that data[fromIndexInclusive] is not NULL_DOUBLE
+        final int beginNullIx = lowerBound(data, fromIndexInclusive + 1, toIndexExclusive, NULL_DOUBLE);
+        if (beginNullIx == toIndexExclusive || data[beginNullIx] != NULL_DOUBLE) {
             // No NULL_DOUBLE
             return;
         }
-        final int negInfCount = firstNullIx - fromIndexInclusive;
-        final int nullCount;
-        {
-            final int firstDoubleAfterNull = binarySearchFirst(data, firstNullIx + 1, toIndexExclusive,
-                    MIN_FINITE_DOUBLE, MIN_FINITE_DOUBLE_BITS);
-            final int lastNullIx = (firstDoubleAfterNull >= 0 ? firstDoubleAfterNull : ~firstDoubleAfterNull) - 1;
-            nullCount = lastNullIx - firstNullIx + 1;
-        }
-        final int size = Math.min(nullCount, negInfCount);
-        Arrays.fill(data, fromIndexInclusive, fromIndexInclusive + size, NULL_DOUBLE);
-        {
-            final int negInftoIndexExclusive = fromIndexInclusive + nullCount + negInfCount;
-            Arrays.fill(data, negInftoIndexExclusive - size, negInftoIndexExclusive, Double.NEGATIVE_INFINITY);
-        }
+        final int endNullIx = upperBound(data, fromIndexInclusive + 1, toIndexExclusive, NULL_DOUBLE);
+
+        final int negInfCount = beginNullIx - fromIndexInclusive;
+        final int nullCount = endNullIx - beginNullIx;
+
+        // Only some of the -Inf and some of the NULL_FLOATS are wrong. For example (with m = 3 and n = 5)
+        // [ -Inf, -Inf, -Inf, NULL_FLOAT, NULL_FLOAT, NULL_FLOAT, NULL_FLOAT, NULL_FLOAT, ...]
+        // ->
+        // [ NULL_FLOAT, NULL_FLOAT, NULL_FLOAT, unchanged, unchanged, -Inf, -Inf, -Inf, ...]
+
+        final int numWrong = Math.min(nullCount, negInfCount);
+        Arrays.fill(data, fromIndexInclusive, fromIndexInclusive + numWrong, NULL_DOUBLE);
+        Arrays.fill(data, endNullIx - numWrong, endNullIx, Double.NEGATIVE_INFINITY);
     }
 
     private static final Comparator<Comparable<Object>> COMPARATOR = Comparator.nullsFirst(Comparator.naturalOrder());
@@ -186,90 +187,81 @@ final class WritableChunkUtils {
     }
 
     public static <T> void sortUnsafe(T[] data, int fromIndexInclusive, int toIndexExclusive) {
-        // noinspection unchecked
         Arrays.sort(data, fromIndexInclusive, toIndexExclusive, (Comparator<? super T>) COMPARATOR);
     }
 
-    private static int binarySearchFirst(char[] a, int fromIndexInclusive, int toIndexExclusive, char key) {
-        int low = fromIndexInclusive;
-        int high = toIndexExclusive - 1;
-        while (low <= high) {
-            final int mid = (low + high) >>> 1;
-            final char midVal = a[mid];
-            if (midVal < key) {
-                low = mid + 1;
-            } else if (midVal > key) {
-                high = mid - 1;
-            } else if (low != mid) {
-                // This is where we differ from Arrays.binarySearch;
-                // we continue searching downwards to find the first instance.
-                high = mid;
+    /**
+     * @return the smallest i in [begin, end) where a[i] >= key, or end if no such element
+     */
+    private static int lowerBound(char[] a, int begin, int end, char key) {
+        while (begin < end) {
+            final int mid = (begin + end) >>> 1;
+            if (a[mid] < key) {
+                begin = mid + 1;
             } else {
-                return mid;
+                end = mid;
             }
         }
-        return -(low + 1);
+        return begin;
     }
 
-    private static int binarySearchFirst(float[] a, int fromIndexInclusive, int toIndexExclusive, float key,
-            int keyBits) {
-        int low = fromIndexInclusive;
-        int high = toIndexExclusive - 1;
-        while (low <= high) {
-            final int mid = (low + high) >>> 1;
-            final float midVal = a[mid];
-            if (midVal < key) {
-                low = mid + 1;
-            } else if (midVal > key) {
-                high = mid - 1;
+    /**
+     * @return the smallest i in [begin, end) where a[i] >= key, or end if no such element
+     */
+    private static int lowerBound(float[] a, int begin, int end, float key) {
+        while (begin < end) {
+            final int mid = (begin + end) >>> 1;
+            if (Float.compare(a[mid], key) < 0) {
+                begin = mid + 1;
             } else {
-                final int midBits = Float.floatToIntBits(midVal);
-                if (midBits < keyBits) {
-                    // (-0.0, 0.0) or (!NaN, NaN)
-                    low = mid + 1;
-                } else if (midBits > keyBits) {
-                    // (0.0, -0.0) or (NaN, !NaN)
-                    high = mid - 1;
-                } else if (low != mid) {
-                    // This is where we differ from Arrays.binarySearch;
-                    // we continue searching downwards to find the first instance.
-                    high = mid;
-                } else {
-                    return mid;
-                }
+                end = mid;
             }
         }
-        return -(low + 1);
+        return begin;
     }
 
-    private static int binarySearchFirst(double[] a, int fromIndexInclusive, int toIndexExclusive, double key,
-            long keyBits) {
-        int low = fromIndexInclusive;
-        int high = toIndexExclusive - 1;
-        while (low <= high) {
-            final int mid = (low + high) >>> 1;
-            final double midVal = a[mid];
-            if (midVal < key) {
-                low = mid + 1;
-            } else if (midVal > key) {
-                high = mid - 1;
+    /**
+     * @return the smallest i in [begin, end) where a[i] > key, or end if no such element
+     */
+    private static int upperBound(float[] a, int begin, int end, float key) {
+        while (begin < end) {
+            final int mid = (begin + end) >>> 1;
+            if (Float.compare(a[mid], key) <= 0) {
+                begin = mid + 1;
             } else {
-                final long midBits = Double.doubleToLongBits(midVal);
-                if (midBits < keyBits) {
-                    // (-0.0, 0.0) or (!NaN, NaN)
-                    low = mid + 1;
-                } else if (midBits > keyBits) {
-                    // (0.0, -0.0) or (NaN, !NaN)
-                    high = mid - 1;
-                } else if (low != mid) {
-                    // This is where we differ from Arrays.binarySearch;
-                    // we continue searching downwards to find the first instance.
-                    high = mid;
-                } else {
-                    return mid;
-                }
+                end = mid;
             }
         }
-        return -(low + 1);
+        return begin;
+    }
+
+    /**
+     * @return the smallest i in [begin, end) where a[i] >= key, or end if no such element
+     */
+    private static int lowerBound(double[] a, int begin, int end, double key) {
+        while (begin < end) {
+            final int mid = (begin + end) >>> 1;
+            if (Double.compare(a[mid], key) < 0) {
+                begin = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return begin;
+    }
+
+    /**
+     * @return the smallest i in [begin, end) where a[i] > key, or end if no such element
+     */
+    private static int upperBound(double[] a, int begin, int end, double key) {
+        while (begin < end) {
+            final int mid = (begin + end) >>> 1;
+            if (Double.compare(a[mid], key) <= 0) {
+                begin = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return begin;
     }
 }
