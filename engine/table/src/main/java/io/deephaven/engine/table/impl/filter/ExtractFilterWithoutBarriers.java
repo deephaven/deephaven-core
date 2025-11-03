0ed0873c37d1759d785f801f190421888167c4ec
Larry Booker <lbooker42@gmail.com>
2025-11-03 16:50:48 -0600

    fix: DH-20464: Fix pushdown filtering for parquet table locations with barriers (#7379)

---
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/filter/ExtractFilterWithoutBarriers.java
@@ -0,0 +1,75 @@
+//
+// Copyright (c) 2016-2025 Deephaven Data Labs and Patent Pending
+//
+package io.deephaven.engine.table.impl.filter;
+
+import io.deephaven.base.verify.Assert;
+import io.deephaven.engine.table.impl.select.*;
+
+/**
+ * Extract a simple filter from a {@link WhereFilter}, walking the tree and removing all barrier and serial wrappers.
+ */
+public enum ExtractFilterWithoutBarriers implements WhereFilter.Visitor<WhereFilter> {
+    INSTANCE;
+
+    public static WhereFilter of(final WhereFilter filter) {
+        return filter.walkWhereFilter(INSTANCE);
+    }
+
+    @Override
+    public WhereFilter visitWhereFilter(final WhereFilter filter) {
+        final WhereFilter retValue = WhereFilter.Visitor.super.visitWhereFilter(filter);
+        if (retValue == null) {
+            return filter;
+        }
+        return retValue;
+    }
+
+    @Override
+    public WhereFilter visitWhereFilter(final WhereFilterInvertedImpl filter) {
+        return WhereFilterInvertedImpl.of(of(filter.getWrappedFilter())); // must unwrap, then re-wrap inverted
+    }
+
+    @Override
+    public WhereFilter visitWhereFilter(final WhereFilterSerialImpl filter) {
+        return of(filter.getWrappedFilter()); // must unwrap
+    }
+
+    @Override
+    public WhereFilter visitWhereFilter(final WhereFilterWithDeclaredBarriersImpl filter) {
+        return of(filter.getWrappedFilter()); // must unwrap
+    }
+
+    @Override
+    public WhereFilter visitWhereFilter(final WhereFilterWithRespectedBarriersImpl filter) {
+        return of(filter.getWrappedFilter()); // must unwrap
+    }
+
+    @Override
+    public WhereFilter visitWhereFilter(final DisjunctiveFilter filter) {
+        final WhereFilter[] innerUnwrapped = filter.getFilters().stream()
+                .map(this::visitWhereFilter)
+                .toArray(WhereFilter[]::new);
+
+        // Verify we have exactly the same number of filters after unwrapping.
+        Assert.eq(innerUnwrapped.length, "innerUnwrapped.length", filter.getFilters().size(),
+                "filter.getFilters().size()");
+
+        // return a single DisjunctiveFilter containing all the unwrapped inner filters.
+        return DisjunctiveFilter.of(innerUnwrapped);
+    }
+
+    @Override
+    public WhereFilter visitWhereFilter(final ConjunctiveFilter filter) {
+        final WhereFilter[] innerUnwrapped = filter.getFilters().stream()
+                .map(this::visitWhereFilter)
+                .toArray(WhereFilter[]::new);
+
+        // Verify we have exactly the same number of filters after unwrapping.
+        Assert.eq(innerUnwrapped.length, "innerUnwrapped.length", filter.getFilters().size(),
+                "filter.getFilters().size()");
+
+        // return a single ConjunctiveFilter containing all the unwrapped inner filters.
+        return ConjunctiveFilter.of(innerUnwrapped);
+    }
+}
