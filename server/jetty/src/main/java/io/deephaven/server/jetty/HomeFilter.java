c3582dc660b099135d9841bb1947a0dac7e82d50
Melloware <mellowaredev@gmail.com>
2025-11-11 13:11:43 -0500

    Add import for IOException in HomeFilter

--- server/jetty/src/main/java/io/deephaven/server/jetty/HomeFilter.java
+++ server/jetty/src/main/java/io/deephaven/server/jetty/HomeFilter.java
@@ -10,6 +10,7 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 public class HomeFilter implements Filter {
