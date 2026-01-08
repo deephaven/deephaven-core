230634d779691e2edcbe47b3c44809113dbda5c8
Colin Alworth <colin@colinalworth.com>
2026-01-08 10:47:26 -0600

    chore: Update copyright headers for 2026

--- cpp-client/deephaven/dhclient/src/arrowutil/arrow_client_table.cc
+++ cpp-client/deephaven/dhclient/src/arrowutil/arrow_client_table.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2025 Deephaven Data Labs and Patent Pending
+ * Copyright (c) 2016-2026 Deephaven Data Labs and Patent Pending
  */
 #include "deephaven/client/arrowutil/arrow_client_table.h"
 #include <cstddef>
