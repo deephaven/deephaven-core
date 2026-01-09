0b6cbef24c0b1a04168832d7e40a482593401df1
Colin Alworth <colinalworth@deephaven.io>
2026-01-09 08:48:16 -0600

    chore: Update copyright headers for 2026
    Cherry-pick from #7558

--- engine/table/src/main/java/io/deephaven/engine/table/impl/sort/timsort/NullAwareCharByteTimsortDescendingKernel.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/sort/timsort/NullAwareCharByteTimsortDescendingKernel.java
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2016-2025 Deephaven Data Labs and Patent Pending
+// Copyright (c) 2016-2026 Deephaven Data Labs and Patent Pending
 //
 // ****** AUTO-GENERATED CLASS - DO NOT EDIT MANUALLY
 // ****** Edit NullAwareCharByteTimsortDescendingKernel and run "./gradlew replicateSortKernel" to regenerate
