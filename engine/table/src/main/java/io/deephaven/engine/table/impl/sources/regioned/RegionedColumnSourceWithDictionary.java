fcf0e311e8833784db7506d46c3854a94a299f17
Charles P. Wright <charleswright@deephaven.io>
2026-01-27 16:17:41 -0500

    Another loop needed fixing.

--- engine/table/src/main/java/io/deephaven/engine/table/impl/sources/regioned/RegionedColumnSourceWithDictionary.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/sources/regioned/RegionedColumnSourceWithDictionary.java
@@ -308,11 +308,13 @@ class RegionedColumnSourceWithDictionary<DATA_TYPE>
 
             try (final RowSet.SearchIterator keysToVisit = upstream.added().searchIterator();
                     final RowSequence.Iterator knownKeys = symbolTable.getRowSet().getRowSequenceIterator()) {
+                boolean remaining = true;
                 keysToVisit.nextLong(); // Safe, since sourceIndex must be non-empty
-                do {
-                    dictionaryColumn.lookupRegion(keysToVisit.currentValue()).gatherDictionaryValuesRowSet(keysToVisit,
-                            knownKeys, symbolTableAddedBuilder);
-                } while (keysToVisit.hasNext());
+                while (remaining) {
+                    ColumnRegionObject<DATA_TYPE, Values> region =
+                            dictionaryColumn.lookupRegion(keysToVisit.currentValue());
+                    remaining = region.gatherDictionaryValuesRowSet(keysToVisit, knownKeys, symbolTableAddedBuilder);
+                }
             }
 
             final RowSet symbolTableAdded = symbolTableAddedBuilder.build();
