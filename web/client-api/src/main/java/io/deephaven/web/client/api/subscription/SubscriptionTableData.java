eb8593437aaa6386035c027f2c20703e6019b879
elijahpetty <elijahpetty@illumon.com>
2026-02-12 11:20:33 -0600

    format

--- web/client-api/src/main/java/io/deephaven/web/client/api/subscription/SubscriptionTableData.java
+++ web/client-api/src/main/java/io/deephaven/web/client/api/subscription/SubscriptionTableData.java
@@ -18,10 +18,11 @@ import jsinterop.annotations.JsProperty;
  * in some cases actually be cheaper than maintaining state separately and updating only the changes, though both
  * options should be considered.
  * <p>
- * The {@link JsRangeSet} objects allow iterating over the {@link io.deephaven.web.client.api.LongWrapper} indexes in the table. Note that these "indexes"
- * are not necessarily contiguous and may be negative, and represent some internal state on the server, allowing it to
- * keep track of data efficiently. Those {@link io.deephaven.web.client.api.LongWrapper} objects can be passed to the various methods on this
- * instance to read specific rows or cells out of the {@link io.deephaven.web.client.api.JsTable}.
+ * The {@link JsRangeSet} objects allow iterating over the {@link io.deephaven.web.client.api.LongWrapper} indexes in
+ * the table. Note that these "indexes" are not necessarily contiguous and may be negative, and represent some internal
+ * state on the server, allowing it to keep track of data efficiently. Those
+ * {@link io.deephaven.web.client.api.LongWrapper} objects can be passed to the various methods on this instance to read
+ * specific rows or cells out of the {@link io.deephaven.web.client.api.JsTable}.
  */
 @TsInterface
 @TsName(name = "SubscriptionTableData", namespace = "dh")
