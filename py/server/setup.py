563d1439e46e18f81cef23f3f21f3628658475e3
Jianfeng Mao <4297243+jmao-denver@users.noreply.github.com>
2026-03-06 15:57:19 +0000

    fix: DH-21891: Remove numba from install-required of Core Py API (#7752) (#7753)
    numba has already been made as a soft dep instead of a hard one. Users
    would need to explicitly opt in to use numba by installing it
    themselves.

--- py/server/setup.py
+++ py/server/setup.py
@@ -65,11 +65,6 @@ setup(
         "numpy",
         "pandas>=1.5.0",
         "pyarrow",
-        # TODO(deephaven-core#3082): Clarify dependency requirements wrt numba
-        # It took 6 months for numba to support 3.11 after it was released, we want to make sure deephaven-core will be
-        # installable when 3.13 is out. When we decide to upgrade to 3.13 or higher for testing/production, CI check
-        # will alert us that numba isn't available.
-        'numba; python_version < "3.13"',
     ],
     extras_require={
         "autocomplete": ["jedi==0.19.1", "docstring_parser>=0.16"],
