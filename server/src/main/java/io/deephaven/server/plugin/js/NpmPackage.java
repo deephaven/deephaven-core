d80622b53b1f33defdc946a98a470fa2edb21ccf
mikebender <mikebender@deephaven.io>
2026-08-11 13:40:08 -0400

    Convert NpmPackage to BuildableStyle

--- server/src/main/java/io/deephaven/server/plugin/js/NpmPackage.java
+++ server/src/main/java/io/deephaven/server/plugin/js/NpmPackage.java
@@ -5,32 +5,41 @@ package io.deephaven.server.plugin.js;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.deephaven.annotations.SimpleStyle;
+import io.deephaven.annotations.BuildableStyle;
 import org.immutables.value.Value.Immutable;
-import org.immutables.value.Value.Parameter;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 
 import static io.deephaven.server.plugin.js.Jackson.OBJECT_MAPPER;
 
 @Immutable
-@SimpleStyle
+@BuildableStyle
 abstract class NpmPackage {
 
     static final String LOADER = "loader";
+
+    public static Builder builder() {
+        return ImmutableNpmPackage.builder();
+    }
 
     @JsonCreator
     public static NpmPackage of(
             @JsonProperty(value = JsPluginNpmPackageRegistration.NAME, required = true) String name,
             @JsonProperty(value = JsPluginNpmPackageRegistration.VERSION, required = true) String version,
             @JsonProperty(value = JsPluginNpmPackageRegistration.MAIN, required = true) String main,
-            @JsonProperty(value = LOADER) Map<String, Object> loader) {
-        return ImmutableNpmPackage.of(name, version, main, loader);
+            @JsonProperty(value = LOADER) Object loader) {
+        final Builder builder = builder()
+                .name(name)
+                .version(version)
+                .main(main);
+        if (loader != null) {
+            builder.loader(loader);
+        }
+        return builder.build();
     }
 
     public static NpmPackage read(Path packageJson) throws IOException {
@@ -40,23 +49,31 @@ abstract class NpmPackage {
         }
     }
 
-    @Parameter
     @JsonProperty(JsPluginNpmPackageRegistration.NAME)
     public abstract String name();
 
-    @Parameter
     @JsonProperty(JsPluginNpmPackageRegistration.VERSION)
     public abstract String version();
 
-    @Parameter
     @JsonProperty(JsPluginNpmPackageRegistration.MAIN)
     public abstract String main();
 
     /**
-     * The optional "loader" field, an arbitrary plugin-specific JSON object.
+     * The optional "loader" field, an arbitrary plugin-specific JSON value.
      */
     @Nullable
-    @Parameter
     @JsonProperty(LOADER)
-    public abstract Map<String, Object> loader();
+    public abstract Object loader();
+
+    public interface Builder {
+        Builder name(String name);
+
+        Builder version(String version);
+
+        Builder main(String main);
+
+        Builder loader(Object loader);
+
+        NpmPackage build();
+    }
 }
