01d89d57872619db9748e964ab9e2de2fc5da953
Charles P. Wright <charleswright@deephaven.io>
2026-08-19 19:56:49 -0400

    chore: DH-23398: Remove the dead DoubleChunkReader replication fixup

--- replication/static/src/main/java/io/deephaven/replicators/ReplicateBarrageUtils.java
+++ replication/static/src/main/java/io/deephaven/replicators/ReplicateBarrageUtils.java
@@ -24,9 +24,6 @@ public class ReplicateBarrageUtils {
 
         ReplicatePrimitiveCode.charToAllButBoolean("replicateBarrageUtils",
                 CHUNK_PACKAGE + "/CharChunkReader.java");
-        // ReplicatePrimitiveCode.floatToAllFloatingPoints("replicateBarrageUtils",
-        // CHUNK_PACKAGE + "/FloatChunkReader.java", "Float16");
-        fixupDoubleChunkReader(CHUNK_PACKAGE + "/DoubleChunkReader.java");
         fixupByteChunkReader(CHUNK_PACKAGE + "/ByteChunkReader.java");
 
         ReplicatePrimitiveCode.charToAllButBoolean("replicateBarrageUtils",
@@ -90,27 +87,4 @@ public class ReplicateBarrageUtils {
         FileUtils.writeLines(file, lines);
     }
 
-    private static void fixupDoubleChunkReader(final @NotNull String path) throws IOException {
-        final File file = new File(path);
-        List<String> lines = FileUtils.readLines(file, Charset.defaultCharset());
-        lines = globalReplacements(lines,
-                "Float16.toDouble", "Float16.toFloat",
-                "doubleing point precision", "floating point precision",
-                "half-precision doubles", "half-precision floats");
-        lines = replaceRegion(lines, "PrecisionSingleDhNulls", List.of(
-                "                    final float v = is.readFloat();",
-                "                    chunk.set(offset + ii, doubleCast(v));"));
-        lines = replaceRegion(lines, "PrecisionDoubleDhNulls", List.of(
-                "                    chunk.set(offset + ii, is.readDouble());"));
-        lines = replaceRegion(lines, "PrecisionSingleValidityBuffer", List.of(
-                "                elementSize = Float.BYTES;",
-                "                supplier = () -> doubleCast(is.readFloat());"));
-        lines = replaceRegion(lines, "PrecisionDoubleValidityBuffer", List.of(
-                "                supplier = is::readDouble;"));
-        lines = replaceRegion(lines, "FPCastHelper", List.of(
-                "    private static double doubleCast(float a) {",
-                "        return a == QueryConstants.NULL_FLOAT ? QueryConstants.NULL_DOUBLE : (double) a;",
-                "    }"));
-        FileUtils.writeLines(file, lines);
-    }
 }
