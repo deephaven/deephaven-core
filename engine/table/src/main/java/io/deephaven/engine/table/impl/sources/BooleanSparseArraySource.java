be79f1af2f45ca0f0c995ab46ce6309f521cfc7f
Charles P. Wright <charleswright@deephaven.io>
2026-08-27 09:08:20 -0400

    fix: DH-23494: Close SparseArraySource shift iterators

--- engine/table/src/main/java/io/deephaven/engine/table/impl/sources/BooleanSparseArraySource.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/sources/BooleanSparseArraySource.java
@@ -141,13 +141,16 @@ public class BooleanSparseArraySource extends SparseArrayColumnSource<Boolean>
 
     @Override
     public void shift(final RowSet keysToShift, final long shiftDelta) {
-        final RowSet.SearchIterator it =
-                (shiftDelta > 0) ? keysToShift.reverseIterator() : keysToShift.searchIterator();
-        it.forEachLong((i) -> {
-            set(i + shiftDelta, getBoolean(i));
-            setNull(i);
-            return true;
-        });
+        // Shifting up has to walk the keys in descending order so that a shifted value never overwrites one that has
+        // not been read yet.
+        try (final RowSet.SearchIterator it =
+                (shiftDelta > 0) ? keysToShift.reverseIterator() : keysToShift.searchIterator()) {
+            it.forEachLong((i) -> {
+                set(i + shiftDelta, getBoolean(i));
+                setNull(i);
+                return true;
+            });
+        }
     }
 
     // region boxed methods
