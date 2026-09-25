a66508b67141c3a49451b54d0bb9df4224563b84
Charles P. Wright <charleswright@deephaven.io>
2026-09-25 16:18:30 -0400

    perf: DH-23738: Allocate the segmented sorted array leaf removal list only when a leaf is removed

--- engine/table/src/main/java/io/deephaven/engine/table/impl/ssa/FloatReverseSegmentedSortedArray.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/ssa/FloatReverseSegmentedSortedArray.java
@@ -930,7 +930,8 @@ public final class FloatReverseSegmentedSortedArray implements SegmentedSortedAr
                 int firstValuesPosition = 0;
                 int totalCount = 0;
 
-                final IntList leavesToRemove = new IntArrayList();
+                // allocated when the first leaf is removed, which most removals never do
+                IntList leavesToRemove = null;
 
                 while (firstValuesPosition < removeSize) {
                     // we need to find out where our valuesToRemove should go using a binary search of the directory
@@ -955,6 +956,9 @@ public final class FloatReverseSegmentedSortedArray implements SegmentedSortedAr
                         // we are going to remove the whole leaf
                         final long firstPrior =
                                 priorRedirections == null ? RowSequence.NULL_ROW_KEY : getFirstPrior(firstLeaf);
+                        if (leavesToRemove == null) {
+                            leavesToRemove = new IntArrayList();
+                        }
                         leavesToRemove.add(firstLeaf);
                         leafSizes[firstLeaf] = 0;
                         if (priorRedirections != null) {
@@ -977,7 +981,7 @@ public final class FloatReverseSegmentedSortedArray implements SegmentedSortedAr
                         directoryValues[firstLeaf] = leafValues[firstLeaf][leafSizes[firstLeaf] - 1];
                         directoryRowKeys[firstLeaf] = leafRowKeys[firstLeaf][leafSizes[firstLeaf] - 1];
 
-                        final boolean hasLeft = firstLeaf > 0 && (leavesToRemove.isEmpty()
+                        final boolean hasLeft = firstLeaf > 0 && (leavesToRemove == null || leavesToRemove.isEmpty()
                                 || (leavesToRemove.getInt(leavesToRemove.size() - 1) != (firstLeaf - 1)));
                         final boolean hasRight = firstLeaf < leafCount - 1;
 
@@ -991,6 +995,9 @@ public final class FloatReverseSegmentedSortedArray implements SegmentedSortedAr
                         final boolean leftMerge = !threeWay && leftSize + middleSize < leafSize;
                         final boolean rightMerge = !threeWay && rightSize + middleSize < leafSize;
 
+                        if ((threeWay || leftMerge || rightMerge) && leavesToRemove == null) {
+                            leavesToRemove = new IntArrayList();
+                        }
                         if (threeWay) {
                             mergeThreeLeaves(firstLeaf - 1, leavesToRemove);
                         } else if (leftMerge) {
@@ -1001,12 +1008,14 @@ public final class FloatReverseSegmentedSortedArray implements SegmentedSortedAr
                     }
                     firstValuesPosition += count;
 
-                    if (leafCount - leavesToRemove.size() > 1) {
+                    if (leafCount - (leavesToRemove == null ? 0 : leavesToRemove.size()) > 1) {
                         if (SEGMENTED_SORTED_ARRAY_VALIDATION) {
                             Assert.eq(computeLeafSizes(), "computeLeafSizes()", size - totalCount, "size - totalCount");
                         }
                     } else if (firstValuesPosition < removeSize) {
-                        leavesToRemove.clear();
+                        if (leavesToRemove != null) {
+                            leavesToRemove.clear();
+                        }
                         // we need to promote the last remaining leaf to the directory values, because there is only a
                         // single leaf left
                         promoteLastLeafToDirectory();
@@ -1029,7 +1038,7 @@ public final class FloatReverseSegmentedSortedArray implements SegmentedSortedAr
                     }
                 }
 
-                if (!leavesToRemove.isEmpty()) {
+                if (leavesToRemove != null && !leavesToRemove.isEmpty()) {
 
                     int destIdx = leavesToRemove.getInt(0);
                     int srcIdx = destIdx + 1;
--- engine/table/src/main/java/io/deephaven/engine/table/impl/ssa/NullAwareCharSegmentedSortedArray.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/ssa/NullAwareCharSegmentedSortedArray.java
@@ -933,7 +933,8 @@ public final class NullAwareCharSegmentedSortedArray implements SegmentedSortedA
                 int firstValuesPosition = 0;
                 int totalCount = 0;
 
-                final IntList leavesToRemove = new IntArrayList();
+                // allocated when the first leaf is removed, which most removals never do
+                IntList leavesToRemove = null;
 
                 while (firstValuesPosition < removeSize) {
                     // we need to find out where our valuesToRemove should go using a binary search of the directory
@@ -958,6 +959,9 @@ public final class NullAwareCharSegmentedSortedArray implements SegmentedSortedA
                         // we are going to remove the whole leaf
                         final long firstPrior =
                                 priorRedirections == null ? RowSequence.NULL_ROW_KEY : getFirstPrior(firstLeaf);
+                        if (leavesToRemove == null) {
+                            leavesToRemove = new IntArrayList();
+                        }
                         leavesToRemove.add(firstLeaf);
                         leafSizes[firstLeaf] = 0;
                         if (priorRedirections != null) {
@@ -980,7 +984,7 @@ public final class NullAwareCharSegmentedSortedArray implements SegmentedSortedA
                         directoryValues[firstLeaf] = leafValues[firstLeaf][leafSizes[firstLeaf] - 1];
                         directoryRowKeys[firstLeaf] = leafRowKeys[firstLeaf][leafSizes[firstLeaf] - 1];
 
-                        final boolean hasLeft = firstLeaf > 0 && (leavesToRemove.isEmpty()
+                        final boolean hasLeft = firstLeaf > 0 && (leavesToRemove == null || leavesToRemove.isEmpty()
                                 || (leavesToRemove.getInt(leavesToRemove.size() - 1) != (firstLeaf - 1)));
                         final boolean hasRight = firstLeaf < leafCount - 1;
 
@@ -994,6 +998,9 @@ public final class NullAwareCharSegmentedSortedArray implements SegmentedSortedA
                         final boolean leftMerge = !threeWay && leftSize + middleSize < leafSize;
                         final boolean rightMerge = !threeWay && rightSize + middleSize < leafSize;
 
+                        if ((threeWay || leftMerge || rightMerge) && leavesToRemove == null) {
+                            leavesToRemove = new IntArrayList();
+                        }
                         if (threeWay) {
                             mergeThreeLeaves(firstLeaf - 1, leavesToRemove);
                         } else if (leftMerge) {
@@ -1004,12 +1011,14 @@ public final class NullAwareCharSegmentedSortedArray implements SegmentedSortedA
                     }
                     firstValuesPosition += count;
 
-                    if (leafCount - leavesToRemove.size() > 1) {
+                    if (leafCount - (leavesToRemove == null ? 0 : leavesToRemove.size()) > 1) {
                         if (SEGMENTED_SORTED_ARRAY_VALIDATION) {
                             Assert.eq(computeLeafSizes(), "computeLeafSizes()", size - totalCount, "size - totalCount");
                         }
                     } else if (firstValuesPosition < removeSize) {
-                        leavesToRemove.clear();
+                        if (leavesToRemove != null) {
+                            leavesToRemove.clear();
+                        }
                         // we need to promote the last remaining leaf to the directory values, because there is only a
                         // single leaf left
                         promoteLastLeafToDirectory();
@@ -1032,7 +1041,7 @@ public final class NullAwareCharSegmentedSortedArray implements SegmentedSortedA
                     }
                 }
 
-                if (!leavesToRemove.isEmpty()) {
+                if (leavesToRemove != null && !leavesToRemove.isEmpty()) {
 
                     int destIdx = leavesToRemove.getInt(0);
                     int srcIdx = destIdx + 1;
